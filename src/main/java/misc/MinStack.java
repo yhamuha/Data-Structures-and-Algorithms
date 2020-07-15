a4ef8a38c8e2660f216526ecc8fbd2c180356432
Yaroslav Hamuha <yhamuha@gmail.com>
2020-07-14 22:35:58 -0700

    Finding Min Stack value using one stack and two stacks with auxiliary support

---
+++ src/main/java/misc/MinStack.java
@@ -0,0 +1,127 @@
+package misc;
+
+import java.util.Stack;
+
+/**
+ * MinStack a class that implemented solution
+ * for finding a minimal element of stack
+ *
+ *
+ *
+ * insert operation: O(1)
+ * delete operation: O(1)
+ * ‘Get Min’ operation: O(1)
+ * Auxiliary Space: O(n) Use of auxiliary stack for storing values.
+ */
+class MinStack
+{
+    private Stack<Integer> s;	// main stack to store elements
+    private Stack<Integer> aux;  // auxiliary stack to store min elements
+
+    // Constructor
+    public MinStack() {
+        s = new Stack<>();
+        aux = new Stack<>();
+    }
+
+    // Inserts a given element on top of the stack
+    public void push(int x)
+    {
+        // push the given element into main stack
+        s.push(x);
+
+        // if auxiliary stack is empty, push the given element into it
+        if (aux.empty()) {
+            aux.push(x);
+        }
+        else {
+            // push the given element into the auxiliary stack
+            // if it is less than or equal to the current minimum
+            if (aux.peek() >= x) {
+                aux.push(x);
+            }
+        }
+    }
+
+    // Removes top element from the stack and returns it
+    public int pop()
+    {
+        if (empty()) {
+            System.out.println("Stack underflow!!");
+            return -1;
+        }
+
+        // remove top element from the main stack
+        int top = s.pop();
+
+        // remove top element from the auxiliary stack
+        // only if it is minimum
+        if (top == aux.peek()) {
+            aux.pop();
+        }
+
+        // return the removed element
+        return top;
+    }
+
+    // Returns top element of the stack
+    public int peek()
+    {
+        return s.peek();
+    }
+
+    // Returns number of elements in the stack
+    public int size()
+    {
+        return s.size();
+    }
+
+    // Returns true if stack is empty; false otherwise
+    public boolean empty()
+    {
+        return s.empty();
+    }
+
+    // Returns the minimum element from the stack in constant time
+    public int min()
+    {
+        if (aux.empty()) {
+            System.out.print("Stack underflow!! ");
+            return -1;
+        }
+
+        return aux.peek();
+    }
+
+    public static void main (String[] args)
+    {
+        MinStack s = new MinStack();
+
+        s.push(6);
+        System.out.println(s.min());	// prints 6
+
+        s.push(7);
+        System.out.println(s.min());	// prints 6
+
+        s.push(8);
+        System.out.println(s.min());	// prints 6
+
+        s.push(5);
+        System.out.println(s.min());	// prints 5
+
+        s.push(3);
+        System.out.println(s.min());	// prints 3
+
+        s.pop();
+        System.out.println(s.min());	// prints 5
+
+        s.push(10);
+        System.out.println(s.min());	// prints 5
+
+        s.pop();
+        System.out.println(s.min());	// prints 5
+
+        s.pop();
+        System.out.println(s.min());	// prints 6
+    }
+}
---
+++ src/main/java/misc/MinStackWithoutAux.java
@@ -0,0 +1,73 @@
+package misc;
+
+/**
+ * MinStack implementation without auxiliary second stack
+ */
+
+import java.util.Stack;
+
+class MinStackWithoutAux {
+    // main stack to store elements
+    private Stack<Integer> s = new Stack<>();
+
+    // variable to store minimum element
+    private int min;
+
+    // Inserts a given element on top of the stack
+    public void push(int x)
+    {
+        if (s.empty()) {
+            s.push(x);
+            min = x;
+        }
+        else if (x > min) {
+            s.push(x);
+        }
+        else {
+            s.push(2 * x - min);
+            min = x;
+        }
+    }
+
+    // Removes top element from the stack and returns it
+    public void pop()
+    {
+        if (s.empty()) {
+            System.out.println("Stack underflow!!");
+        }
+
+        int top = s.peek();
+        if (top < min)
+            min = 2 * min - top;
+        s.pop();
+    }
+
+    // Returns the minimum element from the stack in constant time
+    public int min()
+    {
+        return min;
+    }
+
+    public static void main (String[] args)
+    {
+        MinStack s = new MinStack();
+
+        s.push(6);
+        System.out.println(s.min());
+
+        s.push(7);
+        System.out.println(s.min());
+
+        s.push(5);
+        System.out.println(s.min());
+
+        s.push(3);
+        System.out.println(s.min());
+
+        s.pop();
+        System.out.println(s.min());
+
+        s.pop();
+        System.out.println(s.min());
+    }
+}
