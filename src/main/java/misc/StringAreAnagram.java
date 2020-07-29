55f763005bafb0d937c4fe7544616a81bd1907fd
Yaroslav Hamuha <yhamuha@gmail.com>
2020-07-29 16:59:22 -0700

    String are anagram

---
+++ src/main/java/misc/StringAreAnagram.java
@@ -0,0 +1,61 @@
+package misc;
+
+/**
+ * JAVA program to check whether two strings
+ * are anagrams of each other
+ */
+
+import java.util.Arrays;
+
+class StringAreAnagram {
+
+    /**
+     * function to check whether two strings are
+     * anagram of each other
+     *
+     * @param str1, str2 input arrays for comparing
+     * @return boolean isAnagram or not
+     */
+    static boolean areAnagram(char[] str1, char[] str2)
+    {
+        // Get lengths of both strings
+        int n1 = str1.length;
+        int n2 = str2.length;
+
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
+            return false;
+
+        // Sort both strings
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        // Compare sorted strings
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+
+        return true;
+    }
+
+    /**
+     * The entry point of application:
+     * - created arrays
+     * - use isAnagram function
+     * @param args the input arguments do no using
+     */
+    public static void main(String args[])
+    {
+        char str1[] = { 't', 'e', 's', 't' };
+        char str2[] = { 't', 's', 'e', 't' };
+        if (areAnagram(str1, str2))
+            System.out.println("The two strings are"
+                    + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                    + " anagram of each other");
+    }
+}
+
+
