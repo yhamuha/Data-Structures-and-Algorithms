a47e67472d9019ba057a05155e46be835e124efa
Yaroslav Hamuha <yhamuha@gmail.com>
2020-08-01 23:30:02 -0700

    Reverse String Recursively

---
+++ src/main/java/misc/RecursiveReverseString.java
@@ -0,0 +1,18 @@
+package misc;
+
+public class RecursiveReverseString {
+
+    public static void main(String[] args) {
+        String str = "Recursive String";
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str)
+    {
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
