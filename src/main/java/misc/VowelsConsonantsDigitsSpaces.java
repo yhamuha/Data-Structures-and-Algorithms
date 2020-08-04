b511e8dadfc427d89723d7a6b183e61c16d6023a
Yaroslav Hamuha <yhamuha@gmail.com>
2020-08-03 22:49:29 -0700

    Count: Vowels Consonants Digits Spaces in given string

---
+++ src/main/java/misc/VowelsConsonantsDigitsSpaces.java
@@ -0,0 +1,43 @@
+package misc;
+
+/**
+ * app to count vowels, consonants, digits and spaces
+ * for given string
+ */
+public class VowelsConsonantsDigitsSpaces {
+
+    /**
+     * entry piont
+     * @param args the input arguments do no using
+     */
+    public static void main(String[] args) {
+        String line = "this is a string with number 1000";
+        int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+        line = line.toLowerCase();
+        for(int i = 0; i < line.length(); ++i)
+        {
+            char ch = line.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i'
+                    || ch == 'o' || ch == 'u') {
+                ++vowels;
+            }
+            else if((ch >= 'a'&& ch <= 'z')) {
+                ++consonants;
+            }
+            else if( ch >= '0' && ch <= '9')
+            {
+                ++digits;
+            }
+            else if (ch ==' ')
+            {
+                ++spaces;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("White spaces: " + spaces);
+    }
+}
