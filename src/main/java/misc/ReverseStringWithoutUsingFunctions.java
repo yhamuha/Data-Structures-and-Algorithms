eae5f6d0aab716e8596c63ca667edaa5f5f68783
Yaroslav Hamuha <yhamuha@gmail.com>
2020-08-08 20:36:05 -0700

    Reverse String Without using Functions

---
+++ src/main/java/misc/ReverseStringWithoutUsingFunctions.java
@@ -0,0 +1,28 @@
+package misc;
+
+import java.util.Scanner;
+
+public class ReverseStringWithoutUsingFunctions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Original string : ");
+
+        String originalStr = scanner.nextLine();
+        scanner.close();
+
+        String words[] = originalStr.split("\\s");
+        String reversedString = "";
+
+        for (int i = 0; i < words.length; i++)
+        {
+            String word = words[i];
+            String reverseWord = "";
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reversedString = reversedString + reverseWord + " ";
+        }
+
+        System.out.print("Reversed string : " + reversedString);
+    }
+}
