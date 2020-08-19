d7e9f359ad122e066d60388ae6c9c50946705ff9
Yaroslav Hamuha <yhamuha@gmail.com>
2020-08-18 22:11:25 -0700

    MD5 Hashing

---
+++ src/main/java/misc/SecureHashing.java
@@ -0,0 +1,28 @@
+package misc;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SecureHashing {
+    public static void main(String[] args)
+    {
+        String passwordToHash = "passwordString";
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(passwordToHash.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        System.out.println(generatedPassword);
+    }
+}
