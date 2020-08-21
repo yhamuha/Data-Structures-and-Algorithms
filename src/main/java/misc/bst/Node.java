61305627b81e2c75e773922d97b4e5890f40068d
Yaroslav Hamuha <yhamuha@gmail.com>
2020-08-20 22:24:32 -0700

    BST Traversal, Remove node

--- src/main/java/misc/bst/Node.java
+++ src/main/java/misc/bst/Node.java
@@ -64,7 +64,7 @@ public class Node {
      * @return  min Node
      */
     Node getMin (Node node) {
-        if (!nodeExists(node))) return null;
+        if (!nodeExists(node)) return null;
         if (!nodeExists(node.left)) return node;
         return getMin(node.left);
     }
@@ -75,10 +75,105 @@ public class Node {
      * @return  max Node
      */
     Node getMax (Node node) {
-        if (!nodeExists(node))) return null;
+        if (!nodeExists(node)) return null;
         if (!nodeExists(node.right)) return node;
         return getMin(node.right);
     }
 
+    /**
+     * BST In-Order Traversal:
+     * ( left -> root -> right )
+     * @param node Node  - root
+     * @return  void
+     */
+    void inOrderTraversal(Node node) {
+        if (!nodeExists(node)) return;
+        inOrderTraversal(node.left);
+        System.out.println(node.value);
+        inOrderTraversal(node.right);
+    }
+
+    /**
+     * BST Post-Order Traversal:
+     * ( left -> right -> root )
+     * @param node Node  - root
+     * @return  void
+     */
+    void postOrderTraversal(Node node) {
+        if (!nodeExists(node)) return;
+        postOrderTraversal(node.left);
+        postOrderTraversal(node.right);
+        System.out.println(node.value);
+    }
+
+    /**
+     * BST Pre-Order Traversal:
+     * ( left -> right -> root )
+     * @param node Node  - root
+     * @return  void
+     */
+    void preOrderTraversal(Node node) {
+        if (!nodeExists(node)) return;
+        System.out.println(node.value);
+        postOrderTraversal(node.left);
+        postOrderTraversal(node.right);
+    }
+
+    void transplantNode (Node toNode, Node fromNode) {
+        toNode.value = fromNode.value;
+        toNode.left = fromNode.left;
+        toNode.right = fromNode.right;
+    }
+
+    int getChildrenCount(Node node) {
+        int count = 0;
+        if (nodeExists(node.left)) count +=1;
+        if (nodeExists(node.right)) count +=1;
+        return count;
+    }
+
+    Node getChildOrNill(Node node) {
+        return nodeExists(node.left)
+                ? node.left
+                : node.right;
+    }
+
+    /**
+     * Delete node with 0 or 1 child
+     * @param nodeToDelete Node  - destination node which must be deleted
+     * @return  void
+     */
+    void removeNodeWithOneOrZeroChild (Node nodeToDelete) {
+        Node childOrNill = getChildOrNill(nodeToDelete);
+        transplantNode(nodeToDelete, childOrNill);
+    }
+
+    /**
+     * Delete node with 0 or 1 child
+     * @param root Node  - root node
+     * @param value int  - value
+     * @return  void
+     */
+    boolean remove(Node root, int value) {
+        // search node for delete
+        Node nodeToDelete = search(root, value);
+        // if not - return false
+        if (!nodeExists(nodeToDelete)) return false;
+        // get amount of children
+        int childrenCount = getChildrenCount(nodeToDelete);
+
+        if (childrenCount < 2) {
+            // if < 2 move child or null
+            removeNodeWithOneOrZeroChild(nodeToDelete);
+        } else {
+            // get min node in the right subtree
+            Node minNode = getMin(nodeToDelete.right);
+            // copying
+            nodeToDelete.value = minNode.value;
+            // delete old node
+            removeNodeWithOneOrZeroChild(minNode);
+        }
+        return true;
+    }
 
 }
