a68d70d7bf959dc0f2917e7e406bf05caeb625b9
Yaroslav Hamuha <yhamuha@gmail.com>
2020-09-03 22:56:39 -0700

    FindNonexistentNumber

--- src/main/java/misc/FindNonexistentNumber.java
+++ src/main/java/misc/FindNonexistentNumber.java
@@ -4,11 +4,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
+
 public class FindNonexistentNumber {
     int[] getFilledCounters(String path) throws FileNotFoundException {
+
+        // space complexity = 4KB
         int[] counters = new int[1000];
 
         Scanner in = new Scanner(new FileReader(path));
+
+        // time complexity = O(N)
         while (in.hasNextInt()) {
             int counterIndex = in.nextInt() / 1_000_000;
             counters[counterIndex]++;
