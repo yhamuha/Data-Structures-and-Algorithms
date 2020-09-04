661628b76a0c5417fdf9ee2bab41564259ceee92
Yaroslav Hamuha <yhamuha@gmail.com>
2020-09-03 23:41:27 -0700

    add getBitVector

--- src/main/java/misc/FindNonexistentNumber.java
+++ src/main/java/misc/FindNonexistentNumber.java
@@ -1,11 +1,16 @@
 package misc;
 
+import jdk.nashorn.internal.objects.NativeInt8Array;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.BitSet;
 import java.util.Scanner;
 
 
 public class FindNonexistentNumber {
+    private BitSet bitSet;
+
     int[] getFilledCounters(String path) throws FileNotFoundException {
 
         // space complexity = 4KB
@@ -20,6 +25,23 @@ public class FindNonexistentNumber {
         }
         in.close();
         return counters;
+    }
+
+    BitSet getBitVector(String path, int start, int end) throws FileNotFoundException {
+
+        BitSet bitsSet = new BitSet(1_000_000);
+        Scanner in = new Scanner(new FileReader(path));
+
+        while (in.hasNextInt()) {
+            int number = in.nextInt();
+            if (number >= start && number <= end) {
+                int bitIndex = number - start;
+                bitSet.set(bitIndex);
+            }
+        }
+        in.close();
+        return bitSet;
+
     }
 }
 
