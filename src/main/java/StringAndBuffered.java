029366aa0e606897983f342a4d1cdaead629b757
Yaroslav Hamuha <yhamuha@gmail.com>
2020-09-12 11:16:47 -0700

    StringBuffer

---
+++ src/main/java/StringAndBuffered.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class StringAndBuffered {
+
+
+    public static void main(String[] args) {
+//         String str = new String("string");
+//         String str2 = new String("string");
+//         String str2 = "string";
+//         String str3 = "string";
+
+//        int a = 10;
+//        int b = 10;
+//
+//        System.out.println(a == b);
+//
+//        Integer a2 = 10;
+//        Integer b2= 20;
+//        System.out.println(a2 == b2);
+//        System.out.println(a2.equals(b2));
+
+//        if (str3 == str2) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+//        System.out.println(str.equals(str2));
+
+//        Character a = new Character('2');
+//        Character b = new Character('5');
+
+//        Integer a = new Integer('5');
+//        Integer b = new Integer('2');
+
+//        System.out.println(Character.isDigit(a));
+//        System.out.println(a.compareTo(b));
+
+//        Integer c = new Integer(2);
+//        c = a + b;
+//        System.out.println(c);
+
+//        System.out.println(Character.toLowerCase(a));
+//        System.out.println(str.toLowerCase());
+//
+//        System.out.println(Character.toUpperCase(a));
+//        System.out.println(str.toUpperCase());
+
+
+//        String str2 = new String("string with chars");
+//        System.out.println(str2.charAt(4));
+//        System.out.println(str2.indexOf("s"));
+//
+//        StringBuilder strBldr = new StringBuilder("string with chars");
+//
+//            int indexOfChar = strBldr.indexOf("s");
+//            strBldr.delete(indexOfChar, indexOfChar + 1);
+//            System.out.println(strBldr);
+//            int indexOfChar2 = strBldr.indexOf("s");
+//            strBldr.delete(indexOfChar2, indexOfChar2 + 1);
+//            System.out.println(strBldr);
+
+
+
+//        Character a = new Character('g');
+//        Character b = new Character('5');
+
+//        for (int i=0; i<str.length();i++) {
+//            System.out.print(str.charAt(i));
+//            if (i == str.length()-1) {
+//                break;
+//            } else {
+//                System.out.print(" * ");
+//            }
+//        }
+//        System.out.println();
+//        System.out.println(str.substring(7,9));
+//        System.out.println(str);
+//        System.out.println(str.trim());
+
+//        String str = new String("String");
+//
+//        Scanner in = new Scanner(System.in);
+//
+//        String ch = in.nextLine();
+//        System.out.println("echo: " + ch);
+//
+//        if (str.contains(ch)) {
+//            System.out.println("contains");
+//            int indexOfElement = str.indexOf(ch);
+//            int startIndexLeft = 0;
+//            int endIndexLeft = str.length() - indexOfElement;
+//            int startIndexRight = indexOfElement + 1;
+//            int endIndexRight = str.length();
+//
+//            String newString1 = str.substring(startIndexLeft, endIndexLeft);
+//            String newString2 = str.substring(startIndexRight, endIndexRight);
+//            String newString = newString1 + newString2;
+//            System.out.println("new string: " + newString);
+//        }
+
+//        String str4 = str.valueOf(15);
+//        if (str4 instanceof String) {
+//            System.out.println("String");
+
+//        }
+//        System.out.println();
+//        if (str.) {
+//            str.replace(ch.toUpperCase());
+//        }
+//        System.out.println(str);
+
+//        StringBuffer strbuf = new StringBuffer();
+//        strbuf.append("S");
+//        strbuf.append("t");
+//        strbuf.append("r");
+//        strbuf.append("i");
+//        strbuf.append("n");
+//        strbuf.append("g");
+//        System.out.println(strbuf);
+//
+//        try {
+//            while (strbuf.length() != 0) {
+//                Scanner in = new Scanner(System.in);
+//                String ch = in.nextLine();
+//                strbuf.delete(strbuf.indexOf(ch), (strbuf.indexOf(ch) + 1));
+//                System.out.println(strbuf);
+//                if (strbuf.length() == 0) {
+//                    System.out.println("String doesn't have symbols already");
+//                }
+//            }
+//        } catch (Exception e) {
+//            System.out.println("String doesn't have this symbol");
+//        }
+
+    }
+}
