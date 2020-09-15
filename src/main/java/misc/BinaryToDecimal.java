55f644c4c3d30a4748931001bb8b2fe489590f3d
Yaroslav Hamuha <yhamuha@gmail.com>
2020-09-15 00:25:49 -0700

    BinaryToDecimal

---
+++ src/main/java/misc/BinaryToDecimal.java
@@ -0,0 +1,21 @@
+package misc;
+
+import java.util.Scanner;
+
+class BinaryToDecimal {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int binNum, d, s = 0, power = 0;
+        System.out.print("Binary number: ");
+        binNum = sc.nextInt();
+        while (binNum != 0) {
+            d = binNum % 10;
+            s += d * (int) Math.pow(2, power++);
+            binNum /= 10;
+        }
+        System.out.println("Decimal: " + s);
+        sc.close();
+    }
+}
