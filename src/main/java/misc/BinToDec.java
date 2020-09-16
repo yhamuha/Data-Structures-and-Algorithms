1cc1e00eba11f6e2d18b080fe1a3f81df5a59cf4
Yaroslav Hamuha <yhamuha@gmail.com>
2020-09-15 22:18:02 -0700

    BinToDec fix

--- src/main/java/misc/BinToDec.java
+++ src/main/java/misc/BinToDec.java
@@ -1,22 +1,22 @@
 package misc;
 
-import java.util.Arrays;
-
 public class BinToDec {
 
     public static final int BASE_DIGIT = 2;
 
     public static void main(String[] args) {
 
-        int[] binaryNumber = {0,0,0,1};
+        int[] binaryNumber = {1,0,0,0};
         int[] decimalNumber = new int[binaryNumber.length];
         int decimal = 0;
+        int power = 0;
 
-        for (int i = binaryNumber.length - 1; i >=0; i--) {
-            decimalNumber[i] = (int) Math.pow(BASE_DIGIT, i);
-                decimal += decimalNumber[i];
-                System.out.println(Arrays.toString(decimalNumber));
+        for (int i=binaryNumber.length-1; i>=0; i--) {
+            decimalNumber[i] = binaryNumber[i] * (int) Math.pow(BASE_DIGIT, power++);
+            decimal += decimalNumber[i];
         }
         System.out.println(decimal);
     }
+
+
 }
