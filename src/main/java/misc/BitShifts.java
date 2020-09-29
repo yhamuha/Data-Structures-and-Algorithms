75d9063f545d79b8ae6cb85ad9bdfd3e451b4509
Yaroslav Hamuha <yhamuha@gmail.com>
2020-09-29 13:36:04 -0700

    bitwises

--- src/main/java/misc/BitShifts.java
+++ src/main/java/misc/BitShifts.java
@@ -1,33 +1,117 @@
 package misc;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.Arrays;
-
 public class BitShifts {
 
     // driver method
-    public static void main(String[] args) {
-        /*try {
-            System.out.println(Arrays.toString(initBitVector(16)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        // LSRfor32bit();
-        int i = 20;
-        System.out.println(getHighestBit(i) + " " + getLowestBit(i));
-        // traversal
-        lowToHigh(i);
-
-
-        // how to implement Math.ceil inside JDK
-        Math.ceil(100.0);
-        System.out.println();
-
+    public static void main(String[] args) throws Exception {
+//        try {
+//            System.out.println(Arrays.toString(initBitVector(16)));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        // LSRfor32bit();
+//        /*int i = 20;
+//        System.out.println(getHighestBit(i) + " " + getLowestBit(i));*/
+//        // traversal
+//        /*lowToHigh(i);*/
+//
+//        // how to implement Math.ceil inside JDK
+//        /*Math.ceil(100.0);
+//        System.out.println();*/
+//
         // 2^6
-        System.out.println((int)Math.pow(2,6));
+        // bitwiseSandbox();
+
+        System.out.println(inverseBits((byte) 15, 2));
+        System.out.println(setBits((byte) 16, 2));
+        System.out.println(unsetBits((byte) 15, 2));
+        System.out.println(unsetLSBits((byte) 15, 6));
+        System.out.println(unsetMSBits((byte) 15, 2));
+        System.out.println(getBit((byte) 15, 3));
 
 
+
+    }
+
+    static void bitwiseSandbox() throws Exception {
+
+        long[] bitVector = BitShifts.initBitVector(12);
+
+        // needs bitVector methods
+
+        // logical operations
+        byte x = 0000_0000_0000_0011;
+        byte y = 0000_0000_0000_0101;
+        System.out.print("&:"+ (x&y) + " |: " + (x|y) + " ^: " + (x^y));
+
+
+
+
+    }
+
+
+
+    // (data>>>i)&1u
+    static byte getBit(byte data, int i) {
+        // data>>>1
+        byte tmp = (byte)(data>>>i);
+        // (data>>>i)&1u
+        return (byte)(tmp&1);
+    }
+
+    // (~0u>>>i)&data
+    static byte unsetMSBits(byte data, int i) {
+        // ~0u>>>1
+        byte tmp = (byte)(~0<<i);
+        // (~0u>>>1)|data
+        return (byte)(tmp&data);
+    }
+
+    // (~0u<<i)&data
+    static byte unsetLSBits(byte data, int i) {
+        // ~0u<<1
+        byte tmp = (byte)(~0<<i);
+        // (~0u<<1)|data
+        return (byte)(tmp&data);
+    }
+
+    // ~(1u<<i)&data
+    static byte unsetBits(byte data, int i) {
+        // ~1u<<1
+        byte tmp = (byte)~(1<<i);
+        // ~(1u<<1)|data
+        return (byte)(tmp&data);
+    }
+
+    // (1u<<i)|data
+    static byte setBits(byte data, int i) {
+        // 1u<<1
+        byte tmp = (byte)(1<<i);
+        // (1u<<1)|data
+        return (byte)(tmp|data);
+    }
+
+    // (1u<<i)^data
+    static byte inverseBits(byte data, int i){
+        // 1u<<1
+        byte tmp = (byte)(1 << i);
+        // (1u<<1)^data
+        return (byte)(tmp^data);
+    }
+
+    // arithmetic right shift
+    static byte asr (byte n, int count) {
+        return (byte) ((byte) n >>> count);
+    }
+
+    // logical left shift
+    static byte lsl (byte n, int count) {
+        return (byte) ((byte) n << count);
+    }
+    // logical right shift
+    static byte lsr (byte n, int count) {
+        return (byte) ((byte) n >> count);
     }
 
     // array for bit vector
@@ -43,6 +127,7 @@ public class BitShifts {
 
         return new long[bucketCount];
     }
+
 
     // ASR(L) doesn't work with float and double types
     static void floatDoubleShiftTest () {
@@ -87,7 +172,5 @@ public class BitShifts {
             data = data >> 1;
         }
     }
-
-
 
 }
