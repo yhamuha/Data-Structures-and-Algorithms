582922f46a849e2d3e3ff2ee1d5ce2807b16c9d4
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-07 16:48:36 -0700

    StringPermutations: permutationTimes

--- src/main/java/misc/charset/StringsPermutation.java
+++ src/main/java/misc/charset/StringsPermutation.java
@@ -18,4 +18,19 @@ public class StringsPermutation {
         if (s1.length() != s2.length()) return false;
         return sort(s1).equals(sort(s2));
     }
+
+    boolean permutationTimes(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int[] letters = new int[128];
+        for (char letter : s1.toCharArray()) {
+            int letterCode = (int) letter;
+            letters[letterCode]++;
+        }
+        for (char letter : s2.toCharArray()) {
+            int letterCode = (int) letter;
+            letters[letterCode]--;
+            if (letters[letterCode] != -1) return false;
+        }
+        return true;
+    }
 }
