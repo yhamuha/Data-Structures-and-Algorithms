ddc7a609a8ffd1a78fd0556ad83b82f4a6079963
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-08 01:33:51 -0700

    String replaceSpaces

---
+++ src/main/java/misc/charset/ReplaceSpaces.java
@@ -0,0 +1,20 @@
+package misc.charset;
+
+public class ReplaceSpaces {
+    public static void main(String[] args) {
+        String stringIncludedReplacedSymbols = "dog is a good boy            ";
+        int nativeStringSize = 17;
+        System.out.println(replaceSpaces(stringIncludedReplacedSymbols, nativeStringSize));
+    }
+
+    static String replaceSpaces(String stringIncludedReplacedSymbols, int nativeStringSize) {
+        String resultString = "";
+        for (int i = 0; i < nativeStringSize; i++) {
+            if (stringIncludedReplacedSymbols.charAt(i) == 32)
+                resultString += "%20";
+            else
+                resultString += stringIncludedReplacedSymbols.charAt(i);
+        }
+        return resultString;
+    }
+}
