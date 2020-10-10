1a40abf41f07616d0a30924dfd314ca03f67314e
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-10 16:17:34 -0700

    Recursion: Fibonacchi, Factorial

---
+++ src/main/java/misc/recursion/FibEx.java
@@ -0,0 +1,13 @@
+package misc.recursion;
+
+public class FibEx {
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+    }
+
+    static int fib (int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n-1) + fib (n-2);
+    }
+}
