1609437e331a48e931cccf7930fa20e71fa7cd92
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-13 15:50:34 -0700

    Recursion: Find Max fix

--- src/main/java/misc/recursion/RecursionFindMax.java
+++ src/main/java/misc/recursion/RecursionFindMax.java
@@ -1,15 +1,20 @@
 package misc.recursion;
 
+/**
+ *  The {@code RecursionFindMax} class
+ *  find Max element of array
+ */
 public class RecursionFindMax {
 
     /**
-     *  The {@code RecursionFindMax} class
-     *  find Max element of array
+     * driver method
+     * @param args the command-line arguments (none)
      */
     public static void main(String[] args) {
         int[] arr = {1,3,4,5};
         System.out.println(findMax(arr, 4));
     }
+
 
     /**
      * looking for a replaces
@@ -18,8 +23,12 @@ public class RecursionFindMax {
      * @return int      max element of array
      */
     static int findMax(int[] arr, int n) {
+        // min base case
         if (n == 1) return arr[0];
+        // min size task
+        // put to stack areas each elem of array
         int maxElementIn_N_minus_1_arr = findMax(arr, n - 1);
+        // current task and return
         return maxElementIn_N_minus_1_arr > arr[n - 1]
                 ? maxElementIn_N_minus_1_arr
                 : arr[n - 1];
