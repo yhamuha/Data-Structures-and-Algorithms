b464dadabf1932b516361e1cc9f01ae7cfb7cc0f
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-20 15:03:15 -0700

    Probability: generatePowerSet function

--- src/main/java/misc/probability/PowerSet.java
+++ src/main/java/misc/probability/PowerSet.java
@@ -1,5 +1,7 @@
 package misc.probability;
 
+import java.util.ArrayList;
+
 /**
  * The {@code PowerSet} class contain function
  * to return power set from the set of elements
@@ -14,9 +16,22 @@ public class PowerSet {
 
     }
 
-    int[] generatePowerSet (int[]arr) {
+    /**
+     * generate power set fro a given set
+     * @param set   given set
+     * @return  powerSet as ArraylList<int[]>
+     */
+    ArrayList<int[]> generatePowerSet (int[]set) {
+        ArrayList<int[]> powerSet = new ArrayList<>();
+        powerSet.add(new int[0]);
 
-        return new int[0];
+        for (int element : set) {
+            for (int j = 0, l = powerSet.size(); j < l; j++) {
+                int[] powerSubSetExtended = cloneAndExtend(powerSet.get(j), element);
+                powerSet.add(powerSubSetExtended);
+            }
+        }
+        return powerSet;
     }
 
     /**
