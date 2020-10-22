28f651c90fef4a0f01852abd0c03971b0d87fe1b
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-21 22:18:04 -0700

    LinkedList: add after, search

--- src/main/java/misc/linkedList/MyLinkedList.java
+++ src/main/java/misc/linkedList/MyLinkedList.java
@@ -13,7 +13,7 @@ public class MyLinkedList {
     }
 
     /**
-     *
+     * time complexity for add to head or tail equals: O(1)
      * @param data
      */
     void addBeforeHead(int data) {
@@ -25,6 +25,30 @@ public class MyLinkedList {
         head = newHead;
     }
 
+    /**
+     *
+     * @param data
+     */
+    void addAfterTail(int data) {
+        Node newTail = new Node();
+        newTail.data = data;
+        if (head == null) head = newTail;
+        newTail.next = tail;
+        tail = newTail;
+    }
 
+    /**
+     *
+     * @param data
+     * @return
+     */
+    Node search(int data) {
+        Node node = head;
+        while (node !=null) {
+            if (node.data == data) return node;
+            node = node.next;
+        }
+        return null;
+    }
 }
 
