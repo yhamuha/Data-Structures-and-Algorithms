793635927f755866844f5a720fb634af881578cd
Yaroslav Hamuha <yhamuha@gmail.com>
2020-10-30 16:11:58 -0700

    recursion dynamic: getSeries using LinkedList

--- src/main/java/misc/recursionDynamic/PrintSeriesExample.java
+++ src/main/java/misc/recursionDynamic/PrintSeriesExample.java
@@ -1,11 +1,14 @@
 package misc.recursionDynamic;
 
+import java.util.LinkedList;
+
 /**
  * print series numbers from given n to 0 using step
  */
 public class PrintSeriesExample {
     public static void main(String[] args) {
         printSeries(5,3);
+        System.out.println(getSeries(5,3));
     }
 
     static void printSeries(int n, int step){
@@ -17,5 +20,21 @@ public class PrintSeriesExample {
         System.out.println(n);
         // print from n to 0
         printSeries(n-step, step);
+    }
+
+    static LinkedList<Integer> getSeries( int n, int step) {
+        // base case
+        if (n <= 0) {
+            LinkedList<Integer> list  = new LinkedList<>();
+            // put 0 to the last
+            list.addLast(0);
+            return list;
+        }
+
+        // get list from n-step to 0
+        LinkedList<Integer> result = getSeries(n - step, step);
+        // add n to start of list
+        result.addFirst(n);
+        return result;
     }
 }
