be0fcbc91f16059ac7045029e91587f84d706dc9
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-01 20:36:54 -0800

    some: generate input

---
+++ src/main/java/misc/some/GenerateInput.java
@@ -0,0 +1,15 @@
+package misc.some;
+
+/**
+ *
+ */
+public class GenerateInput {
+
+    public static void main(String[] args) {
+        for (int a=1,b=10,c=11,d=20,e=21,f=30;
+             a<=10; a++,b--,c++,d--,e++,f--){
+            System.out.println(a+" " +b +" " + c + " " +
+                    d + " " + e + " ");
+        }
+    }
+}
