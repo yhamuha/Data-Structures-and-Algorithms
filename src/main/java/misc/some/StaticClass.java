cc9b069f8c7c337c6056b9cdc0fec4e164fb92c2
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-03 03:49:45 -0800

    some: static inner class

---
+++ src/main/java/misc/some/StaticClass.java
@@ -0,0 +1,24 @@
+package misc.some;
+
+/**
+ *
+ */
+public class StaticClass {
+
+    static int i = 0;
+    int j = 0;
+
+
+    static class InnerClass {
+        static int a = i;
+        // has a problem
+        // static int b = j;
+
+        // has a problem too
+        // int b = j;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
