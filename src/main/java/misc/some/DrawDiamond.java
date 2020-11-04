f861f60f621d69683f017af7e5a19792fe64a3be
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-04 03:24:32 -0800

    some: DrawDiamond

---
+++ src/main/java/misc/some/DrawDiamond.java
@@ -0,0 +1,45 @@
+package misc.some;
+
+/**
+ *
+ */
+public class DrawDiamond {
+
+    public static void main(String[] args) {
+
+        for(int i=6;i>1;i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("#");
+
+            for (int k = 6; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 6; k > i; k--) {
+                System.out.print(" ");
+            }
+            System.out.print("#");
+            System.out.println();
+        }
+
+        for(int i=6;i>=1;i--) {
+            for (int j = 6; j > i; j--) {
+                System.out.print(" ");
+            }
+            System.out.print("#");
+
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+            System.out.print("#");
+            System.out.println();
+        }
+
+    }
+}
