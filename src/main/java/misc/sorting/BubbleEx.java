3f65a8a8ddfb788ca84e9c3580da778bdb63d32c
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-16 22:07:26 -0800

    sorting: estimate for Bubble

---
+++ src/main/java/misc/sorting/BubbleEx.java
@@ -0,0 +1,35 @@
+package misc.sorting;
+
+/**
+ *
+ */
+public class BubbleEx {
+
+    public static void main(String[] args) {
+        int[] arr = new int[100_000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i;
+        }
+        long start = System.currentTimeMillis();
+        arr  = sort(arr);
+        long end = System.currentTimeMillis();
+        System.out.println("Bubble sort:");
+        System.out.println("Execution time for " + arr.length + "  elements of array is: "
+                + (end - start) + " ms");
+        /*for(int number : arr){
+            System.out.println(number);
+        }*/
+    }
+    static int[] sort(int[] arr){
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=arr.length-1; j>i; j--){
+                if(arr[j]<arr[j-1]){
+                    int tmp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+}
