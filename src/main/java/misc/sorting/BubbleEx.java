83bb9fb0c9b064c199c1ea5b86ad77500d743172
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-16 22:17:44 -0800

    sorting: Bubble fix

--- src/main/java/misc/sorting/BubbleEx.java
+++ src/main/java/misc/sorting/BubbleEx.java
@@ -16,14 +16,19 @@ public class BubbleEx {
         System.out.println("Bubble sort:");
         System.out.println("Execution time for " + arr.length + "  elements of array is: "
                 + (end - start) + " ms");
-        /*for(int number : arr){
-            System.out.println(number);
-        }*/
     }
+
+    /**
+     * Bubble sort
+     * @param arr
+     * @return
+     */
     static int[] sort(int[] arr){
         for(int i=0; i<arr.length-1; i++){
             for(int j=arr.length-1; j>i; j--){
                 if(arr[j]<arr[j-1]){
+
+                    // swap
                     int tmp = arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=tmp;
