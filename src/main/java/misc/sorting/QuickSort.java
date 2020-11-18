6b3729e7bb4a3731f2c9ce3ef196eaa94752738d
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-17 23:19:45 -0800

    sorting: quick sort

---
+++ src/main/java/misc/sorting/QuickSort.java
@@ -0,0 +1,39 @@
+package misc.sorting;
+
+import java.util.Comparator;
+import java.util.Random;
+
+public class QuickSort {
+    public static final Random RND = new Random();
+
+    private void swap(Object[] array, int i, int j) {
+        Object tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    private int partition(Object[] array, int begin, int end, Comparator cmp) {
+        int index = begin + RND.nextInt(end - begin + 1);
+        Object pivot = array[index];
+        swap(array, index, end);
+        for (int i = index = begin; i < end; ++i) {
+            if (cmp.compare(array[i], pivot) <= 0) {
+                swap(array, index++, i);
+            }
+        }
+        swap(array, index, end);
+        return (index);
+    }
+
+    private void qsort(Object[] array, int begin, int end, Comparator cmp) {
+        if (end > begin) {
+            int index = partition(array, begin, end, cmp);
+            qsort(array, begin, index - 1, cmp);
+            qsort(array, index + 1, end, cmp);
+        }
+    }
+
+    public void sort(Object[] array, Comparator cmp) {
+        qsort(array, 0, array.length - 1, cmp);
+    }
+}
