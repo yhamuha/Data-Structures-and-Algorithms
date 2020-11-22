2c402acd79f7a172412c5e2dbe79b62b8fdc45e7
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-21 23:40:48 -0800

    some: IsSmallerOrGreater

---
+++ src/main/java/misc/some/SmallerOrGreater.java
@@ -0,0 +1,32 @@
+package misc.some;
+import java.util.Scanner;
+
+/**
+ *
+ */
+
+public class SmallerOrGreater {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=2, small=0, large=0;
+
+        System.out.println("Num 1: ");
+        int a = sc.nextInt();
+        small =a;
+        large =a;
+
+        while (i<10) {
+            System.out.println("Num " + i + ": ");
+            int b = sc.nextInt();
+            if (b<small) {
+                small = b;
+            }
+            if (b> large) {
+                large = b;
+            }
+            i++;
+        }
+        System.out.print("Larger: " + large + "     Smaller: " + small);
+    }
+}
