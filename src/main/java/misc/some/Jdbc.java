713286f3bcf4befa95e89d8a2c50c3979f8caae0
Yaroslav Hamuha <yhamuha@gmail.com>
2020-11-25 22:05:08 -0800

    some: jdbc

---
+++ src/main/java/misc/some/Jdbc.java
@@ -0,0 +1,41 @@
+package misc.some;
+import java.sql.*;
+
+/**
+ *
+ */
+
+public class Jdbc {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/EMP";
+
+    //  Database credentials
+    static final String USER = "username";
+    static final String PASS = "password";
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            //Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT id, first, last, age FROM Employees";
+            ResultSet rs = stmt.executeQuery(sql);
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }
+    }
+}
