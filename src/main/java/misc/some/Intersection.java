169818117562defa2dd72b5c906033f9841b6c61
Yaroslav Hamuha <yhamuha@gmail.com>
2020-12-01 11:30:45 -0800

    some: Intersection time complexity O(m+n)

--- src/main/java/misc/some/Intersection.java
+++ src/main/java/misc/some/Intersection.java
@@ -6,11 +6,13 @@ package misc.some;
 
 public class Intersection {
 
+
     public static void main(String[] args) {
-        int[] arr1 = {1,5,7,6};
+        int[] arr1 = {1,5,6,7};
         int[] arr2 = {1,2,3,7};
 
         intersectionOfTwoArray_MmultN(arr1,arr2);
+        intersectionOfTwoArray_MPlusN(arr1, arr2);
     }
 
     // Time Complexity O(mn)
@@ -23,7 +25,23 @@ public class Intersection {
             }
         }
     }
-}
+
+    // Complexity O(m+n)
+        static void intersectionOfTwoArray_MPlusN (int[] arr1, int[] arr2){
+            int i = 0;
+            int j = 0;
+
+            while (i < arr1.length && j < arr2.length) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.println(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+
+
+    }
 
 
 
