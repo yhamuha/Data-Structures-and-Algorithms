17b0584c8285abfc778c77ede55856946af2316c
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-12 22:10:57 -0700

    problem_solving: rows

---
+++ src/main/java/misc/problem_solving/row.java
@@ -0,0 +1,17 @@
+package misc.problem_solving;
+
+/**
+ *
+ */
+public class row {
+
+    public static void main(String[] args) {
+        System.out.println();
+        for (int a=1, b=10, c=11, d=20, e=21, f=30; a<= 10; a++, b--, c++, d--, e++, f--) {
+            System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+ f+" ");
+        }
+    }
+
+
+
+}
