8f78f481124780f3bed7d44894125370a6659a1a
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-17 10:18:02 -0700

    problem solving: prime number

---
+++ src/main/java/misc/problem_solving/prime_number.java
@@ -0,0 +1,35 @@
+package misc.problem_solving;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class prime_number {
+    public static void main(String[] args) {
+        checkPrime(1);
+        checkPrime(4);
+        checkPrime(5);
+        checkPrime(7);
+        checkPrime(11);
+    }
+
+    static void checkPrime(int n) {
+        int i, m = 0, flag = 0;
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            System.out.println(n + " is not prime number");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    System.out.println(n + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(n + " is prime number");
+            }
+        }
+    }
+}
