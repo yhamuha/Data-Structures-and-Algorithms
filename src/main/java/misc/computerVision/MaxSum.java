b8a3db7d652a4102d133370183a5c16db0044f31
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-21 12:50:53 -0700

    computer vision: class MaxSum

---
+++ src/main/java/misc/computerVision/MaxSum.java
@@ -0,0 +1,16 @@
+package misc.computerVision;
+
+/**
+ *
+ */
+public class MaxSum {
+    public int start;
+    public int end;
+    public int sum;
+
+    public MaxSum (int start, int end, int sum) {
+        this.start = start;
+        this.end = end;
+        this.sum = sum;
+    }
+}
