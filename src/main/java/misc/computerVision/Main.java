0ade7b4c04873287fa3cf0f5dfca69eae8285949
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-22 10:40:23 -0700

    computer vision: added getMaxSubMatrix

--- src/main/java/misc/computerVision/Main.java
+++ src/main/java/misc/computerVision/Main.java
@@ -6,7 +6,10 @@ package misc.computerVision;
 public class Main {
     public static void main(String[] args) {
         MaxSum ms = new MaxSum(0,0,0);
-        int[] arr = {2,3,4,5,-90,7};
-        System.out.println(ms.getMaxSumRange(arr).toString());
+        SubMatrix sm = new SubMatrix(0,0,0,0,0);
+//        int[] arr = {2,3,4,5,-90,7};
+//        System.out.println(ms.getMaxSumRange(arr).toString());
+        int[][] arr_2d = {{1,2,-3},{4,-5,6},{7,8,9}};
+        System.out.println(sm.getMaxSumMatrix(arr_2d).toString());
     }
 }
--- src/main/java/misc/computerVision/SubMatrix.java
+++ src/main/java/misc/computerVision/SubMatrix.java
@@ -1,12 +1,13 @@
 package misc.computerVision;
+import misc.computerVision.MaxSum;
 
 /**
  *
  */
 public class SubMatrix {
     public int startRow;
-    public int endRow;
     public int startCol;
+    public int endRow;
     public int endCol;
     public int sum;
 
@@ -17,5 +18,36 @@ public class SubMatrix {
         this.endCol = endCol;
         this.sum = sum;
     }
-}
 
+    SubMatrix getMaxSumMatrix (int[][] matrix) {
+        int rowsCount = matrix.length;
+        int rowLength = matrix[0].length;
+        SubMatrix subMatrix = null;
+        MaxSum maxSumRange = new MaxSum(0,0,0);
+
+        for (int rowStart = 0; rowStart < rowsCount; rowStart++) {
+            int[] colsSum = new int[rowLength];
+
+            for (int currRow = rowStart; currRow < rowsCount; currRow++) {
+                for (int i = 0; i < rowLength; i++)
+                    colsSum[i] += matrix[currRow][i];
+                maxSumRange = maxSumRange.getMaxSumRange(colsSum);
+                if (subMatrix == null || maxSumRange.sum > subMatrix.sum)
+                    subMatrix = new SubMatrix(rowStart, maxSumRange.start,
+                            currRow, maxSumRange.end, maxSumRange.sum);
+            }
+        }
+        return subMatrix;
+    }
+
+    @Override
+    public String toString() {
+        return "SubMatrix{" +
+                "startRow=" + startRow +
+                ", startCol=" + startCol +
+                ", endRow=" + endRow +
+                ", endCol=" + endCol +
+                ", sum=" + sum +
+                '}';
+    }
+}
