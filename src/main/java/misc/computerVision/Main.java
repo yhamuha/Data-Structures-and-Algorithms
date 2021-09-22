75b83b5c73087df260458939e538c0e48f3e7117
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-22 12:22:34 -0700

    computer vision: added parse string

--- src/main/java/misc/computerVision/Main.java
+++ src/main/java/misc/computerVision/Main.java
@@ -1,5 +1,6 @@
 package misc.computerVision;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -19,6 +20,27 @@ public class Main {
         return null;
     }
 
+    static String parseStr(String str, HashSet<String> words) {
+        ArrayList<String> parsedWords = new ArrayList<>();
+        int windowStart = 0;
+        int windowEnd = 0;
+        while (windowEnd < str.length()) {
+            String candidate = getAnyWord(windowEnd, str, words);
+            if (candidate == null) {
+                windowEnd++;
+            } else {
+                if (windowStart != windowEnd)
+                    parsedWords.add(str.substring(windowStart, windowEnd));
+                parsedWords.add(candidate);
+                windowEnd += candidate.length();
+                windowStart = windowEnd;
+            }
+        }
+        if (windowStart < windowEnd)
+            parsedWords.add(str.substring(windowStart));
+        return String.join(" ", parsedWords);
+    }
+
     public static void main(String[] args) {
         MaxSum ms = new MaxSum(0,0,0);
         SubMatrix sm = new SubMatrix(0,0,0,0,0);
@@ -28,10 +50,12 @@ public class Main {
 //        int[][] arr_2d = {{1,2,-3},{4,-5,6},{7,8,9}};
 //        System.out.println(sm.getMaxSumMatrix(arr_2d).toString());
 
-        String str = "khkjkjhbkjhbkjhblike";
+        String str = "khkjikjhbkjhbkjhblikekjhkjhvkjvkjhdogkjhbkjhbk";
         HashSet words = new HashSet();
         words.add("like");
+        words.add("dog");
 
-        System.out.println(getAnyWord(16, str, words));
+//        System.out.println(getAnyWord(16, str, words));
+        System.out.println(parseStr(str, words));
     }
 }
