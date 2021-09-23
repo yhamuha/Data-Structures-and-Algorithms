cf38f596dfc6fb8b05ee889162db4fcd9fe4c162
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-23 11:00:10 -0700

    problem solving: reverse signed 32-bit int

---
+++ src/main/java/misc/problem_solving/int_reverse.java
@@ -0,0 +1,27 @@
+package misc.problem_solving;
+
+/**
+ *
+ */
+public class int_reverse {
+    public static void main(String[] args) {
+    int x = 12345;
+    System.out.println(reverse(x));
+    }
+
+    static int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            // just pop last digit
+            int pop = x % 10;
+            // got number without this last digit
+            x /= 10;
+            // checking for extremal cases to signed 32-bit int
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            // construct reverse output
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
