8ca399852e6e9b7867266ca8b3925ebdeb7724df
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-23 21:03:01 -0700

    problem solving: count missing bits

--- src/main/java/misc/problem_solving/find_number.java
+++ src/main/java/misc/problem_solving/find_number.java
@@ -7,6 +7,12 @@ public class find_number {
     public static void main(String[] args) {
     int[] arr = {5,5,6};
     System.out.println(findNumber(arr));
+
+    int a = 29;
+    int b = 15;
+
+    System.out.println(countBits(a,b));
+
     }
     //XOR
     static int findNumber(int[] arr) {
@@ -14,5 +20,17 @@ public class find_number {
         for (int i = 1; i < arr.length; i++)
             number = number ^ arr[i];
         return number;
+    }
+
+    // XOR will return "1" for the different bits
+    static int countBits(int a, int b) {
+        int diffBits = a ^ b;
+        int onesCount = 0;
+        while (diffBits != 0) {
+            // using >>> counting "1"
+            if ((diffBits & 1) == 1) onesCount++;
+            diffBits = diffBits >>> 1;
+        }
+        return onesCount;
     }
 }
