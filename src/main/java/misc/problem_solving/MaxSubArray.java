1e1560764bf3ca787ea3dc49fea52135a117ded5
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-25 12:20:14 -0700

    problem solving: max sub array O(n)

--- src/main/java/misc/problem_solving/MaxSubArray.java
+++ src/main/java/misc/problem_solving/MaxSubArray.java
@@ -6,11 +6,11 @@ package misc.problem_solving;
 public class MaxSubArray {
     public static void main(String[] args) {
         int[] nums = {1,2,-5,3,6};
-        System.out.println(maxSubArray(nums));
+//        System.out.println(maxSubArray(nums));
+        System.out.println(maxSubArray2(nums));
     }
     //O(n^2)
     static int maxSubArray(int[] nums) {
-
         int maxSubarray = Integer.MIN_VALUE;    // to level variable to collect max value
         for (int i = 0; i < nums.length; i++) {
             int currentSubarray = 0;
@@ -19,6 +19,21 @@ public class MaxSubArray {
                 maxSubarray = Math.max(maxSubarray, currentSubarray);
             }
         }
+        return maxSubarray;
+    }
+    // Kadane's Algorithm
+    // t: O(n)
+    // s: O(1)
+    static int maxSubArray2(int[] nums) {
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+
         return maxSubarray;
     }
 }
