05db01eb325004004565e82559fe5a7d5afdece7
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-25 13:18:15 -0700

    problem solving: last word length O(n) one loop pass solution

--- src/main/java/misc/problem_solving/LastWordLength.java
+++ src/main/java/misc/problem_solving/LastWordLength.java
@@ -6,7 +6,8 @@ package misc.problem_solving;
 public class LastWordLength {
     public static void main(String[] args) {
         String str = "yes it is it the world  ";
-        System.out.println(lengthOfLastWord(str));
+//        System.out.println(lengthOfLastWord(str));
+        System.out.println(lengthOfLastWord2(str));
     }
 
     // compute last word from the end, trimmed the trailing spaces
@@ -21,6 +22,21 @@ public class LastWordLength {
         while (p >= 0 && s.charAt(p) != ' ') {
             p--;
             length++;
+        }
+        return length;
+    }
+    // One loop pass solution
+    // O(n), O(1)
+    static int lengthOfLastWord2(String s) {
+        int p = s.length(), length = 0;
+        while (p > 0) {
+            p--;
+            if (s.charAt(p) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                return length;
+            }
         }
         return length;
     }
