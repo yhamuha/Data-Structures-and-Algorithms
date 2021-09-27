04be28f3ccafd4cd15483ea3516db610e90a287d
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-27 12:27:11 -0700

    problem solving: merge sorted array _three point solution

--- src/main/java/misc/problem_solving/MergeSortedArray.java
+++ src/main/java/misc/problem_solving/MergeSortedArray.java
@@ -7,13 +7,15 @@ import java.util.Arrays;
  */
 public class MergeSortedArray {
     public static void main(String[] args) {
-        int[] nums1 = {1,2,3};
+        int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {4,5,6};
-        int m =3;
-        int n =3;
-        System.out.println(Arrays.toString(merge(nums1, m, nums2, n)));
+        int m = 3;
+        int n = 3;
+        merge3(nums1,m,nums2,n);
+        System.out.println(Arrays.toString(nums1));
     }
 
+    // O(m+n)
     static int[] merge(int[] nums1, int m, int[] nums2, int n) {
 
         int[] nums = new int[m+n];
@@ -25,5 +27,49 @@ public class MergeSortedArray {
             nums[i] = nums2[i-3];
         }
         return nums;
+    }
+
+    //
+    static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        // Make a copy of the first m elements of nums1.
+        int[] nums1Copy = new int[m];
+        for (int i = 0; i < m; i++) {
+            nums1Copy[i] = nums1[i];
+        }
+
+        // Read pointers for nums1Copy and nums2 respectively.
+        int p1 = 0;
+        int p2 = 0;
+
+        // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+        for (int p = 0; p < m + n; p++) {
+            // We also need to ensure that p1 and p2 aren't over the boundaries
+            // of their respective arrays.
+            if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+                nums1[p] = nums1Copy[p1++];
+            } else {
+                nums1[p] = nums2[p2++];
+            }
+        }
+    }
+
+    //
+    static void merge3(int[] nums1, int m, int[] nums2, int n) {
+        // Set p1 and p2 to point to the end of their respective arrays.
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        // And move p backwards through the array, each time writing
+        // the smallest value pointed at by p1 or p2.
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
+            }
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
+            } else {
+                nums1[p] = nums2[p2--];
+            }
+        }
     }
 }
