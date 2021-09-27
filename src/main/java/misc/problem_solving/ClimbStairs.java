2efc38ecb962da5774a7b59d82d10bac3df5a94c
Yaroslav Hamuha <yhamuha@gmail.com>
2021-09-27 07:45:30 -0700

    problem solving: climb stairs O(n) Fibonacci

--- src/main/java/misc/problem_solving/ClimbStairs.java
+++ src/main/java/misc/problem_solving/ClimbStairs.java
@@ -6,8 +6,12 @@ package misc.problem_solving;
 public class ClimbStairs {
     public static void main(String[] args) {
         int high = 5;
-        System.out.println(climbStairs(high));
+//        System.out.println(climbStairs(high));
+        System.out.println(climbStairs2(high));
     }
+
+    // using recursion
+    // O(n^2)
     public static int climbStairs(int high) {
         return climb_Stairs(0, high);
     }
@@ -19,5 +23,21 @@ public class ClimbStairs {
             return 1;
         }
         return climb_Stairs(step + 1, high) + climb_Stairs(step + 2, high);
+    }
+
+    // using Fibonacci Number
+    // O(n), O(1)
+    static int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
     }
 }
