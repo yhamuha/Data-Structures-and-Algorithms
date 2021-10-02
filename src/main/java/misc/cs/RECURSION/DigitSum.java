a68171603360d330380925a3a19e423f696dd32d
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-01 18:46:22 -0700

    cs.RECURSION: digitsSum()

--- src/main/java/misc/cs/RECURSION/DigitSum.java
+++ src/main/java/misc/cs/RECURSION/DigitSum.java
@@ -7,6 +7,8 @@ public class DigitSum {
     public static void main(String[] args) {
         int n = 123;
         System.out.println(digitsSum(n));
+        int f = 5;
+        System.out.println(factorial(f));
     }
 
     static int digitsSum(int n) {
@@ -14,5 +16,10 @@ public class DigitSum {
         int digitsSum_except_last = digitsSum(n / 10);
         int currentDigit = n % 10;
         return digitsSum_except_last + currentDigit;
+    }
+
+    static int factorial(int f) {
+        if (f == 0) return 1;
+        else return factorial(f - 1) * f;
     }
 }
