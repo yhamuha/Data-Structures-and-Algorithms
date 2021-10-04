7e4ab088d76cfbda75a52173117a4ca3528d8e38
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-04 12:15:12 -0700

    cs Strings: SingleRowKeyboard using hash

--- src/main/java/misc/problem_solving/SingleRowKeyboard.java
+++ src/main/java/misc/problem_solving/SingleRowKeyboard.java
@@ -1,7 +1,9 @@
 package misc.problem_solving;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -9,8 +11,8 @@ import java.util.List;
 public class SingleRowKeyboard {
     public static void main(String[] args) {
         String keyboard = "abcdefghijklmnopqrstuvwxyz";
-        String word = "ebho";
-        System.out.println(calculateTime(keyboard,word));
+        String word = "ki";
+        System.out.println(calculateTime2(keyboard,word));
     }
 
     static int calculateTime(String keyboard, String word) {
@@ -31,5 +33,22 @@ public class SingleRowKeyboard {
             time+=nextMove;
         }
         return time;
+    }
+
+    // using hash
+    static int calculateTime2(String keyboard, String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < keyboard.length(); i++) {
+            map.put(keyboard.charAt(i), i);
+        }
+
+        int ans = 0;
+        int cur = 0;
+        for (char c : word.toCharArray()) {
+            ans += Math.abs(cur - map.get(c));
+            cur = map.get(c);
+        }
+
+        return ans;
     }
 }
