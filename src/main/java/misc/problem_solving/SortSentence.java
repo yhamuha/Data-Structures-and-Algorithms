93dc20d4836815e05e0a1a5e2137e53227ead4f5
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-03 21:36:28 -0700

    cs: sortSentence() StringBuider, pure java approach

--- src/main/java/misc/problem_solving/SortSentence.java
+++ src/main/java/misc/problem_solving/SortSentence.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
 public class SortSentence {
     public static void main(String[] args) {
         String s = "This1 is2 world4 the3";
-        System.out.println(sortSentence(s));
+        System.out.println(sortSentence3(s));
     }
 
     // O(n), O(n)
@@ -23,5 +23,46 @@ public class SortSentence {
         }
         //Join the values which are ordered
         return String.join(" ", hash.values());
+    }
+
+    // using StringBuilder
+    static String sortSentence2(String s) {
+
+        String[] ques = s.split(" ");
+        String[] ans = new String[ques.length];
+
+        for(int i=0;i<ques.length;i++)
+        {
+            StringBuilder sb = new StringBuilder(ques[i]);
+            char last = ques[i].charAt(ques[i].length()-1);
+            int a = Character.getNumericValue(last);
+            sb.deleteCharAt(ques[i].length()-1);
+            ans[a-1] = sb.toString();
+        }
+
+        StringBuilder st = new StringBuilder();
+        for(int z=0;z<ans.length-1;z++)
+        {
+            st.append(ans[z] + " ");
+        }
+        st.append(ans[ans.length-1]);
+
+        return st.toString();
+    }
+
+    // pure java solution
+    static String sortSentence3(String s) {
+
+        String[] str = s.split(" ");
+        String[] temp = new String[str.length];
+        String res = "";
+        for(int i=0;i<str.length;i++){
+            int index = str[i].charAt(str[i].length()-1 )- '0';
+            temp[index-1] = str[i].substring(0,str[i].length()-1);
+        }
+        for(int i=0;i<str.length;i++){
+            res=res + temp[i] + " ";
+        }
+        return res.trim();
     }
 }
