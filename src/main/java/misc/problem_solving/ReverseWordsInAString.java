8b540bc73088c25e06363810d438d386839203c7
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-06 21:10:21 -0700

    cs String: ReverseWordsInAString

---
+++ src/main/java/misc/problem_solving/ReverseWordsInAString.java
@@ -0,0 +1,50 @@
+package misc.problem_solving;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class ReverseWordsInAString {
+    public static void main(String[] args) {
+        String s = "Let's code";
+        System.out.println(reverseWords2(s));
+    }
+
+    // O(n) O(n)
+    static String reverseWords(String s) {
+        String words[] = s.split(" ");
+        StringBuilder res=new StringBuilder();
+        for (String word: words)
+            res.append(new StringBuffer(word).reverse().toString() + " ");
+        return res.toString().trim();
+    }
+
+    // without predefined split and reverse
+    static String reverseWords2(String s) {
+        String words[] = split(s);
+        StringBuilder res=new StringBuilder();
+        for (String word: words)
+            res.append(reverse(word) + " ");
+        return res.toString().trim();
+    }
+    static String[] split(String s) {
+        ArrayList< String > words = new ArrayList < > ();
+        StringBuilder word = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                words.add(word.toString());
+                word = new StringBuilder();
+            } else
+                word.append( s.charAt(i));
+        }
+        words.add(word.toString());
+        return words.toArray(new String[words.size()]);
+    }
+    static String reverse(String s) {
+        StringBuilder res=new StringBuilder();
+        for (int i = 0; i < s.length(); i++)
+            res.insert(0,s.charAt(i));
+        return res.toString();
+    }
+}
