6aae224dbd1fdd1ae5367afdaa45be08656a1253
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-06 17:23:12 -0700

    cs String: IncreasingDecreasingString

---
+++ src/main/java/misc/problem_solving/IncreasingDecreasingString.java
@@ -0,0 +1,37 @@
+package misc.problem_solving;
+
+/**
+ *
+ */
+public class IncreasingDecreasingString {
+    public static void main(String[] args) {
+        String s = "aaaabbbbcccc";
+        System.out.println(sortString(s));
+    }
+
+    static String sortString(String s) {
+
+        StringBuilder temp = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder();
+
+        while(temp.length() != 0){
+            for(char c='a';c<='z';c++){
+                int index = temp.indexOf(c+"");
+                if(index != -1){
+                    sb.append(c);
+                    temp.deleteCharAt(index);
+                }
+
+            }
+            for(char c='z';c>='a';c--){
+                int index = temp.indexOf(c+"");
+                if(index != -1){
+                    sb.append(c);
+                    temp.deleteCharAt(index);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
