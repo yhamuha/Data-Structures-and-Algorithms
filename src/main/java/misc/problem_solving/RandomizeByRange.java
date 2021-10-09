294b087bde7fa581e1715f246fbfcabf3f147b1d
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-08 22:15:35 -0700

    cs Array: max array

--- src/main/java/misc/problem_solving/RandomizeByRange.java
+++ src/main/java/misc/problem_solving/RandomizeByRange.java
@@ -11,6 +11,8 @@ public class RandomizeByRange {
         int i_from = 1;
         int i_to = 10;
         System.out.println(Arrays.toString(randomizeByRange(size, i_from, i_to)));
+        int[] array = {2,5,11,16};
+        System.out.println(maxArray(array));
     }
 
     static int[] randomizeByRange (int size, int i_from, int i_to){
@@ -20,5 +22,16 @@ public class RandomizeByRange {
             a[i] = (int) ((Math.random() * (Math.abs(i_from - i_to) + 1)) + Math.min(i_from, i_to));
         }
         return a;
+    }
+
+    // max array
+
+    public static int maxArray (int[] array){
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
     }
 }
