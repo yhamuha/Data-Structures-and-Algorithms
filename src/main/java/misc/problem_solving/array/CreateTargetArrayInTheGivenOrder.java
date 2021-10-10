c7bdf51496aeaee1815c7f357afd85a8b5c22877
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-10 15:10:49 -0700

    cs Array: CreateTargetArrayInTheGivenOrder 0ms

--- src/main/java/misc/problem_solving/array/CreateTargetArrayInTheGivenOrder.java
+++ src/main/java/misc/problem_solving/array/CreateTargetArrayInTheGivenOrder.java
@@ -9,7 +9,7 @@ public class CreateTargetArrayInTheGivenOrder {
     public static void main(String[] args) {
         int[] nums = {0,1,2,3,4};
         int[] index = {0,1,2,2,1};
-        System.out.println(Arrays.toString(createTargetArray(nums, index)));
+        System.out.println(createTargetArray2(nums, index));
     }
 
     // O(n) 1ms; O(n)
@@ -23,5 +23,23 @@ public class CreateTargetArrayInTheGivenOrder {
             nums[i]=out.get(i);
         }
         return nums;
+    }
+
+    // O(n) 0ms; O(1)
+    static int[] createTargetArray2(int[] nums, int[] index) {
+        int countInx=-1;
+        int res[]=new int[nums.length];
+        for(int r=0;r<res.length;r++)
+            res[r]=999;
+        for(int i=0;i<nums.length;i++){
+            if(res[index[i]]!=999){
+                for(int j=countInx;j>=index[i];j--){
+                    res[j+1]=res[j];
+                }
+            }
+            res[index[i]]=nums[i];
+            countInx++;
+        }
+        return res;
     }
 }
