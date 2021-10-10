5f60edeedb9dda768805d761cfbecbb8a3029f45
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-10 13:33:13 -0700

    cs Array: upgraded to 2 ms from 18 ms DecompressRunLengthEncodedList

--- src/main/java/misc/problem_solving/array/DecompressRunLengthEncodedList.java
+++ src/main/java/misc/problem_solving/array/DecompressRunLengthEncodedList.java
@@ -9,8 +9,8 @@ import java.util.List;
  */
 public class DecompressRunLengthEncodedList {
     public static void main(String[] args) {
-        int[] nums = {1,1,2,3};
-        System.out.println(Arrays.toString(decompressRLElist(nums)));
+        int[] nums = {55,11,70,26,62,64};
+        System.out.println(Arrays.toString(decompressRLElist2(nums)));
     }
 
 
@@ -32,5 +32,31 @@ public class DecompressRunLengthEncodedList {
         return result;
     }
 
+    // upgraded version
+    // avoided List and stream
+    // time: O(N) with better performance;
+    // space: O(1)
+    static int[] decompressRLElist2(int[] nums) {
+
+        int solutionLength = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            solutionLength += nums[i];
+        }
+        int[] solution = new int[solutionLength];
+        int startIdx = 0;
+        for (int i=startIdx; i< nums.length; i+=2) {
+            int value = nums[i+1];
+            int freq = nums[i];
+            for (int j = startIdx; j < startIdx+freq; j++) {
+                if (startIdx == 0) {
+                    solution[j] = value;
+                } else {
+                    solution[j] = value;
+                }
+            }
+            startIdx += freq;
+        }
+        return solution;
     }
+}
 
