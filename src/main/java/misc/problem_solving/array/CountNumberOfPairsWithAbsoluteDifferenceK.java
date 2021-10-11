ba879fe98d6003444e1ed87d292956ad9f9d7dfb
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-10 20:33:30 -0700

    cs Array: CountNumberOfPairsWithAbsoluteDifferenceK brute force O(n)

--- src/main/java/misc/problem_solving/array/CountNumberOfPairsWithAbsoluteDifferenceK.java
+++ src/main/java/misc/problem_solving/array/CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -7,12 +7,13 @@ public class CountNumberOfPairsWithAbsoluteDifferenceK {
     public static void main(String[] args) {
         int[] nums = {3,2,1,5,4};
         int k = 2;
-        System.out.println(countKDifference(nums, k));
+        System.out.println(countKDifference2(nums, k));
     }
 
     // brute force
     // 16 ms
     static int countKDifference(int[] nums, int k) {
+
         int result = 0;
 
         for( int i=0; i<nums.length; i++) {
@@ -24,5 +25,18 @@ public class CountNumberOfPairsWithAbsoluteDifferenceK {
         }
 
         return result;
+    }
+
+    // O(n)
+    static int countKDifference2(int[] nums, int k) {
+        int sum=0;
+        int[] arr=new int[100];
+        for(int i=0;i<nums.length;i++) {
+            arr[nums[i]-1]++;
+        }
+        for(int i=0;i<100-k;i++) {
+            sum+=arr[i]*arr[i+k];
+        }
+        return sum;
     }
 }
