c7bcf7ba6532c4945d895b15feba1ebc44b4181e
Yaroslav Hamuha <yhamuha@gmail.com>
2021-10-13 13:11:04 -0700

    cs Sliding Windows: LongestSubstringWithoutRepeatingCharacters

---
+++ src/main/java/misc/problem_solving/sliding_window/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,67 @@
+package misc.problem_solving.sliding_window;
+
+/**
+ *
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    // sliding window approach
+    // O(n) O(k)
+    static int lengthOfLongestSubstring(String s) {
+        int[] chars = new int[128];
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
+        }
+        return res;
+    }
+
+    // naive O(n^3)
+    static int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkRepetition(s, i, j)) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+    // naive
+    private static boolean checkRepetition(String s, int start, int end) {
+        int[] chars = new int[128];
+
+        for (int i = start; i <= end; i++) {
+            char c = s.charAt(i);
+            chars[c]++;
+            if (chars[c] > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
