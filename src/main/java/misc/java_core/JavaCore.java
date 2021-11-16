6d8339bfa02e9e283dd34b0ef6d6b1a288662cd6
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-16 09:44:58 -0800

    JC: List is dynamic

---
+++ src/main/java/misc/java_core/JavaCore.java
@@ -0,0 +1,29 @@
+package misc.java_core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class JavaCore {
+
+    // List is dynamic
+    /*public static void main( String[] args ) {
+        List list = new ArrayList( 3 ); //1
+
+        list.add( new Integer( 100 ) );
+        list.add( new Integer( 200 ) );
+        list.add( new Integer( 300 ) );
+        list.add( new Integer( 400 ) ); //2
+
+        System.out.println( list.size() ); //3
+    }
+
+    >> 4
+    */
+
+
+
+
+}
