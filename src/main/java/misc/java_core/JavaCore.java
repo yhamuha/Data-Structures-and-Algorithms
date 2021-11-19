e67a19e5827c7d8a9ae5fa21419bfc405fccc863
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-18 18:22:32 -0800

    JC: overloading - choice between int and Integer

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -176,4 +176,28 @@ public class JavaCore extends Thread {
 
     }*/
 
+    // compiler doesn't choice between int and Integer
+    // compile error
+    /*public static void test(int ...a) {
+        System.out.println("int...");
+    }
+
+    public static void test(Integer ...a) {
+        System.out.println("Integer...");
+    }
+
+    public static void test(Number ...a) {
+        System.out.println("Number...");
+    }
+
+    public static void test(Object ...a) {
+        System.out.println("Object...");
+    }
+
+    public static void main(String args[]){
+        Number n = new Integer(1);
+        test(n.intValue());
+    }*/
+
+
 }
