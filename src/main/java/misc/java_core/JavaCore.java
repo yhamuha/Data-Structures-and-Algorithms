40ed8283872fd0444df39434dd033498e7e08a59
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-20 10:53:38 -0800

    JC: float -> double

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -203,7 +203,6 @@ public class JavaCore extends Thread {
             System.out.print(--i);
         }
     }*/
-}
 
     // hiding
     /*class A {
@@ -226,4 +225,24 @@ public class JavaCore extends Thread {
             System.out.println(obj.increment());
         }
     }*/
+
+    // 29.1f + 1.0d = 30.100000XXXXd
+    /*public static void main(String[] args) {
+        try {
+            String value = "29.1";
+            System.out.println((Float.valueOf(value) + 1.0) == 30.1);
+            System.out.println((Double.valueOf(value) + 1.0) == 30.1);
+            System.out.println(Float.valueOf(value)/0);
+            System.out.println(Double.valueOf(value)/0);
+        }
+        catch (NumberFormatException ex) {
+            System.out.println("NumberFormatException");
+        }
+        catch (ArithmeticException ex) {
+            System.out.println("ArithmeticException");
+        }
+    }*/
+}
+
+
 
