95228ac0627b912af770ec11b55e41f06d1b2c5c
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-20 11:21:50 -0800

    JC: class initialization order

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -256,6 +256,26 @@ public class JavaCore extends Thread {
         m(1, 2);
     }*/
 
+    // class initialization order
+    /*{
+        System.out.print("1 ");
+    }
+
+    JavaCore(){
+        System.out.print("2 ");
+    }
+
+    public static void main(String[] args) {
+        System.out.print("3 ");
+        JavaCore m = new JavaCore();
+        System.out.print("4 ");
+    }
+
+    static {
+        System.out.print("5 ");
+    }*/
+
+
 }
 
 
