a2a23f141d65b2db7644b41c297940e360c1eb2c
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-20 10:55:28 -0800

    JC: overloading - ambiguous methods

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -242,6 +242,20 @@ public class JavaCore extends Thread {
             System.out.println("ArithmeticException");
         }
     }*/
+
+    // ambiguous methods; compile error
+    /*static void m(int ... a) {
+        System.out.println("1");
+    }
+
+    static void m(Integer ... a) {
+        System.out.println("2");
+    }
+
+    public static void main(String[] args) {
+        m(1, 2);
+    }*/
+
 }
 
 
