a66d20c9ee0c174164837a23a88d0985eaf6064e
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-20 17:49:30 -0800

    JC: psvm contains String[]

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -304,6 +304,18 @@ public class JavaCore extends Thread {
         System.out.println(a == b);
         System.out.println(c == d);
     }*/
+
+    // psvm contains String[]
+    /*static public void main(String[] args) {
+        try {
+            int k = Integer.parseInt(args[1]);
+            System.out.println(args[k]);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }*/
+    /*java.lang.NumberFormatException: For input string: "Two"*/
 }
 
 
