d6acd3b78bf53ecd7489e6c86e2cfa7c34821095
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-20 18:01:11 -0800

    JC: labels; continue, break

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -332,6 +332,17 @@ public class JavaCore extends Thread {
     }*/
     /*NullPointerException*/
 
+    // labels; continue, break
+    /*public static void main(String[] args) {
+        label1: for (int i = 0; i < 3; i++) {
+            if (i == 1)
+                continue label1;
+            label2:
+            System.out.print("TEST ");
+            label3:;
+        }
+    }*/
+
 
 }
 
