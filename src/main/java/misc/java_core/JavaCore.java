64e4fdce0d8f31d8cbd12d441291b060f182d2e7
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-22 12:02:09 -0800

    JC: varargs is analyzing on a last step

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -405,7 +405,24 @@ public class JavaCore extends Thread {
         }
     }*/
 
-    //
+    // varargs is analyzing on a last step
+    /*private void process(String... s) {
+        System.out.print("*");
+    }
+    private void process(String s) {
+        System.out.print("1");
+    }
+    private void process(String s, String a) {
+        System.out.print("2");
+    }
+    public static void main(String[] args) {
+        JavaCore c = new JavaCore();
+        c.process("asd");
+        c.process("asd","asd");
+        c.process("asd","asd","asd");
+    }*/
+
+
 }
 
 
