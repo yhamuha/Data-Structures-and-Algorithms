159315d2914ebde9ed8b5a4f2f2987fe5a7c029a
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-22 11:55:30 -0800

    JC: using method calls in for

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -377,6 +377,21 @@ public class JavaCore extends Thread {
             new D();
         }
     }*/
+
+    // using method calls in for
+    /*static boolean foo(char c) {
+        System.out.print(c);
+        return true;
+    }
+    public static void main(String[] args) {
+        int i = 0;
+        for ( foo('A'); foo('B') && (i < 2); foo('C')) {
+            i++;
+            foo('D');
+        }
+    }*/
+
+
 }
 
 
