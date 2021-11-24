71cb0e32cf347c7ad24313486cc09af4cf021c77
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-24 09:03:53 -0800

    JC: non-typyng collection

--- src/main/java/misc/java_core/JavaCore.java
+++ src/main/java/misc/java_core/JavaCore.java
@@ -448,6 +448,27 @@ public class JavaCore extends Thread {
     public static void main(String[] args) {
         System.out.println(DUMMY);
     }*/
+
+    // non-typyng collection
+    /*public static void main(String[] args) {
+        try {
+            ArrayList<Integer> ar = new ArrayList<Integer>();
+            List temp = ar; //1
+            temp.add(new java.util.Date()); //2
+            temp.add(new Float(1.66));
+            Iterator it = ar.iterator();
+            while (it.hasNext())
+                System.out.println(it.next());
+            System.out.println(ar.get(0));
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+        }
+    }*/
+    /*could use: List temp = Collections.checkedList(ar, Integer.class);
+    *  for checking the right object come into collection*/
+
+    //
+
 }
 
 
