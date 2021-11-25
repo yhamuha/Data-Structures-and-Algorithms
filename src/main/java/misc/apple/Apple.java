eac4bad479badc58ddbe7a9f0d3b370d60b91083
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-25 13:34:23 -0800

    Apple: find majority element

--- src/main/java/misc/apple/Apple.java
+++ src/main/java/misc/apple/Apple.java
@@ -1,6 +1,8 @@
 package misc.apple;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -35,8 +37,37 @@ public class Apple {
         return missingNum;
     }*/
 
+    // find majority element
+    /*public static int majorityElement(int[] nums) {
+        // initial checking
+        int size = 0;
+        if(nums == null || nums.length == 0) {
+            return size;
+        }
+        //init max; times
+        int max = nums[0];
+        int times = 1;
+        // using map with number as key and value at quantity of times
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // for each num in array
+        for(int num : nums) {
+            // increase the internal _times
+            int _times = map.getOrDefault(num, 0) + 1;
+            // update times value for each number in map
+            map.put(num, _times);
+
+            // update global max and times
+            if(_times > times) {
+                max = num;
+                times = _times;
+            }
+        }
+
+        return max;
+    }*/
+
     public static void main(String[] args) {
-        int[] nums = {0,1,3};
-        System.out.println(missingNumber(nums));
+        int[] nums = {0,1,3,7,2,15,43,2,5,6,6,0,0,1,1,0,13,13,13,6,6,6};
+        System.out.println();
     }
 }
