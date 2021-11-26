c7cf6d98610b7cd383e46d650eb67d46570e2df7
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-26 14:34:11 -0800

    FuncProgramming: a few threads as Lambda

--- src/main/java/misc/func_programming/TestFuncProgramming.java
+++ src/main/java/misc/func_programming/TestFuncProgramming.java
@@ -6,18 +6,8 @@ package misc.func_programming;
 public class TestFuncProgramming {
     public static void main(String[] args) {
         System.out.println("~ : main");
-        Thread thread0 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("~ : thread0");
-            }
-        });
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("~ : thread1");
-            }
-        });
+        Thread thread0 = new Thread(() -> System.out.println("~ : thread0"));
+        Thread thread1 = new Thread(() -> System.out.println("~ : thread1"));
         thread0.start();
         thread1.start();
     }
