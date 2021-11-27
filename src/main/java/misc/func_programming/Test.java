c1c42d50647e7e2e512e8be3d6b40e52924b2f33
Yaroslav Hamuha <yhamuha@gmail.com>
2021-11-26 16:24:36 -0800

    FuncProgramming: implement comparator itself through lambda

--- src/main/java/misc/func_programming/Test.java
+++ src/main/java/misc/func_programming/Test.java
@@ -16,7 +16,7 @@ public class Test {
         list.add("T");
         list.add("Th");
 
-       /* list.sort(new Comparator<String>() {
+        /* list.sort(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
                 if (o1.length() > o2.length()) {
@@ -29,7 +29,7 @@ public class Test {
             }
         });*/
 
-        list.sort((s1, s2) -> {
+        /*list.sort((s1, s2) -> {
             if (s1.length() > s2.length()) {
                 return 1;
             } else if (s1.length() < s2.length()) {
@@ -37,7 +37,15 @@ public class Test {
             } else {
                 return 0;
             }
-        });
+        });*/
+
+        // implement comparator itself
+        Comparator<String> comparator = (s1, s2) -> {
+            if (s1.length() > s2.length()) return 1;
+            else if (s1.length() < s2.length()) return -1;
+            else return 0;
+        };
+        list.sort(comparator);
         System.out.println(list);
     }
 }
