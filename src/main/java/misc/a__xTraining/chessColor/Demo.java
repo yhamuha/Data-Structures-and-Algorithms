4aada5016a255b7e65ba00269fd731db64f0ccf1
Yaroslav Hamuha <yhamuha@gmail.com>
2022-05-23 20:42:40 -0700

    #rpt

--- src/main/java/misc/a__xTraining/chessColor/Demo.java
+++ src/main/java/misc/a__xTraining/chessColor/Demo.java
@@ -5,11 +5,11 @@ package misc.a__xTraining.chessColor;
  */
 public class Demo {
     public static void main(String[] args) {
-        String str = "d4";
-        System.out.println(isWhite(str));
+        String str = "a1";
+        System.out.println( isWhite(str) );
     }
 
-    static boolean isWhite(String str) {
+    static boolean isWhite (String str) {
         return (str.charAt(0) + str.charAt(1)) % 2 != 0 ? true : false;
     }
 }
