2e96728f8baf2ebe82a870d934d9031fbebdfeef
Yaroslav Hamuha <yhamuha@gmail.com>
2023-01-21 00:02:12 -0800

    palindrome3

--- src/main/java/misc/java_2023/Palindrome.java
+++ src/main/java/misc/java_2023/Palindrome.java
@@ -10,10 +10,20 @@ public class Palindrome {
         StringBuilder sb = new StringBuilder(original);
 
         if (original.equals(sb.reverse().toString())) {
-            System.out.println(original + " is a palindrome");
+            System.out.println("\"" + original + "\"" + " is a palindrome\n");
         } else {
-            System.out.println(original + " is not palindrome");
+            System.out.println("\"" + original + "\"" + " is not palindrome\n");
         }
+    }
+
+    public static boolean isPalindrome (String s) {
+        int n = s.length();
+        for (int i=0; i<(n/2); ++i) {
+            if (s.charAt(i) != s.charAt(n-i-1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -29,14 +39,24 @@ public class Palindrome {
         System.out.println("palindrome 1:");
         System.out.println("=============");
         if (original.equals(reversed)) {
-            System.out.println(original + " is a palindrome\n");
+            System.out.println("\"" + original + "\"" + " is a palindrome\n");
         } else {
-            System.out.println(original + " is not a palindrome\n");
+            System.out.println("\"" + original + "\"" +  " is not a palindrome\n");
         }
 
         /* palindrome 2 */
         System.out.println("palindrome 2:");
         System.out.println("=============");
         palindrome_StringBuffer("saab");
+
+        /* palindrome 3 */
+        System.out.println("palindrome 3:");
+        System.out.println("=============");
+        String str = "soap";
+        if (isPalindrome(str)) {
+            System.out.println("\"" + str + "\"" + " is a palindrome\n");
+        } else {
+            System.out.println("\"" + str + "\"" + " is not a palindrome\n");
+        }
     }
 }
