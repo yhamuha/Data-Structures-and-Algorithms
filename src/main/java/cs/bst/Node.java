2bb309a42571feeef647f78a6438d9d3cc94c801
Yaroslav Hamuha <yhamuha@gmail.com>
2023-07-19 15:07:18 -0700

    BST. Insert Node.

--- src/main/java/cs/bst/Node.java
+++ src/main/java/cs/bst/Node.java
@@ -1,10 +1,10 @@
 package cs.bst;
 
 public class Node {
-    Integer key;
-    Integer value;
-    Node left;
-    Node right;
+    Integer key = null;
+    Integer value = null;
+    Node left = null;
+    Node right = null;
 
     static void createNode (Node node, int key, int value) {
         node.key = key;
@@ -13,18 +13,37 @@ public class Node {
         node.right = new Node();
     }
 
+    static boolean nodeExist (Node node) {
+        return node.key != null;
+    }
+
+    static void insert (Node node, int key, int value) {
+        if (!nodeExist(node)) createNode(node, key, value);
+        else if (key < node.key)
+            insert (node.left, key, value);
+        else
+            insert (node.right, key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "key=" + key +
+                ", value=" + value +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
     public static void main(String[] args) {
         Node root = new Node();
-        createNode(root, 1, 2);
 
-        Node left = new Node();
-        createNode(left, 2, 3);
+        insert(root, 3, 12);
+        insert(root, 1, 6);
+        insert(root, 4, 33);
 
-        Node right = new Node();
-        createNode(right, 2, 3);
+        System.out.println(" " + root.key);
+        System.out.print(root.left.key); System.out.println(" " + root.right.key);
 
-        System.out.println(root.key + ":" + root.value);
-        System.out.print(left.key + ":" + left.value);
-        System.out.print(" " + right.key + ":" + right.value);
     }
 }
