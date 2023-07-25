3b7c6fb33e34b70e30ec2faef525f9752619a0f9
Yaroslav Hamuha <yhamuha@gmail.com>
2023-07-25 15:55:00 -0700

    Stack

---
+++ src/main/java/cs/stack/Stack.java
@@ -0,0 +1,28 @@
+package cs.stack;
+
+public class Stack {
+
+    static int STACK_TOP =  -1;
+    static int STACK_SIZE = 3;
+    static int[] STACK = new int[STACK_SIZE];
+
+    static void push(int value) throws Exception {
+        if (STACK_TOP == STACK_SIZE - 1) {
+            throw new Exception("Stack overflow");
+        }
+        STACK_TOP += 1;
+        STACK[STACK_TOP] = value;
+        System.out.println(STACK[STACK_TOP]);
+    }
+
+    public static void main(String[] args) {
+        try {
+            push(1);
+            push(1);
+            push(1); // 1 1 1
+            push(1); // Stack overflow
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
