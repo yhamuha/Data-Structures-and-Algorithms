33b561bfe6f03816afadde4c7a122a34f6670dd0
Yaroslav Hamuha <yhamuha@gmail.com>
2023-07-24 22:43:42 -0700

    BigO Notation

--- src/main/java/leetcode/bigo/BigOMeasurements.java
+++ src/main/java/leetcode/bigo/BigOMeasurements.java
@@ -8,8 +8,37 @@ public class BigOMeasurements {
             for (int j = 0; j < arrayB.length; j++) {
                 for (int k = 0; k < 100_000; k++) {
                     System.out.println(arrayA[i] + "," + arrayB[j]);
-                            }
                 }
             }
+        }
+    }
+
+    // O(sqrt(N))
+    static boolean isPrime(int n) {
+        for (int x = 2; x * x <= n; x++) {
+            if (n % x == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // O(n) linear
+    int factorial(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    // O(2^N) expotential
+    int fib(int n) {
+        if (n <= 0) return 0;
+        else if (n==1) return 1;
+        return fib(n-1) + fib (n-2);
+
     }
 }
