8f8e713f08b56b05fcb661eaf1c3ad54aad24098
Yaroslav Hamuha <yhamuha@gmail.com>
2023-08-03 22:31:18 -0700

    Bit Manipulations. highToLow traversal

--- src/main/java/cs/bitmanipulations/BitManipulations.java
+++ src/main/java/cs/bitmanipulations/BitManipulations.java
@@ -6,7 +6,26 @@ public class BitManipulations {
         return data & 1;
     }
 
+    // for byte, short, long -> 7, 15 and 63 respectively
     int getHighestBit (int data) {
         return data >>> 31;
+    }
+
+    void lowToHigh(int bits) {
+        int data = bits;
+        for (int i = 0; i < 32; i++) {
+            int bit = getLowestBit(data);
+            System.out.print(bit);
+            data = data >>> 1;
+        }
+    }
+
+    void highToLow(int bits) {
+        int data = bits;
+        for (int i=0; i<32; i++) {
+            int bit = getHighestBit(data);
+            System.out.println(bit);
+            data = data << 1;
+        }
     }
 }
