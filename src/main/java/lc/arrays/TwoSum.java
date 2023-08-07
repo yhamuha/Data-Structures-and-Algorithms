daf24edf6e93ec78cfa40980a1933c3698706355
Yaroslav Hamuha <yhamuha@gmail.com>
2023-08-07 15:11:09 -0700

    LC: Arrays. TwoSumImproved

--- src/main/java/lc/arrays/TwoSum.java
+++ src/main/java/lc/arrays/TwoSum.java
@@ -1,19 +1,18 @@
 package lc.arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-
+    // O(n^2)
     static public int[] findTwoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++)
-        {
+        for(int i = 0; i < nums.length; i++) {
             int num1 = nums[i];
-            for(int j = i + 1; j < nums.length; j++)
-            {
+            for(int j = i + 1; j < nums.length; j++) {
                 int num2 = nums[j];
                 int sum = num1 + num2;
-                if(sum == target)
-                {
+                if(sum == target) {
                     return new int[] {i , j };
                 }
             }
@@ -21,8 +20,22 @@ public class TwoSum {
         return null;
     }
 
+    // O(n)
+    static public int[] findTwoSumImproved(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        } return null;
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[] {1,2,5,6,7};
+        int[] arr = new int[] {1,5,6,2,7};
         System.out.println(Arrays.toString(findTwoSum(arr, 3)));
+
+        System.out.println(Arrays.toString(findTwoSumImproved(arr, 3)));
     }
 }
