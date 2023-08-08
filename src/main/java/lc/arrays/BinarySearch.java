d13113bbf63178487ef33d61d4a2d379278363e4
Yaroslav Hamuha <yhamuha@gmail.com>
2023-08-08 10:26:02 -0700

    LC: Arrays. binarySearch

---
+++ src/main/java/lc/arrays/BinarySearch.java
@@ -0,0 +1,25 @@
+package lc.arrays;
+
+public class BinarySearch {
+    static public int binarySearch(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,9,22,31};
+        System.out.println(binarySearch(arr, 9));
+    }
+}
