3f811ce752a9d3b69965c96174660510e2a61ff8
Yaroslav Hamuha <yhamuha@gmail.com>
2023-08-09 16:41:25 -0700

    Strings. joinWords

--- src/main/java/cs/sorting/BubbleSort.java
+++ src/main/java/cs/sorting/BubbleSort.java
@@ -42,4 +42,11 @@ public class BubbleSort {
             }
         }
     }
+
+    String joinWords(String[] words) {
+        String sentence = "";
+        for (String w: words)
+            sentence = sentence + w;
+        return sentence;
+    }
 }
