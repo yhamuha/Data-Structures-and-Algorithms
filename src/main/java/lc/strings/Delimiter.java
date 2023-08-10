d2c9169565dc8457c0c2fa244751337ac9794e95
Yaroslav Hamuha <yhamuha@gmail.com>
2023-08-10 10:16:29 -0700

    Strings. joinByDelimiter_func

--- src/main/java/lc/strings/Delimiter.java
+++ src/main/java/lc/strings/Delimiter.java
@@ -1,5 +1,8 @@
 package lc.strings;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Delimiter {
     public static String joinByDelimiter(char delimiter, String...args) {
         StringBuilder result = new StringBuilder();
@@ -11,10 +14,17 @@ public class Delimiter {
         return result.toString();
     }
 
+    public static String joinByDelimiter_func(char delimiter, String...args) {
+        return Arrays.stream(args, 0, args.length)
+                .collect(Collectors.joining(String.valueOf(delimiter)));
+    }
+
     public static void main(String[] args) {
         String str1 = "str1";
         String str2 = "str2";
         char delim = '*';
         System.out.println(joinByDelimiter(delim,str1,str2));
+
+        System.out.println(joinByDelimiter_func(delim, str1, str2));
     }
 }
