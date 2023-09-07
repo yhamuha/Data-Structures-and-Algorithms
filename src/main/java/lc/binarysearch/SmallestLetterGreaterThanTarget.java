ad51f57ff677310db2003128373ac0a8be9e15a3
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-07 13:00:48 -0700

    lc. binarysearch. nextGreatestLetter

---
+++ src/main/java/lc/binarysearch/SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,30 @@
+package lc.binarysearch;
+
+public class SmallestLetterGreaterThanTarget {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target > letters[mid]){
+                start = mid + 1;
+            }
+            else if(target < letters[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return  letters[start % letters.length];
+
+    }
+
+    public static void main(String[] args) {
+        char[] letters = {'a', 'c', 'x', 'b'};
+        char target = 'v';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+}
