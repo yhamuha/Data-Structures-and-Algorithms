2e14ec160a8df7576796b805d55649a61cce1e0c
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-16 19:14:43 -0700

    lc. strings twopointer. reverseString

---
+++ src/main/java/lc/strings/ReverseString.java
@@ -0,0 +1,25 @@
+package lc.strings;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    // TC: O(n); SC O(1)
+    public static void reverseString(char[] s) {
+        int len = s.length;
+        int left = 0;
+        int right = s.length - 1;
+        while(left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] str = {'H', 'e', 'l', 'l', 'o'};
+        reverseString(str);
+        System.out.println(Arrays.toString(str));
+    }
+}
