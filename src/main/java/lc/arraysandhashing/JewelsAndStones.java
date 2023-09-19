60f3d0e31e0fc2715ef109566e9560f5ee3f4744
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-19 12:30:24 -0700

    lc. arraysandhashing. numJewelsInStones

---
+++ src/main/java/lc/arraysandhashing/JewelsAndStones.java
@@ -0,0 +1,31 @@
+package lc.arraysandhashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+        public int numJewelsInStones(String jewels, String stones) {
+            Set<Character> setOfJewels = new HashSet<>();
+
+            for (int i=0; i< jewels.length(); i++) {
+                setOfJewels.add(jewels.charAt(i));
+            }
+
+            int score = 0;
+
+            for (int i=0; i< stones.length(); i++) {
+                if (setOfJewels.contains(stones.charAt(i))) score++;
+            }
+
+            return score;
+
+        }
+    }
+
+
+// jewels "aAc"
+// stones "AaaaABbcccCC"
+
+// score 8
+
+// TC: O(n+m); SC: O(n)
