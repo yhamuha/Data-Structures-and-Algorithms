39a4e556157cf44ea3e4d812720c662d817ad830
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-20 11:56:56 -0700

    lc. arraysandhashing. intToRoman

--- src/main/java/lc/arraysandhashing/IntToRoman.java
+++ src/main/java/lc/arraysandhashing/IntToRoman.java
@@ -1,31 +1,36 @@
 package lc.arraysandhashing;
 
 public class IntToRoman {
-    private static final String[] numerals =
-            new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
-                    "V", "IV", "I"};
-    private static final int[] values =
-            new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+    public String intToRoman(int num) {
+        final String[] numerals = new String[]
+                {   "M",    "CM",   "D",    "CD",   "C",
+                        "XC",   "L",    "XL",   "X",
+                        "IX",   "V",    "IV",   "I"};
+        final int[] values = new int[]
+                {   1000, 900,  500,  400,  100,
+                        90,   50,    40,   10,
+                        9,    5,    4,   1};
 
 
-    public static String integerToRoman(int value) {
-        if (value > 3999 || value < 1) throw new IllegalArgumentException();
+        if (num > 3999 || num < 1) throw new IllegalArgumentException();
 
         StringBuilder numeral = new StringBuilder();
-        int i = 0;
-        while (value > 0) {
-            if (value - values[i] >= 0) {
+        int i =0;
+        while (num > 0) {
+            if (num >= values[i]) {
                 numeral.append(numerals[i]);
-                value -= values[i];
+                num -= values[i];
             } else {
                 i++;
             }
         }
-
         return numeral.toString();
+
     }
 
     public static void main(String[] args) {
-        System.out.println(integerToRoman(12));
+        IntToRoman itr = new IntToRoman();
+        System.out.println(itr.intToRoman(8));
     }
 }
