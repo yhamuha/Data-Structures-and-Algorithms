4144ca7f02f6462ea3ff9b8f9da0926d69926dcf
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-20 14:18:07 -0700

    lc. arraysandhashing. groupAnagrams

---
+++ src/main/java/lc/arraysandhashing/GroupAnagrams.java
@@ -0,0 +1,30 @@
+package lc.arraysandhashing;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groupedAnagrams = new HashMap<>();
+        for (String str : strs){
+
+
+            char[] charArr = str.toCharArray();
+            Arrays.sort(charArr);
+
+            String sortedStr = String.valueOf(charArr);
+
+            if (!groupedAnagrams.containsKey(sortedStr)) {
+                groupedAnagrams.put(sortedStr, new ArrayList<>());
+            }
+            groupedAnagrams.get(sortedStr).add(str);
+
+        }
+        return new ArrayList<>(groupedAnagrams.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams ga = new GroupAnagrams();
+        String[] strs = new String[] {"eat", "tea", "cat", "bca", "abc"};
+        System.out.println(ga.groupAnagrams(strs));
+    }
+}
