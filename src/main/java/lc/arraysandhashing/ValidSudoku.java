848921337b2e50ce47d3cb84359e1bd2d5583dfc
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-22 17:57:21 -0700

    lc. arraysandhashing. validSudoku TC/SC: O(n^2)

---
+++ src/main/java/lc/arraysandhashing/ValidSudoku.java
@@ -0,0 +1,38 @@
+package lc.arraysandhashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<>();
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                char number = board[row][column];
+                if (number != '.') {
+                    if (!set.add(number + " in row " + row) ||
+                            !set.add(number + " in column " + column) ||
+                            !set.add(number + " in block " + (row / 3) + "," + (column / 3)))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        ValidSudoku vs = new ValidSudoku();
+        System.out.println(vs.isValidSudoku(board));
+    }
+}
