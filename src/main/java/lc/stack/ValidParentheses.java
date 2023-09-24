f285802254a774296395bf9a53340481f0ac5659
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-24 15:58:07 -0700

    lc. stack. ValidParentheses. TC/SC: O(n)

---
+++ src/main/java/lc/stack/ValidParentheses.java
@@ -0,0 +1,40 @@
+package lc.stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "{()}";
+        ValidParentheses vp = new ValidParentheses();
+        System.out.println(vp.isValid(s));
+    }
+}
+
+
+
+/*
+    () parentheses
+    [] brackets
+    {} braces
+
+    { push [ push ] pop } pop
+
+
+ */
