a434caa7c00ab6c7531ef68c8c438fc33c27ca38
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-25 19:07:10 -0700

    lc. stack. CrawlerLogFolder. TC: O(n), SC: O(1)

---
+++ src/main/java/lc/stack/CrawlerLogFolder.java
@@ -0,0 +1,23 @@
+package lc.stack;
+
+public class CrawlerLogFolder {
+    // TC: O(n), SC: O(1)
+    public int minOperations(String[] logs) {
+        int depth =0;
+        for (String log : logs) {
+            if (log.equals("./")) continue;
+            else if (log.equals("../")) {
+                depth--;
+                if (depth<0) depth =0;
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    public static void main(String[] args) {
+        String[] logs = {"d1/","d2/","../","d21/","./"};
+        CrawlerLogFolder clf = new CrawlerLogFolder();
+        System.out.println(clf.minOperations(logs));
+    }
+}
