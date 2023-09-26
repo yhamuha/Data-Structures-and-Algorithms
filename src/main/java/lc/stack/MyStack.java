fb72f2c58741c8941cc520e21b202ac62d9ad7a7
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-25 20:32:59 -0700

    lc. stack. Implement Stack using Queues. TC: O(n), SC: O(1)

---
+++ src/main/java/lc/stack/MyStack.java
@@ -0,0 +1,56 @@
+package lc.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+
+        Queue<Integer> q = new LinkedList<>();
+        public MyStack() {
+
+        }
+
+        // O(n), O(1)
+        public void push(int x) {
+          q.add(x);
+          int k = q.size();
+          for (int i=0; i< k-1; i++) {
+              int first = q.remove();
+              q.add(first);
+          }
+        }
+
+        // O(n), O(1)
+        public int pop() {
+            return q.remove();
+        }
+
+        public int top() {
+            return q.peek();
+        }
+
+        public boolean empty() {
+            return q.isEmpty();
+        }
+
+    /*
+     Your MyStack object will be instantiated and called as such:
+
+     MyStack obj = new MyStack();
+     obj.push(x);
+     int param_2 = obj.pop();
+     int param_3 = obj.top();
+     boolean param_4 = obj.empty();
+     */
+
+
+    public static void main(String[] args) {
+        MyStack obj = new MyStack();
+        int x = 1;
+        int y = 2;
+        obj.push(x);
+        obj.push(y);
+        System.out.println(obj.pop());
+        System.out.println(obj.empty());
+    }
+}
