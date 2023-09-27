a72c45ad324fb5bcbf554cbff4ce8bdb6c92d07d
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-26 21:05:06 -0700

    lc. stack. EvaluateReversePolishNotation. TC: O(n), SC: O(n)

---
+++ src/main/java/lc/stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,46 @@
+package lc.stack;
+
+import java.util.Set;
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+        // TC: O(n), SC: O(n)
+        public int evalRPN(String[] tokens) {
+            Stack<Integer> stack = new Stack<>();
+            Set<String> ops = Set.of("+", "-", "*", "/");
+
+            for (String s : tokens) {
+                if (ops.contains(s)) {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    int c = 0;
+
+                    switch(s) {
+                        case "+" -> {c = b + a;}
+                        case "-" -> {c = b - a;}
+                        case "*" -> {c = b * a;}
+                        case "/" -> {c = b / a;}
+                    }
+
+                    stack.push(c);
+                }
+
+                else stack.push(Integer.parseInt(s));
+            }
+
+            return stack.peek();
+        }
+
+    public static void main(String[] args) {
+        String[] tokens = {"2","1","+","3","*"};
+        EvaluateReversePolishNotation erpn = new EvaluateReversePolishNotation();
+        System.out.println(erpn.evalRPN(tokens));
+    }
+    }
+
+/*
+    infix notation      5+3
+    polish notation     53+
+    For example, the infix expression (3 × 4) + (5 × 6) becomes 3 4 × 5 6 × + in reverse Polish notation
+        advantage - it removes the need of order operations and parentheses that required for infix notation
+ */
