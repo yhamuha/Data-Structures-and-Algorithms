c9390f99189b41198dc8e5a50c6a26c18a78df31
Yaroslav Hamuha <yhamuha@gmail.com>
2023-09-28 21:51:18 -0700

    lc. stack. RemovingStarsFromAString. O(n) O(n)

---
+++ src/main/java/lc/stack/RemovingStarsFromAString.java
@@ -0,0 +1,31 @@
+package lc.stack;
+
+import java.util.Stack;
+
+public class RemovingStarsFromAString {
+    // O(n) O(n)
+    public String removeStars(String s) {
+        StringBuilder str = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++){
+            if (!stack.isEmpty() && s.charAt(i) == '*'){
+                stack.pop();
+            }
+            else if (s.charAt(i) != '*'){
+                stack.push(s.charAt(i));
+            }
+        }
+
+        for (char chr : stack){
+            str.append(chr);
+        }
+
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "leet**cod*e";
+        RemovingStarsFromAString rsfs = new RemovingStarsFromAString();
+        System.out.println(rsfs.removeStars(s));
+    }
+}
