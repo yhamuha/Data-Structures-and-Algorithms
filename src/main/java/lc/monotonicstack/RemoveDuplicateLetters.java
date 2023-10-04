0528d95cc49fbb24d1a1803074cab79120c359e5
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-04 10:27:42 -0700

    lc. monotonicstack. removeDuplicateLetters. O(n)

---
+++ src/main/java/lc/monotonicstack/RemoveDuplicateLetters.java
@@ -0,0 +1,36 @@
+package lc.monotonicstack;
+
+import java.util.Stack;
+
+public class RemoveDuplicateLetters {
+    public String removeDuplicateLetters(String s) {
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            lastIndex[s.charAt(i) - 'a'] = i; // track the lastIndex of character presence
+        }
+
+        boolean[] seen = new boolean[26]; // keep track seen
+        Stack<Integer> st = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (seen[curr]) continue; // if seen continue as we need to pick one char only
+            while (!st.isEmpty() && st.peek() > curr && i < lastIndex[st.peek()]){
+                seen[st.pop()] = false; // pop out and mark unseen
+            }
+            st.push(curr); // add into stack
+            seen[curr] = true; // mark seen
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty())
+            sb.append((char) (st.pop() + 'a'));
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "bcabc";
+        RemoveDuplicateLetters rdl = new RemoveDuplicateLetters();
+        System.out.println(rdl.removeDuplicateLetters(s));
+    }
+}
