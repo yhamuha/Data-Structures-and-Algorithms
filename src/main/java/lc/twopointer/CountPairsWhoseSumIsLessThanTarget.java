d20a34a844720e751cf804c7988ace9dfb7b74f8
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-09 10:55:46 -0700

    lc. twopointers. countPairs. O(n) O(1)

---
+++ src/main/java/lc/twopointer/CountPairsWhoseSumIsLessThanTarget.java
@@ -0,0 +1,32 @@
+package lc.twopointer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CountPairsWhoseSumIsLessThanTarget {
+
+    public int countPairs(List<Integer> nums, int target) {
+        Collections.sort(nums);
+        int count=0, left=0, right=nums.size()-1;
+
+        while(left < right){
+            if(nums.get(left) + nums.get(right) < target){
+                count += right - left;
+                left++;
+            }
+            else
+                right--;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountPairsWhoseSumIsLessThanTarget countPairs = new CountPairsWhoseSumIsLessThanTarget();
+        List<Integer> nums = Arrays.asList(-6, 2, 5, -2, -7, -1, 3);
+        int target = -2;
+        System.out.println(countPairs.countPairs(nums, target));
+    }
+
+}
