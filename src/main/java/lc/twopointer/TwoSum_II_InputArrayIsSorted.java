1bea697f1207f3bfba3e38da46ce5c08a410aeb9
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-09 16:45:22 -0700

    lc. twopointers. TwoSum_II_InputArrayIsSorted. O(n) O(1)

---
+++ src/main/java/lc/twopointer/TwoSum_II_InputArrayIsSorted.java
@@ -0,0 +1,26 @@
+package lc.twopointer;
+
+import java.util.Arrays;
+
+public class TwoSum_II_InputArrayIsSorted {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int i = 0, j = nums.length-1;
+        while(i < j){
+            int check = nums[i]+nums[j];
+            if(check == target) break;
+            if(check < target) i++;
+            else j--;
+        }
+        res[0] = i+1;
+        res[1] = j+1;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        TwoSum_II_InputArrayIsSorted twoSum = new TwoSum_II_InputArrayIsSorted();
+        System.out.println(Arrays.toString(twoSum.twoSum(nums, target)));
+    }
+}
