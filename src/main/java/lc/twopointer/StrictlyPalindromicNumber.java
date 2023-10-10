39d54c3e43a30c86b5013b6fbe4bbd421e8eeadc
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-10 16:31:44 -0700

    lc. twopointers. isStrictlyPalindromic. O(n) O(1)

---
+++ src/main/java/lc/twopointer/StrictlyPalindromicNumber.java
@@ -0,0 +1,42 @@
+package lc.twopointer;
+
+public class StrictlyPalindromicNumber {
+    boolean isPalindrom(String str){
+        int left = 0, right = str.length()-1;
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    String convert(int n, int i){
+        StringBuilder sb = new StringBuilder();
+        while(n!=0){
+            int rem = n%i;
+            n/=i;
+            sb.insert(0,rem);
+        }
+        return sb.toString();
+    }
+
+    public boolean isStrictlyPalindromic(int n) {
+        for(int i=2; i<=(n-2); i++){
+            String str = convert(n, i);
+            if(isPalindrom(str)){
+                continue;
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        StrictlyPalindromicNumber spn = new StrictlyPalindromicNumber();
+        System.out.println(spn.isStrictlyPalindromic(9));
+    }
+}
