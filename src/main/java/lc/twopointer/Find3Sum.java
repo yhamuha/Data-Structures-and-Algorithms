d2bd0876b84340d4246e54e61cef7da980aa402e
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-09 20:21:34 -0700

    lc. twopointers. findThreeSum. O(n^2) O(n)

---
+++ src/main/java/lc/twopointer/Find3Sum.java
@@ -0,0 +1,45 @@
+package lc.twopointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Find3Sum {
+    // O(n^2)
+    public List<List<Integer>> findThreeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int  i = 0; i<n ; i++ ){
+            if(i != 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int j =i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else {
+                    List<Integer> temp = Arrays.asList(nums[i] , nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(k>j &&nums[k]== nums[k+1]) k--;
+                    while(k>j && nums[j]== nums[j-1]) j++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Find3Sum threeSum = new Find3Sum();
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum.findThreeSum(nums));
+    }
+}
