5d01bcf5d5a853d39d6739c7af078e5c498e8a5f
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-11 11:02:11 -0700

    lc. twopointers. MinimizeMaximumPairSumInArray. O(n log n) O(1)

---
+++ src/main/java/lc/twopointer/MinimizeMaximumPairSumInArray.java
@@ -0,0 +1,22 @@
+package lc.twopointer;
+
+import java.util.Arrays;
+
+public class MinimizeMaximumPairSumInArray {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int i=0,j=nums.length-1;
+        int max = Integer.MIN_VALUE;
+        while(i<j){
+            max = Math.max(nums[i]+nums[j],max);
+            i++;j--;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MinimizeMaximumPairSumInArray mmp = new MinimizeMaximumPairSumInArray();
+        int[] nums = {3,5,4,2,4,6};
+        System.out.println(mmp.minPairSum(nums));
+    }
+}
