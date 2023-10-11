ab8af3c8f187cd57987b1c0c032c543fbc8c1ce6
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-11 10:28:30 -0700

    lc. twopointers. RearrangeArrayElementsBySign. O(n) O(1)

---
+++ src/main/java/lc/twopointer/RearrangeArrayElementsBySign.java
@@ -0,0 +1,29 @@
+package lc.twopointer;
+
+import java.util.Arrays;
+
+public class RearrangeArrayElementsBySign {
+    public int[] rearrangeArray(int[] arr) {
+        int n = arr.length;
+        int [] ans = new int[n];
+        int start =0;
+        int end = 1;
+        for(int i=0; i<n; i++){
+            if(arr[i] >=0){
+                ans[start] = arr[i];
+                start+=2;
+            }
+            else{
+                ans[end] = arr[i];
+                end+=2;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+    RearrangeArrayElementsBySign rearr = new RearrangeArrayElementsBySign();
+    int[] nums = {3,1,-2,-5,2,-4};
+    System.out.println(Arrays.toString(rearr.rearrangeArray(nums)));
+    }
+}
