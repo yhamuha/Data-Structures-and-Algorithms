81c9a212c9778bb743675f13288206738b0751a6
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-12 12:27:02 -0700

    lc. twopointers. Find4Sum. O(n^3) O(n)

---
+++ src/main/java/lc/twopointer/Find4Sum.java
@@ -0,0 +1,36 @@
+package lc.twopointer;
+
+import java.util.*;
+
+public class Find4Sum {
+
+    // O (n^3)
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                int start = j+1;
+                int end = nums.length-1;
+                while(end > start){
+                    int sum = nums[i]+ nums[j] + nums[start] + nums[end];
+                    if(sum == target){
+                        set.add(Arrays.asList(nums[i], nums[j], nums[start++], nums[end--]));
+                    }
+                    else if (sum > target) end --;
+                    else start++;
+                }
+            }
+        }
+        return new ArrayList<>(set);
+    }
+
+    // O(n^2)
+
+    public static void main(String[] args) {
+        int[] nums = {1,0,-1,0,-2,2};
+        int target = 0;
+        Find4Sum find4sum = new Find4Sum();
+        System.out.println(find4sum.fourSum(nums,target));
+    }
+}
