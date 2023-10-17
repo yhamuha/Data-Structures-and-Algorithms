57d02cbc79458b66cda71e0806d9a6ab61a5d8d3
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-16 18:25:11 -0700

    lc. arraysandhashing.

--- src/main/java/lc/arraysandhashing/GroupAnagrams.java
+++ src/main/java/lc/arraysandhashing/GroupAnagrams.java
@@ -6,18 +6,14 @@ public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> groupedAnagrams = new HashMap<>();
         for (String str : strs){
-
-
             char[] charArr = str.toCharArray();
             Arrays.sort(charArr);
-
             String sortedStr = String.valueOf(charArr);
 
             if (!groupedAnagrams.containsKey(sortedStr)) {
                 groupedAnagrams.put(sortedStr, new ArrayList<>());
             }
             groupedAnagrams.get(sortedStr).add(str);
-
         }
         return new ArrayList<>(groupedAnagrams.values());
     }
--- src/main/java/lc/arraysandhashing/JewelsAndStones.java
+++ src/main/java/lc/arraysandhashing/JewelsAndStones.java
@@ -4,22 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class JewelsAndStones {
-        public int numJewelsInStones(String jewels, String stones) {
-            Set<Character> setOfJewels = new HashSet<>();
 
-            for (int i=0; i< jewels.length(); i++) {
-                setOfJewels.add(jewels.charAt(i));
-            }
-
-            int score = 0;
-
-            for (int i=0; i< stones.length(); i++) {
-                if (setOfJewels.contains(stones.charAt(i))) score++;
-            }
-
-            return score;
-
+    // O(n) O(n)
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> setOfJewels = new HashSet<>();
+        for (int i=0; i< jewels.length(); i++) {
+            setOfJewels.add(jewels.charAt(i));
         }
+        int score = 0;
+        for (int i=0; i< stones.length(); i++) {
+            if (setOfJewels.contains(stones.charAt(i))) score++;
+        }
+        return score;
+    }
 
     public static void main(String[] args) {
         JewelsAndStones jas = new JewelsAndStones();
