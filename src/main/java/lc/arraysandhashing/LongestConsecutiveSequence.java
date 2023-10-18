c60f31f7e37b5da43a38bcd770e29192dcca54c8
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-18 12:28:54 -0700

    lc. arraysandhashing

--- src/main/java/lc/arraysandhashing/LongestConsecutiveSequence.java
+++ src/main/java/lc/arraysandhashing/LongestConsecutiveSequence.java
@@ -11,37 +11,28 @@ public class LongestConsecutiveSequence {
         for (int num : nums) {
             numberTravelledMap.put(num, Boolean.FALSE);
         }
-
         for (int num : nums) {
             int currentLength = 1;
-
             // Check in forward direction
             int nextNum = num + 1;
             while (numberTravelledMap.containsKey(nextNum) &&
                     numberTravelledMap.get(nextNum) == false) {
-
                 currentLength++;
                 numberTravelledMap.put(nextNum, Boolean.TRUE);
-
                 // Move to the next number
                 nextNum++;
             }
-
             // Check in reverse direction
             int prevNum = num - 1;
             while (numberTravelledMap.containsKey(prevNum) &&
                     numberTravelledMap.get(prevNum) == false) {
-
                 currentLength++;
                 numberTravelledMap.put(prevNum, Boolean.TRUE);
-
                 // Move to the previous number
                 prevNum--;
             }
-
             longestLength = Math.max(longestLength, currentLength);
         }
-
         return longestLength;
     }
 
