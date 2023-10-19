fd6edd1c8ec5021098be69d90c267dcd59851621
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-19 12:13:42 -0700

    lc. monotonicstack.

--- src/main/java/lc/monotonicstack/FinalPricesWithASpecialDiscount.java
+++ src/main/java/lc/monotonicstack/FinalPricesWithASpecialDiscount.java
@@ -6,10 +6,10 @@ import java.util.Stack;
 public class FinalPricesWithASpecialDiscount {
     public int[] finalPrices(int[] prices) {
         Stack<Integer> st=new Stack<>();
-        for(int i=prices.length-1;i>=0;i--){
-            while(st.size()>0&&st.peek()>prices[i])
+        for(int i=prices.length-1; i>=0; i--){
+            while( st.size()>0 && st.peek()>prices[i])
                 st.pop();
-            int n=prices[i];
+            int n = prices[i];
             if(!st.isEmpty())
                 prices[i]=prices[i]- st.peek();
             st.push(n);
@@ -18,7 +18,8 @@ public class FinalPricesWithASpecialDiscount {
     }
 
     public static void main(String[] args) {
-        int[] prices = {8,4,6,2,3};
+        // int[] prices = {8,4,6,2,3};
+        int[] prices = {2,4,3,1,5,7};
         FinalPricesWithASpecialDiscount fpd = new FinalPricesWithASpecialDiscount();
         System.out.println(Arrays.toString(fpd.finalPrices(prices)));
     }
--- src/main/java/lc/monotonicstack/RemoveKDigits.java
+++ src/main/java/lc/monotonicstack/RemoveKDigits.java
@@ -2,14 +2,6 @@ package lc.monotonicstack;
 
 import java.util.Stack;
 
-
-/*
-    mums    1432219
-    k       3
-
-
- */
-
 public class RemoveKDigits {
         public String removeKdigits(String num, int k) {
             Stack<Character> st = new Stack<>();
@@ -18,19 +10,14 @@ public class RemoveKDigits {
                     st.pop();
                     k--;
                 }
-
                 if(!st.isEmpty() || ch != '0') {
                     st.push(ch);
                 }
             }
-
-            // This is a special edge case --> 1 2 3 4
             while(!st.isEmpty() && k-- > 0) {
                 st.pop();
             }
             if(st.isEmpty()) return "0";
-
-            // Store the ans -->
             String ans = "";
             while(!st.isEmpty()) {
                 ans = st.peek() + ans;
