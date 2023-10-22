60f82a422697bbdc9d3957486016f2a8271e361d
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-22 13:24:14 -0700

    lc. arraysandhashing.

--- src/main/java/lc/arraysandhashing/LongestConsecutiveSequence.java
+++ src/main/java/lc/arraysandhashing/LongestConsecutiveSequence.java
@@ -4,40 +4,68 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestConsecutiveSequence {
-    int longestConsecutive(int[] nums) {
 
+    // O(n) O(n)
+/*    public int longestConsecutive(int[] nums) {
         int longestLength = 0;
-        Map<Integer, Boolean> numberTravelledMap = new HashMap<>();
-        for (int num : nums) {
-            numberTravelledMap.put(num, Boolean.FALSE);
+        Map<Integer,Boolean> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, Boolean.FALSE);
         }
-        for (int num : nums) {
+
+        for (int num : nums)  {
             int currentLength = 1;
-            // Check in forward direction
+
             int nextNum = num + 1;
-            while (numberTravelledMap.containsKey(nextNum) &&
-                    numberTravelledMap.get(nextNum) == false) {
+            while ( map.containsKey(nextNum) &&
+                    map.containsValue(nextNum) == false) {
                 currentLength++;
-                numberTravelledMap.put(nextNum, Boolean.TRUE);
-                // Move to the next number
+                map.put(nextNum, Boolean.TRUE);
                 nextNum++;
             }
-            // Check in reverse direction
+
             int prevNum = num - 1;
-            while (numberTravelledMap.containsKey(prevNum) &&
-                    numberTravelledMap.get(prevNum) == false) {
+            while (map.containsKey(prevNum) &&
+                   map.containsValue(prevNum) == false) {
                 currentLength++;
-                numberTravelledMap.put(prevNum, Boolean.TRUE);
-                // Move to the previous number
+                map.put(num, Boolean.FALSE);
                 prevNum--;
             }
             longestLength = Math.max(longestLength, currentLength);
         }
         return longestLength;
+    }*/
+
+    public int longestConsecutive(int[] nums) {
+
+        int longestLength = 0;
+
+        Map<Integer, Boolean> map = new HashMap();
+        for (int num : nums) {
+            map.put(num, Boolean.FALSE);
+        }
+
+        for (int num : nums) {
+            int currentLength = 1;
+            int nextNum = num + 1;
+            while (map.containsKey(nextNum) && map.get(nextNum) == false) {
+                currentLength++;
+                map.put(nextNum,Boolean.TRUE);
+                nextNum++;
+            }
+            int prevNum = num + 1;
+            while (map.containsKey(prevNum) && map.get(prevNum) == false) {
+                currentLength++;
+                map.put(prevNum,Boolean.TRUE);
+                prevNum++;
+            }
+            longestLength = Math.max(longestLength, currentLength);
+        }
+    return longestLength;
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,1,6,-1};
+        int[] nums = {100,4,200,1,3,2};
         LongestConsecutiveSequence lcs = new LongestConsecutiveSequence();
         System.out.println(lcs.longestConsecutive(nums));
     }
