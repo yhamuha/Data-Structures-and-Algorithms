dadd2afef6be9d3b098b2eace2fcad7eb0baf897
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-22 15:07:22 -0700

    lc. binarysearch. nextGreatestLetter. O(n) O(1)

--- src/main/java/lc/binarysearch/SmallestLetterGreaterThanTarget.java
+++ src/main/java/lc/binarysearch/SmallestLetterGreaterThanTarget.java
@@ -1,27 +1,25 @@
 package lc.binarysearch;
 
 public class SmallestLetterGreaterThanTarget {
+    // O(n) O(1)
     public static char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
         int end = letters.length - 1;
 
-        while(start <= end){
-            int mid = start + (end - start) / 2;
+        while (start < end) {
 
-            if(target > letters[mid]){
+            int mid = end - (start+end) / 2;
+
+            if ( target > letters[mid] ) {
                 start = mid + 1;
-            }
-            else if(target < letters[mid]){
-                end = mid - 1;
-            }
-            else{
+            } else if ( target < letters[mid]) {
+                end = mid-1;
+            } else {
                 start = mid + 1;
             }
         }
-        return  letters[start % letters.length];
-
+        return letters[start % letters.length];
     }
-
     public static void main(String[] args) {
         char[] letters = {'a', 'c', 'x', 'b'};
         char target = 'v';
