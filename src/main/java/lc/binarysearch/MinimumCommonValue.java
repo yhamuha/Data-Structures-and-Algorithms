47fc3a7e34d9f6470f4f2be057eaaa2c3d64f9b3
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-22 20:38:22 -0700

    lc. binarysearch. MinimumCommonValue. O(n log n) O(1)

---
+++ src/main/java/lc/binarysearch/MinimumCommonValue.java
@@ -0,0 +1,30 @@
+package lc.binarysearch;
+
+public class MinimumCommonValue {
+    //Overall time complexity: O(n log n)
+    public int getCommon(int[] arr, int[] nums2) {
+        // O(n)
+        for(int num:nums2){
+            int start=0;
+            int end=arr.length-1;
+            // O(log n)
+            while(start<=end){
+                int mid=start + (end-start)/2;
+                if(arr[mid]==num){
+                    return arr[mid];
+                }
+                else if(arr[mid]>num){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        var mcv = new MinimumCommonValue();
+        System.out.println(mcv.getCommon(new int[]{1,2,3,6}, new int[]{2,3,4,5}));
+    }
+}
