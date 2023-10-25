59772000da7694e7a4e3df6687d42f85b3e7dab2
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-24 19:31:43 -0700

    lc. binarysearch. Searcha2DMatrix. O(log(m*n)) O(1)

---
+++ src/main/java/lc/binarysearch/m/Searcha2DMatrix.java
@@ -0,0 +1,28 @@
+package lc.binarysearch.m;
+
+public class Searcha2DMatrix {
+    // O(log(m*n))
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
+
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var s2dm = new Searcha2DMatrix();
+        System.out.println(s2dm.searchMatrix(new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 13));
+    }
+}
