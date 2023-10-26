a19c532b41fe2aca1775f6d097b603a28ffecff1
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-25 18:30:20 -0700

    lc. binarysearch. MinimumArrayLengthAfterPairRemovals. O(n) solution

--- src/main/java/lc/binarysearch/m/MinimumArrayLengthAfterPairRemovals.java
+++ src/main/java/lc/binarysearch/m/MinimumArrayLengthAfterPairRemovals.java
@@ -1,11 +1,14 @@
 package lc.binarysearch.m;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MinimumArrayLengthAfterPairRemovals {
+
+    // O(n)
     public int minLengthAfterRemovals(List<Integer> nums) {
+
         int N = nums.size(), max = 1, temp = 1;
+
         for(int i = 1; i < N; i++){
             if(nums.get(i) - nums.get(i - 1) == 0){
                 temp++;
@@ -14,11 +17,39 @@ public class MinimumArrayLengthAfterPairRemovals {
             }else
                 temp = 1;
         }
+
         if(max > N / 2)
             return (max * 2 - N);
         if(N % 2 == 1)
             return 1;
         return 0;
+    }
+
+    // O(n log n)
+    public int minLengthAfterRemovals_(List<Integer> nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int x : nums) {
+            cnt.merge(x, 1, Integer::sum);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int x : cnt.values()) {
+            pq.offer(x);
+        }
+        int ans = nums.size();
+        while (pq.size() > 1) {
+            int x = pq.poll();
+            int y = pq.poll();
+            x--;
+            y--;
+            if (x > 0) {
+                pq.offer(x);
+            }
+            if (y > 0) {
+                pq.offer(y);
+            }
+            ans -= 2;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
