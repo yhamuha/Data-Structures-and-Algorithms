bfb67eb6f65eb256345f07d848358d45cb5c141d
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-30 13:05:32 -0700

    lc. slidingwindow. MaxSumRange. O(n)

---
+++ src/main/java/lc/slidingwindow/MaxSumRange.java
@@ -0,0 +1,49 @@
+package lc.slidingwindow;
+
+public class MaxSumRange {
+    public int start;
+    public int end;
+    public int sum;
+
+    public MaxSumRange() {
+    }
+
+    public MaxSumRange(int start, int end, int sum) {
+        this.start = start;
+        this.end = end;
+        this.sum = sum;
+    }
+
+    @Override
+    public String toString() {
+        return "MaxSumRange{" +
+                "start=" + start +
+                ", end=" + end +
+                ", sum=" + sum +
+                '}';
+    }
+
+    MaxSumRange getMaxSumRange (int[] arr) {
+        if (arr== null || arr.length == 0) return null;
+        int windowStart = 0;
+        int windowEnd =0;
+        int currSum =0;
+        MaxSumRange maxSumRange = new MaxSumRange(windowStart,windowEnd, arr[0]);
+        while (windowEnd < arr.length) {
+            currSum += arr[windowEnd];
+            if (currSum > maxSumRange.sum)
+                maxSumRange = new MaxSumRange(windowStart, windowEnd, currSum);
+            if(currSum <=0) {
+                currSum = 0;
+                windowStart = windowEnd+1;
+            }
+            windowEnd++;
+        }
+        return maxSumRange;
+    }
+
+    public static void main(String[] args) {
+        MaxSumRange msr = new MaxSumRange();
+        System.out.println(msr.getMaxSumRange(new int[] {2,-8,3,-2,4,-10}));
+    }
+}
