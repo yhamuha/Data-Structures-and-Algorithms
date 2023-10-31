11bcfda7708aededf3f3a3566b5f7839d3eb9779
Yaroslav Hamuha <yhamuha@gmail.com>
2023-10-30 17:47:02 -0700

    lc. slidingwindow. PermutationInString. O(n) O(n)

--- src/main/java/lc/slidingwindow/m/PermutationInString.java
+++ src/main/java/lc/slidingwindow/m/PermutationInString.java
@@ -1,39 +1,33 @@
 package lc.slidingwindow.m;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PermutationInString {
-    public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0;i<s1.length();i++){
-            char c = s1.charAt(i);
-            int val = map.getOrDefault(c,0);
-            map.put(c,val+1);
+    public boolean checkInclusion(String s, String p) {
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            sMap.merge(c, 1, Integer::sum);
         }
-        HashMap<Character,Integer> originalMap = new HashMap<>();
-        originalMap.putAll(map);
-        int i =0, j=0;
-        int count = map.size(), totalCount = s1.length();
-        while(j<s2.length()){
-            char c = s2.charAt(j);
-            if(map.containsKey(c)){
-                int val = map.get(c);
-                map.put(c,val-1);
-                totalCount-=1;
-                if(val==1){
-                    count-=1;
-                    map.remove(c);
+        Map<Character, Integer> window = new HashMap<>();
+        for (int r = 0; r < p.length(); r++) {
+            char c = p.charAt(r);
+            // Increment the characters in the window
+            window.merge(c, 1, Integer::sum);
+            if (r >= s.length()) {
+                // r - s.length() will give us the position of the left pointer
+                c = p.charAt(r - s.length());
+                if (window.get(c) == 1) {
+                    window.remove(c);
+                } else {
+                    // Decrement the character in the window
+                    window.merge(c, -1, Integer::sum);
                 }
-                if(totalCount==0)
-                    return true;
-                j+=1;
-            }else if(!map.containsKey(c) && totalCount<s1.length()){
-                map.putAll(originalMap);
-                totalCount = s1.length();
-                i+=1;
-                j=i;
-            }else
-                j+=1;
+            }
+            if (window.equals(sMap)) {
+                return true;
+            }
         }
         return false;
     }
