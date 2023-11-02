0565dba58f03f6510b931c126801b3e977e073c3
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-01 22:17:08 -0700

    lc. dynamicprogramming. ClimbingStairs. O(n)

---
+++ src/main/java/lc/dynamicprogramming/ClimbingStairs.java
@@ -0,0 +1,23 @@
+package lc.dynamicprogramming;
+
+public class ClimbingStairs {
+    // O(n)
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        var cs = new ClimbingStairs();
+        System.out.println(cs.climbStairs(5));
+    }
+}
