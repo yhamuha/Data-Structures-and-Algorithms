dadf56e8ac567e0b4ad198fc7b145afb3d539ec2
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-02 16:47:51 -0700

    lc. slidingwindow. SlidingWindowMaximum. O(n)

---
+++ src/main/java/lc/slidingwindow/h/SlidingWindowMaximum.java
@@ -0,0 +1,33 @@
+package lc.slidingwindow.h;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+
+public class SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        Deque<Integer> window = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!window.isEmpty() && window.peekFirst() < i - k + 1) {
+                window.pollFirst();
+            }
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[i]) {
+                window.pollLast();
+            }
+            window.offerLast(i);
+            if (i >= k - 1)
+                result[i - k + 1] = nums[window.peekFirst()];
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        var swm = new SlidingWindowMaximum();
+        System.out.println(Arrays.toString(swm.maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3)));
+    }
+}
