e18982a669df71983bda007a6f4a81e8333966b5
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-04 15:05:16 -0700

    lc. slidingwindow. LongestRepeatingCharacterReplacement. TC O(n), SC O(26)

--- src/main/java/lc/slidingwindow/m/LongestRepeatingCharacterReplacement.java
+++ src/main/java/lc/slidingwindow/m/LongestRepeatingCharacterReplacement.java
@@ -1,25 +1,23 @@
 package lc.slidingwindow.m;
 
 public class LongestRepeatingCharacterReplacement {
-    // O(n) O(26)
+
     public int characterReplacement(String s, int k) {
-        // Space O(26)
-        int[] dic = new int[26];
-        int start = 0;
-        int maxLen = 0;
-        // Time O(n)
-        for (int end = 0; end < s.length(); end++) {
-            maxLen = Math.max(maxLen, ++dic[s.charAt(end) - 'A']);
-            // comparison (end - start + 1 > maxLen + k) needs to determine that
-            // the window with (maxLength of unique element + k (quantity of character changes))
-            // should be exact size
-            // if not: decrease char quantity and move pointer
-            if (end - start + 1 > maxLen + k) {
-                dic[s.charAt(start) - 'A']--;
-                start++;
+        // SC O(26)
+        int[] letters = new int[26];
+        int left = 0;
+        int maxLength = 0;
+        // TC O(n)
+        for(int right = 0; right < s.length(); right++) {
+            maxLength = Math.max(maxLength, ++letters[s.charAt(right) - 'A']);
+            // If the window size (end - start + 1) is greater than the maxLength of unique elements + k,
+            // decrease the character quantity and move the pointer.
+            if (right - left + 1 > maxLength + k) {
+                letters[s.charAt(left)-'A']--;
+                left +=1;
             }
         }
-        return s.length() - start;
+        return (s.length() - left);
     }
 
     public static void main(String[] args) {
