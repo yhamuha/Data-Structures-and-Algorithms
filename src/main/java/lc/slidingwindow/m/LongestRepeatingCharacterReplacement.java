8461521782a4fbc2f92ecb94e724b83a0d0645c9
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-04 14:48:49 -0700

    lc. slidingwindow.

--- src/main/java/lc/slidingwindow/m/LongestRepeatingCharacterReplacement.java
+++ src/main/java/lc/slidingwindow/m/LongestRepeatingCharacterReplacement.java
@@ -3,21 +3,27 @@ package lc.slidingwindow.m;
 public class LongestRepeatingCharacterReplacement {
     // O(n) O(26)
     public int characterReplacement(String s, int k) {
-        int[] letters = new int[26];
-        int left = 0;
+        // Space O(26)
+        int[] dic = new int[26];
+        int start = 0;
         int maxLen = 0;
-        for (int right = 0; right < s.length(); right++) {
-            maxLen = Math.max(maxLen, letters[s.charAt(right) - 'A']+=1);
-            if (right - left + 1 > maxLen + k) {
-                letters[s.charAt(left) - 'A'] -= 1;
-                left += 1;
+        // Time O(n)
+        for (int end = 0; end < s.length(); end++) {
+            maxLen = Math.max(maxLen, ++dic[s.charAt(end) - 'A']);
+            // comparison (end - start + 1 > maxLen + k) needs to determine that
+            // the window with (maxLength of unique element + k (quantity of character changes))
+            // should be exact size
+            // if not: decrease char quantity and move pointer
+            if (end - start + 1 > maxLen + k) {
+                dic[s.charAt(start) - 'A']--;
+                start++;
             }
         }
-        return s.length() - left;
+        return s.length() - start;
     }
 
     public static void main(String[] args) {
         var lrc = new LongestRepeatingCharacterReplacement();
-        System.out.println(lrc.characterReplacement(new String("AABBAA"),2));
+        System.out.println(lrc.characterReplacement(new String("AABABBA"),1));
     }
 }
