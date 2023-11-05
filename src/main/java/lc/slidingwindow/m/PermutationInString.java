fbe7b19fa2ef7a1e4d0002d9fb46e0c63bdcd65a
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-05 09:49:31 -0800

    lc. slidingwindow. PermutationInString. O(n), O(n)

--- src/main/java/lc/slidingwindow/m/PermutationInString.java
+++ src/main/java/lc/slidingwindow/m/PermutationInString.java
@@ -4,21 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PermutationInString {
-    public boolean checkInclusion(String s, String p) {
+    public boolean checkInclusion(String s1, String s2) {
         Map<Character, Integer> sMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
             // TC avg O(1), worst O(n)
             sMap.merge(c, 1, Integer::sum);
         }
         Map<Character, Integer> window = new HashMap<>();
-        for (int right = 0; right < p.length(); right++) {
-            char c = p.charAt(right);
+        for (int right = 0; right < s2.length(); right++) {
+            char c = s2.charAt(right);
             // Increment the characters in the window
             window.merge(c, 1, Integer::sum);
-            if (right >= s.length()) {
-                // right - s.length() will give us the position of the left pointer
-                c = p.charAt(right - s.length());
+            if (right >= s1.length()) {
+                // right - s1.length() will give us the position of the left pointer
+                c = s2.charAt(right - s1.length());
                 if (window.get(c) == 1) {
                     window.remove(c);
                 } else {
@@ -26,7 +26,7 @@ public class PermutationInString {
                     window.merge(c, -1, Integer::sum);
                 }
             }
-            // checking on each iteration if windows map conatins elements of s
+            // checking on each iteration if windows map conatins elements of s1
             if (window.equals(sMap)) {
                 return true;
             }
