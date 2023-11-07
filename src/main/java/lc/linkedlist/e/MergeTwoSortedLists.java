30c7a1bea6f57eb53af2e0318dbfd9f8574eb73a
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-07 11:34:33 -0800

    lc. linkedlist. MergeTwoSortedLists. O(n)

---
+++ src/main/java/lc/linkedlist/e/MergeTwoSortedLists.java
@@ -0,0 +1,32 @@
+package lc.linkedlist.e;
+
+public class MergeTwoSortedLists {
+    // O(n+m)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode root = new ListNode(), curr = root;
+        ListNode index1=list1, index2=list2;
+        while(index1!=null && index2!=null){
+            if(index1.val<index2.val){
+                curr.next=index1; curr=index1; index1=index1.next;
+            }else { curr.next=index2; curr=index2; index2=index2.next;}
+        }
+        while(index1!=null)
+        { curr.next=index1; curr=index1; index1=index1.next; }
+        while(index2!=null)
+        { curr.next=index2; curr=index2; index2=index2.next;}
+        return root.next;
+    }
+
+    public static void main(String[] args) {
+        var mtsl = new MergeTwoSortedLists();
+        ListNode list1 = new ListNode(1); list1.next = new ListNode(3); list1.next.next = new ListNode(5);
+        ListNode list2 = new ListNode(2); list2.next = new ListNode(4); list2.next.next = new ListNode(6);
+
+        ListNode result = mtsl.mergeTwoLists(list1, list2);
+
+        while(result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
