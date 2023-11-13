a3a14fdc22ebe730e86e8ba431f84d2ae1976d2e
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-13 14:22:19 -0800

    lc. binarytree. MaximumDepthOfBinaryTree. O(n)

---
+++ src/main/java/lc/binarytree/e/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,16 @@
+package lc.binarytree.e;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        return root==null ? 0 : 1 + Math.max(maxDepth(root.left) , maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+
+        MaximumDepthOfBinaryTree maxDepth = new MaximumDepthOfBinaryTree();
+        System.out.println(maxDepth.maxDepth(root));
+    }
+}
