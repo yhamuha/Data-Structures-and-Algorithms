f29a88cf231e59f7553fdd45846154aa9cecf93b
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-12 18:13:06 -0800

    lc. binarytree. InvertBinaryTree. O(n) O(1)

---
+++ src/main/java/lc/binarytree/e/InvertBinaryTree.java
@@ -0,0 +1,46 @@
+package lc.binarytree.e;
+
+      class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+      public class InvertBinaryTree {
+        public TreeNode invertTree(TreeNode root) {
+            if(root== null)
+                return null;
+            TreeNode temp= root.left;
+            root.left= root.right;
+            root.right= temp;
+            invertTree(root.left);
+            invertTree(root.right);
+            return root;
+        }
+
+      public static void printInOrder(TreeNode node) {
+          if (node != null) {
+              printInOrder(node.left);
+              System.out.print(node.val + " ");
+              printInOrder(node.right);
+          }
+      }
+
+        public static void main(String[] args) {
+            TreeNode root = new TreeNode(4);
+            root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+            root.right = new TreeNode(7, new TreeNode(6), new TreeNode(9));
+
+            InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
+            TreeNode invertedRoot = invertBinaryTree.invertTree(root);
+            printInOrder(invertedRoot);
+
+        }
+}
