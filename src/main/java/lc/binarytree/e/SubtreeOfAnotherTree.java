89917d2363f82e8458ecc5e84bd01bf0b39841f0
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-15 12:29:32 -0800

    lc. binarytree. SubtreeOfAnotherTree. O(mn)

---
+++ src/main/java/lc/binarytree/e/SubtreeOfAnotherTree.java
@@ -0,0 +1,24 @@
+package lc.binarytree.e;
+
+import lc.binarytree.TreeNode;
+
+public class SubtreeOfAnotherTree {
+    // O(mn)
+    // m - root; n - subRoot
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+        if(subRoot == null) return true;
+        if(root == null) return false;
+        if(helper(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+    }
+
+    public boolean helper(TreeNode root1, TreeNode root2)
+    {
+        if(root1 == null && root2 == null)
+            return true;
+        return (root1 != null && root2 != null && root1.val == root2.val) &&
+                helper(root1.left , root2.left) && helper(root1.right , root2.right);
+    }
+}
