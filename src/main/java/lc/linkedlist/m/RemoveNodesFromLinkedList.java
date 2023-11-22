9641eb31991c404ae074de6db9662fb27e22baeb
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-21 18:10:31 -0800

    lc. linkedlist. RemoveNodesFromLinkedList

--- src/main/java/lc/linkedlist/m/RemoveNodesFromLinkedList.java
+++ src/main/java/lc/linkedlist/m/RemoveNodesFromLinkedList.java
@@ -5,34 +5,54 @@ import java.util.Stack;
 public class RemoveNodesFromLinkedList {
 
     public ListNode removeNodes(ListNode head) {
-    Stack<Integer> stack = new Stack();
-    ListNode rev = reverse(head);
-    while(rev != null) {
-        if(stack.isEmpty() || (!stack.isEmpty() && rev.val >= stack.peek())) {
-            stack.push(rev.val);
+        Stack<Integer> stack = new Stack();
+        ListNode rev = reverse(head);
+        while (rev != null) {
+            if (stack.isEmpty() || (!stack.isEmpty() && rev.val >= stack.peek())) {
+                stack.push(rev.val);
+            }
+            rev = rev.next;
         }
-        rev = rev.next;
+        ListNode res = new ListNode();
+        ListNode r = res;
+        while (!stack.isEmpty()) {
+            r.next = new ListNode(stack.pop());
+            r = r.next;
+        }
+        return res.next;
     }
-    ListNode res = new ListNode();
-    ListNode r = res;
-    while(!stack.isEmpty()) {
-        r.next = new ListNode(stack.pop());
-        r = r.next;
-    }
-    return res.next;
-}
 
     ListNode reverse(ListNode head) {
         ListNode curr = head;
         ListNode prev = null;
         ListNode next;
-        while(curr != null) {
+        while (curr != null) {
             next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
         return prev;
+    }
+
+    public static void main(String[] args) {
+        // Create a linked list
+        ListNode head = new ListNode(5);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(13);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(8);
+
+        var remNodes = new RemoveNodesFromLinkedList();
+        // Call the removeNodes method
+        ListNode result = remNodes.removeNodes(head);
+
+        // Print the result
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+
     }
 
 }
