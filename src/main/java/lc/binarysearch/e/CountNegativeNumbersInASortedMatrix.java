f6d5549219ea7570f58894071d411ab10c5dc610
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-22 23:30:15 -0800

    lc. binarysearch

--- src/main/java/lc/binarysearch/e/CountNegativeNumbersInASortedMatrix.java
+++ src/main/java/lc/binarysearch/e/CountNegativeNumbersInASortedMatrix.java
@@ -1,42 +1,25 @@
 package lc.binarysearch.e;
 
 public class CountNegativeNumbersInASortedMatrix {
-    // O(n^2) not acceptable
-    public int countNegatives_(int[][] grid) {
+    // O(m log n)
+    public int countNegatives(int[][] grid) {
         int count = 0;
-        for(int i = grid.length - 1; i >= 0; i--) {
-            for(int j = grid[0].length - 1; j >= 0; j--) {
-                if(grid[i][j] < 0) {
-                    count++;
-                } else {
-                    break;
-                }
-            }
+        for (int line[]: grid) {
+            count += bs(line);
         }
         return count;
     }
-
-    // O(m log n)
-    public int countNegatives(int[][] grid) {
-        int cnt = 0;
-        for(int arr[]:grid){
-            cnt+= bs(arr);
-        }
-        return cnt;
+    private int bs(int[] line) {
+    int left = 0;
+    int right = line.length - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (line[mid] >= 0)
+            left = mid + 1;
+        else right = mid - 1;
     }
-    public static int bs(int arr[]){
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]>=0){
-                start=mid+1;
-            }
-            else end=mid-1;
-        }
-        return arr.length-start;
+    return line.length - left;
     }
-
 
     public static void main(String[] args) {
         var cn = new CountNegativeNumbersInASortedMatrix();
