752f96fca218fb28e879ab536420288c828de556
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-25 20:29:12 -0800

    lc. binarytree. DiameterOfBinaryTree

--- src/main/java/lc/binarytree/e1/DiameterOfBinaryTree.java
+++ src/main/java/lc/binarytree/e1/DiameterOfBinaryTree.java
@@ -3,7 +3,7 @@ package lc.binarytree.e1;
 import lc.binarytree.TreeNode;
 
 public class DiameterOfBinaryTree {
-    public int diameterOfBinaryTree(TreeNode root) {
+    /*public int diameterOfBinaryTree(TreeNode root) {
         // SC O(1)
         int[] ans = new int[1];
         height(root, ans);
@@ -17,5 +17,29 @@ public class DiameterOfBinaryTree {
         int rh = height(node.right, ans);
         ans[0] = Math.max(ans[0], lh + rh);
         return 1 + Math.max(lh, rh);
+    }*/
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] ans = new int[1];
+        height(root, ans);
+        return ans[0];
+    }
+
+    private int height(TreeNode root, int[] ans) {
+        if (root == null)
+            return 0;
+        int lh = height(root.left, ans);
+        int rh = height(root.right, ans);
+        ans[0] = Math.max(ans[0], lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        root.left = new TreeNode(2, new TreeNode(4), new TreeNode(5));
+        root.right = new TreeNode(3, null, null);
+
+        DiameterOfBinaryTree dobt = new DiameterOfBinaryTree();
+        int diameter = dobt.diameterOfBinaryTree(root);
+        System.out.println(diameter);
     }
 }
