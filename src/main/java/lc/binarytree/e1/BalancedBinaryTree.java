831b70bd8b69e86fa07574ceedf546be88b2ab35
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-25 20:56:33 -0800

    lc. binarytree. BalancedBinaryTree

--- src/main/java/lc/binarytree/e1/BalancedBinaryTree.java
+++ src/main/java/lc/binarytree/e1/BalancedBinaryTree.java
@@ -5,17 +5,19 @@ import lc.binarytree.TreeNode;
 
 public class BalancedBinaryTree {
     public boolean isBalanced(TreeNode root) {
-        if (root == null)  return true;
-        if (height(root) == -1)  return false;
+        if (root == null) return true;
+        if (height(root) == -1) return false;
         return true;
     }
-    public int height(TreeNode root) {
-        if (root == null)  return 0;
-        int leftHeight = height(root.left);
-        int rightHight = height(root.right);
-        if (leftHeight == -1 || rightHight == -1)  return -1;
-        if (Math.abs(leftHeight - rightHight) > 1)  return -1;
-        return Math.max(leftHeight, rightHight) + 1;
+
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        if (lh == -1 || rh == -1) return -1;
+        if (Math.abs (lh - rh) > 1) return -1;
+        return 1 + Math.max(lh, rh);
+
     }
 
     public static void main(String[] args) {
