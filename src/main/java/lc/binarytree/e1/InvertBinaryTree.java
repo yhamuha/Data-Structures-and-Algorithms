7f8654dd26b2fb2e1af73a4de4742dca12b0e35c
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-25 19:40:16 -0800

    lc. binarytree. InvertBinaryTree

--- src/main/java/lc/binarytree/e1/InvertBinaryTree.java
+++ src/main/java/lc/binarytree/e1/InvertBinaryTree.java
@@ -4,11 +4,11 @@ import lc.binarytree.TreeNode;
 
 public class InvertBinaryTree {
         public TreeNode invertTree(TreeNode root) {
-            if(root== null)
+            if (root == null)
                 return null;
-            TreeNode temp= root.left;
-            root.left= root.right;
-            root.right= temp;
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
             invertTree(root.left);
             invertTree(root.right);
             return root;
