4e6b84eb8573cc0f92a004af15cf8eefc02ba7ae
Yaroslav Hamuha <yhamuha@gmail.com>
2023-11-28 18:40:54 -0800

    lc. bst. bfs. BinaryTreeRightSideView. O(n)

---
+++ src/main/java/lc/bst/bfs/m/BinaryTreeRightSideView.java
@@ -0,0 +1,51 @@
+package lc.bst.bfs.m;
+
+import lc.bst.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        bfs(root, result);
+        return result;
+    }
+    private void bfs(TreeNode root, List<Integer> container) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (i == 0) {
+                    container.add(node.val);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeRightSideView btrsv = new BinaryTreeRightSideView();
+
+        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        root.left = new TreeNode(2, null, new TreeNode(5));
+        root.right = new TreeNode(3, null, new TreeNode (4));
+
+        System.out.println(btrsv.rightSideView(root));
+    }
+}
