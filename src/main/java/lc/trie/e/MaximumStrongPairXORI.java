6583bf7932785bfea3f82305586fb1436ef15172
Yaroslav Hamuha <yhamuha@gmail.com>
2023-12-11 17:05:49 -0800

    lc. trie. maximumStrongPairXor. O(n logn)

--- src/main/java/lc/trie/e/MaximumStrongPairXORI.java
+++ src/main/java/lc/trie/e/MaximumStrongPairXORI.java
@@ -1,24 +1,66 @@
 package lc.trie.e;
 
-public class MaximumStrongPairXORI {
-    // identify pairs
-    // get max XOR using two numbers from a pairs
-    // XOR gives 1 only by 01 10
+import java.util.Arrays;
+
+class MaximumStrongPairXORI {
+    // O (n log n)
     public int maximumStrongPairXor(int[] nums) {
-        int ans = 0;
-        for (int x : nums) {
-            for (int y : nums) {
-                if (Math.abs(x - y) <= Math.min(x, y)) {
-                    ans = Math.max(ans, x ^ y);
-                }
+        Arrays.sort(nums);
+        Trie tree = new Trie();
+        int ans = 0, i = 0;
+        for (int y : nums) {
+            tree.insert(y);
+            while (y > nums[i] * 2) {
+                tree.remove(nums[i++]);
             }
+            ans = Math.max(ans, tree.search(y));
         }
         return ans;
     }
 
     public static void main(String[] args) {
         var mspx = new MaximumStrongPairXORI();
-        System.out.println(mspx.maximumStrongPairXor
-                (new int[] {1,2,3,4,5}));
+        int[] nums = new int[] {1,2,3,4,5};
+        System.out.println(mspx.maximumStrongPairXor(nums));
+    }
+}
+
+class Trie {
+    private Trie[] children = new Trie[2];
+    private int cnt = 0;
+    public Trie() {
+    }
+    public void insert(int x) {
+        Trie node = this;
+        for (int i = 7; i >= 0; --i) {
+            int v = x >> i & 1;
+            if (node.children[v] == null) {
+                node.children[v] = new Trie();
+            }
+            node = node.children[v];
+            ++node.cnt;
+        }
+    }
+    public int search(int x) {
+        Trie node = this;
+        int ans = 0;
+        for (int i = 7; i >= 0; --i) {
+            int v = x >> i & 1;
+            if (node.children[v ^ 1] != null && node.children[v ^ 1].cnt > 0) {
+                ans |= 1 << i;
+                node = node.children[v ^ 1];
+            } else {
+                node = node.children[v];
+            }
+        }
+        return ans;
+    }
+    public void remove(int x) {
+        Trie node = this;
+        for (int i = 7; i >= 0; --i) {
+            int v = x >> i & 1;
+            node = node.children[v];
+            --node.cnt;
+        }
     }
 }
