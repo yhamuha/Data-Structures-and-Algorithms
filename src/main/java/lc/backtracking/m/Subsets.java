0c11c23fbd101e059e5b8a49c6d040261e62fffd
Yaroslav Hamuha <yhamuha@gmail.com>
2023-12-12 20:16:06 -0800

    lc. backtracking. Subsets. O (n * 2^n). O(2^n) - worst

--- src/main/java/lc/backtracking/m/Subsets.java
+++ src/main/java/lc/backtracking/m/Subsets.java
@@ -5,23 +5,25 @@ import java.util.List;
 
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList();
-        generateSubset(0, nums, new ArrayList(), list);
-        return list;
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        backtrack(resultList, new ArrayList<>(), nums, 0);
+        return resultList;
     }
 
-    public void generateSubset(int index, int[]nums, List<Integer> current, List<List<Integer>> list){
-        list.add(new ArrayList<>(current));
-        for(int i = index; i < nums.length; i++){
-            current.add(nums[i]);
-            generateSubset(i+1, nums, current, list);
-            current.remove(current.size()-1);
+    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet,
+                           int[] nums, int start) {
+        resultSets.add(new ArrayList<>(tempSet));
+        for (int i = start; i < nums.length; i++) {
+            tempSet.add(nums[i]);
+            backtrack(resultSets, tempSet, nums, i + 1);
+            tempSet.remove(tempSet.size() - 1);
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
         var s = new Subsets();
+        int[] nums = {1,2,3};
         System.out.println(s.subsets(nums));
     }
 }
