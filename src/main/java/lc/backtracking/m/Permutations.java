c6b2b8761ac9ccb0a59b0605bf319d9a4fbdf2e3
Yaroslav Hamuha <yhamuha@gmail.com>
2023-12-15 15:02:00 -0800

    lc. backtracking. Permutations.

--- src/main/java/lc/backtracking/m/Permutations.java
+++ src/main/java/lc/backtracking/m/Permutations.java
@@ -5,23 +5,38 @@ import java.util.List;
 
 // ! impractical for large input sizes
 // must be refactored it using a tabulation dynamic
+
 public class Permutations {
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(list, new ArrayList<>(), nums);
-        return list;
+
+        List<List<Integer>> resultList = new ArrayList<>();
+
+        backtrack(resultList, new ArrayList<>(), nums);
+        return resultList;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-        if(tempList.size() == nums.length){
-            list.add(new ArrayList<>(tempList));
-        } else{
-            for(int i = 0; i < nums.length; i++){
-                if(tempList.contains(nums[i])) continue; // element already exists, skip
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
-            }
+    private void backtrack(List<List<Integer>> resultList,
+                           ArrayList<Integer> tempList, int[] nums) {
+        // If we match the length, it is a permutation
+        if (tempList.size() == nums.length) {
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int number : nums) {
+            // Skip if we get same element
+            if (tempList.contains(number))
+                continue;
+
+            // Add the new element
+            tempList.add(number);
+
+            // Go back to try other element
+            backtrack(resultList, tempList, nums);
+
+            // Remove the element
+            tempList.remove(tempList.size() - 1);
         }
     }
 
