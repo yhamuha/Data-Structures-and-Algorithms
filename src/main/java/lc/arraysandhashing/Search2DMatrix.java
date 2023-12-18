b109911445f0b1d602db272793c7988d20ef8431
Yaroslav Hamuha <yhamuha@gmail.com>
2023-12-17 18:56:34 -0800

    lc. arraysandhashing. Search2DMatrix. O(m+n)

---
+++ src/main/java/lc/arraysandhashing/Search2DMatrix.java
@@ -0,0 +1,37 @@
+package lc.arraysandhashing;
+
+import lc.binarysearch.m.Searcha2DMatrix;
+
+public class Search2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var s2dm = new Searcha2DMatrix();
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+        int target = 30;
+        System.out.println(s2dm.searchMatrix(matrix, target));
+    }
+}
