73f82678d3541912841db0fb9f4c53bc637f51c5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-07 20:16:43 -0800

    lc. graphs. NumberOfIslands. Quadratic TC

---
+++ src/main/java/lc/graphs/m/NumberOfIslands.java
@@ -0,0 +1,36 @@
+package lc.graphs.m;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i=0; i<grid.length; i++) {
+            for (int j=0; j<grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count += 1;
+                    callBFS(grid, i, j);
+                }
+            }
+        } return count;
+    }
+
+    private void callBFS(char[][] grid, int i, int j) {
+        if (i<0 || i>=grid.length || j<0 || j>= grid[i].length || grid[i][j] == '0')
+            return;
+        grid[i][j] ='0';
+        callBFS(grid, i+1, j);
+        callBFS(grid, i-1, j);
+        callBFS(grid, i, j-1);
+        callBFS(grid, i, j+1);
+    }
+
+    public static void main(String[] args) {
+        NumberOfIslands numberOfIslands = new NumberOfIslands();
+        char[][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+        System.out.println(numberOfIslands.numIslands(grid));
+    }
+}
