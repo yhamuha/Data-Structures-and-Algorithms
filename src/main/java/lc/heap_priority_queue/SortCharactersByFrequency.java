a8e6dc96eca45e2bc1242a3c86e7fbc6afad69cd
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-13 18:42:56 -0800

    lc. heap_priority_queue. SortCharactersByFrequency.

--- src/main/java/lc/heap_priority_queue/SortCharactersByFrequency.java
+++ src/main/java/lc/heap_priority_queue/SortCharactersByFrequency.java
@@ -5,18 +5,13 @@ import java.util.PriorityQueue;
 
 public class SortCharactersByFrequency {
     public String frequencySort(String s) {
-        // Count the occurence on each character
         HashMap<Character, Integer> cnt = new HashMap<>();
         for (char c: s.toCharArray()) {
             cnt.put(c, cnt.getOrDefault(c, 0) + 1);
         }
-
-        // Building heap
         // O (n log k); k - number of unique in String
         PriorityQueue<Character> heap = new PriorityQueue<>((a, b) -> (cnt.get(b) - cnt.get(a)));
         heap.addAll(cnt.keySet());
-
-        // Building string
         // O (n log k); n == s.length, k - unique chars in s
         StringBuilder sb = new StringBuilder();
         while (!heap.isEmpty()) {
