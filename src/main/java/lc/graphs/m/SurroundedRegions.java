fe0734f91a97de3d076a0a4be39a0e675c85b3ad
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-19 18:21:06 -0800

    lc. graphs. SurroundedRegions. O(m*n)

--- src/main/java/lc/graphs/m/SurroundedRegions.java
+++ src/main/java/lc/graphs/m/SurroundedRegions.java
@@ -1,7 +1,7 @@
 package lc.graphs.m;
 
 public class SurroundedRegions {
-    public void solve(char[][] board) {
+    /*public void solve(char[][] board) {
         if (board.length == 0 || board[0].length == 0)
             return;
         int rows = board.length;
@@ -41,6 +41,42 @@ public class SurroundedRegions {
         if(j<board[0].length-1 && board[i][j+1] == 'O')
             boundaryDFS(board,i,j+1);
         return;
+    }*/
+
+    private int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+    // O(m+n)
+    public void solve(char[][] board) {
+        // DFS
+        int m = board.length, n = board[0].length;
+        for(int i=0; i<m; i++){
+            markBoard(board,m,n,i,0);   // left border
+            markBoard(board,m,n,n-1,i);   // right border
+        }
+        for(int i=0; i<n; i++){
+            markBoard(board,m,n,0,i); // top border
+            markBoard(board,m,n,m-1,i); // bottom border
+        }
+        flipBoard(board, m, n);
+    }
+    // O(m*n)
+    private void markBoard(char[][] board, int m, int n, int i, int j){
+        if(i<0 || j<0 || i>=m || j>= n || board[i][j] != 'O') return;
+        board[i][j] = 'M';
+        for (int[] direction : directions){
+            int x = direction[0]+i, y = direction[1]+j;
+            markBoard(board,m,n,x,y);
+        }
+    }
+    // O(m*n)
+    private void flipBoard(char[][] board, int m, int n){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j] == 'M')
+                    board[i][j] = 'O';
+                else if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+            }
+        }
     }
 
     public static void main(String[] args) {
