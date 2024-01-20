8af6f586dd752f3d8991912552079826444c8b07
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-19 16:36:37 -0800

    lc. graphs. maxAreaOfIsland. O(n*m) O(n*m)

--- src/main/java/lc/graphs/m/MaxAreaOfIsland.java
+++ src/main/java/lc/graphs/m/MaxAreaOfIsland.java
@@ -2,7 +2,7 @@ package lc.graphs.m;
 
 public class MaxAreaOfIsland {
 
-    boolean seen[][];
+    /*boolean seen[][];
     // overall TC: (O n * m * n * m) <==> O(n^2 * m^2)
     public int maxAreaOfIsland(int[][] grid) {
         //O(n * m) iterates through n rows * m columns
@@ -31,7 +31,30 @@ public class MaxAreaOfIsland {
                 area(row+1,column, grid) +
                 area(row-1,column, grid)) +
                 area(row,column-1, grid) +
-                area(row,column+1, grid);
+                area(row,column+1, grid);*/
+    // O(n*m) O(n*m)
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0, n=grid.length, m = grid[0].length;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m;j++){
+                if (grid[i][j] == 1){
+                    int area = getArea(grid, i, j, n ,m);
+                    max = Math.max(max, area);
+                }
+            }
+        }
+        return max;
+    }
+
+    private int getArea(int[][] grid, int i, int j, int n, int m){
+        if ( i<0 || j<0 || i>= n || j>= m || grid[i][j]==0)
+            return 0;
+        grid[i][j] = 0;
+        int left = getArea(grid, i, j-1,n,m);
+        int right = getArea(grid, i, j+1,n,m);
+        int up = getArea(grid, i-1, j,n,m);
+        int down = getArea(grid, i+1, j,n,m);
+        return left + right + up + down + 1;
     }
 
     public static void main(String[] args) {
