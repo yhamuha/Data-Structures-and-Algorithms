04bf443447cee99c0e7bd8ed284054800ca83d32
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-26 17:41:59 -0800

    lc. dp. HouseRobber. O(n) O(n)

--- src/main/java/lc/dp/m/HouseRobber.java
+++ src/main/java/lc/dp/m/HouseRobber.java
@@ -1,24 +1,23 @@
 package lc.dp.m;
 
 public class HouseRobber {
-    // O(n) O(n)
+    // O(n)
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
-
-        int[] maxAmount = new int[nums.length];
-        maxAmount[0] = nums[0];
-        maxAmount[1] = Math.max(nums[0], nums[1]);
-
-        for (int i =2; i< nums.length; i++) {
-            maxAmount[i] = Math.max(maxAmount[i-2] + nums[i], maxAmount[i-1]);
-        }
-        return maxAmount[maxAmount.length-1];
+       if (nums.length < 2) return nums[0];     // if 1 elem
+       // SC: O(n)
+       int[] dp = new int[nums.length];         // addit array
+       // Memoize
+       dp[0] = nums[0];                         // for first house
+       dp[1] = Math.max(nums[0], nums[1]);      // for sec house
+       for(int i=2; i<nums.length; i++){
+           dp[i] = Math.max (dp[i-2] + nums[i], dp[i-1]);
+       }
+       return dp[nums.length-1];                // ret last elem
     }
 
     public static void main(String[] args) {
         var hr = new HouseRobber();
-        int[] nums = {1,2,3,1};
+        int[] nums = {2,7,3,1,4,2,1,8};
         System.out.println(hr.rob(nums));
     }
 }
