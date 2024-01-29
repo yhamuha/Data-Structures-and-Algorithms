1acc1dbb8bb7a6b70c3b2b3c9036743a31474c59
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-29 12:23:32 -0800

    lc. dp. PartitionEqualSubsetSum. Recursive + memoization O(2^n)

--- src/main/java/lc/dp/m/PartitionEqualSubsetSum.java
+++ src/main/java/lc/dp/m/PartitionEqualSubsetSum.java
@@ -3,6 +3,7 @@ package lc.dp.m;
 public class PartitionEqualSubsetSum {
     // Recursive approach
     int arr[];
+    Boolean[][] cache;                              // Memoization to avoid redundant calculations
     public boolean canPartition(int[] nums) {
         int sum =0;
         arr=nums;
@@ -11,13 +12,18 @@ public class PartitionEqualSubsetSum {
         if (sum %2 !=0)
             return false;
         int half = sum/2;
+        cache = new Boolean[nums.length][half+1];
         return helper(half,0);
     }
     private boolean helper(int sum, int index){
         if (index>= arr.length || sum <0) return false;
         if(sum == 0) return true;
+        if(cache[index][sum] != null){
+            return cache[index][sum];
+        }
         boolean take = helper(sum-arr[index], index+1);
         boolean skip = helper(sum,index+1);
+        cache[index][sum] = take || skip;
         return take || skip;
     }
     public static void main(String[] args) {
