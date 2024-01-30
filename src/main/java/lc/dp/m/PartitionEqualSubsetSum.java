0c1e904089f84014162e496ab0d7e74ab5cc686d
Yaroslav Hamuha <yhamuha@gmail.com>
2024-01-29 16:12:59 -0800

    lc. dp. PartitionEqualSubsetSum. Dynamic. O(n*m) O(n*m)

--- src/main/java/lc/dp/m/PartitionEqualSubsetSum.java
+++ src/main/java/lc/dp/m/PartitionEqualSubsetSum.java
@@ -2,9 +2,9 @@ package lc.dp.m;
 
 public class PartitionEqualSubsetSum {
     // Recursive approach
-    int arr[];
+    /*int arr[];
     Boolean[][] cache;                              // Memoization to avoid redundant calculations
-    public boolean canPartition(int[] nums) {
+    public boolean canPartitionRec(int[] nums) {
         int sum =0;
         arr=nums;
         for(int num:nums)
@@ -25,6 +25,43 @@ public class PartitionEqualSubsetSum {
         boolean skip = helper(sum,index+1);
         cache[index][sum] = take || skip;
         return take || skip;
+    }*/
+
+    // Dynamic approach
+    Boolean[][] cache;
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
+        }
+        if(sum % 2 != 0){
+            return false;
+        }
+        //we need even sum
+        int half = sum / 2;
+        cache = new Boolean[nums.length][half + 1];
+        for(int row = 0; row < cache.length; row++){
+            cache[row][0] = true;
+        }
+        for(int curSum = 1; curSum < cache[0].length; curSum++){
+            if(curSum == nums[0]){
+                cache[0][curSum] = true;
+            }else{
+                cache[0][curSum] = false;
+            }
+        }
+        for(int curNumIndex = 1; curNumIndex < cache.length; curNumIndex++){
+            for(int curSum = 1; curSum < cache[0].length; curSum++){
+                boolean take = false;
+                int diff = curSum - nums[curNumIndex];
+                if(diff >= 0){
+                    take = cache[curNumIndex - 1][diff];
+                }
+                boolean skip = cache[curNumIndex - 1][curSum];
+                cache[curNumIndex][curSum] = skip || take;
+            }
+        }
+        return cache[nums.length - 1][half];
     }
     public static void main(String[] args) {
         var pes = new PartitionEqualSubsetSum();
