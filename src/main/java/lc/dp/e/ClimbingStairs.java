2b1f17066af0ea35a9ccb782f2e3a2172e6a0401
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-01 11:10:07 -0800

    lc. dp. ClimbingStairs. O(n) O(n)

--- src/main/java/lc/dp/e/ClimbingStairs.java
+++ src/main/java/lc/dp/e/ClimbingStairs.java
@@ -3,13 +3,13 @@ package lc.dp.e;
 public class ClimbingStairs {
     //O(n) O(n)
     public int climbStairs(int n){
-        if (n == 1) return 1;
-
+        if(n==1) return 1;
+        // O(n)
         int[] dp = new int[n+1];
         dp[1] = 1;
         dp[2] = 2;
-        // SC: O(n) use array space; filled in of calculated results
-        for(int i=3; i<=n; i++){
+        // O(n)
+        for(int i=3; i<=n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
