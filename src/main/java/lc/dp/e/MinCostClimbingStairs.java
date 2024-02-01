0099b7c720acb5f9f52b189268c004c97a254da1
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-01 11:31:24 -0800

    lc. dp. MinCostClimbingStairs. O(n) O(1)

--- src/main/java/lc/dp/e/MinCostClimbingStairs.java
+++ src/main/java/lc/dp/e/MinCostClimbingStairs.java
@@ -3,11 +3,13 @@ package lc.dp.e;
 public class MinCostClimbingStairs {
     // O(n) O(1)
     public int minCostClimbingStairs(int[] cost) {
-        int step1 = 0;
-        int step2 = 0;
+        // O(n)
+        int step1=0;
+        int step2=0;
+        // O(n)
         for(int i=cost.length-1; i>=0; i--){
             int current_step = cost[i] + Math.min(step1, step2);
-            step1 = step2;
+            step1=step2;
             step2 = current_step;
         }
         return Math.min(step1, step2);
