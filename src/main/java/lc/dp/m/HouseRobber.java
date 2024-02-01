2f0ffd09ee5a56203db5d4d505463907c140b2fa
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-01 11:54:45 -0800

    lc. dp. HouseRobber. O(n) O(n)

--- src/main/java/lc/dp/m/HouseRobber.java
+++ src/main/java/lc/dp/m/HouseRobber.java
@@ -1,18 +1,17 @@
 package lc.dp.m;
 
 public class HouseRobber {
-    // O(n)
     public int rob(int[] nums) {
-       if (nums.length < 2) return nums[0];     // if 1 elem
-       // SC: O(n)
-       int[] dp = new int[nums.length];         // addit array
-       // Memoize
-       dp[0] = nums[0];                         // for first house
-       dp[1] = Math.max(nums[0], nums[1]);      // for sec house
-       for(int i=2; i<nums.length; i++){
-           dp[i] = Math.max (dp[i-2] + nums[i], dp[i-1]);
-       }
-       return dp[nums.length-1];                // ret last elem
+        if (nums.length <2) return nums[0];
+        // SC: O(n)
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        // TC: O(n)
+        for(int i=2; i<nums.length; i++){
+            dp[i] = (Math.max(dp[i-2] + nums[i],dp[i-1]));
+        }
+        return dp[nums.length-1];
     }
 
     public static void main(String[] args) {
