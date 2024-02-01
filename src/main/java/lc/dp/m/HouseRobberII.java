324e8632466742864deaeb00dc054cd6bc61b92c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-01 12:29:43 -0800

    lc. dp. HouseRobberII. O(n) O(n)

--- src/main/java/lc/dp/m/HouseRobberII.java
+++ src/main/java/lc/dp/m/HouseRobberII.java
@@ -1,31 +1,31 @@
 package lc.dp.m;
 
 public class HouseRobberII {
-    // O(n) O(n)
-    int rob(int[] nums) {
-        if (nums.length < 2)
-            return nums[0];
-        int[] skipLastHouse = new int[nums.length - 1];
-        int[] skipFirstHouse = new int[nums.length - 1];
-        for (int i = 0; i < nums.length - 1; i++) {
+    public int rob(int[] nums) {
+        if(nums.length < 2) return nums[0];
+        // SC: O(n)
+        int[] skipLastHouse = new int[nums.length-1];
+        int[] skipFirstHouse = new int[nums.length-1];
+        // TC: O(n)
+        for(int i=0; i<nums.length-1; i++){
             skipLastHouse[i] = nums[i];
-            skipFirstHouse[i] = nums[i + 1];
+            skipFirstHouse[i] = nums[i+1];
         }
-        int lootSkippingLast = robHelper(skipLastHouse);
-        int lootSkippingFirst = robHelper(skipFirstHouse);
-        return Math.max(lootSkippingLast, lootSkippingFirst);
+        int maxFromSkipLastHouse = houseRobber(skipFirstHouse);
+        int maxFromSkipFirstHouse = houseRobber(skipLastHouse);
+        return Math.max(maxFromSkipLastHouse, maxFromSkipFirstHouse);
+
     }
 
-    private int robHelper(int[] nums) {
-        if (nums.length < 2)
-            return nums[0];
+    private int houseRobber(int[] nums){
+        if(nums.length<2) return nums[0];
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        dp[1]= Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length; i++){
+            dp[i] = (Math.max(dp[i-2] + nums[i], dp[i-1]));
         }
-        return dp[nums.length - 1];
+        return dp[nums.length-1];
     }
 
     public static void main(String[] args) {
