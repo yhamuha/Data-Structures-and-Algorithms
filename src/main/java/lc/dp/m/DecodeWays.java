61966f6eb20a7d4d9633ccbcbd2326bc5422979f
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-02 18:02:38 -0800

    lc. dp. DecodeWays. O(n^2)O(n)

--- src/main/java/lc/dp/m/DecodeWays.java
+++ src/main/java/lc/dp/m/DecodeWays.java
@@ -1,8 +1,38 @@
 package lc.dp.m;
 
 public class DecodeWays {
-    //O(n) O(n)
+    // TC: O(n^2)
+    // Recursive + memoization
+    char[] arr;
+    Integer[] cache;
+    // SC: O(n)
     public int numDecodings(String s) {
+        this.arr = s.toCharArray();
+        this.cache = new Integer[arr.length];
+        return helper(0);
+    }
+    private int helper(int index){
+        if(index == arr.length) return 1;
+        if(cache[index] != null) return cache[index];
+        int totalCodeWays = 0;
+        StringBuilder str = new StringBuilder();
+        for(int i=index; i<arr.length && i<index+2; i++){
+            str.append(arr[i]);
+            if(decodeCheck(str.toString())){
+                totalCodeWays += helper(i+1);
+            }
+        }
+        cache[index] = totalCodeWays;
+        return totalCodeWays;
+    }
+    private boolean decodeCheck (String str){
+        if(str.charAt(0)=='0') return false;
+        Integer num = Integer.parseInt(str);
+        return num < 27;
+    }
+    //O(n) O(n)
+    // Dynamic
+    /*public int numDecodings(String s) {
         char[] arr = s.toCharArray();
         int[] cache = new int[arr.length];
         int n = cache.length;
@@ -25,8 +55,7 @@ public class DecodeWays {
             }
         }
         return cache[0];
-    }
-
+    }*/
     public static void main(String[] args) {
         var dw = new DecodeWays();
         String s = "226";
