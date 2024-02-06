e6130199e29d70bce3eb85771555ffea8c3ef0d1
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-06 09:23:45 -0800

    lc. backtracking. CombinationSum. O(2^n) O(n)

--- src/main/java/lc/backtracking/m/CombinationSum.java
+++ src/main/java/lc/backtracking/m/CombinationSum.java
@@ -5,26 +5,23 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum {
+    // overall TC: O(2^n)
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // SC: O(n)
         List<List<Integer>> list = new ArrayList<>();
-        // O (n log n)
         Arrays.sort(candidates);
-        backtrack(list, new ArrayList<>(), candidates, target, 0);
+        backtracking (list, new ArrayList<>(), candidates, target, 0);
         return list;
     }
 
-    // briefly: O (n^2)
-    // deeply: O (n ^ (T/M + 1)) where T is target, M - min of candidates;
-    // in worst case extent depends on T-target and M-min of candidates array
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList,
-                           int [] candidates, int remain, int start){
-        if (remain < 0) return;
+    private void backtracking(List<List<Integer>> list, List<Integer> tempList, int[] candidates, int remain, int start){
+        if (remain<0) return;
         else if (remain == 0) list.add(new ArrayList<>(tempList));
         else {
-            for(int i = start; i < candidates.length; i++){
+            for(int i=start; i<candidates.length; i++){
                 tempList.add(candidates[i]);
-                backtrack(list, tempList, candidates, remain - candidates[i], i);
-                tempList.remove(tempList.size() - 1);
+                backtracking(list, tempList,candidates, remain - candidates[i], i);
+                tempList.remove(tempList.size()-1);
             }
         }
     }
