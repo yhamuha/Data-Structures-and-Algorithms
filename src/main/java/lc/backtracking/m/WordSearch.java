fe6c33d49c8ccebfb89fbf1d12c09a7a189db9de
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-06 13:03:28 -0800

    lc. backtracking. WordSearch. O(n*m*k) O(n)

--- src/main/java/lc/backtracking/m/WordSearch.java
+++ src/main/java/lc/backtracking/m/WordSearch.java
@@ -2,9 +2,7 @@ package lc.backtracking.m;
 
 public class WordSearch {
 
-    // O(n * m * 4^k)
-    // n,m - dimension of word matrix
-    // k - length of word string
+    // O(n * m * 4^k; n,m - dimension of word matrix; k - length of word string
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -20,8 +18,8 @@ public class WordSearch {
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!=word.charAt(index))
             return false;
         char temp=board[i][j];
-        board[i][j]=' ';
-
+        board[i][j]=' '; // mark founded letter
+        // check the letter by all the directions
         boolean found = find(board,word,i+1,j,index+1) ||
                 find(board,word,i,j+1,index+1) ||
                 find(board,word,i-1,j,index+1) ||
