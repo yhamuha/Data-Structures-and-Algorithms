5adf4985972a200c63a23dda68eaa7d409d85906
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-06 11:45:35 -0800

    lc. backtracking. CombinationSumII. O(2^n) O(n)

--- src/main/java/lc/backtracking/m/CombinationSumII.java
+++ src/main/java/lc/backtracking/m/CombinationSumII.java
@@ -5,27 +5,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSumII {
-    // O(2^n); n - number of input array elements
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums, target, 0);
-        return list;
-
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // SC:O(n)
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtracking(resultList, new ArrayList<>(), candidates, target, 0);
+        return resultList;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-        if(remain < 0) return;
-        else if(remain == 0) list.add(new ArrayList<>(tempList));
-        else{
-            for(int i = start; i < nums.length; i++){
-                if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums, remain - nums[i], i + 1);
+    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] candidates, int remain, int start){
+
+        if (remain < 0) return;
+        else if (remain == 0) {
+            resultList.add(new ArrayList<>(tempList));
+        }
+        else {
+            for (int i = start; i < candidates.length; i++) {
+                // TC:O(2^n)
+                if (i > start && candidates[i] == candidates[i - 1]) continue;
+                tempList.add(candidates[i]);
+                backtracking(resultList, tempList, candidates, remain - candidates[i], i+1);
                 tempList.remove(tempList.size() - 1);
             }
         }
     }
+
+
 
     public static void main(String[] args) {
         var csII = new CombinationSumII();
