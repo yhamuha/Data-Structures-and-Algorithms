f69f9be5dfa4f7847ff79c30c38347347808575c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-06 17:08:08 -0800

    lc. backtracking. LetterCombinationsOfaPhoneNumber. O(4^n) O(n)

--- src/main/java/lc/backtracking/m/LetterCombinationsOfaPhoneNumber.java
+++ src/main/java/lc/backtracking/m/LetterCombinationsOfaPhoneNumber.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 public class LetterCombinationsOfaPhoneNumber {
+    // TC: O(4^n); '7','9' has 4 symbols associated
         public List<String> letterCombinations(String digits) {
-            List<String> res=new ArrayList<>();
-            if(digits.length()==0) return res;
-            Map<Character,String> hm=new HashMap<>();
+        // TC: O(n) HashMap
+            List<String> resultList = new ArrayList<>();
+            if (digits.length()==0) return resultList;
+            Map<Character,String> hm = new HashMap<>();
             hm.put('2',"abc");
             hm.put('3',"def");
             hm.put('4',"ghi");
@@ -18,21 +20,19 @@ public class LetterCombinationsOfaPhoneNumber {
             hm.put('7',"pqrs");
             hm.put('8',"tuv");
             hm.put('9',"wxyz");
-
-            backtrack(digits,0,hm,new StringBuilder(),res);
-            return res;
+            backtrack(digits,0,hm,new StringBuilder(), resultList);
+            return resultList;
         }
-        private static void backtrack(String digits, int i,Map<Character,String> hm,StringBuilder sb,List<String>res){
-            if(i==digits.length()){
-                res.add(sb.toString());
+        private void backtrack(String digits, int i, Map<Character,String> hm, StringBuilder sb, List<String> resultList){
+            if(i == digits.length()){
+                resultList.add(sb.toString());
                 return;
             }
-
-            String curr=hm.get(digits.charAt(i));
-            for(int k=0;k<curr.length();k++){
-                sb.append(curr.charAt(k));
-                backtrack(digits, i+1, hm,  sb, res);
-                sb.deleteCharAt(sb.length() -1);
+            String curr = hm.get(digits.charAt(i));
+            for(int k=0; k<curr.length(); k++){
+                sb.append(curr.charAt(i));
+                backtrack(digits,k+1,hm,sb,resultList);
+                sb.deleteCharAt(sb.length()-1);
             }
         }
 
