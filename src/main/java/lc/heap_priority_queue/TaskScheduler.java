da787d0a74f95ccc8877ce7de0cfbd44ffa0c088
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-07 16:14:01 -0800

    lc. heap_priority_queue. TaskScheduler. O(n log n) O(n)

--- src/main/java/lc/heap_priority_queue/TaskScheduler.java
+++ src/main/java/lc/heap_priority_queue/TaskScheduler.java
@@ -3,30 +3,28 @@ package lc.heap_priority_queue;
 import java.util.*;
 
 public class TaskScheduler {
-    // O(m * n + n * log(n))
-    // m - number of tasks
-    // n - number of unique tasks
+    // TC: O(n log n)
     public int leastInterval(char[] tasks, int n) {
+        // SC: O(n)
         Map<Character, Integer> map = new HashMap<>();
-        for(char task : tasks) {
+        for(char task : tasks)
             map.put(task, map.getOrDefault(task,0)+1);
-        }
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b-a);
-        maxHeap.addAll(map.values());
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b-a);
+        heap.addAll(map.values());
+
         int cycle = 0;
-        while(!maxHeap.isEmpty()) {
+        while(!heap.isEmpty()) {
             List<Integer> temp = new ArrayList<>();
-            for(int i=0; i<n+1 ; i++) {
-                if(!maxHeap.isEmpty()) {
-                    temp.add(maxHeap.poll());
-                }
+            for(int i=0; i<n+1; i++) {
+                if(!heap.isEmpty())
+                    temp.add(heap.poll());
             }
-            for(int i=0; i<temp.size(); i++) {
-                if(temp.get(i) - 1  > 0) {
-                    maxHeap.add(temp.get(i)-1);
-                }
+            for(int i=0; i<temp.size(); i++){
+                if (temp.get(i)-1 > 0)
+                    heap.offer(temp.get(i)-1);
             }
-            cycle += maxHeap.isEmpty() ? temp.size() : n+1;
+            cycle += heap.isEmpty() ? temp.size() : n+1;
         }
         return cycle;
     }
