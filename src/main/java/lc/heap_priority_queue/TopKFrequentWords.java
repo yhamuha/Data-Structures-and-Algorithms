a13d5e702d438b12f6b459e3bbd58402b6073203
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-08 11:43:17 -0800

    lc. heap_priority_queue. TopKFrequentWords. O(n log n) O(n)

--- src/main/java/lc/heap_priority_queue/TopKFrequentWords.java
+++ src/main/java/lc/heap_priority_queue/TopKFrequentWords.java
@@ -3,32 +3,26 @@ package lc.heap_priority_queue;
 import java.util.*;
 
 public class TopKFrequentWords {
+    // O(n log n) O(n)
     public List<String> topKFrequent(String[] words, int k) {
-        // O(n) count up all occurrences of each word
-        HashMap<String, Integer> wordCounts = new HashMap<>();
-        for(String word : words)
-            wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+        Map<String, Integer> map = new HashMap<>(); for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1); }
 
-        // define comparator for heap
-        Comparator<String> wordComparator = (s1, s2) -> {
-            return wordCounts.get(s1) == wordCounts.get(s2) ? s2.compareTo(s1)
-                                                            : wordCounts.get(s1) - wordCounts.get(s2);
-        };
+        PriorityQueue<String> minHeap = new PriorityQueue<>(
+                (s1, s2) -> map.get(s1).equals(map.get(s2)) ? s2.compareTo(s1) : map.get(s1) - map.get(s2));
 
-        // minHeap: lowest occurrences always on the top
-        PriorityQueue<String> minHeap = new PriorityQueue<>(wordComparator);
-        // O(n log k) fill in heap up to k
-        for(String word : wordCounts.keySet()) {
+        for (String word : map.keySet()) {
             minHeap.offer(word);
-            if(minHeap.size() > k) minHeap.poll();
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
 
-        LinkedList<String> toReturn = new LinkedList<>();
-        // O (k log k) as polling elements from the heap
-        // fill in List
-
-        while (!minHeap.isEmpty())
-            toReturn.addFirst(minHeap.poll());
+        List<String> toReturn = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            toReturn.add(minHeap.poll());
+        }
+        Collections.reverse(toReturn);
 
         return toReturn;
     }
