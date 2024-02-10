7e97f22f08cdbc54e7c377380b42d1fc0cb5ab92
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-09 17:03:03 -0800

    lc. heap_priority_queue. KthSmallestElementInASortedMatrix. O(n log n) O(n)

--- src/main/java/lc/heap_priority_queue/KthSmallestElementInASortedMatrix.java
+++ src/main/java/lc/heap_priority_queue/KthSmallestElementInASortedMatrix.java
@@ -4,20 +4,20 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class KthSmallestElementInASortedMatrix {
-    // O(k * log(min(N, k)))
+    // TC: O(k * log n)
+    // SC: O(n)
     public int kthSmallest(int[][] matrix, int k) {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> matrix[a[0]][a[1]] - matrix[b[0]][b[1]]);
-        int N = matrix.length;
-        for(int i = 0; i < Math.min(N, k); i++){
-            minHeap.add(new int[]{i, 0});
-        }
-        while(k > 0){
+        int n = matrix.length;
+        for(int i=0; i<Math.min(n,k); i++)
+            minHeap.offer(new int[] {i,0});
+        while(k>0){
             int[] element = minHeap.poll();
             k--;
-            if(k == 0)
+            if (k == 0)
                 return matrix[element[0]][element[1]];
-            if(element[1] + 1 < matrix[0].length)
-                minHeap.add(new int[]{element[0], element[1] + 1});
+            if (element[1]+1 < matrix[0].length)
+                minHeap.offer(new int[] {element[0],element[1]+1});
         }
         return -1;
     }
