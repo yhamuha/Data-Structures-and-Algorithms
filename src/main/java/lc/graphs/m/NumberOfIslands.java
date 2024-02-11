25112f4d565ae6122711ea70b38c6998bad020df
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-10 16:26:13 -0800

    lc. graphs. NumberOfIslands. O(n*m) O(1)

--- src/main/java/lc/graphs/m/NumberOfIslands.java
+++ src/main/java/lc/graphs/m/NumberOfIslands.java
@@ -1,49 +1,30 @@
 package lc.graphs.m;
 
 public class NumberOfIslands {
-    /*public int numIslands(char[][] grid) {
-        int count = 0;
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[i].length; j++) {
-                if (grid[i][j] == '1') {
-                    count += 1;
-                    callBFS(grid, i, j);
-                }
-            }
-        } return count;
-    }
-
-    private void callBFS(char[][] grid, int i, int j) {
-        if (i<0 || i>=grid.length || j<0 || j>= grid[i].length || grid[i][j] == '0')
-            return;
-        grid[i][j] ='0';
-        callBFS(grid, i+1, j);
-        callBFS(grid, i-1, j);
-        callBFS(grid, i, j-1);
-        callBFS(grid, i, j+1);
-    }*/
+    // O(m*n) O(1)
     public int numIslands(char[][] grid) {
         if (grid == null) return 0;
         int islandCount = 0;
-        for(int i=0; i< grid.length; i++){
-            for(int j=0; j<grid[0].length;j++){
-                if(grid[i][j] == '1') {
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if (grid[i][j] == '1'){
                     ++islandCount;
-                    changeLandToWater(grid, i, j);
+                    markAsWater(grid, i, j);
                 }
             }
         }
         return islandCount;
     }
-    // DFS
-    private void changeLandToWater(char[][] grid, int i, int j){
-        // base case
-        if (i<0 || i>= grid.length || j<0 || j>=grid[0].length || grid[i][j] == '0') return;
+    // dfs
+    private void markAsWater(char[][] grid, int i, int j){
+        // boundary check
+        if(i<0 || j<0 || i>=grid.length ||j>=grid[0].length || grid[i][j] == '0' )
+            return;
         grid[i][j] = '0';
-        changeLandToWater(grid,i-1,j);  // up
-        changeLandToWater(grid,i+1,j);  // down
-        changeLandToWater(grid,i,j-1);  // left
-        changeLandToWater(grid,i,j+1);  // right
+        markAsWater(grid, i-1,j);
+        markAsWater(grid, i+1,j);
+        markAsWater(grid, i,j-1);
+        markAsWater(grid, i,j+1);
     }
 
     public static void main(String[] args) {
