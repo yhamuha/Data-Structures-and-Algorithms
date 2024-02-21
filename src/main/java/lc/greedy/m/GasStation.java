63d0aac9fe0736778c9e61b7ae7bc89f318b8143
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-20 19:56:29 -0800

    lc. greedy. GasStation. O(n) O(1)

--- src/main/java/lc/greedy/m/GasStation.java
+++ src/main/java/lc/greedy/m/GasStation.java
@@ -1,47 +1,18 @@
 package lc.greedy.m;
 
 public class GasStation {
-
-    /*public int canCompleteCircuit(int[] gas, int[] cost) {
-        int remaining=0;
-        int total=0;
-        int start=0;
-
-        for(int i=0;i<gas.length;i++){
-            int diff=gas[i]-cost[i];
-            if(remaining>=0){
-                remaining+=diff;
-            }
-            else{
-                remaining=diff;
-                start=i;
-            }
-            total+=diff;
-        }
-        if(total>=0){
-            return start;
-        }
-        else{
-            return -1;
-        }
-    }*/
-
-    // O(n)
+    // O(n) O(1)
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int position =0 , sum =0 , total =0;
-
         for(int index=0;index<gas.length;index++){
             sum += gas[index] - cost[index];
-
             if(sum<0){
                 total+=sum;
                 sum=0;
                 position = index+1;
             }
         }
-
         total += sum;
-
         return total>=0?position:-1;
     }
 
