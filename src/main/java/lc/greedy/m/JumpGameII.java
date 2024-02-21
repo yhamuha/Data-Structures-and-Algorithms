7f09a4969d4465addbfccebe0e4001e819e2cd4b
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-20 19:16:12 -0800

    lc. greedy. JumpGameII. O(n) O(1)

--- src/main/java/lc/greedy/m/JumpGameII.java
+++ src/main/java/lc/greedy/m/JumpGameII.java
@@ -1,15 +1,12 @@
 package lc.greedy.m;
 
 public class JumpGameII {
+    // O(n) O(1)
     int jump(int[] nums) {
-
         int totalJumps = 0;
-
         int destination = nums.length - 1;
         int coverage = 0, lastJumpIdx = 0;
-
         if (nums.length == 1) return 0;
-
         for (int i = 0; i < nums.length; i++) {
             coverage = Math.max(coverage, i + nums[i]);
             if (i == lastJumpIdx) {
