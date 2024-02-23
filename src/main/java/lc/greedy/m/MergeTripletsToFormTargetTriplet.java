67489ccd8b66d0a47df7e6cafa0153d1c5cdf7f5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-22 16:48:56 -0800

    lc. greedy. MergeTripletsToFormTargetTriplet. O(n) O(1)

--- src/main/java/lc/greedy/m/MergeTripletsToFormTargetTriplet.java
+++ src/main/java/lc/greedy/m/MergeTripletsToFormTargetTriplet.java
@@ -7,16 +7,16 @@ public class MergeTripletsToFormTargetTriplet {
     // O(n) O(1)
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         boolean i = false;
-        boolean j= false;
-        boolean k=false;
-        for(int[] t: triplets)
-        {
-            if(t[0]<=target[0] && t[1]<=target[1] && t[2]<=target[2])
-            {    if(t[0]==target[0]) i = true;
-                if(t[1]==target[1]) j = true;
-                if(t[2]==target[2]) k = true;
+        boolean j = false;
+        boolean k = false;
+
+        for(int[] t : triplets) {
+            if (t[0]<=target[0] && t[1]<=target[1] && t[2]<=target[2]) {
+                if (t[0]==target[0]) i = true;
+                if (t[1]==target[1]) j = true;
+                if (t[2]==target[2]) k = true;
             }
-            if(i && j && k)
+            if (i && j && k)
                 return true;
         }
         return false;
