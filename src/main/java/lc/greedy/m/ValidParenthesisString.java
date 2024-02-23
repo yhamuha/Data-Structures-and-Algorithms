e8511157e71e5421c04487b6ffdcff8458ef7eeb
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-23 12:29:55 -0800

    lc. greedy. ValidParenthesisString. O(n) O(1)

--- src/main/java/lc/greedy/m/ValidParenthesisString.java
+++ src/main/java/lc/greedy/m/ValidParenthesisString.java
@@ -4,28 +4,29 @@ public class ValidParenthesisString {
     // O(n) O(1)
     public boolean checkValidString(String s)
     {
-        int right =0,left =0;
-        for(int i=0; i<s.length();i++)
-        {
+        int left = 0, right =0;
+        for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(ch == '(')
+            if (ch == '(')
                 left++;
             else
-                left--;
-            if(ch != ')')
+                left--; // *)
+
+            if (ch !=')')
                 right++;
             else
-                right--;
-            if(right < 0)
+                right--; //
+
+            if (right < 0)
                 break;
-            left = Math.max(0 , left);
+            left = Math.max(0, left);
         }
         return left == 0;
     }
 
     public static void main(String[] args) {
         var vps = new ValidParenthesisString();
-        String s = "((*)";
+        String s = "(*))";
         System.out.println(vps.checkValidString(s));
     }
 }
