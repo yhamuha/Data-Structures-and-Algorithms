b98c244e6607369711a250aa29b19fc3b882e8b6
Yaroslav Hamuha <yhamuha@gmail.com>
2024-02-24 17:26:18 -0800

    lc. adv_graph. MinCostToConnectAllPoints. O(n^2) O(1)

---
+++ src/main/java/lc/adv_graph/m/MinCostToConnectAllPoints.java
@@ -0,0 +1,36 @@
+package lc.adv_graph.m;
+
+import java.util.Arrays;
+
+public class MinCostToConnectAllPoints {
+    // O(n^2) O(1)
+    public int minCostConnectPoints(int[][] points) {
+        int[] dist = new int[points.length];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        int ans = 0;
+
+        for (int i = 0; i < points.length - 1; ++i) {
+            for (int j = i + 1; j < points.length; ++j) {
+                dist[j] = Math.min(dist[j], Math.abs(points[i][0] - points[j][0]) +
+                        Math.abs(points[i][1] - points[j][1]));
+                if (dist[j] < dist[i + 1]) {
+                    final int[] tempPoint = points[j];
+                    points[j] = points[i + 1];
+                    points[i + 1] = tempPoint;
+                    final int tempDist = dist[j];
+                    dist[j] = dist[i + 1];
+                    dist[i + 1] = tempDist;
+                }
+            }
+            ans += dist[i + 1];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        var mctcap = new MinCostToConnectAllPoints();
+        int[][] points = {{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}};
+        System.out.println(mctcap.minCostConnectPoints(points));
+    }
+}
