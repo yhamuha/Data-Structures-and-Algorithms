2f85072a4b50678ad71442ea0612b6047a9b0359
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-03 17:43:24 -0800

    lc. greedy. BestTimeToBuyAndSellStock. O(n) O(1)

---
+++ src/main/java/lc/greedy/e/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,21 @@
+package lc.greedy.e;
+
+public class BestTimeToBuyAndSellStock {
+    // O(n) O(1)
+    public int maxProfit(int[] prices) {
+        int max=0,min=prices[0];
+        for(int i=1;i<prices.length;i++)   {
+            if(min<prices[i])
+                max=Math.max(prices[i]-min,max);
+            else
+                min=prices[i];
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock stock = new BestTimeToBuyAndSellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = stock.maxProfit(prices);
+        System.out.println("Maximum profit: " + maxProfit);
+    }
+}
--- src/main/java/lc/slidingwindow/e/BestTimeToBuyAndSellStock.java
+++ src/main/java/lc/slidingwindow/e/BestTimeToBuyAndSellStock.java
@@ -1,25 +1,6 @@
 package lc.slidingwindow.e;
 
-
-/*
-*   7 1 5 3 6 4
-*   res: 5 (buy for 1 -> sell for 6)
-*/
 public class BestTimeToBuyAndSellStock {
-    /*int maxProfit (int[] prices) {
-        int prevDay = 0;
-        int maxProfit = 0;
-        for(int nextDay=0; nextDay<prices.length; nextDay++) {
-            if (prices[prevDay] < prices[nextDay]) {
-                int currProfit = prices[nextDay] - prices[prevDay];
-                maxProfit = Math.max(maxProfit, currProfit);
-            } else {
-                prevDay = nextDay;
-            }
-        }
-        return maxProfit;
-    }*/
-
     public int maxProfit(int[] prices) {
         int prevDay = 0;
         int maxProfit = 0;
