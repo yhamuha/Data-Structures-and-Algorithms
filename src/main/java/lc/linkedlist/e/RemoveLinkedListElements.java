1ef98a14dcc4b857e0f027e59f8f607144b51d07
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-07 21:11:11 -0800

    lc. linkedlist. RemoveLinkedListElements. O(n) O(1)

---
+++ src/main/java/lc/linkedlist/e/RemoveLinkedListElements.java
@@ -0,0 +1,38 @@
+package lc.linkedlist.e;
+
+public class RemoveLinkedListElements {
+    // O(n) O(1)
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode temp = newHead;
+        if(head == null){
+            return head;
+        }
+        while(temp.next!=null){
+            if(temp.next.val == val){
+                temp.next = temp.next.next;
+            }else{
+                temp = temp.next;
+            }
+        }
+        return newHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next = new ListNode(6);
+        int val = 6;
+        RemoveLinkedListElements rlle = new RemoveLinkedListElements();
+        ListNode result = rlle.removeElements(head, val);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
