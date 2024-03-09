680f1797d989cc62b0508c002d03db436b4ba2d6
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-08 21:47:13 -0800

    lc. linkedlist. RemoveDuplicatesFromSortedList. O(n) O(1)

---
+++ src/main/java/lc/linkedlist/e/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,34 @@
+package lc.linkedlist.e;
+
+public class RemoveDuplicatesFromSortedList {
+    // O(n) O(1)
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        ListNode start = head;
+        ListNode end = head;
+        while(end != null){
+            if(start.val != end.val ){
+                start.next = end;
+                start = end;
+            }
+            end = end.next;
+        }
+        start.next = null;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedList solution = new RemoveDuplicatesFromSortedList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        ListNode result = solution.deleteDuplicates(head);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
