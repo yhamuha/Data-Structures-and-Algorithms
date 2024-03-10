70099cd5adde3bf40839e83e60668b67a241407f
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-09 20:51:40 -0800

    lc. adv_graph. NetworkDelayTime. O(E + N log N) O(E + N)

--- src/main/java/lc/adv_graph/m/NetworkDelayTime.java
+++ src/main/java/lc/adv_graph/m/NetworkDelayTime.java
@@ -3,53 +3,34 @@ package lc.adv_graph.m;
 import java.util.*;
 
 public class NetworkDelayTime {
-    // TC: O(E log N); Edges, Nodes
-    // SC: O(N + E)
+    // TC: O(E + N log N); Edges, Nodes
+    // SC: O(E + N)
     public int networkDelayTime(int[][] times, int n, int k) {
-        List[] adj = new List[n+1]; // using adj List
-        for(int[] time:times){
-            int u = time[0];
-            int v = time[1];
-            int t = time[2];
-            if(adj[u]==null){
-                adj[u] = new ArrayList<int[]>();
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for(int[] time : times){
+            int src = time[0], tar = time[1], weight = time[2];
+            if(!graph.containsKey(src)){
+                graph.put(src, new LinkedList<int[]>());
             }
-            adj[u].add(new int[]{v,t});
+            graph.get(src).add(new int[]{tar, weight});
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->Integer.compare(a[1],b[1])); // pq; comparator by times
-        boolean[] vis = new boolean[n+1];  // visited
-
-        int[] reachTime = new int[n+1]; // reach time
-        Arrays.fill(reachTime, Integer.MAX_VALUE);
-        int count = 0;
-        int max = 0;
-        pq.add(new int[]{k,0});
-        reachTime[k] = 0;
-
-        while(!pq.isEmpty()){
-            int[] node = pq.poll();
-            int u = node[0];
-            int reachTime_u = node[1];
-            if(vis[u]){
-                continue;
-            }
-            vis[u] = true;
-            count++;
-            max = Math.max(max, reachTime_u);
-            if(adj[u]!=null){ // if node has neighbors
-                Iterator<int[]> it = adj[u].iterator();
-                while(it.hasNext()){
-                    int[] neighbor = it.next();
-                    int v = neighbor[0];
-                    int cost_uv = neighbor[1];
-                    if(!vis[v] && (reachTime_u+cost_uv)<reachTime[v]){
-                        reachTime[v] = reachTime_u+cost_uv;
-                        pq.add(new int[]{v, reachTime[v]});
-                    }
-                }
+        Queue<int[]> minHeap = new PriorityQueue<>((a,b)->a[1] - b[1]);
+        Set<Integer> visited = new HashSet<>();
+        minHeap.add(new int[]{k, 0});
+        int res = 0;
+        while(!minHeap.isEmpty()){              // BFS
+            int[] top = minHeap.poll();
+            int src = top[0], srcWeight = top[1];
+            if(visited.contains(src)) continue;
+            res = srcWeight;
+            visited.add(src);
+            if(!graph.containsKey(src)) continue;
+            for(int[] edge : graph.get(src)){
+                int tar = edge[0], tarWeight = edge[1];
+                minHeap.add(new int[]{tar, srcWeight + tarWeight });
             }
         }
-        return count==n?max:-1;
+        return visited.size() == n ? res : -1;
     }
 
     public static void main(String[] args) {
