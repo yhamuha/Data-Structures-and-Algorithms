5d487df4ea4fc500759ec4cdb8da3ab73d4d09e4
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-12 21:26:36 -0700

    lc. binarysearch. FindSmallestLetterGreaterThanTarget. O(log n) O(1)

--- src/main/java/lc/binarysearch/e/FindSmallestLetterGreaterThanTarget.java
+++ src/main/java/lc/binarysearch/e/FindSmallestLetterGreaterThanTarget.java
@@ -1,22 +1,8 @@
 package lc.binarysearch.e;
 
-public class SmallestLetterGreaterThanTarget {
+public class FindSmallestLetterGreaterThanTarget {
     // O(log n) O(1)
     public char nextGreatestLetter(char[] letters, char target) {
-        /*int start = 0;
-        int end = letters.length - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if ( target > letters[mid] ) {
-                start = mid + 1;
-            } else if ( target < letters[mid]) {
-                end = mid-1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return letters[start % letters.length];*/
         int left = 0;
         int right = letters.length - 1;
 
@@ -32,9 +18,9 @@ public class SmallestLetterGreaterThanTarget {
         return letters[left % letters.length];
     }
     public static void main(String[] args) {
-        SmallestLetterGreaterThanTarget sgl = new SmallestLetterGreaterThanTarget();
+        FindSmallestLetterGreaterThanTarget fslgtt = new FindSmallestLetterGreaterThanTarget();
         char[] letters = {'x', 'x', 'y','y'};
         char target = 'z';
-        System.out.println(sgl.nextGreatestLetter(letters, target));
+        System.out.println(fslgtt.nextGreatestLetter(letters, target));
     }
 }
