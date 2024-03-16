ec8271e8fb1e2cbb97094bcdcb6e3b7fad8469fd
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-15 19:43:00 -0700

    lc. bst. dfs. PathSum. O(n) O(h)

--- src/main/java/lc/bst/dfs/e2/PathSum.java
+++ src/main/java/lc/bst/dfs/e2/PathSum.java
@@ -3,6 +3,7 @@ package lc.bst.dfs.e2;
 import lc.bst.TreeNode;
 
 public class PathSum {
+    // O(n) O(h); h - tree height -> call stack
     public boolean rootToLeafPathSum(TreeNode root, int targetSum, int sum){
         if(root == null)
             return false;
@@ -14,8 +15,23 @@ public class PathSum {
         return rootToLeafPathSum(root.left, targetSum, sum + root.val)
                 || rootToLeafPathSum(root.right, targetSum, sum + root.val);
     }
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {   // wrapper func
         int sum = 0;
         return rootToLeafPathSum(root, targetSum, sum);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+        int targetSum = 22;
+        var ps = new PathSum();
+        System.out.println(ps.hasPathSum(root, targetSum));
     }
 }
