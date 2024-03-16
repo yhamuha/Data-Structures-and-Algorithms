f1d9b46176e5953cb0e82c7775088af67c3ba0c3
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-15 20:17:37 -0700

    lc. bst. dfs. MaximumDepthOfBinaryTree. O(n) O(h)

--- src/main/java/lc/bst/dfs/e1/MaximumDepthOfBinaryTree.java
+++ src/main/java/lc/bst/dfs/e1/MaximumDepthOfBinaryTree.java
@@ -3,17 +3,16 @@ package lc.bst.dfs.e1;
 import lc.bst.TreeNode;
 
 public class MaximumDepthOfBinaryTree {
+    // O(n) O(h)
     public int maxDepth(TreeNode root) {
         return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(9);
         root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
-
-        MaximumDepthOfBinaryTree maxDepth = new MaximumDepthOfBinaryTree();
-        System.out.println(maxDepth.maxDepth(root));
+        var md = new MaximumDepthOfBinaryTree();
+        System.out.println(md.maxDepth(root));
     }
 }
