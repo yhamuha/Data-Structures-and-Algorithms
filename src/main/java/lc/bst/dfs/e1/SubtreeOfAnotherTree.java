f3ac93553b6b4049b49b1419b96f5e2e5f777e67
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-16 21:27:52 -0700

    lc. bst. dfs. SubtreeOfAnotherTree. O(m * n) O(n)

--- src/main/java/lc/bst/dfs/e1/SubtreeOfAnotherTree.java
+++ src/main/java/lc/bst/dfs/e1/SubtreeOfAnotherTree.java
@@ -1,10 +1,9 @@
 package lc.bst.dfs.e1;
 
-import lc.bst.BinarySearchTreeFromArray;
 import lc.bst.TreeNode;
 
 public class SubtreeOfAnotherTree {
-    // O(mn)
+    // O(m * n) O(n)
     // m - root; n - subRoot
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (root == null) return false;
@@ -12,23 +11,22 @@ public class SubtreeOfAnotherTree {
         if (helper(root, subRoot)) return true;
         return isSubtree(root.left, subRoot) || isSubtree(root.right,subRoot);
     }
-
+    // O(m) O(n)
     private boolean helper(TreeNode root1, TreeNode root2) {
         if (root1 == null && root2 == null) return true;
         return (root1 !=null && root2 != null && root1.val == root2.val) &&
                 helper(root1.left, root2.left) && helper (root1.right, root2.right);
     }
-
     public static void main(String[] args) {
-
-        SubtreeOfAnotherTree sat = new SubtreeOfAnotherTree();
-
-        BinarySearchTreeFromArray searchTreeFromArray = new BinarySearchTreeFromArray();
-        Integer[] inputArray = {3,4,5,1,2,null,null,null,null,0};
-        Integer[] inputArray2 = {4,1,2};
-        TreeNode root = searchTreeFromArray.arrayToBST(inputArray);
-        TreeNode root2 = searchTreeFromArray.arrayToBST(inputArray2);
-
-        System.out.println(sat.isSubtree(root, root2));
+        SubtreeOfAnotherTree checker = new SubtreeOfAnotherTree();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        TreeNode subRoot = new TreeNode(4);
+        subRoot.left = new TreeNode(1);
+        subRoot.right = new TreeNode(2);
+        System.out.println(checker.isSubtree(root, subRoot));
     }
 }
