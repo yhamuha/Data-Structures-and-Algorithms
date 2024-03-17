b3dfc10e7d9d49d92625bca77ff4007fc11e03f8
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-16 21:36:57 -0700

    lc. bst. dfs. InvertBinaryTree. O(n) O(h)

--- src/main/java/lc/bst/dfs/e1/InvertBinaryTree.java
+++ src/main/java/lc/bst/dfs/e1/InvertBinaryTree.java
@@ -3,33 +3,34 @@ package lc.bst.dfs.e1;
 import lc.bst.TreeNode;
 
 public class InvertBinaryTree {
-        public TreeNode invertTree(TreeNode root) {
-            if (root == null)
-                return null;
-            TreeNode temp = root.left;
-            root.left = root.right;
-            root.right = temp;
-            invertTree(root.left);
-            invertTree(root.right);
-            return root;
+    // O(n) O(h)
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+    // O(n) O(h)
+    public static void PreorderTraversal(TreeNode node) {
+        if (node != null) {
+            System.out.print(node.val + " ");
+            PreorderTraversal(node.left);
+            PreorderTraversal(node.right);
         }
+    }
 
-      public static void printInOrder(TreeNode node) {
-          if (node != null) {
-              printInOrder(node.left);
-              System.out.print(node.val + " ");
-              printInOrder(node.right);
-          }
-      }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        root.right = new TreeNode(7, new TreeNode(6), new TreeNode(9));
 
-        public static void main(String[] args) {
-            TreeNode root = new TreeNode(4);
-            root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
-            root.right = new TreeNode(7, new TreeNode(6), new TreeNode(9));
-
-            InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
-            TreeNode invertedRoot = invertBinaryTree.invertTree(root);
-            printInOrder(invertedRoot);
-
-        }
+        InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
+        TreeNode invertedRoot = invertBinaryTree.invertTree(root);
+        PreorderTraversal(invertedRoot);
+    }
 }
+
