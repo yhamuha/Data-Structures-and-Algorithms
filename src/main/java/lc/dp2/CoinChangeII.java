148e74d5fcd2ab6ceff58a0f7730d5c1a96baeb0
Yaroslav Hamuha <yhamuha@gmail.com>
2024-03-23 22:24:41 -0700

    lc. dp2. CoinChangeII (bottom-up). O(m * n) O(m * n)

--- src/main/java/lc/dp2/CoinChangeII.java
+++ src/main/java/lc/dp2/CoinChangeII.java
@@ -4,28 +4,19 @@ import java.util.Arrays;
 
 public class CoinChangeII {
     // O(m*n) O(m*n)
-    public int count(int i,int[][] dp,int[] coins,int amount){
-        if(amount==0)
-            return 1;
-        if(i<coins.length){
-            if(dp[i][amount]==-1){
-                if(coins[i]>amount)
-                    return 0;
-                return dp[i][amount] = count(i,dp,coins,amount-coins[i]) +
-                                       count(i+1,dp,coins,amount);
-            }
-            return dp[i][amount];
-        }
-        return 0;
-    }
     public int change(int amount, int[] coins) {
-        int n=coins.length;
-        int[][] dp=new int[n][amount+1];
-        for(int i=0;i<n;i++)
-            Arrays.fill(dp[i],-1);
-        Arrays.sort(coins);
-        return count(0,dp,coins,amount);
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.length; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i-1][j] + (j >= coins[i-1] // if current amount >= denomination
+                                            ? dp[i][j-coins[i-1]] : 0);
+            }
+        }
+        return dp[coins.length][amount];
     }
+
     public static void main(String[] args) {
         CoinChangeII coinChange = new CoinChangeII();
         int amount = 5;
