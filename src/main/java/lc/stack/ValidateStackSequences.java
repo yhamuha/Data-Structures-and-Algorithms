46325838b746253e53b42f15878a029c828dbcdb
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-06 23:27:31 -0700

    lc. stack. ValidateStackSequences. O(n) O(n)

--- src/main/java/lc/stack/ValidateStackSequences.java
+++ src/main/java/lc/stack/ValidateStackSequences.java
@@ -3,14 +3,13 @@ package lc.stack;
 import java.util.Stack;
 
 public class ValidateStackSequences {
+    // T:O(n) S:O(n)
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> stack = new Stack<>();
         int j=0;
         for(int each : pushed){
             stack.push(each);
-            while(!stack.isEmpty()
-                    && j<popped.length //take care of overflow case
-                    && stack.peek()==popped[j]){
+            while(!stack.isEmpty() && j<popped.length && stack.peek()==popped[j]){
                 j++;
                 stack.pop();
             }
---
+++ src/test/java/lc/stack/ValidateStackSequencesTest.java
@@ -0,0 +1,16 @@
+package lc.stack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateStackSequencesTest {
+    @Test
+    void initTest(){
+        int[] pushed = {1,2,3,4,5}, popped = {4,3,5,1,2};
+        ValidateStackSequences solution = new ValidateStackSequences();
+        boolean expected = false;
+        boolean result = solution.validateStackSequences(pushed, popped);
+        assertEquals(expected,result);
+    }
+}
