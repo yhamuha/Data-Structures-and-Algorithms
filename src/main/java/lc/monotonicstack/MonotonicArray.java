d024cab394a46ccc15e112ed935a66b6c73312bf
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-08 13:32:19 -0700

    lc. monotonicstack. MonotonicArray. O(n) O(n)

---
+++ src/main/java/lc/monotonicstack/MonotonicArray.java
@@ -0,0 +1,36 @@
+package lc.monotonicstack;
+
+import java.util.Stack;
+
+public class MonotonicArray {
+    // O(n) O(n)
+    public boolean isMonotonic(int[] nums) {
+        boolean isAsc=nums[0]<nums[nums.length-1];
+        return ans(nums,isAsc);
+
+    }
+    public boolean ans(int[] nums, boolean isAsc){
+        Stack<Integer> stack=new Stack<>();
+        if(isAsc){
+            for(int i:nums){
+                if(!stack.isEmpty() && stack.peek()>i){
+                    return false;
+                }
+                stack.push(i);
+            }
+            return true;
+        }
+        for(int i:nums){
+            if(!stack.isEmpty() && stack.peek()<i){
+                return false;
+            }
+            stack.push(i);
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        MonotonicArray monotonicArray = new MonotonicArray();
+        int[] nums = {5,3,2,4,1};
+        System.out.println(monotonicArray.isMonotonic(nums));
+    }
+}
---
+++ src/test/java/lc/monotonicstack/MonotonicArrayTest.java
@@ -0,0 +1,41 @@
+package lc.monotonicstack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonotonicArrayTest {
+
+    private MonotonicArray monotonicArray;
+
+    @BeforeEach
+    public void setUp() {
+        monotonicArray = new MonotonicArray();
+    }
+
+    @Test
+    public void testIsMonotonic1() {
+        int[] nums1 = {1, 2, 2, 3};
+        assertTrue(monotonicArray.isMonotonic(nums1));
+    }
+
+    @Test
+    public void testIsMonotonic2() {
+        int[] nums2 = {3, 2, 1};
+        assertTrue(monotonicArray.isMonotonic(nums2));
+    }
+
+    @Test
+    public void testIsMonotonic3() {
+        int[] nums3 = {1, 3, 2};
+        assertFalse(monotonicArray.isMonotonic(nums3));
+    }
+
+    @Test
+    public void testIsMonotonic4() {
+        int[] nums4 = {5, 3, 2, 4, 1};
+        assertFalse(monotonicArray.isMonotonic(nums4));
+    }
+
+}
