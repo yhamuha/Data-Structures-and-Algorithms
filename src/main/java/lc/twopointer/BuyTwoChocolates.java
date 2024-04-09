c2633aec65868241f6bdb603db7db35bbb2426b6
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-09 16:29:22 -0700

    lc. twopointers. BuyTwoChocolates. O(n) O(1)

--- src/main/java/lc/twopointer/BuyTwoChocolates.java
+++ src/main/java/lc/twopointer/BuyTwoChocolates.java
@@ -10,14 +10,14 @@ public class BuyTwoChocolates {
                 minA=prices[i];
             }else if(prices[i]<minB)
                 minB=prices[i];
-        if(minA+minB<=money)
-            return money-minA-minB;
+            if(minA+minB<=money)
+                return money-minA-minB;
         return money;
     }
     public static void main(String[] args) {
         BuyTwoChocolates buyTwoChocolates = new BuyTwoChocolates();
-        int[] prices = {1, 2, 2};
-        int money = 3;
+        int[] prices = {98,54,6,34,66,63,52,39};
+        int money = 62;
         int remainingMoney = buyTwoChocolates.buyChoco(prices, money);
         System.out.println(remainingMoney);
     }
