099047f50f8ac325029b8340e7dd57fc50f387a0
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-15 13:12:48 -0700

    lc. bitmanipulation. SumOfTwoIntegers. O(n) O(1)

--- src/main/java/lc/bitmanipulation/m/SumOfTwoIntegers.java
+++ src/main/java/lc/bitmanipulation/m/SumOfTwoIntegers.java
@@ -5,10 +5,10 @@ public class SumOfTwoIntegers {
     public int getSum(int a, int b) {
         while ((a&b) != 0) {
             int olda = a;
-            a = olda^b;
-            b = (olda&b) << 1;
+            a = olda^b; //
+            b = (olda&b) << 1; //
         }
-        return a|b;
+        return a|b; //
     }
     public static void main(String[] args) {
         SumOfTwoIntegers sumOfTwoIntegers = new SumOfTwoIntegers();
