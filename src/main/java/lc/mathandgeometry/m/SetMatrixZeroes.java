f0d9de07f48559195bd8af25aed72102b3093bce
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-20 21:57:15 -0700

    lc. mathandgeometry. SetMatrixZeroes. O(m*n) O(m+n)

---
+++ src/main/java/lc/mathandgeometry/m/SetMatrixZeroes.java
@@ -0,0 +1,40 @@
+package lc.mathandgeometry.m;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SetMatrixZeroes {
+    // O(m*n) O(m+n)
+    public static void setZeroes(int[][] matrix) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    arr.add(i);
+                    arr.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < arr.size(); i += 2) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[j][arr.get(i + 1)] = 0;
+            }
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[arr.get(i)][j] = 0;
+            }
+        }
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        printMatrix(matrix);
+        setZeroes(matrix);
+        System.out.println();
+        printMatrix(matrix);
+    }
+}
