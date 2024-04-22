5c1b6cfd8b939f8eefa04a5a40e9563ebd47fa1c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-22 14:27:08 -0700

    lc. mathandgeometry. MultiplyStrings. O(n) O(m+n)

--- src/main/java/lc/mathandgeometry/m/MultiplyStrings.java
+++ src/main/java/lc/mathandgeometry/m/MultiplyStrings.java
@@ -3,16 +3,36 @@ package lc.mathandgeometry.m;
 import java.math.BigInteger;
 
 public class MultiplyStrings {
-    // O(n) O(1)
+    // O(n) O(m+n)
     public String multiply(String num1, String num2) {
-        BigInteger a=new BigInteger(num1);
-        BigInteger b=new BigInteger(num2);
-        BigInteger c=a.multiply(b);
-        return c+"";
+        int m = num1.length();
+        int n = num2.length();
+        int[] pos = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int sum = mul + pos[p2];
+
+                pos[p1] += sum / 10;
+                pos[p2] = (sum) % 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int p : pos) {
+            if (!(sb.length() == 0 && p == 0)) {
+                sb.append(p);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
     }
     public static void main(String[] args) {
-        String num1 = "123";
-        String num2 = "456";
+        String num1 = "2";
+        String num2 = "3";
         MultiplyStrings multiplier = new MultiplyStrings();
         System.out.println(multiplier.multiply(num1, num2));
     }
