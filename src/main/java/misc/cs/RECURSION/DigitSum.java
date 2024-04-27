2a1ba4246c8bec551a874dff92c3ccc68e14adc4
Yaroslav Hamuha <yhamuha@gmail.com>
2024-04-26 18:38:18 -0700

    misc. cs. DigitSum. O(log n) O(log n)

--- src/main/java/misc/cs/RECURSION/DigitSum.java
+++ src/main/java/misc/cs/RECURSION/DigitSum.java
@@ -1,16 +1,7 @@
 package misc.cs.RECURSION;
 
-/**
- *
- */
 public class DigitSum {
-    public static void main(String[] args) {
-        int n = 123;
-        System.out.println(digitsSum(n));
-        int f = 5;
-        System.out.println(factorial(f));
-    }
-
+    // O(log n) O(log n)
     static int digitsSum(int n) {
         if (n == 0) return 0;
         int digitsSum_except_last = digitsSum(n / 10);
@@ -18,8 +9,9 @@ public class DigitSum {
         return digitsSum_except_last + currentDigit;
     }
 
-    static int factorial(int f) {
-        if (f == 0) return 1;
-        else return factorial(f - 1) * f;
+    public static void main(String[] args) {
+        int n = 123;
+        System.out.println(digitsSum(n));
     }
+
 }
