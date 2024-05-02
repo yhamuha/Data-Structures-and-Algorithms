dba7f653b5100d7687c970f04b6f3d26c9bc0cb9
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-02 08:18:13 -0700

    lc. arraysandhashing. FirstNonRepeating. O(n) O(n)

--- src/main/java/lc/arraysandhashing/FirstNonRepeating.java
+++ src/main/java/lc/arraysandhashing/FirstNonRepeating.java
@@ -8,12 +8,7 @@ public class FirstNonRepeating {
     static int firstNonRepeating(int arr[], int n) {
         Map<Integer, Integer> m = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            if (m.containsKey(arr[i])) {
-                m.put(arr[i], m.get(arr[i]) + 1);
-            }
-            else {
-                m.put(arr[i], 1);
-            }
+            m.put(arr[i], m.getOrDefault(arr[i],0) +1);
         }
         for (int i = 0; i < n; i++)
             if (m.get(arr[i]) == 1)
