f9bf0da67491ad42fceeebe831e98733c511709b
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-05 15:41:26 -0700

    lc. backtracking. WordSearch. O(n * m * 4^k)

--- src/main/java/lc/backtracking/m/WordSearch.java
+++ src/main/java/lc/backtracking/m/WordSearch.java
@@ -2,28 +2,27 @@ package lc.backtracking.m;
 
 public class WordSearch {
 
-    // O(n * m * 4^k; n,m - dimension of word matrix; k - length of word string
+    // O(n * m * 4^k) O(n)
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if(find(board,word,i,j,0)) // check for every char
+                if(backtrack(board,word,i,j,0))
                     return true;
             }
         }
         return false;
     }
-    private boolean find(char[][] board, String word, int i, int j, int index) {
-        if(index==word.length()) // word found
+    private boolean backtrack(char[][] board, String word, int i, int j, int index) {
+        if(index==word.length())
             return true;
         if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!=word.charAt(index))
             return false;
         char temp=board[i][j];
-        board[i][j]=' '; // mark founded letter
-        // check the letter by all the directions
-        boolean found = find(board,word,i+1,j,index+1) ||
-                find(board,word,i,j+1,index+1) ||
-                find(board,word,i-1,j,index+1) ||
-                find(board,word,i,j-1,index+1);
+        board[i][j]=' ';
+        boolean found = backtrack(board,word,i+1,j,index+1) ||
+                backtrack(board,word,i,j+1,index+1) ||
+                backtrack(board,word,i-1,j,index+1) ||
+                backtrack(board,word,i,j-1,index+1);
 
         board[i][j]=temp;
         return found;
