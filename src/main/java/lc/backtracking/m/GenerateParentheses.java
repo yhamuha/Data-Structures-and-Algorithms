c8a3c2e737b2c2dc0590af053064651ce11c0884
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-07 21:25:15 -0700

    lc. backtracking. GenerateParentheses. O(2^n) O(n)

--- src/main/java/lc/backtracking/m/GenerateParentheses.java
+++ src/main/java/lc/backtracking/m/GenerateParentheses.java
@@ -4,24 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
-    // TC: O(2^n)
-    public List<String> generateParenthesis(int n) {
-        List<String> resultList = new ArrayList<>();
+    // O(2^n) O(n)
+    public List<String> generateParenthesis(int n){
+        List<String>  resultList = new ArrayList<>();
         backtrack(resultList, "", 0, 0, n);
         return resultList;
     }
 
-    private void backtrack(List<String> resultList, String current_string, int open, int close,  int max){
-        // SC: O(2n)
-        if(current_string.length() == max * 2){ // base case: "((()))"
-            resultList.add(current_string);
-            return; // return from the particular stack call
+    private void backtrack(List<String> resultList, String tempStr, int open, int close, int max) {
+        if (tempStr.length() == max * 2) {
+            resultList.add(tempStr);
+            return;
         }
-        if (open < max)
-            backtrack(resultList, current_string + "(", open + 1, close, max);
-
-        if (close < open)
-            backtrack(resultList, current_string + ")", open, close + 1, max);
+        if(open<max)
+            backtrack(resultList, tempStr+"(", open+1, close, max);
+        if(close<open)
+            backtrack(resultList, tempStr+")", open, close+1, max);
     }
 
     public static void main(String[] args) {
