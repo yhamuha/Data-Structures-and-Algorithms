4427c04a25664d924c3c4ad814e7d80f358b449c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-08 21:59:07 -0700

    lc. arraysandhashing. ArraysIntersection. O(m+n) O(m+n)

--- src/main/java/lc/arraysandhashing/ArraysIntersection.java
+++ src/main/java/lc/arraysandhashing/ArraysIntersection.java
@@ -3,22 +3,24 @@ package lc.arraysandhashing;
 import java.util.*;
 
 public class ArraysIntersection {
-    // TC: O(n+m); SC: O(n+m)
+    // O(m+n) O(m+n)
     public static int[] arraysIntersection(int[] nums1, int[] nums2) {
 
         Set<Integer> set = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
-
-        for (int val : nums1) {
+        for (int val : nums1)
             set.add(val);
-        }
-
         for (int val : nums2) {
-            if (set.contains(val)) {
+            if (set.contains(val))
                 set2.add(val);
-            }
         }
-        int[] ints = set2.stream().mapToInt(Integer::intValue).toArray();
+        // int[] ints = set2.stream().mapToInt(Integer::intValue).toArray();
+        int[] ints = new int[set2.size()];
+        int index = 0;
+        for (Integer num : set2) {
+            ints[index] = num.intValue();
+            index++;
+        }
         return ints;
     }
 
