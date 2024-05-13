590ad8b30424ef73de6faad8c0eb9753c3b454a8
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-13 13:37:54 -0700

    lc. backtracking. LetterCombinationsOfaPhoneNumber. O(4^n) O(n)

--- src/main/java/lc/backtracking/m/LetterCombinationsOfaPhoneNumber.java
+++ src/main/java/lc/backtracking/m/LetterCombinationsOfaPhoneNumber.java
@@ -6,38 +6,38 @@ import java.util.List;
 import java.util.Map;
 
 public class LetterCombinationsOfaPhoneNumber {
-    // TC: O(4^n); '7','9' has 4 symbols associated
-        public List<String> letterCombinations(String digits) {
-        // TC: O(n) HashMap
-            List<String> resultList = new ArrayList<>();
-            if (digits.length()==0) return resultList;
-            Map<Character,String> hm = new HashMap<>();
-            hm.put('2',"abc");
-            hm.put('3',"def");
-            hm.put('4',"ghi");
-            hm.put('5',"jkl");
-            hm.put('6',"mno");
-            hm.put('7',"pqrs");
-            hm.put('8',"tuv");
-            hm.put('9',"wxyz");
-            backtrack(digits,0,hm,new StringBuilder(), resultList);
-            return resultList;
+    // O(4^n) O(n)
+    public List<String> letterCombinations(String digits) {
+        List<String> resultList = new ArrayList<>();
+        if (digits.length() == 0) return resultList;
+        Map<Character, String> hm = new HashMap<>();
+        hm.put('2', "abc");
+        hm.put('3', "def");
+        hm.put('4', "ghi");
+        hm.put('5', "jkl");
+        hm.put('6', "mno");
+        hm.put('7', "pqrs");
+        hm.put('8', "tuv");
+        hm.put('9', "wxyz");
+        backtrack(digits, 0, hm, new StringBuilder(), resultList);
+        return resultList;
+    }
+
+    private void backtrack(String digits, int i, Map<Character, String> hm, StringBuilder sb, List<String> resultList) {
+        if (i == digits.length()) {
+            resultList.add(sb.toString());
+            return;
         }
-        private void backtrack(String digits, int i, Map<Character,String> hm, StringBuilder sb, List<String> resultList){
-            if(i == digits.length()){
-                resultList.add(sb.toString());
-                return;
-            }
-            String curr = hm.get(digits.charAt(i));
-            for(int k=0; k<curr.length(); k++){
-                sb.append(curr.charAt(i));
-                backtrack(digits,k+1,hm,sb,resultList);
-                sb.deleteCharAt(sb.length()-1);
-            }
+        String curr = hm.get(digits.charAt(i));
+        for (int k = 0; k < curr.length(); k++) {
+            sb.append(curr.charAt(k));
+            backtrack(digits, i + 1, hm, sb, resultList);
+            sb.deleteCharAt(sb.length() - 1);
         }
+    }
 
     public static void main(String[] args) {
-        var lcopn = new LetterCombinationsOfaPhoneNumber();
+        LetterCombinationsOfaPhoneNumber lcopn = new LetterCombinationsOfaPhoneNumber();
         String digits = "23";
         System.out.println(lcopn.letterCombinations(digits));
     }
