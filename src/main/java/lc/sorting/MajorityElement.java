451016c91aaddcb18f48d7ed1f1b3411f0211f93
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-22 23:08:41 -0700

    lc. Majority Element. O(n) O(1)

--- src/main/java/lc/sorting/MajorityElement.java
+++ src/main/java/lc/sorting/MajorityElement.java
@@ -3,18 +3,26 @@ package lc.sorting;
 public class MajorityElement {
     // O(n) O(1)
     public int majorityElement(int[] nums) {
-        int res = 0, count = 0;
+   /*     int res = 0, count = 0;
         for(int n: nums) {
             if(count == 0)
                 res = n;
             count += (n == res? 1: -1);
         }
+        return res;*/
+        int res=0, count=0;
+        for(int num : nums) {
+            if (count == 0)
+                res = num;
+            count += (res == num?1:-1);
+        }
         return res;
+
     }
 
     public static void main(String[] args) {
         var me = new MajorityElement();
-        int[] nums= {2,2,1,1,1,2,2};
+        int[] nums= {4,4,3,2,3};
         System.out.println(me.majorityElement(nums));
     }
 }
