bde59aa6ce6533cbd07e4466672282aea152c569
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-22 23:59:44 -0700

    lc. arraysandhashing. ContainsDuplicate

--- src/main/java/lc/arraysandhashing/ContainsDuplicate.java
+++ src/main/java/lc/arraysandhashing/ContainsDuplicate.java
@@ -8,8 +8,8 @@ public class ContainsDuplicate {
     static public boolean containsDuplicate(int[] nums) {
         int n = nums.length;
         // O(n^2) O(1)
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i=0; i<n-1;i++){
+            for(int j=i+1; j<n; j++){
                 if (nums[i] == nums[j])
                     return true;
             }
@@ -20,9 +20,9 @@ public class ContainsDuplicate {
     // O(n) O(n)
     static public boolean containsDuplicateEnhanced(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
+        for (int num: nums) {
             if (set.contains(num))
-                    return true;
+                return true;
             set.add(num);
         }
         return false;
@@ -42,10 +42,10 @@ public class ContainsDuplicate {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,3,4,5};
-        // System.out.println(containsDuplicate(arr));
-        // System.out.println(containsDuplicateEnhanced(arr));
-        System.out.println(containsDuplicateBitmanipulation(arr));
+        int[] arr = {3,2,3};
+         // System.out.println(containsDuplicate(arr));
+         // System.out.println(containsDuplicateEnhanced(arr));
+         System.out.println(containsDuplicateBitmanipulation(arr));
 
     }
 }
--- src/main/java/lc/sorting/MajorityElement.java
+++ src/main/java/lc/sorting/MajorityElement.java
@@ -3,13 +3,6 @@ package lc.sorting;
 public class MajorityElement {
     // O(n) O(1)
     public int majorityElement(int[] nums) {
-   /*     int res = 0, count = 0;
-        for(int n: nums) {
-            if(count == 0)
-                res = n;
-            count += (n == res? 1: -1);
-        }
-        return res;*/
         int res=0, count=0;
         for(int num : nums) {
             if (count == 0)
