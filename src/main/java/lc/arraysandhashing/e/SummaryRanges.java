72867dd6b1a32e9724b7fc8d1b6a31238ea7e51e
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-24 11:55:16 -0700

    lc. arraysandhashing. SummaryRanges. O(n) O(n)

--- src/main/java/lc/arraysandhashing/e/SummaryRanges.java
+++ src/main/java/lc/arraysandhashing/e/SummaryRanges.java
@@ -4,26 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SummaryRanges {
+    // O(n) O(n)
     public static List<String> summaryRanges(int[] nums) {
-        List<String> list = new ArrayList();
+        List<String> ranges = new ArrayList<>();
         int n = nums.length;
-        for (int i = 0; i<n; i++) {
+        for(int i=0; i<n; i++){
             int start = nums[i];
-            while (i+1<n && nums[i+1] == (nums[i])+1) {
+            while (i+1<n && nums[i+1]==(nums[i])+1)
                 i++;
-            }
-            if(start != nums[i]) {
-                list.add("" + start + "->" + nums[i]);
-            }
-            else {
-                list.add("" + start);
-            }
+            if (start != nums[i])
+                ranges.add(start + "->" + nums[i]);
+            else
+                ranges.add("" + start);
         }
-        return list;
+        return ranges;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,5,6,7};
+        int[] nums = {1,2,3,5,6,7,9};
         System.out.println(summaryRanges(nums));
     }
 }
