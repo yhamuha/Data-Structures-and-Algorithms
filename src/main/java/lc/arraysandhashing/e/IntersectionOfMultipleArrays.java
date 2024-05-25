2db9198302873f108d72489fe595f9ac9afebc51
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-24 20:48:12 -0700

    lc. arraysandhashing. IntersectionOfMultipleArrays. O(n log n) O(n)

--- src/main/java/lc/arraysandhashing/e/IntersectionOfMultipleArrays.java
+++ src/main/java/lc/arraysandhashing/e/IntersectionOfMultipleArrays.java
@@ -5,22 +5,21 @@ import java.util.*;
 public class IntersectionOfMultipleArrays {
     //T: O(n log n) S: O(n)
     public List<Integer> intersection(int[][] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int[] arr : nums) {    // freq map from 2d int[][]
-            for (int num : arr) {
-                map.put(num, map.getOrDefault(num, 0) + 1);
-            }
+        Map<Integer,Integer> freqMap = new HashMap<>();
+        for(int arr[] : nums){
+            for(int num : arr)
+                freqMap.put(num, freqMap.getOrDefault(num,0)+1);
         }
-        int n = nums.length;    // num of subarrays
-        List<Integer> res = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == n) {    // if entry contains particular number in each of subarrays
-                res.add(entry.getKey());
-            }
+        int n = nums.length;
+        List<Integer> resultList = new ArrayList<>();
+        for(Map.Entry<Integer,Integer> entry : freqMap.entrySet()){
+            if (entry.getValue() == n)
+                resultList.add(entry.getKey());
         }
-        Collections.sort(res);
-        return res;
+        Collections.sort(resultList);
+        return resultList;
     }
+
     public static void main(String[] args) {
         IntersectionOfMultipleArrays solution = new IntersectionOfMultipleArrays();
         int[][] nums = {{3, 1, 2, 4, 5}, {1, 2, 3, 4}, {3, 4, 5, 6}};
