64afefda34ebbe2a0274a13baf50dbeb508208d0
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-24 22:36:15 -0700

    lc. arraysandhashing. TwoOutOfThree. O(n) O(n)

--- src/main/java/lc/arraysandhashing/e/TwoOutOfThree.java
+++ src/main/java/lc/arraysandhashing/e/TwoOutOfThree.java
@@ -5,28 +5,31 @@ import java.util.HashSet;
 import java.util.List;
 
 public class TwoOutOfThree {
-    // T: O(n) S: O(n)
+    // O(n) O(n)
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         List<Integer> resultList = new ArrayList<>();
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        for(int i:nums1){
-            set1.add(i);
-        }
-        for(int num : nums2){
-            if(set1.contains(num) && !resultList.contains(num))
+
+        HashSet<Integer> nums1Set = new HashSet<>();
+        HashSet<Integer> complementarySet = new HashSet<>();
+
+        for(int i:nums1)        // form nums1Set
+            nums1Set.add(i);
+
+        for(int num : nums2)    // check nums2 and form resultList & complemenarySet
+            if(nums1Set.contains(num) && !resultList.contains(num))
                 resultList.add(num);
             else
-                set2.add(num);
-        }
-        for(int num : nums3){
-            if(set1.contains(num) && !resultList.contains(num))
+                complementarySet.add(num);
+
+        for(int num : nums3){   // check nums3 , nums1Set, complementarySet and form resultList
+            if(nums1Set.contains(num) && !resultList.contains(num))
                 resultList.add(num);
-            if(set2.contains(num) && !resultList.contains(num))
+            if(complementarySet.contains(num) && !resultList.contains(num))
                 resultList.add(num);
         }
         return resultList;
     }
+
     public static void main(String[] args) {
         TwoOutOfThree solution = new TwoOutOfThree();
         int[] nums1 = {3, 1};
