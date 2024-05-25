996e46b694cf12d9447c261e3ebc5f2407928a2d
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-24 23:58:32 -0700

    lc. arraysandhashing. GroupAnagrams. O(n log n) O(n)

--- src/main/java/lc/arraysandhashing/m/GroupAnagrams.java
+++ src/main/java/lc/arraysandhashing/m/GroupAnagrams.java
@@ -3,18 +3,17 @@ package lc.arraysandhashing.m;
 import java.util.*;
 
 public class GroupAnagrams {
-    // O(n) O(n)
+    // O(n log n) O(n)
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> groupedAnagrams = new HashMap<>();
-        for (String str : strs){
-            char[] charArr = str.toCharArray();
-            Arrays.sort(charArr);
-            String sortedStr = String.valueOf(charArr);
+        Map<String,List<String>> groupedAnagrams = new HashMap<>();
+        for (String str : strs){                                // raw str
+            char[] strArray = str.toCharArray();
+            Arrays.sort(strArray);
+            String sortedString = String.valueOf(strArray);     // sorted str
 
-            if (!groupedAnagrams.containsKey(sortedStr)) {
-                groupedAnagrams.put(sortedStr, new ArrayList<>());
-            }
-            groupedAnagrams.get(sortedStr).add(str);
+            if (!groupedAnagrams.containsKey(sortedString))
+                groupedAnagrams.put(sortedString, new ArrayList<>());   // put sorted as Key
+            groupedAnagrams.get(sortedString).add(str);                 // add raw as Value
         }
         return new ArrayList<>(groupedAnagrams.values());
     }
