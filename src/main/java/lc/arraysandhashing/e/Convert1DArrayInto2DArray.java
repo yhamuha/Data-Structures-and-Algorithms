4586fa9cb3910542ec731f40f431bb8f1b3779ac
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-24 18:51:29 -0700

    lc. arraysandhashing. Convert1DArrayInto2DArray. O(m*n) O(m*n)

--- src/main/java/lc/arraysandhashing/e/Convert1DArrayInto2DArray.java
+++ src/main/java/lc/arraysandhashing/e/Convert1DArrayInto2DArray.java
@@ -1,5 +1,7 @@
 package lc.arraysandhashing.e;
 
+import java.util.Arrays;
+
 public class Convert1DArrayInto2DArray {
     // O(m*n) O(m*n)
     public int[][] construct2DArray(int[] original, int m, int n) {
@@ -8,18 +10,20 @@ public class Convert1DArrayInto2DArray {
             return new int[0][0];
         }
         for(int i = 0; i < original.length; i++) {
-            int row = i/n;
-            int col = i%n;
+            int row = i/n; // 0/2=0 1/2=0 2/2=1 3/2=1
+            int col = i%n; // 0%2=0 1%2=1 2%2=0 3%2=1
             arr[row][col] = original[i];
         }
         return arr;
     }
+
     public static void main(String[] args) {
         Convert1DArrayInto2DArray converter = new Convert1DArrayInto2DArray();
         int[] original = {1, 2, 3, 4};
         int m = 2;
         int n = 2;
         int[][] result = converter.construct2DArray(original, m, n);
+        System.out.println("Given array: \n" + Arrays.toString(original));
         System.out.println("Converted 2D Array:");
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[0].length; j++) {
