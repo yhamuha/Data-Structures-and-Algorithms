d6eca9ccf6c599c0848572b5c1c13b5c23efe474
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-25 20:34:13 -0700

    lc. arraysandhashing. ProductOfArrayExceptSelf. O(n) O(1)

--- src/main/java/lc/arraysandhashing/m/ProductOfArrayExceptSelf.java
+++ src/main/java/lc/arraysandhashing/m/ProductOfArrayExceptSelf.java
@@ -3,28 +3,22 @@ package lc.arraysandhashing.m;
 import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf {
+    // O(n) O(1)
     public int[] productExceptSelf(int[] nums) {
-
         int[] left = new int[nums.length];
         int[] right = new int[nums.length];
-
-        // create left array
-        left[0] = 1;
+        left[0] = 1;    // create left array
         for (int i = 1; i < nums.length; i++) {
             left[i] = left[i - 1] * nums[i - 1];
         }
-        // create right array
-        right[nums.length - 1] = 1;
+        right[nums.length - 1] = 1; // create right array
         for (int i = nums.length - 2; i > -1; i--) {
             right[i] = right[i + 1] * nums[i + 1];
         }
-
-        // multiply two arrays
-        int[] ans = new int[nums.length];
+        int[] ans = new int[nums.length];   // multiply two arrays
         for (int i = 0; i < nums.length; i++) {
             ans[i] = left[i] * right[i];
         }
-
         return ans;
     }
 
