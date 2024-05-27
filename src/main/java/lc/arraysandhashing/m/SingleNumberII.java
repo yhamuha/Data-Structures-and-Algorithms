3c24d4eb5e3e5a4dd7ddebac466daabb1c2512f5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-26 21:42:49 -0700

    lc. bitmanipulation. SingleNumberII. O(n) O(1)

---
+++ src/main/java/lc/arraysandhashing/m/SingleNumberII.java
@@ -0,0 +1,19 @@
+package lc.arraysandhashing.m;
+
+public class SingleNumberII {
+    // O(n) O(1)
+    public int singleNumber(int[] nums) {
+        int ones=0, twos=0;
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+        return ones;
+    }
+
+    public static void main(String[] args){
+        SingleNumberII snII = new SingleNumberII();
+        int[] nums = {2,2,3,2};
+        System.out.println(snII.singleNumber(nums));
+    }
+}
