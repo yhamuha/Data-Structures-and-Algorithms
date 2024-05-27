d9ed839ab3bc1239725e06886155dfaf602cdd64
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-27 13:43:26 -0700

    lc. linkedlist. MergeTwoSortedLists. O(m+n) O(1)

--- src/main/java/lc/linkedlist/e/MergeTwoSortedLists.java
+++ src/main/java/lc/linkedlist/e/MergeTwoSortedLists.java
@@ -1,26 +1,22 @@
 package lc.linkedlist.e;
 
-
 public class MergeTwoSortedLists {
-    // O(n+m)
+    // O(m+n) O(1)
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode root = new ListNode();
         ListNode curr = root;
-        ListNode index1 = list1;
-        ListNode index2 = list2;
-        while (index1 != null && index2 != null) {
-            if (index1.val < index2.val) {
-                curr.next = index1; curr = index1; index1 = curr.next;
-            } else {
-                curr.next = index2; curr = index2; index2 = curr.next;
-            }
+        ListNode id1 = list1;
+        ListNode id2 = list2;
+        while (id1 != null && id2 != null) {
+            if(id1.val < id2.val){
+                curr.next = id1; curr = id1; id1=curr.next;}
+            else {
+                curr.next = id2; curr = id2; id2=curr.next;}
         }
-        if (index1 != null) {
-            curr.next = index1; curr = index1; index1 = curr.next;
-        }
-        if (index2 != null) {
-            curr.next = index2; curr = index2; index2 = curr.next;
-        }
+        if (id1!=null){
+            curr.next = id1; curr = id1; id1=curr.next;}
+        if (id2!=null){
+            curr.next = id2; curr = id2; id2=curr.next;}
         return root.next;
     }
 
@@ -28,9 +24,7 @@ public class MergeTwoSortedLists {
         var mtsl = new MergeTwoSortedLists();
         ListNode list1 = new ListNode(1); list1.next = new ListNode(3); list1.next.next = new ListNode(5);
         ListNode list2 = new ListNode(2); list2.next = new ListNode(4); list2.next.next = new ListNode(6);
-
         ListNode result = mtsl.mergeTwoLists(list1, list2);
-
         while(result != null) {
             System.out.print(result.val + " ");
             result = result.next;
