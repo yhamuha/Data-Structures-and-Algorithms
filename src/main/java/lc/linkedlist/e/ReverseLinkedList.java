6368723f015cfc366bf84ccb6f62155c4a4257fc
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-27 12:52:06 -0700

    lc. linkedlist. ReverseLinkedList. O(n) O(1)

--- src/main/java/lc/linkedlist/e/ReverseLinkedList.java
+++ src/main/java/lc/linkedlist/e/ReverseLinkedList.java
@@ -11,15 +11,15 @@ class ListNode {
 public class ReverseLinkedList {
     // O(n) O(1)
     public ListNode reverseList(ListNode head) {
-       ListNode prev = null;
-       ListNode curr = head;
-       while (curr != null) {
-           ListNode next = curr.next;
-           curr.next = prev;
-           prev = curr;
-           curr = next;
-       }
-       return prev;
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
     }
 
     public static void main(String[] args) {
