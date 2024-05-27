bb1fca66fb188121be5303e4a5d3f4df51422a26
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-27 14:31:06 -0700

    lc. linkedlist. LinkedListCycle. O(n) O(1)

--- src/main/java/lc/linkedlist/e/LinkedListCycle.java
+++ src/main/java/lc/linkedlist/e/LinkedListCycle.java
@@ -4,11 +4,14 @@ public class LinkedListCycle {
     // "runner" Floyd's Cycle Detection Algorithm
     // O(n) O(1)
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head, fast = head;
-        while(fast != null && fast.next != null) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) return true;
+            if (slow == fast){
+                return true;
+            }
         }
         return false;
     }
@@ -18,7 +21,7 @@ public class LinkedListCycle {
         head.next = new ListNode(2);
         head.next.next = new ListNode(0);
         head.next.next.next = new ListNode(-4);
-        head.next.next.next.next = head.next;
+//        head.next.next.next.next = head.next;
         var llc = new LinkedListCycle();
         System.out.println(llc.hasCycle(head));
     }
