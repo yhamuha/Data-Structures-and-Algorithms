fbd81fe63d53587d07992a5c458762fdeef3e249
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-28 10:45:54 -0700

    lc. linkedlist. PalindromeLinkedList. O(n) O(1)

--- src/main/java/lc/linkedlist/e/PalindromeLinkedList.java
+++ src/main/java/lc/linkedlist/e/PalindromeLinkedList.java
@@ -3,35 +3,33 @@ package lc.linkedlist.e;
 public class PalindromeLinkedList {
     // O(n) O(1)
     public boolean isPalindrome(ListNode head) {
-        if (head == null) {
-            return true;
-        }
+        if (head == null) return true;
         ListNode slow = head;
         ListNode fast = head;
-        while (fast != null && fast.next != null) {
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode sec_half = reverseList(slow);
-        while (head != null && sec_half != null) {
-            if (head.val != sec_half.val) {
+        ListNode halfList = reverseList(slow);
+        while(head!=null && halfList!=null){
+            if(head.val!=halfList.val){
                 return false;
             }
-            head = head.next;
-            sec_half = sec_half.next;
+            head=head.next;
+            halfList=halfList.next;
         }
         return true;
     }
 
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode cur=head;
-        ListNode next=null;
-        while(cur!=null){
-            next=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=next;
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
         return prev;
     }
