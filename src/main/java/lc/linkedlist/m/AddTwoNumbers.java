97d44282824949460043447f18bde42bfd835dc3
Yaroslav Hamuha <yhamuha@gmail.com>
2024-05-30 20:48:46 -0700

    lc. linkedlist. AddTwoNumbers. O(n) O(1)

--- src/main/java/lc/linkedlist/m/AddTwoNumbers.java
+++ src/main/java/lc/linkedlist/m/AddTwoNumbers.java
@@ -1,11 +1,11 @@
 package lc.linkedlist.m;
 
 public class AddTwoNumbers {
+    // O(n) O(1)
     public ListNode addTwoNumbers(ListNode a, ListNode b) {
         ListNode n=new ListNode(0);int c=0;
         ListNode dummy=n;
         while(a!=null||b!=null){
-
             int x=a!=null?a.val:0;
             int y=b!=null?b.val:0;
             int k=x+y+c;
@@ -21,5 +21,15 @@ public class AddTwoNumbers {
             dummy.next=new ListNode(c);
         }
         return n.next;
+    }
+    public static void main(String[] args) {
+        ListNode a = new ListNode(2); a.next = new ListNode(4); a.next.next = new ListNode(3);
+        ListNode b = new ListNode(5); b.next = new ListNode(6); b.next.next = new ListNode(6);
+        AddTwoNumbers solution = new AddTwoNumbers();
+        ListNode result = solution.addTwoNumbers(a, b);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
     }
 }
