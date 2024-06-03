1df90375dcf1ef7edf0319707ffabeed63614a24
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-03 12:45:22 -0700

    lc. graphs. CloneGraph. O(V+E) O(V)

--- src/main/java/lc/graphs/m/CloneGraph.java
+++ src/main/java/lc/graphs/m/CloneGraph.java
@@ -4,27 +4,7 @@ import java.util.*;
 import lc.graphs.Node;
 
 public class CloneGraph {
-    /* if (node == null) {
-            return null;
-        }
-        HashMap<Node, Node> visited = new HashMap<>();
-        Node newNode = new Node(node.val);
-        visited.put(node, newNode);
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(node);
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            List<Node> newNeighbors = visited.get(cur).neighbors;
-            for (Node n : cur.neighbors) {
-                if (!visited.containsKey(n)) {
-                    visited.put(n, new Node(n.val));
-                    queue.offer(n);
-                }
-                newNeighbors.add(visited.get(n));
-            }
-        }
-        return newNode;*/
-    // O(n) O(n)
+    // O(V+E) nodes+edges O(V) nodes
     public Node cloneGraph(Node node) {
         if (node == null) return null;
         Map<Integer, Node> map = new HashMap<>();
