a76656628e759dfd7584ebafc749ce7650e304bc
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-04 13:56:47 -0700

    lc. graphs. MaxAreaOfIsland. O(m*n) O(m*n)

--- src/main/java/lc/graphs/m/MaxAreaOfIsland.java
+++ src/main/java/lc/graphs/m/MaxAreaOfIsland.java
@@ -1,38 +1,7 @@
 package lc.graphs.m;
 
 public class MaxAreaOfIsland {
-
-    /*boolean seen[][];
-    // overall TC: (O n * m * n * m) <==> O(n^2 * m^2)
-    public int maxAreaOfIsland(int[][] grid) {
-        //O(n * m) iterates through n rows * m columns
-        int maxArea = 0;
-        int rows = grid.length;
-        int columns = grid[0].length;
-        seen = new boolean[rows][columns];
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<columns; j++) {
-                //O(n * m) recursion
-                maxArea = Math.max(maxArea, area(i,j,grid));
-            }
-        }
-        return maxArea;
-    }
-
-    private int area(int row, int column, int[][] grid) {
-        if (    row<0 || row >= grid.length ||
-                column<0 || column >= grid[row].length ||
-                seen[row][column] ||
-                grid[row][column]==0) {
-            return 0;
-        }
-        seen[row][column] = true;
-        return (1 +
-                area(row+1,column, grid) +
-                area(row-1,column, grid)) +
-                area(row,column-1, grid) +
-                area(row,column+1, grid);*/
-    // O(n*m) O(n*m)
+    // O(m*n) O(m*n)
     public int maxAreaOfIsland(int[][] grid) {
         int max = 0, n=grid.length, m = grid[0].length;
         for(int i=0; i<n; i++) {
@@ -45,7 +14,7 @@ public class MaxAreaOfIsland {
         }
         return max;
     }
-
+    // dfs
     private int getArea(int[][] grid, int i, int j, int n, int m){
         if ( i<0 || j<0 || i>= n || j>= m || grid[i][j]==0)
             return 0;
