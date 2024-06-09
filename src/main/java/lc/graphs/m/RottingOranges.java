b17e555a5aca6244e610074210c3ba9667469a77
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-08 19:42:41 -0700

    lc. graphs. RottingOranges. O(m*n) O(m*n)

--- src/main/java/lc/graphs/m/RottingOranges.java
+++ src/main/java/lc/graphs/m/RottingOranges.java
@@ -4,36 +4,32 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class RottingOranges {
-    int[][] directions = {  {-1,0}, // top
-                            {0,-1}, // left
-                            {1,0},  // bottom
-                            {0,1}}; // right
-
+    int[][] directions = {{-1,0}, {0,-1}, {1,0}, {0,1}};
+    // O(m*n) O(m*n)
     public int orangesRotting(int[][] grid) {
-        int m = grid.length, n = grid[0].length;    // O(m * n)
-        int freshOranges = 0;                       // count freshOranges
+        int m = grid.length, n = grid[0].length;
+        int freshOranges = 0;
         for(int i=0; i< m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 1){
-                    ++freshOranges;
+                    ++freshOranges;         // count fresh oranges
                 }
             }
         }
-        Queue<Integer> queue = new LinkedList<>();  // O(m * n)
-        for(int i=0; i<m; i++){                     // determine "2" positions
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if (grid[i][j] == 2)
-                    queue.add(i * n +j);            // converting 2d->1d coordinates
+                    queue.add(i * n +j);    // determine "2" positions and convert 2d->1d
             }
         }
         int minutes = 0;
-        // conditions for BFS
-        while(!queue.isEmpty() && freshOranges > 0){    // O(m * n)
+        while(!queue.isEmpty() && freshOranges > 0){    // bfs
             int size = queue.size();
             for(int i=0; i< size; i++){
                 int num = queue.poll(), row = num / n, col = num % n;
                 for(int[] direction : directions) {
-                    int x = direction[0] + row, y = direction[1] + col; // converting 1d->2d coordinates
+                    int x = direction[0] + row, y = direction[1] + col; // convert 1d->2d
                     if(x>-1 && y>-1 && x<m && y< n && grid[x][y] == 1){
                         queue.add(x*n + y);
                         --freshOranges;
