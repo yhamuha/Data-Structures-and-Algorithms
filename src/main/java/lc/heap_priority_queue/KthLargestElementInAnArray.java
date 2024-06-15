dfbc7017ebee02a490cfe9efd256627ee36e9675
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-15 10:01:12 -0700

    lc. heap_priority_queue. KthLargestElementInAnArray. O(n log n) O(n)

--- src/main/java/lc/heap_priority_queue/KthLargestElementInAnArray.java
+++ src/main/java/lc/heap_priority_queue/KthLargestElementInAnArray.java
@@ -1,20 +1,15 @@
 package lc.heap_priority_queue;
 
 import java.util.PriorityQueue;
-import java.util.Queue;
 
-/*
- return the kth largest element in the array
- */
 public class KthLargestElementInAnArray {
     public int findKthLargest(int[] nums, int k) {
-        // SC: O(n)
+        // O(n log n) O(n)
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        // TC: (O n log n)
-        for(int num : nums) {
-            if (minHeap.size() < k) {
+        for(int num : nums){
+            if (minHeap.size() < k){
                 minHeap.offer(num);
-            } else if ( num > minHeap.peek()) {
+            } else if ( num > minHeap.peek()){
                 minHeap.poll();
                 minHeap.offer(num);
             }
