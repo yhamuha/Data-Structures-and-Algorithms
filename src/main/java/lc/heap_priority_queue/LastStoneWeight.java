b32e14db7705f6cb8570c8e66f9c043692d23cd5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-14 20:22:04 -0700

    lc. heap_priority_queue. LastStoneWeight. O(n log n) O(n)

--- src/main/java/lc/heap_priority_queue/LastStoneWeight.java
+++ src/main/java/lc/heap_priority_queue/LastStoneWeight.java
@@ -6,16 +6,6 @@ import java.util.PriorityQueue;
 public class LastStoneWeight {
     public int lastStoneWeight(int[] stones) {
         // O(n log n) O(n)
-        /*PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int stone : stones)
-            maxHeap.offer(stone);
-        while (maxHeap.size() > 1) {
-            int x = maxHeap.poll();
-            int y = maxHeap.poll();
-            if (x != y)
-                maxHeap.offer(Math.abs(y-x));
-        }
-        return maxHeap.isEmpty() ? 0 : maxHeap.peek();*/
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         for(int stone:stones)
             maxHeap.offer(stone);
