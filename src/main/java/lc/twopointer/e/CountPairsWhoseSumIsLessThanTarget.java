b2e13e384f3fa3995522da53be6325327c078d36
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-23 20:11:17 -0700

    lc. twopointer. CountPairsWhoseSumIsLessThanTarget. O(n log n) O(1)

--- src/main/java/lc/twopointer/e/CountPairsWhoseSumIsLessThanTarget.java
+++ src/main/java/lc/twopointer/e/CountPairsWhoseSumIsLessThanTarget.java
@@ -5,11 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class CountPairsWhoseSumIsLessThanTarget {
-
+    // O(n log n) O(1)
     public int countPairs(List<Integer> nums, int target) {
         Collections.sort(nums);
         int count=0, left=0, right=nums.size()-1;
-
         while(left < right){
             if(nums.get(left) + nums.get(right) < target){
                 count += right - left;
@@ -18,14 +17,13 @@ public class CountPairsWhoseSumIsLessThanTarget {
             else
                 right--;
         }
-
         return count;
     }
 
     public static void main(String[] args) {
         CountPairsWhoseSumIsLessThanTarget countPairs = new CountPairsWhoseSumIsLessThanTarget();
-        List<Integer> nums = Arrays.asList(-6, 2, 5, -2, -7, -1, 3);
-        int target = -2;
+        List<Integer> nums = Arrays.asList(-1,1,2,3,1);
+        int target = 2;
         System.out.println(countPairs.countPairs(nums, target));
     }
 
