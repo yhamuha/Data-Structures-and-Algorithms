ecc4b760b609f1e0b8bcfb2094eaa03e9b235197
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-25 20:04:36 -0700

    lc. twopointer. TwoSum. O(n) O(n)

--- src/main/java/lc/twopointer/e/TwoSum.java
+++ src/main/java/lc/twopointer/e/TwoSum.java
@@ -7,23 +7,20 @@ import java.util.Map;
 public class TwoSum {
     // O(n) O(n)
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numToIndex = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (numToIndex.containsKey(target - nums[i])) {
-                return new int[] {numToIndex.get(target - nums[i]), i};
-            }
-            numToIndex.put(nums[i], i);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length;i++){
+            if (map.containsKey(target - nums[i]))
+                return new int[]{map.get(target - nums[i]), i};
+            map.put(nums[i],i);
         }
-        return new int[] {};
+        return new int[]{};
     }
+
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-
         int[] nums = {2, 7, 11, 15};
         int target = 9;
-
         int[] result = twoSum.twoSum(nums, target);
-
         System.out.println("Indices of the two numbers that add up to the target:");
         System.out.println(Arrays.toString(result));
     }
