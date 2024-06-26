8ec5a8a79bceaeabb48d0059f5ca7d79676fae22
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-26 07:17:40 -0700

    lc. twopointer. SquaresOfASortedArray. O(n) O(n)

--- src/main/java/lc/twopointer/e/SquaresOfASortedArray.java
+++ src/main/java/lc/twopointer/e/SquaresOfASortedArray.java
@@ -4,16 +4,12 @@ public class SquaresOfASortedArray {
     // O(n) O(n)
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
-
-        for (int i = 0; i < n; i++) {
+        int result[] = new int[n];
+        for(int i=0; i<n; i++)
             nums[i] = nums[i] * nums[i];
-        }
-
-        int left = 0;
-        int right = n - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[left] > nums[right]) {
+        int left = 0, right = n-1;
+        for(int i=n-1; i>=0; i--){
+            if(nums[left] > nums[right]){
                 result[i] = nums[left];
                 left++;
             } else {
@@ -23,12 +19,11 @@ public class SquaresOfASortedArray {
         }
         return result;
     }
+
     public static void main(String[] args) {
         SquaresOfASortedArray obj = new SquaresOfASortedArray();
-
         int[] nums = {-4, -1, 0, 3, 10};
         int[] result = obj.sortedSquares(nums);
-
         System.out.print("Sorted Squares: ");
         for (int num : result) {
             System.out.print(num + " ");
