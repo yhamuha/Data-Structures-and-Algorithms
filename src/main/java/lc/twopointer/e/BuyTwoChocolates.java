9c7acefc90f3d4250af156e33ca02d00736576a4
Yaroslav Hamuha <yhamuha@gmail.com>
2024-06-29 16:45:43 -0700

    lc. twopointer. BuyTwoChocolates. O(n) O(1)

--- src/main/java/lc/twopointer/e/BuyTwoChocolates.java
+++ src/main/java/lc/twopointer/e/BuyTwoChocolates.java
@@ -4,20 +4,22 @@ public class BuyTwoChocolates {
     // O(n) O(1)
     public int buyChoco(int[] prices, int money) {
         int minA=101,minB=101;
-        for(int i=0;i<prices.length;i++)
-            if(prices[i]<minA){
-                minB=minA;
-                minA=prices[i];
-            }else if(prices[i]<minB)
-                minB=prices[i];
-            if(minA+minB<=money)
-                return money-minA-minB;
+        for(int i=0;i<prices.length;i++) {
+            if (prices[i] < minA) {
+                minB = minA;
+                minA = prices[i];
+            } else if (prices[i] < minB) {
+                minB = prices[i];
+            }
+            if (minA + minB <= money)
+                return money - minA - minB;
+        }
         return money;
     }
     public static void main(String[] args) {
         BuyTwoChocolates buyTwoChocolates = new BuyTwoChocolates();
-        int[] prices = {98,54,6,34,66,63,52,39};
-        int money = 62;
+        int[] prices = {3,2,3};
+        int money = 3;
         int remainingMoney = buyTwoChocolates.buyChoco(prices, money);
         System.out.println(remainingMoney);
     }
