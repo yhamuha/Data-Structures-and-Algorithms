d54cd287805c674d67987aec59af44551ce092b6
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-03 11:38:27 -0700

    lc. binarysearch arrays. FindMaximumElementOfArray. O(log n) O(1)

--- src/main/java/misc/some/FindMaximumElementOfArray.java
+++ src/main/java/misc/some/FindMaximumElementOfArray.java
@@ -1,46 +1,38 @@
 package misc.some;
 
-/**
- * needs fixing
- */
+
 public class FindMaximumElementOfArray {
-
-    static int findMax(int arr[], int low, int high)
-    {
-
-        if (high < low)
-            return arr[0];
-
-        if (high == low)
-            return arr[low];
-
-        int mid = low + (high - low) / 2;
-
-        if (mid < high && arr[mid + 1] < arr[mid])
-        {
-            return arr[mid];
+    // O(n) O(1)
+    static int findMax(int arr[]) {
+        int max = 0;
+        for(int i=0; i<arr.length; i++){
+            if (arr[i]>max)
+                max = arr[i];
         }
-
-        if (mid > low && arr[mid] < arr[mid - 1])
-        {
-            return arr[mid - 1];
+        return max;
+    }
+    // O(log n) O(1)
+    static int findInSortedArray(int[] arr, int key){
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == key)
+                return mid;
+            if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-
-        if (arr[low] > arr[mid])
-        {
-            return findMax(arr, low, mid - 1);
-        }
-        else
-        {
-            return findMax(arr, mid + 1, high);
-        }
+        return -1;
     }
 
-    public static void main(String[] args)
-    {
-        int arr[] = { 5, 6, 1, 2, 3, 4, 10 };
+    public static void main(String[] args) {
+        int arr[] = {1,2,5,6,10};
         int n = arr.length;
-        System.out.println(findMax(arr, 0, n - 1));
+        System.out.println(findMax(arr));
+        System.out.println(findInSortedArray(arr,5));
     }
 
 }
