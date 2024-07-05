7a70cbb356ba4fceac81771101d7ac2125692242
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-04 22:24:51 -0700

    lc. arrays. ContainerWithMostWater. O(n) O(1)

--- src/main/java/lc/twopointer/m/ContainerWithMostWater.java
+++ src/main/java/lc/twopointer/m/ContainerWithMostWater.java
@@ -1,7 +1,7 @@
 package lc.twopointer.m;
 
 public class ContainerWithMostWater {
-    // O(n^2)
+    // O(n^2) O(1)
     public int maxArea_slow(int[] height) {
         int maxArea = 0;
         for (int i=0; i< height.length; i++) {
@@ -13,14 +13,16 @@ public class ContainerWithMostWater {
         return maxArea;
     }
 
-    // O(n)
+    // O(n) O(1)
     public int maxArea(int[] height) {
-        int maxArea =0, left =0, right = height.length -1;
-        while (left < right) {
-            int localArea = (right - left) * Math.min(height[left], height[right]);
-            maxArea = Math.max(maxArea, localArea);
-            if (height[left] < height[right]) left++;
-            else right--;
+        int maxArea = 0, left = 0, right = height.length-1;
+        while (left < right){
+            int localArea = (right-left) * Math.min(height[left],height[right]);
+            maxArea=Math.max(maxArea,localArea);
+            if (height[left]<height[right])
+                left++;
+            else
+                right--;
         }
         return maxArea;
     }
