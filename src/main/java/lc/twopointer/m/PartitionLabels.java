b6eb60e17d06db62647fd4647fcd6e7ebb21c9c7
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-07 21:57:29 -0700

    lc. twopointers. PartitionLabels. O(n log n) O(n)

--- src/main/java/lc/twopointer/m/PartitionLabels.java
+++ src/main/java/lc/twopointer/m/PartitionLabels.java
@@ -4,20 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PartitionLabels {
+    // O(n) O(n)
     public List<Integer> partitionLabels(String s) {
         if (s == null || s.length() == 0) return null;
         List<Integer> output_arr = new ArrayList();
-
-        // create an array with lastIndexes for each of characters
-        int[] lastIndices = new int[26];
+        int[] lastIndexes = new int[26];
         for (int i = 0; i<s.length(); i++) {
-            lastIndices[s.charAt(i)-'a'] = i;
+            lastIndexes[s.charAt(i)-'a'] = i;
         }
-
         int start = 0;
         int end = 0;
         for (int i=0; i<s.length(); i++) {
-            end = Math.max(end, lastIndices[s.charAt(i)-'a']);
+            end = Math.max(end, lastIndexes[s.charAt(i)-'a']);
             if (i == end) {
                 output_arr.add(end-start+1);
                 start = end+1;
