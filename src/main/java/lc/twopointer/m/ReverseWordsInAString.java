7e165f38cf35cbd77d9bf87632062f9a7997fad4
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-09 17:07:57 -0700

    lc. twopointers. ReverseWordsInAString. O(n) O(n)

--- src/main/java/lc/twopointer/m/ReverseWordsInAString.java
+++ src/main/java/lc/twopointer/m/ReverseWordsInAString.java
@@ -1,43 +1,34 @@
 package lc.twopointer.m;
 
 public class ReverseWordsInAString {
-    public void reverseWords(char[] str) {
-        reverse(str, 0, str.length - 1);
-        reverseEachWords(str);
-    }
-
-    // reverse the whole input
-    void reverse(char[] str, int s, int t) {
-        while (s < t) {
-            char tmp = str[s];
-            str[s] = str[t];
-            str[t] = tmp;
-            s++;
-            t--;
+    // O(n) O(n)
+    public String reverseWords(String s) {
+        StringBuilder ans = new StringBuilder();
+        if (s.length() == 1 && s.charAt(0) == ' ') {
+            return ans.toString();
         }
-    }
-
-    // reverse each individual word
-    void reverseEachWords(char[] str) {
-        int i = 0, j = 0;
-        int n = str.length;
-        while (i < n && j < n) {
-            while (i < n && str[i] == ' ') {
-                i++;
+        int i = s.length() - 1;
+        while (i >= 0) {
+            if (s.charAt(i) == ' ') {
+                --i;
+            } else {
+                int j = i;
+                if (ans.length() > 0) {
+                    ans.append(" ");
+                }
+                while (j >= 0 && s.charAt(j) != ' ') {
+                    --j;
+                }
+                ans.append(s.substring(j + 1, i + 1));
+                i = j;
             }
-            j = i;
-            while (j < n && str[j] != ' ') {
-                j++;
-            }
-            reverse(str, i, j - 1);
-            i = j;
         }
+        return ans.toString();
     }
 
     public static void main(String[] args) {
-        char[] str = {' ', 't','h', 'i', 's', ' ', 'i', 's', ' ', 'a'};
+        String s = "the sky is blue";
         ReverseWordsInAString reverse = new ReverseWordsInAString();
-        reverse.reverseWords(str);
-        System.out.println(str);
+        System.out.println(reverse.reverseWords(s));
     }
 }
