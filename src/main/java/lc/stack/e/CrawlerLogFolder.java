572cc48a42ee63219d7ba6eb26b933813e13b474
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-14 14:17:22 -0700

    lc. stack. CrawlerLogFolder. O(n) O(n)

--- src/main/java/lc/stack/e/CrawlerLogFolder.java
+++ src/main/java/lc/stack/e/CrawlerLogFolder.java
@@ -1,17 +1,23 @@
 package lc.stack.e;
 
+import java.util.Stack;
+
 public class CrawlerLogFolder {
-    // O(n) O(1)
+    // O(n) O(n)
     public int minOperations(String[] logs) {
-        int depth = 0;
-        for(String log : logs) {
-            if (log.equals("./")) continue;
-            else if (log.equals("../")){
-                depth--;
-            if (depth<0) depth=0;
-            } else depth++;
+        Stack<String> actions = new Stack<>();
+        String current = "./";
+        String back = "../";
+        for(String action:logs) {
+            if(!action.equals(current) && !action.equals(back)){
+                actions.push(action);
+            } else if (action.equals(back)){
+                if(!actions.isEmpty()) {
+                    actions.pop();
+                }
+            }
         }
-        return depth;
+        return actions.size();
     }
 
     public static void main(String[] args) {
