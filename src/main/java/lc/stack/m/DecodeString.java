64cbd67caba403cf90583fd2020c390275c65159
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-19 20:43:57 -0700

    lc. stack. DecodeString. O(n) O(n)

--- src/main/java/lc/stack/m/DecodeString.java
+++ src/main/java/lc/stack/m/DecodeString.java
@@ -1,4 +1,4 @@
-package lc.stack;
+package lc.stack.m;
 
 import java.util.Stack;
 
@@ -8,26 +8,24 @@ public class DecodeString {
         Stack<Integer>numStack=new Stack<>();
         Stack<StringBuilder>strBuild=new Stack<>();
         StringBuilder str = new StringBuilder();
-        int num=0;
-        for(char c:s.toCharArray()){
-            if(c>='0' && c<='9'){
-                num=num*10 +c -'0';
-            }
+        int num = 0;
+        for(char c : s.toCharArray()){
+            if(c>='0' && c<='9')
+                num=num * 10 + c -'0';
             else if(c=='['){
                 strBuild.push(str);
                 str=new StringBuilder();
                 numStack.push(num);
-                num=0;
-            }else if(c==']'){
+                num = 0;
+            } else if(c==']'){
                 StringBuilder temp=str;
                 str=strBuild.pop();
                 int count=numStack.pop();
-                while(count-->0){
+                while(count-- > 0){
                     str.append(temp);
                 }
-            }else{
+            } else
                 str.append(c);
-            }
         }
         return str.toString();
     }
