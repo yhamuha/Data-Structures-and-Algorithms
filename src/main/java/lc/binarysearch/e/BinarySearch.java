9337ae156b28cc08218dc5e8b260f8a6b1ca54df
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-21 18:58:44 -0700

    lc. binarysearch. BinarySearch. O(log n) O(1)

--- src/main/java/lc/binarysearch/e/BinarySearch.java
+++ src/main/java/lc/binarysearch/e/BinarySearch.java
@@ -1,17 +1,16 @@
 package lc.binarysearch.e;
 
 public class BinarySearch {
-    // O(log n)
+    // O(log n) O(1)
     public int search(int[] nums, int target) {
         int start = 0;
-        int end = nums.length - 1;
-        int mid = 0;
-        while (start <= end ) {
-            mid = start + (end - start)/2;
-            if ( nums[mid] < target)
-                start = mid + 1;
-            else if (nums[mid] > target )
-                end = mid - 1;
+        int end = nums.length-1;
+        while (start < end){
+            int mid = start + (end - start)/2;
+            if (nums[mid] < target)
+                start = mid+1;
+            else if(nums[mid] > target)
+                end = mid -1;
             else return mid;
         }
         return -1;
