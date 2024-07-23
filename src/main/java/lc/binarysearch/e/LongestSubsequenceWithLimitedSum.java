0d0d410f8ebc12960089205dc7d38d75ff1db344
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-22 21:13:33 -0700

    lc. binarysearch. LongestSubsequenceWithLimitedSum. O(n log n) O(n)

--- src/main/java/lc/binarysearch/e/LongestSubsequenceWithLimitedSum.java
+++ src/main/java/lc/binarysearch/e/LongestSubsequenceWithLimitedSum.java
@@ -3,26 +3,21 @@ package lc.binarysearch.e;
 import java.util.Arrays;
 
 public class LongestSubsequenceWithLimitedSum {
-    // T: O( sort: log n + prefix: log n)
-    // S: O(ans + prefix)
+    // O(n log n) O(n)
     public int[] answerQueries(int[] nums, int[] queries) {
         Arrays.sort(nums);
         int n = nums.length;
         int[] ans = new int[queries.length];
         int[] prefix = new int[n+1];
-
-        for (int i=1; i<prefix.length; i++) {       // create prefix array
+        for (int i=1; i<prefix.length; i++)
             prefix[i] = prefix[i-1] + nums[i-1];
-        }
-
-        for (int i=0; i<queries.length; i++) {      // check index for each query
+        for (int i=0; i<queries.length; i++) {
             int val = queries[i];
             ans[i] = bS(val, prefix);
             }
         return ans;
     }
-
-    private int bS(int val, int[] prefix) {         // bs helper func
+    private int bS(int val, int[] prefix) {
         int left = 0;
         int right = prefix.length - 1;
         while (left <= right) {
@@ -38,6 +33,8 @@ public class LongestSubsequenceWithLimitedSum {
 
     public static void main(String[] args) {
         var lcs = new LongestSubsequenceWithLimitedSum();
-        System.out.println(Arrays.toString(lcs.answerQueries(new int[]{4,5,2,1}, new int[]{3,10,21})));
+        System.out.println(Arrays.toString(lcs.answerQueries(
+                new int[]{4,5,2,1},
+                new int[]{3,10,21})));
     }
 }
