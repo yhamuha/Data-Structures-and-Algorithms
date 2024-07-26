7616a99efb1ca4a360f3e18bb425040091eb8dc1
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-25 19:18:15 -0700

    lc. binarysearch. CountNegativeNumbersInASortedMatrix. O( log n) O(1)

--- src/main/java/lc/binarysearch/e/CountNegativeNumbersInASortedMatrix.java
+++ src/main/java/lc/binarysearch/e/CountNegativeNumbersInASortedMatrix.java
@@ -1,7 +1,7 @@
 package lc.binarysearch.e;
 
 public class CountNegativeNumbersInASortedMatrix {
-    // O(m log n)
+    // O(m log n) O(1)
     public int countNegatives(int[][] grid) {
         int count = 0;
         for (int line[]: grid) {
@@ -10,15 +10,15 @@ public class CountNegativeNumbersInASortedMatrix {
         return count;
     }
     private int bs(int[] line) {
-    int left = 0;
-    int right = line.length - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (line[mid] >= 0)
-            left = mid + 1;
-        else right = mid - 1;
-    }
-    return line.length - left;
+        int left = 0;
+        int right = line.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (line[mid] >= 0)
+                left = mid + 1;
+            else right = mid - 1;
+        }
+        return line.length - left;
     }
 
     public static void main(String[] args) {
