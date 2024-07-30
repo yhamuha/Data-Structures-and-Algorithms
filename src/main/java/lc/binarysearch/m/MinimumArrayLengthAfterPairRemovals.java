fff4aa3e5cfd8031713c5738e988d479208f59b6
Yaroslav Hamuha <yhamuha@gmail.com>
2024-07-29 21:14:41 -0700

    lc. binarysearch. MinimumArrayLengthAfterPairRemovals. O(n) O(1)

--- src/main/java/lc/binarysearch/m/MinimumArrayLengthAfterPairRemovals.java
+++ src/main/java/lc/binarysearch/m/MinimumArrayLengthAfterPairRemovals.java
@@ -3,13 +3,10 @@ package lc.binarysearch.m;
 import java.util.*;
 
 public class MinimumArrayLengthAfterPairRemovals {
-
-    // O(n)
+    // O(n) O(1)
     public int minLengthAfterRemovals(List<Integer> nums) {
-
-        int N = nums.size(), max = 1, temp = 1;
-
-        for(int i = 1; i < N; i++){
+        int n = nums.size(), max = 1, temp = 1;
+        for(int i = 1; i < n; i++){
             if(nums.get(i) - nums.get(i - 1) == 0){
                 temp++;
                 if(max < temp)
@@ -17,36 +14,31 @@ public class MinimumArrayLengthAfterPairRemovals {
             }else
                 temp = 1;
         }
-
-        if(max > N / 2)
-            return (max * 2 - N);
-        if(N % 2 == 1)
+        if(max > n / 2)
+            return (max * 2 - n);
+        if(n % 2 == 1)
             return 1;
         return 0;
     }
 
-    // O(n log n)
+    // O(n log n) O(n)
     public int minLengthAfterRemovals_(List<Integer> nums) {
         Map<Integer, Integer> cnt = new HashMap<>();
-        for (int x : nums) {
+        for (int x : nums)
             cnt.merge(x, 1, Integer::sum);
-        }
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int x : cnt.values()) {
+        for (int x : cnt.values())
             pq.offer(x);
-        }
         int ans = nums.size();
         while (pq.size() > 1) {
             int x = pq.poll();
             int y = pq.poll();
             x--;
             y--;
-            if (x > 0) {
+            if (x > 0)
                 pq.offer(x);
-            }
-            if (y > 0) {
+            if (y > 0)
                 pq.offer(y);
-            }
             ans -= 2;
         }
         return ans;
