dc970810eabb95735f01455b0a1ab3a6eaa4e3bf
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-05 16:33:07 -0700

    lc. slidingwindow. MinimumWindowSubstring. O(n) O(n)

--- src/main/java/lc/slidingwindow/h/MinimumWindowSubstring.java
+++ src/main/java/lc/slidingwindow/h/MinimumWindowSubstring.java
@@ -4,41 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MinimumWindowSubstring {
+    // O(n) O(n)
     public String minWindow(String s, String t) {
-        if(s.length() < t.length()){
+        if(s.length() < t.length())
             return "";
-        }
-        // map
-        // *SD - sliding window
-        // K ; V = 1    element present in sliding window
-        // K ; V = 0    element not present in sliding window
-        // K ; V = -1   duplicate of element in  window
-
         Map<Character,Integer> map = new HashMap<>();
         for(int i=0;i<t.length();i++){
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+            char c = t.charAt(i);
+            map.merge(c,1,Integer::sum);
         }
-        int count=0,start=0,min_length = Integer.MAX_VALUE, min_start = 0;
+        int count=0, start=0, min_length = Integer.MAX_VALUE, min_start = 0;
         for(int end=0; end<s.length(); end++){
             if(map.containsKey(s.charAt(end))){
-                if(map.get(s.charAt(end))>0){
+                if(map.get(s.charAt(end))>0)
                     count+=1;
-                }
                 map.put(s.charAt(end), map.get(s.charAt(end))-1);
             }
             if(count == t.length()) {
                 while(start < end && (!map.containsKey(s.charAt(start)) || map.get(s.charAt(start)) < 0)){
-                    if(map.containsKey(s.charAt(start))){
+                    if(map.containsKey(s.charAt(start)))
                         map.put(s.charAt(start), map.get(s.charAt(start))+1);
-                    }
                     start+=1;
                 }
-                if(min_length > end-start+1){
+                if(min_length > end-start+1)
                     min_length = end-(min_start=start)+1;
-                }
-                if(map.containsKey(s.charAt(start))){
+                if(map.containsKey(s.charAt(start)))
                     map.put(s.charAt(start), map.get(s.charAt(start))+1);
-                }
                 count-=1;
                 start+=1;
             }
