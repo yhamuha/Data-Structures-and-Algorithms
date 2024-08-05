878ef4d76dcd5c2c845ec7ff55cc25f9e43388ae
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-05 13:18:30 -0700

    lc. slidingwindow. PermutationInString. O(n) O(n)

--- src/main/java/lc/slidingwindow/m/PermutationInString.java
+++ src/main/java/lc/slidingwindow/m/PermutationInString.java
@@ -4,32 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PermutationInString {
+    // O(n) O(1)
     public boolean checkInclusion(String s1, String s2) {
         Map<Character, Integer> sMap = new HashMap<>();
         for (int i = 0; i < s1.length(); i++) {
             char c = s1.charAt(i);
-            // TC avg O(1), worst O(n)
             sMap.merge(c, 1, Integer::sum);
         }
         Map<Character, Integer> window = new HashMap<>();
         for (int right = 0; right < s2.length(); right++) {
             char c = s2.charAt(right);
-            // Increment the characters in the window
             window.merge(c, 1, Integer::sum);
             if (right >= s1.length()) {
-                // right - s1.length() will give us the position of the left pointer
                 c = s2.charAt(right - s1.length());
-                if (window.get(c) == 1) {
+                if (window.get(c) == 1)
                     window.remove(c);
-                } else {
-                    // Decrement the character in the window
+                else
                     window.merge(c, -1, Integer::sum);
-                }
             }
-            // checking on each iteration if windows map conatins elements of s1
-            if (window.equals(sMap)) {
+            if (window.equals(sMap))
                 return true;
-            }
         }
         return false;
     }
