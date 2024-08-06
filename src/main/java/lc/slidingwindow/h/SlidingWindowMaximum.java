7e2077478e12e36e20301975c99855f89c11786c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-06 07:45:21 -0700

    lc. slidingwindow. SlidingWindowMaximum. O(n) O(n)

--- src/main/java/lc/slidingwindow/h/SlidingWindowMaximum.java
+++ src/main/java/lc/slidingwindow/h/SlidingWindowMaximum.java
@@ -4,30 +4,27 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
-
 public class SlidingWindowMaximum {
+    // O(n) O(n)
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         int[] result = new int[n - k + 1];
         Deque<Integer> window = new ArrayDeque<>();
-
         for (int i = 0; i < n; i++) {
-            while (!window.isEmpty() && window.peekFirst() < i - k + 1) {
+            while (!window.isEmpty() && window.peekFirst() < i - k + 1)
                 window.pollFirst();
-            }
-            while (!window.isEmpty() && nums[window.peekLast()] < nums[i]) {
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[i])
                 window.pollLast();
-            }
             window.offerLast(i);
             if (i >= k - 1)
                 result[i - k + 1] = nums[window.peekFirst()];
-
         }
         return result;
     }
 
     public static void main(String[] args) {
         var swm = new SlidingWindowMaximum();
-        System.out.println(Arrays.toString(swm.maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3)));
+        System.out.println(Arrays.toString(swm.maxSlidingWindow
+                                        (new int[]{1,3,-1,-3,5,3,6,7}, 3)));
     }
 }
