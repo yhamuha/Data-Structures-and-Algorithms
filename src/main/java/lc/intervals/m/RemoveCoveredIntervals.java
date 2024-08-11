68a00a7f82ee565426a01513e32ec2da5c0e135c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-10 17:43:31 -0700

    lc. intervals. RemoveCoveredIntervals. O(n log n) O(1)

--- src/main/java/lc/intervals/m/RemoveCoveredIntervals.java
+++ src/main/java/lc/intervals/m/RemoveCoveredIntervals.java
@@ -5,30 +5,23 @@ import java.util.Arrays;
 public class RemoveCoveredIntervals {
     // O(n log n) O(1)
     public int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, this::compare);
-        int i = 0;
-        int c = 0;
-        while (i < intervals.length) {
-            int min = intervals[i][0];
-            int max = intervals[i][1];
-            while (i < intervals.length - 1 && min <= intervals[i + 1][0] && max >= intervals[i + 1][1]) {
-                c++;
-                i++;
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        int prev = 0;
+        int counter = 0;
+        while (prev < intervals.length) {
+            int min = intervals[prev][0];
+            int max = intervals[prev][1];
+            while (prev < intervals.length - 1 && min <= intervals[prev + 1][0] && max >= intervals[prev + 1][1]) {
+                counter++;
+                prev++;
             }
-            i++;
+            prev++;
         }
-        return intervals.length - c;
-    }
-
-    private int compare(int[] a, int[] b) {
-        if (a[0] == b[0]) {
-            return b[1] - a[1];
-        }
-        return a[0] - b[0];
+        return intervals.length - counter;
     }
 
     public static void main(String[] args) {
-        int[][] intervals = {{1,4},{3,6}, {2,8}};
+        int[][] intervals = {{1,4},{3,6},{2,8}};
         var rci = new RemoveCoveredIntervals();
         System.out.println(rci.removeCoveredIntervals(intervals));
     }
