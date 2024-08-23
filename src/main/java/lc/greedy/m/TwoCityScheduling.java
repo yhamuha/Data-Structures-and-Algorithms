14fd818bbc7c9f4dc67272caa2302dd0883e8d14
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-22 20:39:24 -0700

    lc. greedy. TwoCityScheduling. O(n) O(1)

--- src/main/java/lc/greedy/m/TwoCityScheduling.java
+++ src/main/java/lc/greedy/m/TwoCityScheduling.java
@@ -3,24 +3,20 @@ package lc.greedy.m;
 import java.util.Arrays;
 
 public class TwoCityScheduling {
-    // O (n log n) O(1)
+    // O (n) O(1)
     public int twoCitySchedCost(int[][] costs) {
-        Arrays.sort(costs,
-                (a, b) -> (a[0] - a[1]) - (b[0] - b[1])
-        );
         int total = 0;
         for (int i = 0; i < costs.length; i++) {
-            if (i < costs.length / 2) {
+            if (i < costs.length / 2)
                 total += costs[i][0];
-            } else {
-                total += costs[i][1];
-            }
+            else
+               total += costs[i][1];
         }
         return total;
     }
     public static void main(String[] args) {
         TwoCityScheduling scheduler = new TwoCityScheduling();
-        int[][] costs = {{259, 770}, {448, 54}, {926, 667}, {184, 139}, {840, 118}, {577, 469}};
+        int[][] costs = {{10,20},{30,200},{400,50},{30,20}};
         int minCost = scheduler.twoCitySchedCost(costs);
         System.out.println(minCost);
     }
