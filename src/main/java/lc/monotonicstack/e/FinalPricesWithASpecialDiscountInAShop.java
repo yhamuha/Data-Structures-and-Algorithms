7d683b6d16e4b447c6569e7c73efc80124c28352
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-23 20:17:04 -0700

    lc. monotonicstack. FinalPricesWithASpecialDiscountInAShop. O(n) O(n)

--- src/main/java/lc/monotonicstack/e/FinalPricesWithASpecialDiscountInAShop.java
+++ src/main/java/lc/monotonicstack/e/FinalPricesWithASpecialDiscountInAShop.java
@@ -1,9 +1,10 @@
-package lc.monotonicstack;
+package lc.monotonicstack.e;
 
 import java.util.Arrays;
 import java.util.Stack;
 
-public class FinalPricesWithASpecialDiscount {
+public class FinalPricesWithASpecialDiscountInAShop {
+    // O(n) O(n)
     public int[] finalPrices(int[] prices) {
         Stack<Integer> st=new Stack<>();
         for(int i=prices.length-1; i>=0; i--){
@@ -18,9 +19,8 @@ public class FinalPricesWithASpecialDiscount {
     }
 
     public static void main(String[] args) {
-        // int[] prices = {8,4,6,2,3};
-        int[] prices = {2,4,3,1,5,7};
-        FinalPricesWithASpecialDiscount fpd = new FinalPricesWithASpecialDiscount();
+        int[] prices = {8,4,6,2,3};
+        FinalPricesWithASpecialDiscountInAShop fpd = new FinalPricesWithASpecialDiscountInAShop();
         System.out.println(Arrays.toString(fpd.finalPrices(prices)));
     }
 }
