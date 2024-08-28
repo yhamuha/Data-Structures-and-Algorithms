2604fe3dd003a11a987e968f8cbf7dee9d3010e7
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-27 20:09:03 -0700

    lc. monotonicstack. NextGreaterElementII. O(n) O(n)

--- src/main/java/lc/monotonicstack/m/NextGreaterElementII.java
+++ src/main/java/lc/monotonicstack/m/NextGreaterElementII.java
@@ -1,21 +1,18 @@
-package lc.monotonicstack;
+package lc.monotonicstack.m;
 
 import java.util.Arrays;
 import java.util.Stack;
 
 public class NextGreaterElementII {
-    // cyclic
     // O(n) O(n)
     public int[] nextGreaterElements(int[] nums){
        int n = nums.length;
        int[] output_arr = new int[n];
        Arrays.fill(output_arr, -1);
-
        Stack<Integer> stack = new Stack();
        for (int i=0; i< n*2; i++) {
-           while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n] ) {
+           while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n] )
                output_arr[stack.pop()] = nums[i % n];
-           }
            if (i < n) stack.push(i);
        }
        return output_arr;
