19869599d8735696d21985a4547026d7627f2edb
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-29 21:34:55 -0700

    lc. monotonicstack. StepsToMakeArrayNonDecreasing. O(n) O(n)

--- src/main/java/lc/monotonicstack/m/StepsToMakeArrayNonDecreasing.java
+++ src/main/java/lc/monotonicstack/m/StepsToMakeArrayNonDecreasing.java
@@ -1,23 +1,11 @@
-package lc.monotonicstack;
+package lc.monotonicstack.m;
 
 import javafx.util.Pair;
 
 import java.util.Stack;
 
-/*
-*   Input: nums = [5,3,4,4,7,3,6,11,8,5,11]
-    Output: 3
-    Explanation: The following are the steps performed:
-    - Step 1: [5,3,4,4,7,3,6,11,8,5,11] becomes [5,4,4,7,6,11,11]
-    - Step 2: [5,4,4,7,6,11,11] becomes [5,4,7,11,11]
-    - Step 3: [5,4,7,11,11] becomes [5,7,11,11]
-    [5,7,11,11] is a non-decreasing array. Therefore, we return 3.
-*
-*   In other words, how many steps we need to have non-decreasing array?
-*   Answer: 3
-* */
-
 public class StepsToMakeArrayNonDecreasing {
+    // O(n) O(n)
     public int totalSteps(int[] nums) {
         int size = nums.length;
         Stack<Pair<Integer,Integer>> stack = new Stack();
