abedd221291bec35f7fb5d726d4cf2e3685ba2c9
Yaroslav Hamuha <yhamuha@gmail.com>
2024-08-30 22:13:09 -0700

    lc. monotonicstack. Find132Pattern. O(n) O(n)

--- src/main/java/lc/monotonicstack/m/Find132Pattern.java
+++ src/main/java/lc/monotonicstack/m/Find132Pattern.java
@@ -1,31 +1,14 @@
-package lc.monotonicstack;
+package lc.monotonicstack.m;
 
 import java.util.Stack;
-
-/*
-            3
-         2/  \
-     _1_/         123pattern
-
-           3
-      _1_/   \_2_  132pattern (what we need to find)
-      needs to track this elements sequence
-
-      predefine minimum element
-      going through from right to the left using stack for check on each step
-
- */
-
 // O(n) O(n)
 public class Find132Pattern {
     public boolean find132pattern(int[] nums) {
         int size = nums.length;
         int[] min = new int[size];
         min[0] = nums[0];
-        for (int i = 1; i < size; i++) {
+        for (int i = 1; i < size; i++)
             min[i] = Math.min(min[i-1], nums[i]);
-        }
-
         Stack<Integer> stack = new Stack();
         for (int j = size-1; j>=0; j--) {
             if (nums[j] > min [j]) {
