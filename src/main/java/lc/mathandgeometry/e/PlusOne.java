584231fc33e5fe98fdfbfd411a073978dfd4b909
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-05 07:36:43 -0700

    lc. mathandgeometry. PlusOne. O(n) O(n)

--- src/main/java/lc/mathandgeometry/e/PlusOne.java
+++ src/main/java/lc/mathandgeometry/e/PlusOne.java
@@ -3,12 +3,12 @@ package lc.mathandgeometry.e;
 import java.util.Arrays;
 
 public class PlusOne {
-    // O(n) O(n+1)
+    // O(n) O(n)
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] == 9) {
+            if (digits[i] == 9)
                 digits[i] = 0;
-            } else {
+            else {
                 digits[i]++;
                 return digits;
             }
