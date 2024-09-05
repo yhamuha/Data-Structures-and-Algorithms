187ac274ebf096ebea5037a40696c754d5337dce
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-04 17:50:06 -0700

    lc. mathandgeometry. HappyNumber. O(log n) O(1)

--- src/main/java/lc/mathandgeometry/e/HappyNumber.java
+++ src/main/java/lc/mathandgeometry/e/HappyNumber.java
@@ -4,26 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HappyNumber {
-    // O(log n) O(log n)
+    // O(log n) O(1)
     public boolean isHappy(int n) {
-        Set<Integer> inLoop = new HashSet<Integer>();
-        int squareSum,d;
-        while (inLoop.add(n)) {
+        Set<Integer> inLoop = new HashSet<>();
+        int squareSum, d;
+        while(inLoop.add(n)){
             squareSum = 0;
-            while (n > 0) {
-                d = n%10;
-                squareSum += d*d;
+            while(n > 0){
+                d = n % 10;
+                squareSum = d * d;
                 n /= 10;
             }
-            if (squareSum == 1)
-                return true;
-            else
-                n = squareSum;
+            if (squareSum == 1) return true;
+            else return false;
         }
         return false;
     }
+
     public static void main(String[] args) {
         HappyNumber happyNumber = new HappyNumber();
-        System.out.println(happyNumber.isHappy(19)); // Example usage
+        System.out.println(happyNumber.isHappy(10));
     }
 }
