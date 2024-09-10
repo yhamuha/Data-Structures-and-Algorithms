7d5a3e13b6efc1ae7acae87d693ddb394afba22d
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-09 18:40:24 -0700

    lc. mathandgeometry. PowXN. O(log n) O(1)

--- src/main/java/lc/mathandgeometry/m/PowXN.java
+++ src/main/java/lc/mathandgeometry/m/PowXN.java
@@ -5,9 +5,8 @@ public class PowXN {
     public static double myPow(double x, int n) {
         double ans = 1;
         long n_temp = n;
-        if (n_temp < 0) {
+        if (n_temp < 0)
             n_temp *= -1;
-        }
         while (n_temp > 0) {
             if (n_temp % 2 == 0) {
                 x = x * x;
@@ -17,11 +16,11 @@ public class PowXN {
                 n_temp--;
             }
         }
-        if (n < 0) {
+        if (n < 0)
             return (double) 1 / ans;
-        }
         return ans;
     }
+
     public static void main(String[] args) {
         double x = 2.0;
         int n = 10;
