aa354324e22d54418ffdd04b2cf1dea9cfc0c0ef
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-15 13:25:16 -0700

    lc. bst-dfs. SameTree. O(n), O(n) - unbalanced O(log n) - balanced

--- src/main/java/lc/bst/dfs/e1/SameTree.java
+++ src/main/java/lc/bst/dfs/e1/SameTree.java
@@ -3,23 +3,20 @@ package lc.bst.dfs.e1;
 import lc.bst.TreeNode;
 
 public class SameTree {
-    // O(n) O(n)
+    // O(n), O(n) - unbalanced O(log n) - balanced
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null || q == null) {
-            return (p == q); // true
+            return (p == q);
         }
         return p.val == q.val
                 && isSameTree(p.left, q.left)
                 && isSameTree(p.right, q.right);
     }
+
     public static void main(String[] args) {
         SameTree sameTree = new SameTree();
-        TreeNode p = new TreeNode(1);
-        p.left = new TreeNode(2);
-        p.right = new TreeNode(3);
-        TreeNode q = new TreeNode(1);
-        q.left = new TreeNode(2);
-        q.right = new TreeNode(3);
+        TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         System.out.println(sameTree.isSameTree(p, q));
     }
 }
