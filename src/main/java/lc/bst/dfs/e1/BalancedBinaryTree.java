aeaf0fad0288b62c4796bb711c20fd22881f8aec
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-14 19:23:08 -0700

    lc. bst-dfs. BalancedBinaryTree. O(n), O(n) - unbalanced O(log n) - balanced

--- src/main/java/lc/bst/dfs/e1/BalancedBinaryTree.java
+++ src/main/java/lc/bst/dfs/e1/BalancedBinaryTree.java
@@ -1,15 +1,14 @@
 package lc.bst.dfs.e1;
 
-import lc.bst.BinarySearchTreeFromArray;
 import lc.bst.TreeNode;
 
 public class BalancedBinaryTree {
+    // O(n), O(n) - unbalanced O(log n) - balanced
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
         if (height(root) == -1) return false;
         return true;
     }
-
     private int height(TreeNode root) {
         if (root == null) return 0;
         int lh = height(root.left);
@@ -21,13 +20,10 @@ public class BalancedBinaryTree {
     }
 
     public static void main(String[] args) {
-
         BalancedBinaryTree bbt = new BalancedBinaryTree();
-
-        BinarySearchTreeFromArray searchTreeFromArray = new BinarySearchTreeFromArray();
-        Integer[] inputArray = {3, 9, 20, null, null, 15, 7};
-        TreeNode root = searchTreeFromArray.arrayToBST(inputArray);
-
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20));
+        root.left = new TreeNode(9, null, null);
+        root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
         System.out.println(bbt.isBalanced(root));
     }
 }
