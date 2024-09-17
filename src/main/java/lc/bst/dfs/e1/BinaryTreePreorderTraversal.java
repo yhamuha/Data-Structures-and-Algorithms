e68954bc30fc88b5f0159d45b5e41dbc14c348d0
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-16 17:56:59 -0700

    lc. bst-dfs. BinaryTreePreorderTraversal. O(n), O(n) unbalanced O(log n) balanced

---
+++ src/main/java/lc/bst/dfs/e1/BinaryTreePreorderTraversal.java
@@ -0,0 +1,34 @@
+package lc.bst.dfs.e1;
+
+import lc.bst.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    // O(n), O(log n) balanced O(n) unbalanced/skew
+    public void traversal(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        traversal(root.left, res);
+        traversal(root.right, res);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traversal(root, res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        BinaryTreePreorderTraversal btp = new BinaryTreePreorderTraversal();
+        List<Integer> result = btp.preorderTraversal(root);
+        System.out.println(result);
+    }
+}
+
+
