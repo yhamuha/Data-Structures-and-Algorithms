8439a655f83482aa7c5e1a2952bed511a21d0e34
Yaroslav Hamuha <yhamuha@gmail.com>
2024-09-25 14:04:24 -0700

    lc. bst-dfs. PathSumIII. O(n^2), O(n) unbalanced O(log n) balanced

---
+++ src/main/java/lc/bst/dfs/m1/PathSumIII.java
@@ -0,0 +1,38 @@
+package lc.bst.dfs.m1;
+
+import lc.bst.TreeNode;
+
+public class PathSumIII {
+    // O(n^2); O(n) unbalanced O(log n) balanced
+    int total = 0;
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) return 0;
+        dfs(root, sum, 0);
+        pathSum(root.left, sum);
+        pathSum(root.right, sum);
+        return total;
+    }
+    void dfs(TreeNode root, int sum, long curr) {
+        if(root == null) return;
+        curr += root.val;
+        if(curr == sum) total++;
+        dfs(root.left, sum, curr);
+        dfs(root.right, sum, curr);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(-3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(2);
+        root.left.left.left = new TreeNode(3);
+        root.left.left.right = new TreeNode(-2);
+        root.right.right = new TreeNode(11);
+        root.left.right.right = new TreeNode(1);
+        PathSumIII ps = new PathSumIII();
+        int targetSum = 8;
+        int result = ps.pathSum(root, targetSum);
+        System.out.println(result);
+    }
+}
