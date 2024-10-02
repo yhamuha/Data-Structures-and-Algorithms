ae0e4efb586ff6bc75e014b93b4f94a81ff63639
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-02 16:04:28 -0700

    lc. bst-bfs. BinaryTreeRightSideView. O(n), O(n)

--- src/main/java/lc/bst/bfs/m/BinaryTreeRightSideView.java
+++ src/main/java/lc/bst/bfs/m/BinaryTreeRightSideView.java
@@ -7,13 +7,13 @@ import java.util.List;
 import java.util.Queue;
 
 public class BinaryTreeRightSideView {
+    // O(n) O(n)
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null)
             return result;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
@@ -29,14 +29,11 @@ public class BinaryTreeRightSideView {
         return result;
     }
 
-
     public static void main(String[] args) {
         BinaryTreeRightSideView btrsv = new BinaryTreeRightSideView();
-
         TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         root.left = new TreeNode(2, null, new TreeNode(5));
         root.right = new TreeNode(3, null, new TreeNode (4));
-
         System.out.println(btrsv.rightSideView(root));
     }
 }
