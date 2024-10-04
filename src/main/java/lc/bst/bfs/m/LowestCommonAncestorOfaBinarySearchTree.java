d0e8fdb72f250953ac492bf938b2bb6c547f4531
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-03 19:55:47 -0700

    lc. bst-bfs. LowestCommonAncestorOfaBinarySearchTree. O(n); O(n) unbalanced O(log n) balanced

--- src/main/java/lc/bst/bfs/m/LowestCommonAncestorOfaBinarySearchTree.java
+++ src/main/java/lc/bst/bfs/m/LowestCommonAncestorOfaBinarySearchTree.java
@@ -2,32 +2,39 @@ package lc.bst.bfs.m;
 
 import lc.bst.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LowestCommonAncestorOfaBinarySearchTree {
-    // O(log n)
-    // worst case O(n) if tree unbalanced
+    // O(n); O(n) unbalanced O(log n) balanced
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (p.val < root.val && q.val<root.val)
-            return lowestCommonAncestor(root.left,p,q);
-        if (p.val > root.val && q.val > root.val)
-            return lowestCommonAncestor(root.right,p,q);
-        return root;
+        if (root == null)
+            return null;
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val)
+                root = root.left;
+            else if (p.val > root.val && q.val > root.val)
+                root = root.right;
+            else
+                return root;
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        var lca = new LowestCommonAncestorOfaBinarySearchTree();
-
-        TreeNode root = new TreeNode(6,
-                // root.left
-                new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))),
-                // root.right
-                new TreeNode(8, new TreeNode(7), new TreeNode(9)));
-
-        // Create two nodes to find the lowest common ancestor
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
         TreeNode p = root.left;
-        TreeNode q = root.right;
-
-        // Find the lowest common ancestor
-        TreeNode result = lca.lowestCommonAncestor(root, p, q);
-        System.out.println(result.val);
+        TreeNode q = root.left.right;
+        LowestCommonAncestorOfaBinarySearchTree solution = new LowestCommonAncestorOfaBinarySearchTree();
+        TreeNode lca = solution.lowestCommonAncestor(root, p, q);
+        System.out.println(lca.val);
     }
 }
