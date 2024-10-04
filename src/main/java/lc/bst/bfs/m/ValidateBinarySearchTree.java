9b02cc978c6a265d829d3bf91eaa38b3d32d5b77
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-04 15:45:38 -0700

    lc. bst-bfs. ValidateBinarySearchTree. O(n); O(n) unbalanced O(log n) balanced

--- src/main/java/lc/bst/bfs/m/ValidateBinarySearchTree.java
+++ src/main/java/lc/bst/bfs/m/ValidateBinarySearchTree.java
@@ -3,23 +3,22 @@ package lc.bst.bfs.m;
 import lc.bst.TreeNode;
 
 import java.util.Stack;
-// https://www.youtube.com/watch?v=gGsEVFP0aQo&ab_channel=NickWhite
 public class ValidateBinarySearchTree {
+    // O(n); O(n) unbalanced O(log n) balanced
     public boolean isValidBST(TreeNode root) {
-    Stack<TreeNode> stack = new Stack<>();
-    int left_child_val = - Integer.MAX_VALUE;
-
-    while (!stack.isEmpty() || root != null) {
-        while (root != null) {
-            stack.push(root);
-            root = root.left;
+        Stack<TreeNode> stack = new Stack<>();
+        int left_child_val = - Integer.MAX_VALUE;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= left_child_val) return false;
+            left_child_val = root.val;
+            root = root.right;
         }
-        root = stack.pop();
-        if (root.val <= left_child_val) return false;
-        left_child_val = root.val;
-        root = root.right;
-    }
-    return true;
+        return true;
     }
 
     public static void main(String[] args) {
