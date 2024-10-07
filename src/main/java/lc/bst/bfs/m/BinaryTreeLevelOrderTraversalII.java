434e212e9440a966d29647bcc0e8464feb06c1ed
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-06 19:50:36 -0700

    lc. bst-bfs. BinaryTreeLevelOrderTraversalII. O(n) O(n)

--- src/main/java/lc/bst/bfs/m/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/lc/bst/bfs/m/BinaryTreeLevelOrderTraversalII.java
@@ -5,12 +5,12 @@ import lc.bst.TreeNode;
 import java.util.*;
 
 public class BinaryTreeLevelOrderTraversalII {
+    // O(n) O(n)
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         Queue<TreeNode> nodeQueue = new LinkedList<>();
         Stack<List<Integer>> levels = new Stack<>();
-        if(root != null) {
+        if(root != null)
             nodeQueue.offer(root);
-        }
         while(!nodeQueue.isEmpty()) {
             int qSize = nodeQueue.size();
             List<Integer> level = new ArrayList<>();
@@ -22,21 +22,17 @@ public class BinaryTreeLevelOrderTraversalII {
             }
             levels.push(level);
         }
-
         List<List<Integer>> res = new ArrayList<>();
-        while(!levels.isEmpty()) {
+        while(!levels.isEmpty())
             res.add(levels.pop());
-        }
         return res;
     }
 
     public static void main(String[] args) {
         var btlotII = new BinaryTreeLevelOrderTraversalII();
-
         TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20));
         root.left = new TreeNode(9, null, null);
         root.right = new TreeNode(20, new TreeNode(15), new TreeNode (7));
-
         System.out.println(btlotII.levelOrderBottom(root));
     }
 }
