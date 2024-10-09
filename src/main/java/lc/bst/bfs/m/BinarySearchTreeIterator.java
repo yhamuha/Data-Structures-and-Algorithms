6fa6202975ab6bf24386ad4bce1ccd5f96a2fd3c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-09 10:16:55 -0700

    lc. bst-bfs. BinarySearchTreeIterator. O(n) O(n), peak methods O(1) O(n)

--- src/main/java/lc/bst/bfs/m/BinarySearchTreeIterator.java
+++ src/main/java/lc/bst/bfs/m/BinarySearchTreeIterator.java
@@ -7,41 +7,43 @@ import java.util.List;
 import java.util.Stack;
 
 public class BinarySearchTreeIterator {
-
     List<Integer> list = new ArrayList<>();
     int index = 0;
-
-    public BinarySearchTreeIterator(TreeNode root) {
+    public BinarySearchTreeIterator(TreeNode root){
         inorder(root);
     }
+    // O(1) O(n)
+    public int next(){
+        return list.get(index++);
+    }
+    // O(1) O(n)
+    public boolean hasNext(){
+        return index<list.size();
+    }
+    // O(n) O(n)
     private void inorder(TreeNode root) {
         if (root == null) return;
         inorder(root.left);
         list.add(root.val);
         inorder(root.right);
     }
-    public int next() {
-        return list.get(index++);
-    }
-    public boolean hasNext() {
-        return index<list.size();
-    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         TreeNode root = new TreeNode(7, new TreeNode(3), new TreeNode(15));
         root.left = new TreeNode(3, null, null);
         root.right = new TreeNode(15, new TreeNode(9), new TreeNode (20));
-
         BinarySearchTreeIterator obj = new BinarySearchTreeIterator(root);
-
         List<Object> output = new ArrayList<>();
         output.add(null);
-        while (obj.hasNext()) {
-            output.add(obj.next());
-            output.add(obj.hasNext());
-        }
-
+        output.add(obj.next());
+        output.add(obj.next());
+        output.add(obj.hasNext());
+        output.add(obj.next());
+        output.add(obj.hasNext());
+        output.add(obj.next());
+        output.add(obj.hasNext());
+        output.add(obj.next());
+        output.add(obj.hasNext());
         System.out.println(output);
     }
-
 }
