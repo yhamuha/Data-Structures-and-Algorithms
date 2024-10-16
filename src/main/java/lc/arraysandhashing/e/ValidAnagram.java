83bdd821e6cb3557e130ab7d29973b5f717550c5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-16 13:20:41 -0700

    lc. arraysandhashing. ValidAnagram. O(n) O(1)

--- src/main/java/lc/arraysandhashing/e/ValidAnagram.java
+++ src/main/java/lc/arraysandhashing/e/ValidAnagram.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class ValidAnagram {
     // O(n log n) O(n)
     public static boolean isAnagramBySorting (String str1, String str2) {
-        if (str1.length() != str2.length()) return false;   // diff length -> false
+        if (str1.length() != str2.length()) return false;
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
-        Arrays.sort(arr1); // java quicksort O(n log n)
+        Arrays.sort(arr1);
         Arrays.sort(arr2);
         return Arrays.equals(arr1,arr2);
     }
@@ -17,14 +17,12 @@ public class ValidAnagram {
     public static boolean isAnagram (String str1, String str2) {
         if (str1.length() != str2.length()) return false;
         int[] characters = new int[26];
-
-        for(int i=0; i<str1.length(); i++){
+        for (int i=0; i<str1.length(); i++){
             characters[str1.charAt(i)-'a']++;
             characters[str2.charAt(i)-'a']--;
         }
-        for(int j=0; j<characters.length; j++){
+        for (int j=0; j<characters.length; j++)
             if (characters[j] !=0) return false;
-        }
         return true;
     }
 
@@ -34,3 +32,4 @@ public class ValidAnagram {
         System.out.println(isAnagram(str1, str2));
     }
 }
+
