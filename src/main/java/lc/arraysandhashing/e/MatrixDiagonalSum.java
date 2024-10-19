81b185185013718f70f97e1bb4be82c585501098
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-18 20:30:16 -0700

    lc. arraysandhashing. MatrixDiagonalSum. O(n) O(1)

---
+++ src/main/java/lc/arraysandhashing/e/MatrixDiagonalSum.java
@@ -0,0 +1,26 @@
+package lc.arraysandhashing.e;
+
+public class MatrixDiagonalSum {
+    // O(n) O(1)
+    public int diagonalSum(int[][] mat) {
+        int j =mat.length-1;
+        int sum =0;
+        for(int i =0 ;i<mat.length;i++){
+            sum+= mat[i][i];
+            if(!(i==j-i)){
+                sum+= mat[i][j-i];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        MatrixDiagonalSum solution = new MatrixDiagonalSum();
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println(solution.diagonalSum(mat));
+    }
+}
