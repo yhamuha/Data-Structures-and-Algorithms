6e95f984ab77dc27ee8fd71fdf173f83e295578d
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-18 17:09:16 -0700

    lc. arraysandhashing. RunningSumOf1dArray. O(n) O(1)

---
+++ src/main/java/lc/arraysandhashing/e/RunningSumOf1dArray.java
@@ -0,0 +1,19 @@
+package lc.arraysandhashing.e;
+
+import java.util.Arrays;
+
+public class RunningSumOf1dArray {
+    // O(n), O(1)
+    static int[] getRunningSum (int[] arr) {
+
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i+1] = arr[i] + arr[i+1];
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        System.out.println(Arrays.toString(getRunningSum(arr)));
+    }
+}
