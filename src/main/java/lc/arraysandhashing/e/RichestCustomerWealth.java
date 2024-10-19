710c428a6ca7944d932d8a5b51f6e6afb796ebf7
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-18 19:35:52 -0700

    lc. arraysandhashing. RichestCustomerWealth. O(n*m) O(1)

---
+++ src/main/java/lc/arraysandhashing/e/RichestCustomerWealth.java
@@ -0,0 +1,25 @@
+package lc.arraysandhashing.e;
+
+public class RichestCustomerWealth {
+    // O(n*m) O(1)
+    public int maximumWealth(int[][] accounts) {
+        int max_wealth=0;
+        for(int i=0; i<accounts.length; i++){
+            int customer_amount=0;
+            for(int j=0; j<accounts[i].length; j++)
+                customer_amount += accounts[i][j];
+            if(customer_amount > max_wealth)
+                max_wealth = customer_amount;
+        }
+        return max_wealth;
+    }
+
+    public static void main(String[] args) {
+        RichestCustomerWealth solution = new RichestCustomerWealth();
+        int[][] accounts = {
+                {1, 2, 3},
+                {3, 2, 1},
+        };
+        System.out.println(solution.maximumWealth(accounts));
+    }
+}
