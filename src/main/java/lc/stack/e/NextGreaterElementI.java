f19742ed44fb7a20582ea8d585279ccceb7c3e6b
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-21 20:34:14 -0700

    lc. stack. NextGreaterElementI. O(n) O(n)

--- src/main/java/lc/stack/e/NextGreaterElementI.java
+++ src/main/java/lc/stack/e/NextGreaterElementI.java
@@ -8,16 +8,15 @@ import java.util.Stack;
 public class NextGreaterElementI {
     // O(n) O(n)
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> next_greater = new HashMap<>();
+        Map<Integer,Integer> next_greater = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         for(Integer num : nums2) {
-            while (!stack.isEmpty() && stack.peek()<num) {
+            while(!stack.isEmpty() && stack.peek() < num)
                 next_greater.put(stack.pop(),num);
-            }
             stack.push(num);
         }
         for(int i=0; i<nums1.length;i++)
-            nums1[i] = next_greater.getOrDefault(nums1[i], -1);
+            nums1[i]=next_greater.getOrDefault(nums1[i],-1);
         return nums1;
     }
 
