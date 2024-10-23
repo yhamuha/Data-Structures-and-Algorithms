9cd255892033058cb70cbe2a3d461a1210bf02b1
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-23 00:51:16 -0700

    lc. stack. ValidPalindrome. O(n) O(n)

---
+++ src/main/java/lc/stack/e/ValidPalindrome.java
@@ -0,0 +1,36 @@
+package lc.stack.e;
+
+import java.util.Stack;
+
+public class ValidPalindrome {
+    // O(n) O(n)
+    public boolean isPalindrome(String s) {
+        char[] chars = s.toLowerCase().toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : chars) {
+            if (Character.isLetterOrDigit(c))
+                stack.push(c);
+        }
+        for(char c : chars) {
+            if(Character.isLetterOrDigit(c)) {
+                if (c != stack.pop())
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        String test1 = "A man, a plan, a canal: Panama";
+        String test2 = "race a car";
+        String test3 = " ";
+        String test4 = "No 'x' in Nixon";
+        String test5 = "0P";
+        System.out.println(vp.isPalindrome(test1));
+        System.out.println(vp.isPalindrome(test2));
+        System.out.println(vp.isPalindrome(test3));
+        System.out.println(vp.isPalindrome(test4));
+        System.out.println(vp.isPalindrome(test5));
+    }
+}
