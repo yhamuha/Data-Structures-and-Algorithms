96776a818295972a5b71bf4ae9d4d0c329e9d5e2
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-23 00:23:54 -0700

    lc. stack. MakeTheStringGreat. O(n) O(n)

---
+++ src/main/java/lc/stack/e/MakeTheStringGreat.java
@@ -0,0 +1,39 @@
+package lc.stack.e;
+
+import java.util.Stack;
+
+public class MakeTheStringGreat {
+    // O(n) O(n)
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (stack.isEmpty())
+                stack.push(ch);
+            else {
+                if ((Character.isUpperCase(stack.peek()) != Character.isUpperCase(ch))
+                        && (Math.abs(stack.peek() - ch) == 32))
+                    stack.pop();
+                else
+                    stack.push(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while (!stack.isEmpty())
+            ans.append(stack.pop());
+        return ans.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        MakeTheStringGreat mg = new MakeTheStringGreat();
+        String test1 = "leEeetcode";
+        String test2 = "abBAcC";
+        String test3 = "s";
+        String test4 = "dDzeE";
+        String test5 = "Pp";
+        System.out.println(mg.makeGood(test1));
+        System.out.println(mg.makeGood(test2));
+        System.out.println(mg.makeGood(test3));
+        System.out.println(mg.makeGood(test4));
+        System.out.println(mg.makeGood(test5));
+    }
+}
