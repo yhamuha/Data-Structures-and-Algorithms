7e8ca65fb62d3e26c7a2c55c8e6cd909ea161408
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-23 20:36:18 -0700

    lc. arraysandhashing. TopKFrequentElements. O(n) O(n)

--- src/main/java/lc/arraysandhashing/m/TopKFrequentElements.java
+++ src/main/java/lc/arraysandhashing/m/TopKFrequentElements.java
@@ -3,29 +3,6 @@ package lc.arraysandhashing.m;
 import java.util.*;
 
 public class TopKFrequentElements {
-    // O(n log n) O(n)
-    public int[] topKFrequent_PQ(int[] nums, int k) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : nums) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-        // O(n log n)
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
-                (a, b) -> a.getValue() - b.getValue()
-        );
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            minHeap.offer(entry);
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            }
-        }
-        int[] result = new int[k];
-        int index = 0;
-        while (!minHeap.isEmpty()) {
-            result[index++] = minHeap.poll().getKey();
-        }
-        return result;
-    }
     // O(n) O(n)
     public int[] topKFrequent(int[] nums, int k) {
         List<Integer>[] bucket = new List[nums.length + 1];
@@ -41,7 +18,6 @@ public class TopKFrequentElements {
         int[] res = new int[k];
         int counter = 0;
         int arrSize = 0;
-
         for (int pos = bucket.length - 1; pos >= 0 && counter < k; pos--) {
             if (bucket[pos] != null ) {
                 for (Integer integer : bucket[pos]) {
@@ -56,10 +32,9 @@ public class TopKFrequentElements {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1,2,2,3,3,4};
+        int[] nums = {1,1,1,2,2,3};
         int k = 2;
         TopKFrequentElements tkfe = new TopKFrequentElements();
         System.out.println(Arrays.toString(tkfe.topKFrequent(nums, k)));
-        // System.out.println(Arrays.toString(tkfe.topKFrequent_PQ(nums, k))); // O(n log n)
     }
 }
