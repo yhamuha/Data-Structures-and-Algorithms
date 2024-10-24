57cdd2b96e63a4c306b484fa07d87c3382aceb2e
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-23 18:31:21 -0700

    lc. stack. BinaryStringWithSubstringsRepresenting1ToN. O(n) O(n)

---
+++ src/main/java/lc/stack/m/BinaryStringWithSubstringsRepresenting1ToN.java
@@ -0,0 +1,27 @@
+package lc.stack.m;
+
+import java.util.Stack;
+
+public class BinaryStringWithSubstringsRepresenting1ToN {
+    // O(n) O(n)
+    public boolean containsAllBinaryRepresentations(String binaryString, int n) {
+        Stack<String> stack = new Stack<>();
+        for (int i = 1; i <= n; i++) {
+            String binaryRepresentation = Integer.toBinaryString(i);
+            stack.push(binaryRepresentation);
+        }
+        while(!stack.isEmpty()) {
+        String substr = stack.pop();
+        if (!binaryString.contains(substr))
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "0110";
+        int n = 3;
+        BinaryStringWithSubstringsRepresenting1ToN solution = new BinaryStringWithSubstringsRepresenting1ToN();
+        System.out.println(solution.containsAllBinaryRepresentations(s, n));
+    }
+}
