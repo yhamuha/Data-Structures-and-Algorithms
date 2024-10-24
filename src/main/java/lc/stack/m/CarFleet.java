5846ee5435f3ad139d2d68dc7bae04e81675dd28
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-24 16:58:26 -0700

    lc. stack. CarFleet. O(n log n) O(n)

--- src/main/java/lc/stack/m/CarFleet.java
+++ src/main/java/lc/stack/m/CarFleet.java
@@ -1,4 +1,30 @@
 package lc.stack.m;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class CarFleet {
+    // O(n log n) O(n)
+    public int carFleet(int target, int[] pos, int[] speed) {
+        Map<Integer, Double> m = new TreeMap<>(Collections.reverseOrder());
+        for (int i = 0; i < pos.length; ++i)
+            m.put(pos[i], (double)(target - pos[i]) / speed[i]);
+        int res = 0; double cur = 0;
+        for (double time : m.values()) {
+            if (time > cur) {
+                cur = time;
+                res++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        CarFleet carFleetCalculator = new CarFleet();
+        int target =12;
+        int[] pos = {10,8,0,5,3};
+        int[] speed = {2,4,1,1,3};
+        System.out.println(carFleetCalculator.carFleet(target, pos, speed));
+    }
 }
+
