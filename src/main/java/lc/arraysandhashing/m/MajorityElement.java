4a977b86cffeb8e646109b80602b5f5d795c70dd
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-25 21:58:56 -0700

    lc. arraysandhashing. MajorityElement. O(n) O(1)

--- src/main/java/lc/arraysandhashing/m/MajorityElement.java
+++ src/main/java/lc/arraysandhashing/m/MajorityElement.java
@@ -1,7 +1,7 @@
-package lc.sorting;
+package lc.arraysandhashing.m;
 
 public class MajorityElement {
-    // O(n) O(1)
+    // O(n) O(1) Boyer-Moore Voting
     public int majorityElement(int[] nums) {
         int res=0, count=0;
         for(int num : nums) {
@@ -10,12 +10,11 @@ public class MajorityElement {
             count += (res == num?1:-1);
         }
         return res;
-
     }
 
     public static void main(String[] args) {
         var me = new MajorityElement();
-        int[] nums= {4,4,3,2,3};
+        int[] nums= {3,2,3};
         System.out.println(me.majorityElement(nums));
     }
 }
