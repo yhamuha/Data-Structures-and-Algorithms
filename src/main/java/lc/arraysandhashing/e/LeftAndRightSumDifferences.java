ea91a4d07a07ac1aef4e3d4f6a0cd3e0745070bd
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-27 15:05:42 -0700

    lc. arraysandhashing. LeftAndRightSumDifferences. O(n) O(n)

--- src/main/java/lc/arraysandhashing/e/LeftAndRightSumDifferences.java
+++ src/main/java/lc/arraysandhashing/e/LeftAndRightSumDifferences.java
@@ -9,16 +9,13 @@ public class LeftAndRightSumDifferences {
         int[] right = new int[nums.length];
         int[] result = new int[nums.length];
         left[0] = 0;
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++)
             left[i] = left[i - 1] + nums[i - 1];
-        }
         right[nums.length - 1] = 0;
-        for (int i = nums.length - 2; i >= 0; i--) {
+        for (int i = nums.length - 2; i >= 0; i--)
             right[i] = right[i + 1] + nums[i + 1];
-        }
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++)
             result[i] = Math.abs(left[i] - right[i]);
-        }
         return result;
     }
 
