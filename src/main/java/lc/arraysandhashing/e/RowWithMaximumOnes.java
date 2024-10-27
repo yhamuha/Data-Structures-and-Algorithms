c76351ba46cb891015882c50cfa0d2ea7cfb151b
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-27 16:29:27 -0700

    lc. arraysandhashing. RowWithMaximumOnes. O(n*m) O(1)

--- src/main/java/lc/arraysandhashing/e/RowWithMaximumOnes.java
+++ src/main/java/lc/arraysandhashing/e/RowWithMaximumOnes.java
@@ -10,9 +10,8 @@ public class RowWithMaximumOnes {
     for (int i = 0; i < mat.length; i++) {
         int count = 0;
         for (int j = 0; j < mat[i].length; j++) {
-            if (mat[i][j] == 1) {
+            if (mat[i][j] == 1)
                 count++;
-            }
         }
         if (count > max) {
             max = count;
