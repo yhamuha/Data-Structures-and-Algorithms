d00a76307d01f39e7ac2121e33d13e801151019c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-27 16:58:20 -0700

    lc. stack. ValidParentheses. O(n) O(n)

--- src/main/java/lc/stack/e/ValidParentheses.java
+++ src/main/java/lc/stack/e/ValidParentheses.java
@@ -10,15 +10,14 @@ public class ValidParentheses {
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[')
                 stack.push(c);
-            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+              else if (c == ')' && !stack.isEmpty() && stack.peek() == '(')
                 stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+              else if (c == '}' && !stack.isEmpty() && stack.peek() == '{')
                 stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+              else if (c == ']' && !stack.isEmpty() && stack.peek() == '[')
                 stack.pop();
-            } else {
+              else
                 return false;
-            }
         }
         return stack.isEmpty();
     }
