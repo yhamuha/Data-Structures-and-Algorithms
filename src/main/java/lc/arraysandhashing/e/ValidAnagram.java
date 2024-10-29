a6cbdbc231cee52065f969c51fd817f57bfa4714
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-28 18:49:03 -0700

    lc. arraysandhashing. ValidAnagram. O(n) O(n)

--- src/main/java/lc/arraysandhashing/e/ValidAnagram.java
+++ src/main/java/lc/arraysandhashing/e/ValidAnagram.java
@@ -1,35 +1,52 @@
 package lc.arraysandhashing.e;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidAnagram {
     // O(n log n) O(n)
-    public static boolean isAnagramBySorting (String str1, String str2) {
-        if (str1.length() != str2.length()) return false;
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
+    public static boolean isAnagramBySorting (String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         return Arrays.equals(arr1,arr2);
     }
-
     // O(n) O(1)
-    public static boolean isAnagram (String str1, String str2) {
-        if (str1.length() != str2.length()) return false;
+    public static boolean isAnagram (String s, String t) {
+        if (s.length() != t.length()) return false;
         int[] characters = new int[26];
-        for (int i=0; i<str1.length(); i++){
-            characters[str1.charAt(i)-'a']++;
-            characters[str2.charAt(i)-'a']--;
+        for (int i=0; i<s.length(); i++){
+            characters[s.charAt(i)-'a']++;
+            characters[t.charAt(i)-'a']--;
         }
         for (int j=0; j<characters.length; j++)
             if (characters[j] !=0) return false;
         return true;
     }
+    // O(n) O(n)
+    public static boolean isAnagram_hash (String s, String t) {
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = s.toCharArray();
+        for(char ch : chars)
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        for(char ch : t.toCharArray()) {
+            if (map.containsKey(ch))
+                map.put(ch,map.get(ch)-1);
+        }
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if (entry.getValue()!=0) return false;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-        String str1 = "str";
-        String str2 = "rst";
-        System.out.println(isAnagram(str1, str2));
+        String s = "str";
+        String t = "rst";
+        System.out.println(isAnagram_hash(s, t));
     }
 }
 
