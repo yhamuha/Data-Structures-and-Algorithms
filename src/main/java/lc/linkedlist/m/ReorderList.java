25bf2ec959a935e1db14f7e878c0f5e9c4e8a614
Yaroslav Hamuha <yhamuha@gmail.com>
2024-10-31 10:13:00 -0700

    lc. linkedlist. ReorderList. O(n) O(1)

--- src/main/java/lc/linkedlist/m/ReorderList.java
+++ src/main/java/lc/linkedlist/m/ReorderList.java
@@ -3,7 +3,7 @@ package lc.linkedlist.m;
 public class ReorderList {
     // O(n) O(1)
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null)return ;
+        if(head==null || head.next==null) return;
         ListNode slow = head;
         ListNode fast= head;
         while(fast!=null && fast.next!=null){
@@ -37,12 +37,12 @@ public class ReorderList {
     }
     public static void main(String[] args) {
         var rl = new ReorderList();
-        ListNode list1 = new ListNode(1);       // node instance's
-        list1.next = new ListNode(3);
-        list1.next.next = new ListNode(5);
-        list1.next.next.next = new ListNode(8);
-        rl.reorderList(list1);                      // reorder
-        while(list1 != null) {                      // traversing
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(3);
+        list1.next.next.next = new ListNode(4);
+        rl.reorderList(list1);
+        while(list1 != null) {
             System.out.print(list1.val + " ");
             list1 = list1.next;
         }
