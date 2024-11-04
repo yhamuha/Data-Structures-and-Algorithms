7b58bd1deff414636826c3d4ad2fde470cb93522
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-03 20:12:18 -0800

    lc. linkedlist. IntersectionOfTwoLinkedLists. O(n+m) O(1)

---
+++ src/main/java/lc/linkedlist/e/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,31 @@
+package lc.linkedlist.e;
+
+public class IntersectionOfTwoLinkedLists {
+    // O(n+m) O(1)
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null? headB : a.next;
+            b = b == null? headA : b.next;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        ListNode intersection = new ListNode(8);
+        intersection.next = new ListNode(4);
+        intersection.next.next = new ListNode(5);
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = intersection;
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = intersection;
+        IntersectionOfTwoLinkedLists solution = new IntersectionOfTwoLinkedLists();
+        ListNode result = solution.getIntersectionNode(headA, headB);
+        System.out.println(result != null ? result.val : "No intersection");
+    }
+}
