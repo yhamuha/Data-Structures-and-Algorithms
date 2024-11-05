02116292265f53de008365129bb77aff612646f5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-04 21:15:12 -0800

    lc. trie. RemoveSubFoldersFromTheFilesystem. O(n*m) O(n*m)

--- src/main/java/lc/trie/m/RemoveSubFoldersFromTheFilesystem.java
+++ src/main/java/lc/trie/m/RemoveSubFoldersFromTheFilesystem.java
@@ -3,8 +3,8 @@ package lc.trie.m;
 import java.util.ArrayList;
 import java.util.List;
 
-// O(n); n - length of the word being inserted
 class RemoveSubFoldersFromTheFilesystem {
+    //O(n*m) O(n*m)
     class TrieNode {
         char data;
         TrieNode children[];
@@ -14,16 +14,13 @@ class RemoveSubFoldersFromTheFilesystem {
             children = new TrieNode[27];
         }
     }
-
     private TrieNode root;
     public RemoveSubFoldersFromTheFilesystem () {
         root = new TrieNode('*');
     }
-
     public void insertWord (String word) {
         insertWordUtil (this.root , word);
     }
-
     private void insertWordUtil (TrieNode Root , String word) {
         if (word.length() == 0) {
             Root.isTerminal = true;
@@ -40,13 +37,11 @@ class RemoveSubFoldersFromTheFilesystem {
         }
         insertWordUtil (child , word.substring(1));
     }
-
-    public List<String> getRootFolders () {
+    public List<String> getRootFolders () { // O(n*m)
         List<String> ans = new ArrayList<>();
         getRootFoldersUtil (this.root , ans , "");
         return ans;
     }
-
     private void getRootFoldersUtil (TrieNode Root , List<String> ans , String path) {
         if (Root.isTerminal) {
             ans.add (path);
@@ -57,28 +52,23 @@ class RemoveSubFoldersFromTheFilesystem {
         }
         for (int i = 0 ; i < 27 ; i++) {
             char ch = i == 26 ? '/' : (char)(i + 97);
-            if (Root.children[i] != null) {
+            if (Root.children[i] != null)
                 getRootFoldersUtil (Root.children[i] , ans , path + ch);
-            } else {
+             else
                 continue;
-            }
         }
     }
-
     private void completePath (TrieNode Root , String path , List<String> ans) {
-        if (Root.isTerminal) {
+        if (Root.isTerminal)
             ans.add(path);
-        }
         for (int i = 0; i < 27; i++) {
-            if (i == 26) {
+            if (i == 26)
                 return;
-            }
             if (Root.children[i] != null) {
                 char ch = (char) (i + 97);
-                completePath(Root.children[i], path + ch, ans);
-            } else {
+                completePath(Root.children[i], path + ch, ans); // O(n*m)
+            } else
                 continue;
-            }
         }
     }
 }
@@ -86,16 +76,16 @@ class RemoveSubFoldersFromTheFilesystem {
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
         var rsfftf  = new RemoveSubFoldersFromTheFilesystem ();
-        for (String folders : folder) {
+        for (String folders : folder)  // O(n*m)
             rsfftf.insertWord (folders);
-        }
         List<String> rootFolders = rsfftf.getRootFolders();
         return rootFolders;
     }
-
     public static void main(String[] args) {
         var solution = new Solution();
         String[] folders = {"/a","/a/b","/c/d","/c/d/e","/c/f"};
         System.out.println(solution.removeSubfolders(folders));
     }
 }
+
+
