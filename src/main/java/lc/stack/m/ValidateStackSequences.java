e4924bf772931d3782e07c10bd507a28e00c355a
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-08 12:26:42 -0800

    lc. stack. ValidateStackSequences. O(n) O(n)

--- src/main/java/lc/stack/m/ValidateStackSequences.java
+++ src/main/java/lc/stack/m/ValidateStackSequences.java
@@ -19,7 +19,7 @@ public class ValidateStackSequences {
 
     public static void main(String[] args) {
         int[] pushed = {1,2,3,4,5};
-        int[] popped = {4,3,5,1,2};
+        int[] popped = {4,5,3,2,1};
         ValidateStackSequences vss = new ValidateStackSequences();
         System.out.println(vss.validateStackSequences(pushed, popped));
     }
