7925300bcaed03a6ed10fa3e244ad3894194b29c
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-09 13:40:10 -0800

    lc. stack. ValidParentheses. O(n) O(n)

--- src/main/java/lc/stack/e/ValidParentheses.java
+++ src/main/java/lc/stack/e/ValidParentheses.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class ValidParentheses {
     // O(n) O(n)
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
+        /*if (s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[')
@@ -17,6 +17,21 @@ public class ValidParentheses {
               else if (c == ']' && !stack.isEmpty() && stack.peek() == '[')
                 stack.pop();
               else
+                return false;
+        }
+        return stack.isEmpty();*/
+        if (s.length() %2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[')
+                stack.push(c);
+            else if (c == '}' && !stack.isEmpty() && stack.peek() == '{')
+                stack.pop();
+            else if (c == ')' && !stack.isEmpty() && stack.peek() == '(')
+                stack.pop();
+            else if (c == ']' && !stack.isEmpty() && stack.peek() == '[')
+                stack.pop();
+            else
                 return false;
         }
         return stack.isEmpty();
