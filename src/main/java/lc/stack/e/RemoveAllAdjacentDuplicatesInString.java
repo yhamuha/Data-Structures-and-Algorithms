98b598081097436af9099d82b06fe39c246afbe5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-10 14:58:17 -0800

    lc. stack. RemoveAllAdjacentDuplicatesInString. O(n) O(n)

--- src/main/java/lc/stack/e/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/lc/stack/e/RemoveAllAdjacentDuplicatesInString.java
@@ -5,31 +5,23 @@ import java.util.Stack;
 public class RemoveAllAdjacentDuplicatesInString {
     // O(n) O(n)
     public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (!st.isEmpty() && st.peek() == s.charAt(i))
-                st.pop();
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i))
+                stack.pop();
             else
-                st.push(s.charAt(i));
+                stack.push(s.charAt(i));
         }
         StringBuilder sb = new StringBuilder("");
-        while (!st.isEmpty())
-            sb.append(st.pop());
+        while (!stack.isEmpty())
+            sb.append(stack.pop());
         return sb.reverse().toString();
     }
 
     public static void main(String[] args) {
         RemoveAllAdjacentDuplicatesInString remover = new RemoveAllAdjacentDuplicatesInString();
         String test1 = "abbaca";
-        String test2 = "azxxzy";
-        String test3 = "aaaaaa";
-        String test4 = "a";
-        String test5 = "";
         System.out.println(remover.removeDuplicates(test1));
-        System.out.println(remover.removeDuplicates(test2));
-        System.out.println(remover.removeDuplicates(test3));
-        System.out.println(remover.removeDuplicates(test4));
-        System.out.println(remover.removeDuplicates(test5));
     }
 }
 
