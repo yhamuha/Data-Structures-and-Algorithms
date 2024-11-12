69af9b64b4cf41ac3698cfbf72f602ecb825c6c9
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-11 16:39:05 -0800

    lc. slidingwindow. FindAllAnagramsInAString. O(n) O(1)

---
+++ src/main/java/lc/slidingwindow/m/FindAllAnagramsInAString.java
@@ -0,0 +1,37 @@
+package lc.slidingwindow.m;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagramsInAString {
+    // O(n) O(1)
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        if (p.length() > s.length())
+            return ans;
+        int[] pFreq = new int[26];
+        int[] sFreq = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            pFreq[p.charAt(i) - 'a']++;
+            sFreq[s.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(pFreq, sFreq))
+            ans.add(0);
+        int i = 0, j = p.length();
+        while (j < s.length()) {
+            sFreq[s.charAt(i++) - 'a']--;
+            sFreq[s.charAt(j++) - 'a']++;
+            if (Arrays.equals(sFreq, pFreq))
+                ans.add(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        FindAllAnagramsInAString finder = new FindAllAnagramsInAString();
+        String s = "cbaebabacd";
+        String p = "abc";
+        System.out.println(finder.findAnagrams(s, p));
+    }
+}
