fa760537b20ef40a1b958158b481a73a15d92029
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-14 22:34:46 -0800

    lc. bst.dfs. DiameterOfBinaryTree. O(n), O(n)-skewed O(log n) - balanced

--- src/main/java/lc/bst/dfs/e/DiameterOfBinaryTree.java
+++ src/main/java/lc/bst/dfs/e/DiameterOfBinaryTree.java
@@ -4,12 +4,12 @@ import lc.bst.TreeNode;
 
 public class DiameterOfBinaryTree {
     // O(n), O(n)-skewed O(log n) - balanced
-    public int diameterOfBinaryTree(TreeNode root) {
+    static int diameterOfBinaryTree(TreeNode root) {
         int[] ans = new int[1];
         height(root, ans);
         return ans[0];
     }
-    private int height(TreeNode root, int[] ans) {
+    private static int height(TreeNode root, int[] ans) {
         if (root == null)
             return 0;
         int lh = height(root.left, ans);
@@ -22,8 +22,6 @@ public class DiameterOfBinaryTree {
         TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
         root.left = new TreeNode(2, new TreeNode(4), new TreeNode(5));
         root.right = new TreeNode(3, null, null);
-        DiameterOfBinaryTree dobt = new DiameterOfBinaryTree();
-        int diameter = dobt.diameterOfBinaryTree(root);
-        System.out.println(diameter);
+        System.out.println(diameterOfBinaryTree(root));
     }
 }
