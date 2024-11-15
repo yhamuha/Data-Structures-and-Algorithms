92fe1332381e3afa4d0a002e3b3783a360cabb97
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-14 21:51:02 -0800

    lc. linkedlist. ReverseLinkedListII. O(n) O(1)

---
+++ src/main/java/lc/linkedlist/m/ReverseLinkedListII.java
@@ -0,0 +1,38 @@
+package lc.linkedlist.m;
+
+public class ReverseLinkedListII {
+    // O(n) O(1)
+    static ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right) return head;
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++)
+            prev = prev.next;
+        ListNode reverseStart = prev.next;
+        ListNode current = reverseStart.next;
+        for (int i = 0; i < right - left; i++) {
+            reverseStart.next = current.next;
+            current.next = prev.next;
+            prev.next = current;
+            current = reverseStart.next;
+        }
+        return dummy.next;
+    }
+    private static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        printList(reverseBetween(head, 2, 4));
+    }
+}
