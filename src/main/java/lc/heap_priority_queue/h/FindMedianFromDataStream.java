ac2af5641a04257777305befc2923beafdfca732
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-21 18:12:48 -0800

    lc. heap_priority_queue. FindMedianFromDataStream. O(n log n) O(n)

--- src/main/java/lc/heap_priority_queue/h/FindMedianFromDataStream.java
+++ src/main/java/lc/heap_priority_queue/h/FindMedianFromDataStream.java
@@ -1,37 +1,32 @@
-package lc.heap_priority_queue;
+package lc.heap_priority_queue.h;
 
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-// in case of using sort: n^2 or n log n(merge);
-// heap - log n;   <-
-
-// TC: O(log n); SC: O(n)
 public class FindMedianFromDataStream {
+    // O(n log n) O(n)
     Queue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b-a);
     Queue<Integer> minHeap = new PriorityQueue<>();
     int size = 0;
     public FindMedianFromDataStream(){}
-
-    public void addNum (int num){
+    public void addNum (int num) {
         size++;
         if(maxHeap.isEmpty() || num <= maxHeap.peek())
             maxHeap.add(num);
-        else{
+        else
             minHeap.add(num);
-        }
-        if (minHeap.size() + 1 < maxHeap.size()){       // balancing
+        if (minHeap.size() + 1 < maxHeap.size()) {
             Integer element = maxHeap.poll();
             minHeap.add(element);
-        } else if (maxHeap.size() < minHeap.size()){
+        } else if (maxHeap.size() < minHeap.size()) {
             Integer element = minHeap.poll();
             maxHeap.add(element);
         }
     }
-    public double findMedian(){
-            if(size%2 != 0)                             // odd
+    public double findMedian() {
+            if(size%2 != 0)
                 return (double) maxHeap.peek();
-        return (maxHeap.peek() + minHeap.peek()) / 2.0; // even
+        return (maxHeap.peek() + minHeap.peek()) / 2.0;
     }
 
     public static void main(String[] args) {
