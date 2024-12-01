6e864b39d77e9dc2c39247e8ac82387df065d3b9
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-30 20:35:41 -0800

    lc. binarysearch. TimeBasedKeyValueStore. set/get: O(1) O(log n); O(n)

--- src/main/java/lc/binarysearch/m/TimeBasedKeyValueStore.java
+++ src/main/java/lc/binarysearch/m/TimeBasedKeyValueStore.java
@@ -6,48 +6,41 @@ import java.util.HashMap;
 class Pair {
     int timestamp;
     String val;
-
     Pair(int timestamp, String val) {
         this.timestamp = timestamp;
         this.val = val;
     }
 }
 public class TimeBasedKeyValueStore {
+    // set/get: O(1) O(log n); O(n)
     private HashMap<String, ArrayList<Pair>> hashMap;
-
     public TimeBasedKeyValueStore() {
         hashMap = new HashMap<>();
     }
-
     public void set(String key, String value, int timestamp) {
-        if (hashMap.containsKey(key)) {
+        if (hashMap.containsKey(key))
             hashMap.get(key).add(new Pair(timestamp, value));
-        } else {
+        else {
             ArrayList<Pair> arr = new ArrayList<>();
             arr.add(new Pair(timestamp, value));
             hashMap.put(key, arr);
         }
     }
-
     public String get(String key, int timestamp) {
-
         String cand = "";
-
         if (hashMap.containsKey(key)) {
             ArrayList<Pair> arr = hashMap.get(key);
             int low = 0, high = arr.size() - 1;
-
             while (low <= high) {
                 int mid = (low + high) / 2;
                 int timeVal = arr.get(mid).timestamp;
-                if (timeVal == timestamp) {
+                if (timeVal == timestamp)
                     return arr.get(mid).val;
-                } else if (timeVal < timestamp) {
+                else if (timeVal < timestamp) {
                     cand = arr.get(low).val;
                     low = mid + 1;
-                } else {
+                } else
                     high = mid - 1;
-                }
             }
         }
         return cand;
