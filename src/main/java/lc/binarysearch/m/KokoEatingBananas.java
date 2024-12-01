db0ae6c8e15d018a5ab63a33cf17739c2d266151
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-01 12:39:11 -0800

    lc. binarysearch. KokoEatingBananas. O(log n) O(1)

--- src/main/java/lc/binarysearch/m/KokoEatingBananas.java
+++ src/main/java/lc/binarysearch/m/KokoEatingBananas.java
@@ -1,37 +1,28 @@
 package lc.binarysearch.m;
 
-// expl: https://www.youtube.com/watch?v=6tUs8Ii3_Qs&ab_channel=ErraK
-
 public class KokoEatingBananas {
-    // O(n)
-    public int maxEl(int[] piles) {
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < piles.length; i++) {
-            maxi = Math.max(maxi, piles[i]);
-        }
-        return maxi;
+    // O(log n) O(1)
+    public int maxElement(int[] piles) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < piles.length; i++)
+            max = Math.max(max, piles[i]);
+        return max;
     }
-
-    // O(n)
     public long totalHours(int[] piles, int hrs) {
         long total = 0;
-        for (int i = 0; i < piles.length; i++) {
-            total += (piles[i] + hrs - 1) / hrs; // Ceiling division
-        }
+        for (int i = 0; i < piles.length; i++)
+            total += (piles[i] + hrs - 1) / hrs;
         return total;
     }
-
-    // O(n log m)
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1, high = maxEl(piles);
+        int low = 1, high = maxElement(piles);
         while (low <= high) {
             int mid = low + (high - low) / 2;
             long total = totalHours(piles, mid);
-            if (total <= h) {
+            if (total <= h)
                 high = mid - 1;
-            } else {
+            else
                 low = mid + 1;
-            }
         }
         return low;
     }
