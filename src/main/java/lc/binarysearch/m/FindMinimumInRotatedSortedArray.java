69ae7cd20d8254745095b4ca051155c6fa8a5ed4
Yaroslav Hamuha <yhamuha@gmail.com>
2024-11-30 21:09:42 -0800

    lc. binarysearch. FindMinimumInRotatedSortedArray. O(log n) O(1)

--- src/main/java/lc/binarysearch/m/FindMinimumInRotatedSortedArray.java
+++ src/main/java/lc/binarysearch/m/FindMinimumInRotatedSortedArray.java
@@ -2,17 +2,17 @@ package lc.binarysearch.m;
 
 public class FindMinimumInRotatedSortedArray {
     // O(log n) O(1)
-    public int findMin(int[] nums) {
+    static int findMin(int[] nums) {
         int low = 0;
         int high = nums.length-1;
         int ans = Integer.MAX_VALUE;
         while(low <= high){
             int mid = low + (high-low)/2;
-            if(nums[low]<=nums[mid]){
+            if(nums[low]<=nums[mid]) {
                 ans = Math.min(ans,nums[low]);
                 low = mid+1;
             }
-            else{
+            else {
                 ans = Math.min(ans, nums[mid]);
                 high = mid-1;
             }
@@ -21,7 +21,6 @@ public class FindMinimumInRotatedSortedArray {
     }
 
     public static void main(String[] args) {
-        var fmir = new FindMinimumInRotatedSortedArray();
-        System.out.println(fmir.findMin(new int[] {3,4,5,1,2}));
+        System.out.println(findMin(new int[] {3,4,5,1,2}));
     }
 }
