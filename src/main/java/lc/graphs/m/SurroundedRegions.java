c0ea1103badda5ed9ed86992394b195350d19ce8
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-02 11:48:15 -0800

    lc. graphs. SurroundedRegions. O(n^2) O(n^2)

--- src/main/java/lc/graphs/m/SurroundedRegions.java
+++ src/main/java/lc/graphs/m/SurroundedRegions.java
@@ -1,22 +1,21 @@
 package lc.graphs.m;
 
 public class SurroundedRegions {
-
+    // O(n^2) O(n^2)
     private int[][] directions = {  {1,0}, {-1,0}, {0,1}, {0,-1}};
-    // O(m*n) O(m*n)
     public void solve(char[][] board) {
         int m = board.length, n = board[0].length;
-        for(int i=0; i<m; i++){
-            markBoard(board,m,n,i,0);   // going through border cells
+        for (int i=0; i<m; i++) {
+            markBoard(board,m,n,i,0);
             markBoard(board,m,n,i,n-1);
         }
-        for(int i=0; i<n; i++){         // going through border cells
+        for (int i=0; i<n; i++) {
             markBoard(board,m,n,0,i);
             markBoard(board,m,n,m-1,i);
         }
         flipBoard(board, m, n);
     }
-    private void markBoard(char[][] board, int m, int n, int i, int j){ // mark in "M" border closed
+    private void markBoard(char[][] board, int m, int n, int i, int j){
         if(i<0 || j<0 || i>=m || j>= n || board[i][j] != 'O') return;
         board[i][j] = 'M';
         for (int[] direction : directions){
@@ -24,7 +23,7 @@ public class SurroundedRegions {
             markBoard(board,m,n,x,y);
         }
     }
-    private void flipBoard(char[][] board, int m, int n){   // flip all "M"'s and "O"'s to "O" and "X" respectively
+    private void flipBoard(char[][] board, int m, int n){
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j] == 'M')
@@ -43,9 +42,8 @@ public class SurroundedRegions {
                             {'X','O','X','X'}};
         sr.solve(board);
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+            for (int j = 0; j < board[i].length; j++)
                 System.out.print(board[i][j] + " ");
-            }
             System.out.println();
         }
     }
