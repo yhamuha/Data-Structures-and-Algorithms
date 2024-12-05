3939bb19b8310c5627f5f79eeb1f3183ee85207a
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-05 01:08:22 -0800

    lc. linkedlist. RemoveNthNodeFromEndOfList. O(n) O(1)

--- src/main/java/lc/linkedlist/m/RemoveNthNodeFromEndOfList.java
+++ src/main/java/lc/linkedlist/m/RemoveNthNodeFromEndOfList.java
@@ -3,23 +3,22 @@ package lc.linkedlist.m;
 public class RemoveNthNodeFromEndOfList {
     // O(n) O(1)
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head==null) return null;
-        if(head.next==null) return null;
+        if (head==null) return null;
+        if (head.next==null) return null;
         int nodeCounter=0;                              
         ListNode curr = head;
-        while(curr!=null){
+        while (curr!=null) {
             curr=curr.next;
             nodeCounter++;
         }
         ListNode del = null; ListNode move = head;
-        if(nodeCounter==n){    // border case if n is the same as node length
+        if (nodeCounter==n) {
             head=move.next;
             move.next=null;
             return head;
         }
-        for(int i=1; i<nodeCounter-n; i++){
+        for (int i=1; i<nodeCounter-n; i++)
             move=move.next;
-        }
         del=move.next;
         move.next = move.next.next;
         del.next=null;
