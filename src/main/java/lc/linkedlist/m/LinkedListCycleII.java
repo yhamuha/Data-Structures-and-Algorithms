c37de8588d427fbcb92aa915885493a703e820d4
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-05 17:51:40 -0800

    lc. linkedlist. LinkedListCycleII. O(n) O(1)

--- src/main/java/lc/linkedlist/m/LinkedListCycleII.java
+++ src/main/java/lc/linkedlist/m/LinkedListCycleII.java
@@ -32,7 +32,7 @@ public class LinkedListCycleII {
         ListNode node1 = new ListNode(3);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(0);
-        ListNode node4 = new ListNode(4);
+        ListNode node4 = new ListNode(-4);
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
