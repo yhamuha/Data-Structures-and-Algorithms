1d3396fb573d01a835f4c50bbfab4dd9145a600b
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-14 20:48:04 -0800

    cs. tries. DesignAddAndSearchWordsDataStructure. O(L) O(L * 26^D), O(N * L)

--- src/main/java/lc/trie/m/DesignAddAndSearchWordsDataStructure.java
+++ src/main/java/lc/trie/m/DesignAddAndSearchWordsDataStructure.java
@@ -10,18 +10,15 @@ class Node {
         this.isEnd = false;
     }
 }
-// O(n*m) n - number of words; m - length of word
 class DesignAddAndSearchWordsDataStructure {
 
     private Node root;
-
     public DesignAddAndSearchWordsDataStructure() {
         this.root = new Node();
     }
-
+    // O(L) O(N * L)
     public void addWord(String word) {
         Node curr = this.root;
-
         for(char ch : word.toCharArray()) {
             if(!curr.children.containsKey(ch))
                 curr.children.put(ch, new Node());
@@ -29,20 +26,17 @@ class DesignAddAndSearchWordsDataStructure {
         }
         curr.isEnd = true;
     }
-
+    // O(L * 26^D) O(N * L)
     public boolean search(String word) {
         return search(word, this.root, 0);
     }
-
     private boolean search(String word, Node curr, int index) {
-
         for(int i=index;i<word.length();i++) {
             char ch = word.charAt(i);
             if(ch == '.') {
-                // look for the word ahead
                 for(char c : curr.children.keySet()) {
                     Node trieNode = curr.children.get(c);
-                    if(search(word, trieNode, i+1))
+                    if(search(word, trieNode, i+1)) // dfs to find any variants of word
                         return true;
                 }
                 return false;
@@ -56,16 +50,14 @@ class DesignAddAndSearchWordsDataStructure {
     }
 
     public static void main(String[] args) {
-        var daaswds = new DesignAddAndSearchWordsDataStructure();
-
-        daaswds.addWord("bad");
-        daaswds.addWord("dad");
-        daaswds.addWord("mad");
-
-        System.out.println(daaswds.search("pad")); // false
-        System.out.println(daaswds.search("bad")); // true
-        System.out.println(daaswds.search(".ad")); // true
-        System.out.println(daaswds.search("b..")); // true
+        var addAndSearchWord = new DesignAddAndSearchWordsDataStructure();
+        addAndSearchWord.addWord("bad");
+        addAndSearchWord.addWord("dad");
+        addAndSearchWord.addWord("mad");
+        System.out.println(addAndSearchWord.search("pad"));
+        System.out.println(addAndSearchWord.search("bad"));
+        System.out.println(addAndSearchWord.search(".ad"));
+        System.out.println(addAndSearchWord.search("b.."));
     }
 }
 
