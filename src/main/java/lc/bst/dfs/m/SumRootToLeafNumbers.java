d3a71b6bb5da1625e693bc51128f2b6204e0aea0
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-17 13:12:02 -0800

    cs. bst.dfs. SumRootToLeafNumbers. O(n) O(n)

--- src/main/java/lc/bst/dfs/m/SumRootToLeafNumbers.java
+++ src/main/java/lc/bst/dfs/m/SumRootToLeafNumbers.java
@@ -3,16 +3,16 @@ package lc.bst.dfs.m;
 import lc.bst.TreeNode;
 
 public class SumRootToLeafNumbers {
-    // O(n); O(n) unbalanced O(log n) balanced
-    public int sumNumbers(TreeNode root) {
+    // O(n) O(n)
+    public int sumNumbers (TreeNode root) {
         if (root == null)
             return 0;
         int[] sum = {0};
-        dfs(root, 0, sum);
+        dfs (root, 0, sum);
         return sum[0];
 
     }
-    private static void dfs(TreeNode root, int currentSum, int[] sum) {
+    private static void dfs (TreeNode root, int currentSum, int[] sum) {
         if (root == null)
             return;
         currentSum = currentSum * 10 + root.val;
@@ -24,11 +24,13 @@ public class SumRootToLeafNumbers {
         dfs(root.right, currentSum, sum);
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        SumRootToLeafNumbers sn = new SumRootToLeafNumbers();
-        System.out.println(sn.sumNumbers(root));
+        System.out.println(new SumRootToLeafNumbers().sumNumbers(root));
     }
 }
+
+
+
