20f6ce8fd653bde657fc6a41f9df67535039d190
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-18 12:13:21 -0800

    cs. bst.dfs. CountGoodNodesInBinaryTree. O(n) O(n)

--- src/main/java/lc/bst/dfs/m/CountGoodNodesInBinaryTree.java
+++ src/main/java/lc/bst/dfs/m/CountGoodNodesInBinaryTree.java
@@ -3,18 +3,18 @@ package lc.bst.dfs.m;
 import lc.bst.TreeNode;
 
 public class CountGoodNodesInBinaryTree {
-    // O(n); O(n) unbalanced O(log n) balanced
-    public int goodNodes(TreeNode root) {
-        if(root == null)    return 0;
+    // O(n) O(n)
+    public int goodNodes (TreeNode root) {
+        if(root == null) return 0;
         int count = 1;
         count += dfs(root.left, root.val);
         count += dfs(root.right, root.val);
         return count;
     }
-    public int dfs(TreeNode root, int max){
+    public int dfs (TreeNode root, int max) {
         int count = 0;
-        if(root == null)    return 0;
-        if(root.val >= max){
+        if(root == null) return 0;
+        if(root.val >= max) {
             count ++;
             max = root.val;
         }
