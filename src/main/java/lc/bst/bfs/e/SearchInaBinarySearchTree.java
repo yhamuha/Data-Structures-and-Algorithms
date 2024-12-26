f03ed498508a57280c8035cf3b7edadb6839e9f5
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-26 12:43:17 -0800

    lc. bst.bfs. SearchInaBinarySearchTree. O(n) O(n)

--- src/main/java/lc/bst/bfs/e/SearchInaBinarySearchTree.java
+++ src/main/java/lc/bst/bfs/e/SearchInaBinarySearchTree.java
@@ -3,14 +3,12 @@ package lc.bst.bfs.e;
 import lc.bst.PrintBst;
 import lc.bst.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class SearchInaBinarySearchTree {
     // O(n) O(n)
-    public TreeNode searchBST(TreeNode root, int val) {
+    static TreeNode searchBST(TreeNode root, int val) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()) {
@@ -26,11 +24,10 @@ public class SearchInaBinarySearchTree {
     }
 
     public static void main(String[] args) {
-        var sbst = new SearchInaBinarySearchTree();
         TreeNode root = new TreeNode(4, new TreeNode(2), new TreeNode(7));
         root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
         root.right = new TreeNode(7, null, null);
-        TreeNode searched = sbst.searchBST(root,2);
+        TreeNode searched = searchBST(root,2);
         PrintBst.printPreOrder(searched);
     }
 }
