507d9ece22d4fb61ed3eb170c9b495bda3449dcd
Yaroslav Hamuha <yhamuha@gmail.com>
2024-12-27 13:54:24 -0800

    lc. bst.bfs. SearchInaBinarySearchTree. O(n) O(1)

--- src/main/java/lc/bst/bfs/e/SearchInaBinarySearchTree.java
+++ src/main/java/lc/bst/bfs/e/SearchInaBinarySearchTree.java
@@ -22,12 +22,25 @@ public class SearchInaBinarySearchTree {
         }
         return null;
     }
+    // O(n) O(1)
+    static TreeNode searchBST_opt(TreeNode root, int val){
+        while (root!=null) {
+            if (root.val>val)
+                root=root.left;
+            else if (root.val<val)
+                root=root.right;
+            else
+                return root;
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(4, new TreeNode(2), new TreeNode(7));
         root.left = new TreeNode(2, new TreeNode(1), new TreeNode(3));
         root.right = new TreeNode(7, null, null);
-        TreeNode searched = searchBST(root,2);
+        // TreeNode searched = searchBST(root,2);
+        TreeNode searched = searchBST_opt(root,2);
         PrintBst.printPreOrder(searched);
     }
 }
