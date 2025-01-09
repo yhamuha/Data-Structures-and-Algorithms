55721dd4696202555768b8277395052eb2365775
Yaroslav Hamuha <yhamuha@gmail.com>
2025-01-08 20:00:05 -0800

    lc. sorting. DivideArrayIntoArraysWithMaxDifference. O(n log n) O(n)

--- src/main/java/lc/sorting/m/DivideArrayIntoArraysWithMaxDifference.java
+++ src/main/java/lc/sorting/m/DivideArrayIntoArraysWithMaxDifference.java
@@ -12,11 +12,10 @@ public class DivideArrayIntoArraysWithMaxDifference {
         for(int i=0,m=0; i<len; i=i+3,m++){
             res[m][0] = nums[i];
             for(int j=1; j<3; j++){
-                if(nums[i+j]-res[m][0]<=k){
+                if(nums[i+j]-res[m][0]<=k)
                     res[m][j]=nums[i+j];
-                } else{
+                else
                     return new int[0][0];
-                }
             }
         }
         return res;
