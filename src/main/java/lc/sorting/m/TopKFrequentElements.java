fb290f6174c71341823ce79970f7b066123812c5
Yaroslav Hamuha <yhamuha@gmail.com>
2025-01-09 22:36:12 -0800

    lc. sorting. TopKFrequentElements. O(n) O(n)

---
+++ src/main/java/lc/sorting/m/TopKFrequentElements.java
@@ -0,0 +1,45 @@
+package lc.sorting.m;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+    // O(n) O(n)
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for(int key: map.keySet()){
+            int frequency = map.get(key);
+            if(bucket[frequency] == null){
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        int[] res = new int[k];
+        int index = 0;
+        for(int i = bucket.length - 1; i >= 0; i--){
+            if(bucket[i] != null){
+                for(int val: bucket[i]){
+                    res[index++] = val;
+                    if(index == k) return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TopKFrequentElements solution = new TopKFrequentElements();
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] result = solution.topKFrequent(nums, k);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
