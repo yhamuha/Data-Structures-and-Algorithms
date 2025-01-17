86514f3e750cd0ec0ce64427f5e25e2fc700b928
Yaroslav Hamuha <yhamuha@gmail.com>
2025-01-16 21:21:17 -0800

    lc. trie. TopKFrequentWords. O(N*L + k log k) O(N*L)

--- src/main/java/lc/trie/m/TopKFrequentWords.java
+++ src/main/java/lc/trie/m/TopKFrequentWords.java
@@ -12,27 +12,25 @@ public class TopKFrequentWords {
         StringBuilder sb = new StringBuilder();
         boolean finish = false;
     }
-
     private Trie root;
     private PriorityQueue<Trie> pq;
     private List<String> res;
+    // O(N*L + k log k) O(N*L)
     public List<String> topKFrequent(String[] words, int k) {
         root = new Trie();
-        pq = new PriorityQueue<>(new Comparator<Trie>(){ // Sort Trie based on its frequency first
-            public int compare(Trie t1, Trie t2){        // and then lexicographical order
-                if(t1.count != t2.count){
+        pq = new PriorityQueue<>(new Comparator<Trie>(){
+            public int compare(Trie t1, Trie t2){
+                if(t1.count != t2.count)
                     return t2.count - t1.count;
-                }
                 else
                     return t1.sb.toString().compareTo(t2.sb.toString());
             }
         });
         res = new ArrayList<>();
-        for(String word : words){ // build the Trie
+        for(String word : words)
             buildTrie(root, word.toCharArray());
-        }
-        traverse(root); // traverse our Trie and put all Trir node with finished String into pq
-        while(k != 0){ // retrieve Trie from pq and save first k
+        traverse(root);
+        while(k != 0){
             res.add(pq.poll().sb.toString());
             k--;
         }
@@ -41,23 +39,18 @@ public class TopKFrequentWords {
 
     public void buildTrie(Trie root, char[] array){
         Trie node = root;
-        for(int i = 0; i < array.length; i++){  // insert each character into Trie
+        for(int i = 0; i < array.length; i++){
             char c = array[i];
-            if(node.child[c - 'a'] == null){
+            if(node.child[c - 'a'] == null)
                 node.child[c - 'a'] = new Trie();
-            }
             StringBuilder sb = new StringBuilder();
-            if(node.sb.length() != 0){
+            if(node.sb.length() != 0)
                 sb = node.sb;
-            }
-
             node = node.child[c - 'a'];
-
             if(node.sb.length() == 0){
                 node.sb.append(sb);
                 node.sb.append(c);
             }
-
             if(i == array.length - 1){
                 node.count++;
                 node.finish = true;
@@ -67,13 +60,11 @@ public class TopKFrequentWords {
 
     public void traverse(Trie node){
         for(Trie child : node.child){
-            if(child == null){ // skip the current Trie
+            if(child == null)
                 continue;
-            }
-            if(child.finish){ // if current node indicates a finished string
+            if(child.finish)
                 pq.add(child);
-            }
-            traverse(child); // continue dfs
+            traverse(child);
         }
     }
 
