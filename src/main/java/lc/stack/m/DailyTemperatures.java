d202fec68e4a8e51802b5e185432a4b89e2f627b
Yaroslav Hamuha <yhamuha@gmail.com>
2025-01-23 16:59:06 -0800

    lc. stack. DailyTemperatures. O(n) O(n)

--- src/main/java/lc/stack/m/DailyTemperatures.java
+++ src/main/java/lc/stack/m/DailyTemperatures.java
@@ -5,24 +5,23 @@ import java.util.Stack;
 
 public class DailyTemperatures {
     // O(n) O(n)
-    public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
+    static int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stackOfIndexes = new Stack<>();
         int n = temperatures.length;
         int[] answer = new int[n];
         for(int i = n-1; i >= 0; i--) {
-            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()])
-                stack.pop();
-            if(!stack.isEmpty())
-                answer[i] = stack.peek() - i;
-            stack.push(i);
+            while(!stackOfIndexes.isEmpty() && temperatures[i] >= temperatures[stackOfIndexes.peek()])
+                stackOfIndexes.pop();
+            if(!stackOfIndexes.isEmpty())
+                answer[i] = stackOfIndexes.peek() - i;
+            stackOfIndexes.push(i);
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        int[] temperatures = {73,74,75,71,69,72,76,73};
-        DailyTemperatures dt = new DailyTemperatures();
-        System.out.println(Arrays.toString(dt.dailyTemperatures(temperatures)));
+        int[] temperatures = {1,5,3,6,10,8};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
     }
 }
 
