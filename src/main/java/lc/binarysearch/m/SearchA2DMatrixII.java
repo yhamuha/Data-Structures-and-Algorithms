dd8935f51fdbbbddf7d36e055a48a049e4b331e4
Yaroslav Hamuha <yhamuha@gmail.com>
2025-01-25 20:04:38 -0800

    lc. binarysearch. SearchA2DMatrixII. O(n log m+n) O(1)

---
+++ src/main/java/lc/binarysearch/m/SearchA2DMatrixII.java
@@ -0,0 +1,29 @@
+package lc.binarysearch.m;
+
+public class SearchA2DMatrixII {
+    // O(m+n) O(1)
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) row++;
+            else col--;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        SearchA2DMatrixII obj = new SearchA2DMatrixII();
+        int[][] matrix = {
+                {1,  4,  7, 11, 15},
+                {2,  5,  8, 12, 19},
+                {3,  6,  9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        int target = 5;
+        boolean result = obj.searchMatrix(matrix, target);
+        System.out.println(result);
+    }
+}
