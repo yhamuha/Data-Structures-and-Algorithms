5fc9c53907ab61f324e2f739a2b646b51a8aa6aa
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-02 22:12:48 -0800

    lc. heap_priority_queue. FindKPairsWithSmallestSums. O(m*n*log k) O(k)

--- src/main/java/lc/heap_priority_queue/m/FindKPairsWithSmallestSums.java
+++ src/main/java/lc/heap_priority_queue/m/FindKPairsWithSmallestSums.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class FindKPairsWithSmallestSums {
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        // O(m*n) O(m*n)
+    static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        // O(m * n * log k) O(k)
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0] + b[1]) - (a[0] + a[1]));
         for(int i = 0; i < nums1.length; i++){
             for(int j = 0; j < nums2.length; j++){
@@ -20,11 +20,11 @@ public class FindKPairsWithSmallestSums {
                 }
             }
         }
-        ArrayList<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         int n = maxHeap.size();
         for(int i = 0; i < n; i++){
             int[] pairArr = maxHeap.poll();
-            ArrayList<Integer> pair = new ArrayList<>();
+            List<Integer> pair = new ArrayList<>();
             pair.add(pairArr[0]);
             pair.add(pairArr[1]);
             result.add(pair);
@@ -33,10 +33,9 @@ public class FindKPairsWithSmallestSums {
     }
 
     public static void main(String[] args) {
-        var fkpwss = new FindKPairsWithSmallestSums();
         int[] nums1 = {1,7,11};
         int[] nums2 = {2,4,6};
         int k = 3;
-        System.out.println(fkpwss.kSmallestPairs(nums1, nums2, k));
+        System.out.println(kSmallestPairs(nums1, nums2, k));
     }
 }
