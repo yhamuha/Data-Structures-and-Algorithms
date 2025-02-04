ec0ab843303da590ffeae8e243e39d774b3f8f83
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-04 15:39:05 -0800

    lc. heap_priority_queue. TopKFrequentWords . O(n log k) O(n)

--- src/main/java/lc/heap_priority_queue/m/TopKFrequentWords.java
+++ src/main/java/lc/heap_priority_queue/m/TopKFrequentWords.java
@@ -3,18 +3,18 @@ package lc.heap_priority_queue.m;
 import java.util.*;
 
 public class TopKFrequentWords {
-    // O(n log n) O(n)
+    // O(n log k) O(n)
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> map = new HashMap<>();
         for (String word : words)
             map.put(word, map.getOrDefault(word, 0) + 1);
         PriorityQueue<String> minHeap = new PriorityQueue<>(
-                (s1, s2) -> map.get(s1).equals(map.get(s2)) ? s2.compareTo(s1) : map.get(s1) - map.get(s2));
+                (s1, s2) -> map.get(s1).equals(map.get(s2)) ? s2.compareTo(s1)
+                                                            : map.get(s1) - map.get(s2));
         for (String word : map.keySet()) {
             minHeap.offer(word);
-            if (minHeap.size() > k) {
+            if (minHeap.size() > k)
                 minHeap.poll();
-            }
         }
         List<String> toReturn = new ArrayList<>();
         while (!minHeap.isEmpty())
