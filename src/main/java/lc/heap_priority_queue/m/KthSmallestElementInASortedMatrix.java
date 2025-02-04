6e37d5948f34bbb598465ab0e6c8b46c23356133
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-04 10:08:01 -0800

    lc. heap_priority_queue. KthSmallestElementInASortedMatrix. O(k log (min(n,k))) O(min(n,k))

--- src/main/java/lc/heap_priority_queue/m/KthSmallestElementInASortedMatrix.java
+++ src/main/java/lc/heap_priority_queue/m/KthSmallestElementInASortedMatrix.java
@@ -3,8 +3,8 @@ package lc.heap_priority_queue.m;
 import java.util.PriorityQueue;
 
 public class KthSmallestElementInASortedMatrix {
-    // O(n log n) O(n)
-    public int kthSmallest(int[][] matrix, int k) {
+    // O(k log (min(n,k))) O(min(n,k))
+    static int kthSmallest(int[][] matrix, int k) {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> matrix[a[0]][a[1]] - matrix[b[0]][b[1]]);
         int n = matrix.length;
         for(int i=0; i<Math.min(n,k); i++)
@@ -21,11 +21,10 @@ public class KthSmallestElementInASortedMatrix {
     }
 
     public static void main(String[] args) {
-        var kseiasm = new KthSmallestElementInASortedMatrix();
         int[][] matrix = {  {1,5,9},
                             {10,11,13},
                             {12,13,15}};
         int k = 8;
-        System.out.println(kseiasm.kthSmallest(matrix, k));
+        System.out.println(kthSmallest(matrix, k));
     }
 }
