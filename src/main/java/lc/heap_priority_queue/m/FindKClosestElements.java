ef1154cc16d1c2f53f6f25a9506acb7394a19da9
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-06 19:54:25 -0800

    lc. heap_priority_queue. FindKClosestElements. O(n log k + k log k) O(k)

--- src/main/java/lc/heap_priority_queue/m/FindKClosestElements.java
+++ src/main/java/lc/heap_priority_queue/m/FindKClosestElements.java
@@ -1,6 +1,7 @@
 package lc.heap_priority_queue.m;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -22,13 +23,31 @@ public class FindKClosestElements {
             result.add(minHeap.poll());
         return result;
     }
+    // O(n log k + k log k) O(k)
+    public List<Integer> findClosestElements_l(int[] arr, int k, int x) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> {
+            int firstDiff = Math.abs(a - x);
+            int secondDiff = Math.abs(b - x);
+            return (firstDiff == secondDiff) ? b - a : secondDiff - firstDiff;
+        });
+        for (int key : arr) {
+            heap.add(key);
+            if (heap.size() > k)
+                heap.remove();
+        }
+        List<Integer> ans = new ArrayList<>();
+        while (!heap.isEmpty())
+            ans.add(heap.remove());
+        Collections.sort(ans);
+        return ans;
+    }
 
     public static void main(String[] args) {
         FindKClosestElements finder = new FindKClosestElements();
         int[] arr = {-1,1,3,4};
         int k = 2;
         int x = 3;
-        List<Integer> closestElements = finder.findClosestElements(arr, k, x);
+        List<Integer> closestElements = finder.findClosestElements_l(arr, k, x);
         System.out.println(closestElements);
     }
 }
