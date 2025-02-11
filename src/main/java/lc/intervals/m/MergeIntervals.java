453b8429964a988b38e18ccec266b58b93b95e35
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-11 13:00:54 -0800

    lc. intervals. MergeIntervals. O(n log n) O(n)

--- src/main/java/lc/intervals/m/MergeIntervals.java
+++ src/main/java/lc/intervals/m/MergeIntervals.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public class MergeIntervals {
         // O(n log n) O(n)
-        public int[][] merge(int[][] intervals) {
+        static int[][] merge(int[][] intervals) {
             Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
             LinkedList<int[]> merged = new LinkedList<>();
             for (int[] interval : intervals) {
@@ -18,16 +18,14 @@ public class MergeIntervals {
         }
 
     public static void main(String[] args) {
-        var mi = new MergeIntervals();
         int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
-        int[][] result = mi.merge(intervals);
+        int[][] result = merge(intervals);
         for (int i = 0; i < result.length; i++) {
             System.out.print("[");
             for (int j = 0; j < result[i].length; j++) {
                 System.out.print(result[i][j]);
-                if (j < result[i].length - 1) {
+                if (j < result[i].length - 1)
                     System.out.print(",");
-                }
             }
             System.out.print("]");
         }
