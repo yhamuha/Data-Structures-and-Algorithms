ef0f69891f8c51a1d52dafaace52b46dd3f7d412
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-10 21:40:33 -0800

    lc. intervals. InsertInterval. O(n) O(n)

--- src/main/java/lc/intervals/m/InsertInterval.java
+++ src/main/java/lc/intervals/m/InsertInterval.java
@@ -5,40 +5,38 @@ import java.util.List;
 
 public class InsertInterval {
     // O(n) O(n)
-        public int[][] insert(int[][] intervals, int[] newInterval) {
-            List<int[]> mergedIntervals = new ArrayList<>();
-            boolean inserted = false;
-            for (int[] interval : intervals) {
-                if (interval[1] < newInterval[0])
-                    mergedIntervals.add(interval);
-                else if (interval[0] > newInterval[1]) {
-                    if (!inserted) {
-                        mergedIntervals.add(newInterval);
-                        inserted = true;
-                    }
-                    mergedIntervals.add(interval);
-                } else {
-                    newInterval[0] = Math.min(interval[0], newInterval[0]);
-                    newInterval[1] = Math.max(interval[1], newInterval[1]);
+    static int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> insertedIntervals = new ArrayList<>();
+        boolean inserted = false;
+        for (int[] interval : intervals) {
+            if (interval[1] < newInterval[0])
+                insertedIntervals.add(interval);
+            else if (interval[0] > newInterval[1]) {
+                if (!inserted) {
+                    insertedIntervals.add(newInterval);
+                    inserted = true;
                 }
+                insertedIntervals.add(interval);
+            } else {
+                newInterval[0] = Math.min(interval[0], newInterval[0]);
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
             }
-            if (!inserted)
-                mergedIntervals.add(newInterval);
-            return mergedIntervals.toArray(new int[mergedIntervals.size()][2]);
+        }
+        if (!inserted)
+            insertedIntervals.add(newInterval);
+        return insertedIntervals.toArray(new int[insertedIntervals.size()][2]);
     }
 
     public static void main(String[] args) {
-        var ii = new InsertInterval();
         int[][] intervals = {{1,3},{6,9}};
         int[] newInterval = {2,5};
-        int[][] result = ii.insert(intervals, newInterval);
+        int[][] result = insert(intervals, newInterval);
         for (int i = 0; i < result.length; i++) {
             System.out.print("[");
             for (int j = 0; j < result[i].length; j++) {
                 System.out.print(result[i][j]);
-                if (j < result[i].length - 1) {
+                if (j < result[i].length - 1)
                     System.out.print(",");
-                }
             }
             System.out.print("]");
         }
