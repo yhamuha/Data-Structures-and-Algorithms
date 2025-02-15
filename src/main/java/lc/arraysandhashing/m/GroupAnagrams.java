ab0b4c606ac66c65cc548f02d0a8d90c221bebe8
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-15 15:49:52 -0800

    lc. arraysandhashing. GroupAnagrams. O(n log n) O(n)

--- src/main/java/lc/arraysandhashing/m/GroupAnagrams.java
+++ src/main/java/lc/arraysandhashing/m/GroupAnagrams.java
@@ -4,22 +4,22 @@ import java.util.*;
 
 public class GroupAnagrams {
     // O(n log n) O(n)
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> groupedAnagrams = new HashMap<>();
-        for (String str : strs) {
-            char[] strArray = str.toCharArray();
-            Arrays.sort(strArray);
-            String sortedString = String.valueOf(strArray);
-            if (!groupedAnagrams.containsKey(sortedString))
-                groupedAnagrams.put(sortedString, new ArrayList<>());
+    static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groupedAnagrams = new HashMap<>();
+        for(String str : strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedString = String.valueOf(chars);
+            if(!groupedAnagrams.containsKey(sortedString)){
+                groupedAnagrams.put(sortedString,new ArrayList<>());
+            }
             groupedAnagrams.get(sortedString).add(str);
         }
         return new ArrayList<>(groupedAnagrams.values());
     }
 
     public static void main(String[] args) {
-        GroupAnagrams ga = new GroupAnagrams();
         String[] strs = new String[] {"eat", "tea", "cat", "bca", "abc"};
-        System.out.println(ga.groupAnagrams(strs));
+        System.out.println(groupAnagrams(strs));
     }
 }
