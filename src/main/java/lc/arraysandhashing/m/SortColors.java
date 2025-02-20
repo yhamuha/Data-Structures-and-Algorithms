3c4a358e1b9b0940d217057b3280241706caa9c1
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-19 21:46:16 -0800

    lc. arraysandhashing. SortColors. O(n) O(1)

--- src/main/java/lc/arraysandhashing/m/SortColors.java
+++ src/main/java/lc/arraysandhashing/m/SortColors.java
@@ -3,27 +3,31 @@ package lc.arraysandhashing.m;
 public class SortColors {
     // O(n) O(1)
     public void sortColors(int[] nums) {
-        int red=0; int white=0; int blue=0;
+        int red=0, white=0, blue=0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i]==0)
+            if (nums[i] == 0)
                 red++;
-            else if(nums[i]==1)
+            else if (nums[i] == 1)
                 white++;
             else
                 blue++;
         }
         for(int i=0; i<nums.length; i++){
-            if(red>0){
-                nums[i]=0;
+            if (red>0){
+                nums[i] = 0;
                 red--;
-            } else if(white>0){
-                nums[i]=1;
+            }
+            else if (white>0){
+                nums[i] = 1;
                 white--;
-            } else{
-                nums[i]=2;
+            }
+            else {
+                nums[i] = 2;
                 blue--;
             }
+
         }
+
     }
 
     public static void main(String[] args) {
