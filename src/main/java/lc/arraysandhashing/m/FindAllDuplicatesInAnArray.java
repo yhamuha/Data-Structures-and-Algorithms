804fe3cb262ed81596b03588cc752f668baea1bb
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-21 11:49:38 -0800

    lc. arraysandhashing. findDuplicates. O(n) O(1)

--- src/main/java/lc/arraysandhashing/m/FindAllDuplicatesInAnArray.java
+++ src/main/java/lc/arraysandhashing/m/FindAllDuplicatesInAnArray.java
@@ -1,12 +1,14 @@
 package lc.arraysandhashing.m;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FindAllDuplicatesInAnArray {
     // O(n) O(n)
-    public List<Integer> findDuplicates(int[] nums) {
+    static List<Integer> findDuplicates_prev(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<nums.length; i++){
@@ -16,9 +18,26 @@ public class FindAllDuplicatesInAnArray {
         return list;
     }
 
+    // O(n) O(1)
+    static List<Integer> findDuplicates(int[] nums) {
+        int[] result = new int[2];
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]-1);
+            if (nums[index] < 0) {
+                result[count++] = nums[index + 1];
+                if (count == 2) break;
+            }
+            nums[index] = -nums[index];
+        }
+        return Arrays.stream(result)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
     public static void main(String[] args) {
-        FindAllDuplicatesInAnArray finder = new FindAllDuplicatesInAnArray();
         int[] nums = {4,3,2,7,8,2,3,1};
-        System.out.println(finder.findDuplicates(nums));
+        System.out.println(findDuplicates(nums));
+
     }
 }
