51c186b5591efc25e66f4ef3e73f21f075d74ac8
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-25 17:31:53 -0800

    lc. twopointer. ReversePrefixOfWord. O(n) O(n)

--- src/main/java/lc/twopointer/e/ReversePrefixOfWord.java
+++ src/main/java/lc/twopointer/e/ReversePrefixOfWord.java
@@ -2,23 +2,22 @@ package lc.twopointer.e;
 
 public class ReversePrefixOfWord {
     // O(n) O(n)
-    public String reversePrefix(String word, char ch) {
-        int right = word.indexOf(ch);
+    static String reversePrefix(String word, char ch) {
+        int left =0, right = word.indexOf(ch);
         if (right == -1)
             return word;
         StringBuilder sb = new StringBuilder(word);
-        int left = 0;
-        for(;left<right;right--){
-            sb.setCharAt(left,word.charAt(right));
-            sb.setCharAt(right,word.charAt(left));
+        while (left < right) {
+            sb.setCharAt(left, word.charAt(right));
+            sb.setCharAt(right, word.charAt(left));
             left++;
+            right--;
         }
         return sb.toString();
     }
     public static void main(String[] args) {
         String word = "abcdefd";
         char ch = 'd';
-        ReversePrefixOfWord rpow = new ReversePrefixOfWord();
-        System.out.println(rpow.reversePrefix(word, ch));
+        System.out.println(reversePrefix(word, ch));
     }
 }
