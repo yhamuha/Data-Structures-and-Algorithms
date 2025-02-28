d14d928d569f8f9c4f2709e278a9e70a037d3ca5
Yaroslav Hamuha <yhamuha@gmail.com>
2025-02-27 21:19:04 -0800

    lc. twopointer. MoveZeroes. O(n) O(1)

--- src/main/java/lc/twopointer/e/MoveZeroes.java
+++ src/main/java/lc/twopointer/e/MoveZeroes.java
@@ -2,7 +2,7 @@ package lc.twopointer.e;
 
 public class MoveZeroes {
     //O(n) O(1)
-    public void moveZeroes(int[] nums) {
+    static void moveZeroes(int[] nums) {
         int n = nums.length;
         int i=0;
         for(int j=0; j<n;j++) {
@@ -15,9 +15,8 @@ public class MoveZeroes {
             nums[k]=0;
     }
     public static void main(String[] args) {
-        MoveZeroes moveZeroesObj = new MoveZeroes();
         int[] nums = {0, 1, 0, 3, 12};
-        moveZeroesObj.moveZeroes(nums);
+        moveZeroes(nums);
         for (int num : nums) {
             System.out.print(num + " ");
         }
