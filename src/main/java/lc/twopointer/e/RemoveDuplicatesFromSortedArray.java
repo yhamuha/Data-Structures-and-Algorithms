b0ac8bf690e176698e402be9d73a395c474b5414
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-02 06:05:05 -0800

    lc. twopointer. RemoveDuplicatesFromSortedArray. O(n) O(1)

--- src/main/java/lc/twopointer/e/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/lc/twopointer/e/RemoveDuplicatesFromSortedArray.java
@@ -2,7 +2,7 @@ package lc.twopointer.e;
 
 public class RemoveDuplicatesFromSortedArray {
     //O(n) O(1)
-    public int removeDuplicates(int[] nums) {
+    static int removeDuplicates(int[] nums) {
         int k=1;
         for (int i=1;i<nums.length;i++) {
             if (nums[i]!=nums[i-1]) {
@@ -14,8 +14,7 @@ public class RemoveDuplicatesFromSortedArray {
     }
 
     public static void main(String[] args) {
-        RemoveDuplicatesFromSortedArray obj = new RemoveDuplicatesFromSortedArray();
-        int[] nums = {1, 1, 2, 2, 3, 4, 4, 5};
-        System.out.println(obj.removeDuplicates(nums));
+        int[] nums = {1,1,2};
+        System.out.println(removeDuplicates(nums));
     }
 }
