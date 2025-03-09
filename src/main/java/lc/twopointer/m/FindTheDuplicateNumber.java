beb5cb67c6185572351f9efcd8a0e87d7c8f6cb6
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-09 13:01:22 -1000

    lc. twopointer. FindTheDuplicateNumber. O(n) O(1)

--- src/main/java/lc/twopointer/m/FindTheDuplicateNumber.java
+++ src/main/java/lc/twopointer/m/FindTheDuplicateNumber.java
@@ -2,29 +2,26 @@ package lc.twopointer.m;
 
 public class FindTheDuplicateNumber {
     // O(n) O(1)
-    public int findDuplicate(int[] nums) {
+    static int findDuplicate(int[] nums) {
         int slow=0, fast=0;
         int check = 0;
         while( true ) {
             slow = nums[slow];
             fast = nums[ nums[fast] ];
-            if( slow == fast ) {
+            if( slow == fast )
                 break;
-            }
         }
         while( true ) {
             slow = nums[slow];
             check = nums[check];
-            if( slow == check ) {
+            if( slow == check )
                 break;
-            }
         }
         return check;
     }
 
     public static void main(String[] args) {
-        FindTheDuplicateNumber obj = new FindTheDuplicateNumber();
         int[] nums = {1, 3, 4, 2, 2};
-        System.out.println(obj.findDuplicate(nums));
+        System.out.println(findDuplicate(nums));
     }
 }
