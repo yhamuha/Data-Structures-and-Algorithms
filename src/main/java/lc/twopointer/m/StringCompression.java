7562a5b471467a658848cedc67502d29bffdf864
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-13 20:33:36 -0700

    lc. twopointer. StringCompression. O(n) O(1)

---
+++ src/main/java/lc/twopointer/m/StringCompression.java
@@ -0,0 +1,27 @@
+package lc.twopointer.m;
+
+public class StringCompression {
+    // O(n) O(1)
+    static int compress(char[] chars) {
+        int ans = 0;
+        for (int i = 0; i < chars.length;) {
+            final char letter = chars[i];
+            int count = 0;
+            while (i < chars.length && chars[i] == letter) {
+                ++count;
+                ++i;
+            }
+            chars[ans++] = letter;
+            if (count > 1) {
+                for (final char c : String.valueOf(count).toCharArray())
+                    chars[ans++] = c;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println(compress(chars));
+    }
+}
