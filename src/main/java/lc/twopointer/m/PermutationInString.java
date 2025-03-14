bb17c1090297149c96df20c78f78a215e9f0568b
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-13 21:14:27 -0700

    lc. twopointer. PermutationInString. O(n+m) O(1)

---
+++ src/main/java/lc/twopointer/m/PermutationInString.java
@@ -0,0 +1,33 @@
+package lc.twopointer.m;
+
+public class PermutationInString {
+    // O(n+m) O(1)
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1Count, s2Count)) return true;
+            s2Count[s2.charAt(i) - 'a']--;
+            s2Count[s2.charAt(i + s1.length()) - 'a']++;
+        }
+        return matches(s1Count, s2Count);
+    }
+    private boolean matches(int[] s1Count, int[] s2Count) {
+        for (int i = 0; i < 26; i++)
+            if (s1Count[i] != s2Count[i]) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        PermutationInString pis = new PermutationInString();
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        boolean result = pis.checkInclusion(s1, s2);
+        System.out.println(result);
+    }
+}
