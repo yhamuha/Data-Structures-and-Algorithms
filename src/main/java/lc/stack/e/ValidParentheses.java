17e9cf0157fb240fc246502de8e9c461fa40414b
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-15 18:26:15 -0700

    lc. stack. ValidParentheses. O(n) O(n)

--- src/main/java/lc/stack/e/ValidParentheses.java
+++ src/main/java/lc/stack/e/ValidParentheses.java
@@ -4,22 +4,7 @@ import java.util.Stack;
 
 public class ValidParentheses {
     // O(n) O(n)
-    public boolean isValid(String s) {
-        /*if (s.length() % 2 != 0) return false;
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[')
-                stack.push(c);
-              else if (c == ')' && !stack.isEmpty() && stack.peek() == '(')
-                stack.pop();
-              else if (c == '}' && !stack.isEmpty() && stack.peek() == '{')
-                stack.pop();
-              else if (c == ']' && !stack.isEmpty() && stack.peek() == '[')
-                stack.pop();
-              else
-                return false;
-        }
-        return stack.isEmpty();*/
+    static boolean isValid(String s) {
         if (s.length() %2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()) {
@@ -37,8 +22,7 @@ public class ValidParentheses {
         return stack.isEmpty();
     }
     public static void main(String[] args) {
-        String s = "(())";
-        ValidParentheses vp = new ValidParentheses();
-        System.out.println(vp.isValid(s));
+        String s = "{()}";
+        System.out.println(isValid(s));
     }
 }
