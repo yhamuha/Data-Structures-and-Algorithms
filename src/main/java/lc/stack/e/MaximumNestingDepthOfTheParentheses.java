7dacebd55786319692a40a55e31c683279fe7ebc
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-16 20:48:18 -0700

    lc. stack. MaximumNestingDepthOfTheParentheses. O(n) O(n)

--- src/main/java/lc/stack/e/MaximumNestingDepthOfTheParentheses.java
+++ src/main/java/lc/stack/e/MaximumNestingDepthOfTheParentheses.java
@@ -4,16 +4,14 @@ import java.util.Stack;
 
 public class MaximumNestingDepthOfTheParentheses {
     // O(n) O(n)
-    public int maxDepth_stack(String s) {
+    static int maxDepth_stack(String s) {
         int depth = 0;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(' || c== '{' || c=='[' ) {
+            if (c == '(' || c== '{' || c=='[' )
                 stack.push(c);
-            }
-            if (c == ')' || c== '}' || c==']') {
+            if (c == ')' || c== '}' || c==']')
                 stack.pop();
-            }
             depth = Math.max(stack.size(), depth);
         }
         return depth;
@@ -21,8 +19,7 @@ public class MaximumNestingDepthOfTheParentheses {
 
     public static void main(String[] args) {
         String s = "(1+(2*3)+((8)/4))+1";
-        MaximumNestingDepthOfTheParentheses maxNestedOfParentheses = new MaximumNestingDepthOfTheParentheses();
-        System.out.println(maxNestedOfParentheses.maxDepth_stack(s));
+        System.out.println(maxDepth_stack(s));
     }
 }
 
