4cf1f7dfb73185619f1c82e17cbc1047b82f644a
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-17 14:01:12 -0700

    lc. stack. RemoveAllAdjacentDuplicatesInString. O(n) O(n)

--- src/main/java/lc/stack/e/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/lc/stack/e/RemoveAllAdjacentDuplicatesInString.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class RemoveAllAdjacentDuplicatesInString {
     // O(n) O(n)
-    public String removeDuplicates(String s) {
+    static String removeDuplicates(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if (!stack.isEmpty() && stack.peek() == s.charAt(i))
@@ -19,9 +19,8 @@ public class RemoveAllAdjacentDuplicatesInString {
     }
 
     public static void main(String[] args) {
-        RemoveAllAdjacentDuplicatesInString remover = new RemoveAllAdjacentDuplicatesInString();
         String test1 = "abbaca";
-        System.out.println(remover.removeDuplicates(test1));
+        System.out.println(removeDuplicates(test1));
     }
 }
 
