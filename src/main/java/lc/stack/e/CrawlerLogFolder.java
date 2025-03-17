71155d515167c8209c3c3bc59f027ac1d4e35251
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-16 23:08:16 -0700

    lc. stack. CrawlerLogFolder. O(n) O(n)

--- src/main/java/lc/stack/e/CrawlerLogFolder.java
+++ src/main/java/lc/stack/e/CrawlerLogFolder.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class CrawlerLogFolder {
     // O(n) O(n)
-    public int minOperations(String[] logs) {
+    static int minOperations(String[] logs) {
         Stack<String> actions = new Stack<>();
         String current = "./";
         String back = "../";
@@ -12,17 +12,27 @@ public class CrawlerLogFolder {
             if(!action.equals(current) && !action.equals(back)){
                 actions.push(action);
             } else if (action.equals(back)){
-                if(!actions.isEmpty()) {
+                if(!actions.isEmpty())
                     actions.pop();
-                }
             }
         }
         return actions.size();
     }
+    // O(n) O(1)
+    static int minOperations_spaceOptimized(String[] logs) {
+        int depth = 0;
+        for (String log : logs){
+            if (log.equals("./")) continue;
+            else if (log.equals("../")){
+                depth--;
+                if (depth<0) depth =0;
+            } else depth++;
+        }
+        return depth;
+    }
 
     public static void main(String[] args) {
         String[] logs = {"d1/","d2/","../","d21/","./"};
-        CrawlerLogFolder clf = new CrawlerLogFolder();
-        System.out.println(clf.minOperations(logs));
+        System.out.println(minOperations_spaceOptimized(logs));
     }
 }
