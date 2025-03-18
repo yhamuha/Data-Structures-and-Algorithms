95a0a759e5e977521cd13400b7216d9d42f2aeaa
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-17 17:13:28 -0700

    lc. stack. ValidPalindrome. O(n) O(n)

--- src/main/java/lc/stack/e/ValidPalindrome.java
+++ src/main/java/lc/stack/e/ValidPalindrome.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ValidPalindrome {
     // O(n) O(n)
-    public boolean isPalindrome(String s) {
+    static boolean isPalindrome(String s) {
         char[] chars = s.toLowerCase().toCharArray();
         Stack<Character> stack = new Stack<>();
         for(char ch : chars) {
@@ -21,16 +21,15 @@ public class ValidPalindrome {
     }
 
     public static void main(String[] args) {
-        ValidPalindrome vp = new ValidPalindrome();
         String test1 = "A man, a plan, a canal: Panama";
         String test2 = "race a car";
         String test3 = " ";
         String test4 = "No 'x' in Nixon";
         String test5 = "0P";
-        System.out.println(vp.isPalindrome(test1));
-        System.out.println(vp.isPalindrome(test2));
-        System.out.println(vp.isPalindrome(test3));
-        System.out.println(vp.isPalindrome(test4));
-        System.out.println(vp.isPalindrome(test5));
+        System.out.println(isPalindrome(test1));
+        System.out.println(isPalindrome(test2));
+        System.out.println(isPalindrome(test3));
+        System.out.println(isPalindrome(test4));
+        System.out.println(isPalindrome(test5));
     }
 }
