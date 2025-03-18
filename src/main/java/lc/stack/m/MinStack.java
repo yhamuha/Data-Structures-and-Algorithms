67a549ec7423072500774f631c16a98bfba6450f
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-17 18:10:29 -0700

    lc. stack. MinStack. O(1) O(n)

--- src/main/java/lc/stack/m/MinStack.java
+++ src/main/java/lc/stack/m/MinStack.java
@@ -5,23 +5,19 @@ import java.util.Stack;
 public class MinStack {
     Stack<Integer> stack = new Stack<>();
     Stack<Integer> min_values = new Stack<>();
-    // Overall O(1) O(n)
+    // O(1) O(n)
     public MinStack() {}
-    // O(1)
     public void push(int val) {
         if (min_values.isEmpty() || val <= min_values.peek())
             min_values.push(val);
         stack.push(val);
     }
-    // O(1)
     public void pop() {
         if(stack.peek().equals(min_values.peek()))
             min_values.pop();
         stack.pop();
     }
-    // O(1)
     public int top() {return stack.peek();}
-    // O(1)
     public int getMin(){return min_values.peek();}
 
     public static void main(String[] args) {
