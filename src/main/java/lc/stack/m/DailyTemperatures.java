1fd201dc18dfe0d95402aac3efa9b7bf4481565d
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-19 12:45:48 -0700

    lc. stack. DailyTemperatures. O(n) O(n)

--- src/main/java/lc/stack/m/DailyTemperatures.java
+++ src/main/java/lc/stack/m/DailyTemperatures.java
@@ -20,7 +20,7 @@ public class DailyTemperatures {
     }
 
     public static void main(String[] args) {
-        int[] temperatures = {1,5,3,6,10,8};
+        int[] temperatures = {73,74,75,71,69,72,76,73};
         System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
     }
 }
