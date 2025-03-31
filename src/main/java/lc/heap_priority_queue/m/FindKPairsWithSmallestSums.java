a866aaf35ec939202a30ed59c8cda8905020e0a7
Yaroslav Hamuha <yhamuha@gmail.com>
2025-03-31 12:57:49 -0700

    lc. heap_priority_queue. FindKPairsWithSmallestSums. O(m*n log k) O(k)

--- src/main/java/lc/heap_priority_queue/m/FindKPairsWithSmallestSums.java
+++ src/main/java/lc/heap_priority_queue/m/FindKPairsWithSmallestSums.java
@@ -6,9 +6,9 @@ import java.util.PriorityQueue;
 
 public class FindKPairsWithSmallestSums {
     static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        // O(m * n * log k) O(k)
+        // O(m*n log k) O(k)
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0] + b[1]) - (a[0] + a[1]));
-        for(int i = 0; i < nums1.length; i++){
+        for(int i = 0; i < nums1.length; i++) {
             for(int j = 0; j < nums2.length; j++){
                 maxHeap.offer(new int[] {nums1[i], nums2[j]});
                 if(maxHeap.size() > k){
