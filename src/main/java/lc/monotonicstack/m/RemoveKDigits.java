53f1902acbd8de47a4d4d32f4afdc43ab70594a7
Yaroslav Hamuha <yhamuha@gmail.com>
2025-04-07 20:35:38 -0700

    lc. monotonicstack. RemoveKDigits. O(n) O(n)

--- src/main/java/lc/monotonicstack/m/RemoveKDigits.java
+++ src/main/java/lc/monotonicstack/m/RemoveKDigits.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class RemoveKDigits {
         // O(n) O(n)
-        public String removeKdigits(String num, int k) {
+        static String removeKdigits(String num, int k) {
             Stack<Character> stack = new Stack<>();
             for(Character ch : num.toCharArray()) {
                 while(!stack.isEmpty() && k > 0 && ch < stack.peek()) {
@@ -18,16 +18,14 @@ public class RemoveKDigits {
                 stack.pop();
             if(stack.isEmpty()) return "0";
             String ans = "";
-            while(!stack.isEmpty()) {
+            while(!stack.isEmpty())
                 ans = stack.pop() + ans;
-            }
             return ans;
         }
 
     public static void main(String[] args) {
         String num = "1432219";
         int k = 3;
-        RemoveKDigits rkd = new RemoveKDigits();
-        System.out.println(rkd.removeKdigits(num,k));
+        System.out.println(removeKdigits(num,k));
     }
 }
