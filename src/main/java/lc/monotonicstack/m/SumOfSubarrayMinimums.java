b7c1e815bf1ce412c612401240b2b0172b9ab1f2
Yaroslav Hamuha <yhamuha@gmail.com>
2025-04-09 12:23:07 -0700

    lc. monotonicstack. SumOfSubarrayMinimums. O(n) O(n)

--- src/main/java/lc/monotonicstack/m/SumOfSubarrayMinimums.java
+++ src/main/java/lc/monotonicstack/m/SumOfSubarrayMinimums.java
@@ -4,42 +4,41 @@ import java.util.Stack;
 
 public class SumOfSubarrayMinimums {
     // O(n) O(n)
-    public int sumSubarrayMins(int[] arr) {
+    static int sumSubarrayMins(int[] arr) {
         int mod = 1000000007; int len = arr.length;
         int[] preSmaller = new int[len]; int[] nextSmaller = new int[len];
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < len; i++){
             preSmaller[i] = -1;
             nextSmaller[i] = len;
         }
         for (int i = 0; i < len; i++){
-            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
-                int index= s.pop();
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
+                int index= stack.pop();
                 nextSmaller[index] = i;
             }
-            s.push(i);
+            stack.push(i);
         }
-        s.clear();
+        stack.clear();
         for (int i = len-1; i >= 0; i--){
-            while(!s.isEmpty() && arr[s.peek()] > arr[i]){
-                int index= s.pop();
+            while(!stack.isEmpty() && arr[stack.peek()] > arr[i]){
+                int index= stack.pop();
                 preSmaller[index] = i;
             }
-            s.push(i);
+            stack.push(i);
         }
-        s.clear();
+        stack.clear();
         long res = 0;
         for (int i = 0; i < len; i++){
-            int left= i -preSmaller[i];
+            int left=i-preSmaller[i];
             int right=nextSmaller[i]-i;
-            res=(res+ ((long)arr[i]*left*right)%mod);
+            res=(res+((long)arr[i]*left*right)%mod);
         }
         return (int)res;
     }
 
     public static void main(String[] args) {
         int[] arr = {3,1,2,4};
-        SumOfSubarrayMinimums sosm = new SumOfSubarrayMinimums();
-        System.out.println(sosm.sumSubarrayMins(arr));
+        System.out.println(sumSubarrayMins(arr));
     }
 }
