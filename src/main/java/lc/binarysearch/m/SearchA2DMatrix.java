e5117dc8eca48c5770558e6ee2ec1ddc276ae8ef
Yaroslav Hamuha <yhamuha@gmail.com>
2025-04-15 11:12:16 -0700

    lc. binarysearch. SearchA2DMatrix. O(log m*n) O(1)

--- src/main/java/lc/binarysearch/m/SearchA2DMatrix.java
+++ src/main/java/lc/binarysearch/m/SearchA2DMatrix.java
@@ -20,8 +20,9 @@ public class SearchA2DMatrix {
     }
 
     public static void main(String[] args) {
-        System.out.println(searchMatrix(new int[][] {       {1,3,5,7},
-                                                            {10,11,16,20},
-                                                            {23,30,34,60}}, 3));
+        System.out.println(searchMatrix(new int[][] { {1,3,5,7},
+                                                      {10,11,16,20},
+                                                      {23,30,34,60}},
+                                                        3));
     }
 }
