491ff3fd7ffab61018bf178dc8e9f88f0854ed6c
Yaroslav Hamuha <yhamuha@gmail.com>
2025-04-23 00:35:14 -0700

    lc. binarysearch. MostBeautifulItemForEachQuery. O(n log n + m*n) O(n+m)

--- src/main/java/lc/binarysearch/m/MostBeautifulItemForEachQuery.java
+++ src/main/java/lc/binarysearch/m/MostBeautifulItemForEachQuery.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class MostBeautifulItemForEachQuery {
     // O(n log n + m*n) O(n+m)
-    public int[] maximumBeauty(int[][] items, int[] queries) {
-        int maxI = Integer.MAX_VALUE;
+    static int[] maximumBeauty(int[][] items, int[] queries) {
+        int maxInteger = Integer.MAX_VALUE;
         List<int[]> res = new ArrayList<>();
-        res.add(new int[] {0, 0, maxI});
+        res.add(new int[] {0, 0, maxInteger});
         Arrays.sort(items, Comparator.comparingInt(a -> a[0]));
         for (int[] item : items) {
             int price = item[0];
@@ -18,7 +18,7 @@ public class MostBeautifulItemForEachQuery {
             int[] lastBracket = res.get(res.size() - 1);
             if (beauty > lastBracket[1]) {
                 lastBracket[2] = price;
-                res.add(new int[] {price, beauty, maxI});
+                res.add(new int[] {price, beauty, maxInteger});
             }
         }
         int[] ans = new int[queries.length];
@@ -35,10 +35,10 @@ public class MostBeautifulItemForEachQuery {
     }
 
     public static void main(String[] args) {
-        MostBeautifulItemForEachQuery solution = new MostBeautifulItemForEachQuery();
         int[][] items = {{1, 4}, {2, 5}, {3, 2}, {4, 6}};
         int[] queries = {1, 2, 3, 4, 5};
-        int[] results = solution.maximumBeauty(items, queries);
-        System.out.println(Arrays.toString(results));
+        System.out.println(Arrays.toString(maximumBeauty(items, queries)));
     }
 }
+
+
