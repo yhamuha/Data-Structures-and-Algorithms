3bd375ec3c08cff3b61624f991683940d023be6a
Yaroslav Hamuha <yhamuha@gmail.com>
2025-04-23 20:57:15 -0700

    lc. binarysearch. FindPeakElement. O(log n) O(1)

--- src/main/java/lc/binarysearch/m/FindPeakElement.java
+++ src/main/java/lc/binarysearch/m/FindPeakElement.java
@@ -6,7 +6,7 @@ public class FindPeakElement {
         int left = 0;
         int right = nums.length - 1;
         while (left < right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] > nums[mid + 1]) {
                 right = mid;
             } else
