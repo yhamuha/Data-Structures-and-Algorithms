54d3701e38159e8f7c039fbc8331c65f8a80740f
Yaroslav Hamuha <yhamuha@gmail.com>
2025-04-30 20:42:00 -0700

    lc. slidingwindow. ContainsDuplicateII. O(n) O(n)

--- src/main/java/lc/slidingwindow/e/ContainsDuplicateII.java
+++ src/main/java/lc/slidingwindow/e/ContainsDuplicateII.java
@@ -7,27 +7,18 @@ public class ContainsDuplicateII {
     // O(n) O(n)
     static boolean containsNearbyDuplicate(int[] nums, int k) {
         Map<Integer,Integer> map = new HashMap();
-        for(int currIdx=0; currIdx<nums.length; currIdx++){
-            if(map.containsKey(nums[currIdx])){
-                int pastIdx = map.get(nums[currIdx]);
-                if(Math.abs(pastIdx - currIdx) <= k)
+        for(int currentIdx=0; currentIdx<nums.length; currentIdx++){
+            if(map.containsKey(nums[currentIdx])){
+                int pastIdx = map.get(nums[currentIdx]);
+                if(Math.abs(pastIdx - currentIdx) <= k)
                     return true;
             }
-            map.put(nums[currIdx],currIdx);
+            map.put(nums[currentIdx],currentIdx);
         }
         return false;
-      /*  int l =0, r=1;
-        while (r<nums.length){
-            if (nums[l] == nums[r] && (r-l) <=k )
-                return true;
-            else
-                r++;
-        }
-        return false;*/
     }
 
     public static void main(String[] args) {
-        ContainsDuplicateII obj = new ContainsDuplicateII();
         int[] nums = {1,3,8,9,2,10,2,1};
         int k = 3;
         System.out.println(containsNearbyDuplicate(nums, k));
