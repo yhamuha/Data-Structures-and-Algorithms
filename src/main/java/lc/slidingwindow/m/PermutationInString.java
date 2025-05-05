c25cfe006dd61f9e000da7bc5793bd65c255d401
Yaroslav Hamuha <yhamuha@gmail.com>
2025-05-04 23:07:36 -0700

    lc. slidingwindow. PermutationInString. O(n) O(1)

--- src/main/java/lc/slidingwindow/m/PermutationInString.java
+++ src/main/java/lc/slidingwindow/m/PermutationInString.java
@@ -6,23 +6,23 @@ import java.util.Map;
 public class PermutationInString {
     // O(n) O(1)
     static boolean checkInclusion(String s1, String s2) {
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> permutationElementsMap = new HashMap<>();
         for (int i = 0; i < s1.length(); i++) {
             char c = s1.charAt(i);
-            map.merge(c, 1, Integer::sum);
+            permutationElementsMap.merge(c, 1, Integer::sum);
         }
         Map<Character, Integer> window = new HashMap<>();
-        for (int right = 0; right < s2.length(); right++) {
-            char c = s2.charAt(right);
+        for (int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
             window.merge(c, 1, Integer::sum);
-            if (right >= s1.length()) {
-                c = s2.charAt(right - s1.length());
+            if (i >= s1.length()) {
+                c = s2.charAt(i - s1.length());
                 if (window.get(c) == 1)
                     window.remove(c);
                 else
                     window.merge(c, -1, Integer::sum);
             }
-            if (window.equals(map))
+            if (window.equals(permutationElementsMap))
                 return true;
         }
         return false;
