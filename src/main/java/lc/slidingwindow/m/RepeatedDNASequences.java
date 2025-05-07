044d7b122498a9f3e2db511dd42900d216ca73bb
Yaroslav Hamuha <yhamuha@gmail.com>
2025-05-06 22:29:54 -0700

    lc. slidingwindow. RepeatedDNASequences. O(n) O(n)

--- src/main/java/lc/slidingwindow/m/RepeatedDNASequences.java
+++ src/main/java/lc/slidingwindow/m/RepeatedDNASequences.java
@@ -10,17 +10,17 @@ public class RepeatedDNASequences {
     static List<String> findRepeatedDnaSequences(String s) {
         Set<String> set = new HashSet<>();
         Set<String> ans = new HashSet<>();
-        int i =0;
-        int j =0;
+        int i = 0;
+        int j = 0;
         StringBuilder sb = new StringBuilder();
         while(j<s.length()){
             sb.append(s.charAt(j));
-            if(j-i+1<10){
+            if(j-i+1 < 10){
                 j++;
             } else {
                 if(set.contains(sb.toString())){
                     ans.add(sb.toString());
-                }else {
+                } else {
                     set.add(sb.toString());
                 }
                 sb.delete(0,1);
