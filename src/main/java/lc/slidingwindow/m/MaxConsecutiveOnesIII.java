3b638c7a3546ab262a7b292eb46fc4d3b814319e
Yaroslav Hamuha <yhamuha@gmail.com>
2025-05-11 23:20:49 -0700

    lc. slidingwindow. MaxConsecutiveOnesIII. O(n) O(n)

--- src/main/java/lc/slidingwindow/m/MaxConsecutiveOnesIII.java
+++ src/main/java/lc/slidingwindow/m/MaxConsecutiveOnesIII.java
@@ -7,42 +7,40 @@ public class MaxConsecutiveOnesIII {
     // O(n) O(n)
     static int longestOnes(int[] nums, int k) {
         if (k != 0) {
-            int l = 0, r = 0, maxLen = 0;
-            List<Integer> indx = new ArrayList<>();
-            while (r < nums.length) {
-                if (nums[r] == 1) {
-                    maxLen = Math.max(maxLen, r - l + 1);
-                    r++;
-                }
-                else if (nums[r] == 0 && k > 0) {
-                    indx.add(r);
+            int left = 0, right = 0, maxLen = 0;
+            List<Integer> index = new ArrayList<>();
+            while (right < nums.length) {
+                if (nums[right] == 1) {
+                    maxLen = Math.max(maxLen, right - left + 1);
+                    right++;
+                } else if (nums[right] == 0 && k > 0) {
+                    index.add(right);
                     k--;
-                    maxLen = Math.max(maxLen, r - l + 1);
-                    r++;
-                }
-                else if (nums[r] == 0 && k == 0) {
-                    l = indx.get(0) + 1;
-                    indx.remove(0);
+                    maxLen = Math.max(maxLen, right - left + 1);
+                    right++;
+                } else if (nums[right] == 0 && k == 0) {
+                    left = index.get(0) + 1;
+                    index.remove(0);
                     k++;
                 }
             }
             return maxLen;
         } else {
-            int l = 0, r = 0, maxLen = 0;
-            while (r < nums.length) {
-                if (nums[r] == 1) {
-                    maxLen = Math.max(maxLen, r - l + 1);
-                } else
-                    l = r + 1;
-                r++;
+            int left = 0, right = 0, maxLen = 0;
+            while (right < nums.length) {
+                if (nums[right] == 1)
+                    maxLen = Math.max(maxLen, right - left + 1);
+                else
+                    left = right + 1;
+                right++;
             }
             return maxLen;
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
-        int k = 2;
+        int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+        int k = 0;
         System.out.println(longestOnes(nums, k));
     }
 }
