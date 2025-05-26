7c006caf80ce1a5281da8c5fe6530d04e34860e5
Yaroslav Hamuha <yhamuha@gmail.com>
2025-05-25 21:11:25 -0700

    lc. linkedlist. RemoveNthNodeFromEndOfList . O(n) O(1)

--- src/main/java/lc/linkedlist/m/RemoveNthNodeFromEndOfList.java
+++ src/main/java/lc/linkedlist/m/RemoveNthNodeFromEndOfList.java
@@ -3,25 +3,26 @@ package lc.linkedlist.m;
 public class RemoveNthNodeFromEndOfList {
     // O(n) O(1)
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head==null) return null;
-        if (head.next==null) return null;
-        int nodeCounter=0;                              
+        if (head == null) return null;
+        if (head.next == null) return null;
+        int nodeCounter = 0;
         ListNode curr = head;
-        while (curr!=null) {
-            curr=curr.next;
+        while (curr != null) {
+            curr = curr.next;
             nodeCounter++;
         }
-        ListNode del = null; ListNode move = head;
-        if (nodeCounter==n) {
-            head=move.next;
-            move.next=null;
+        ListNode del = null;
+        ListNode move = head;
+        if (nodeCounter == n) {
+            head = move.next;
+            move.next = null;
             return head;
         }
-        for (int i=1; i<nodeCounter-n; i++)
-            move=move.next;
-        del=move.next;
+        for (int i = 1; i < nodeCounter - n; i++)
+            move = move.next;
+        del = move.next;
         move.next = move.next.next;
-        del.next=null;
+        del.next = null;
         return head;
     }
 
@@ -32,7 +33,7 @@ public class RemoveNthNodeFromEndOfList {
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
-        ListNode result = rnelem.removeNthFromEnd(head,2);
+        ListNode result = rnelem.removeNthFromEnd(head, 2);
         while (result != null) {
             System.out.print(result.val + " ");
             result = result.next;
