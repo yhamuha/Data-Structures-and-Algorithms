ed88f751b04d1f57f68d275d1c93c926031594a3
Yaroslav Hamuha <yhamuha@gmail.com>
2025-05-27 21:03:42 -0700

    lc. linkedlist. FindTheDuplicateNumber . O(n) O(1)

--- src/main/java/lc/linkedlist/m/FindTheDuplicateNumber.java
+++ src/main/java/lc/linkedlist/m/FindTheDuplicateNumber.java
@@ -2,24 +2,23 @@ package lc.linkedlist.m;
 
 public class FindTheDuplicateNumber {
     // O(n) O(1)
-    public int findDuplicate(int[] nums){
-        if (nums.length < 1)    return -1;
-        int slow = nums[0], fast=nums[0];
-        while(fast != nums.length)
-        {
+    public int findDuplicate(int[] nums) {
+        if (nums.length < 1) return -1;
+        int slow = nums[0], fast = nums[0];
+        while (fast != nums.length) {
             slow = nums[slow];
             fast = nums[nums[fast]];
-            if(slow == fast)
+            if (slow == fast)
                 break;
         }
-        slow=nums[0];
-        while(fast != slow)
-        {
-            fast=nums[fast];
-            slow=nums[slow];
+        slow = nums[0];
+        while (fast != slow) {
+            fast = nums[fast];
+            slow = nums[slow];
         }
         return slow;
     }
+
     public static void main(String[] args) {
         FindTheDuplicateNumber solution = new FindTheDuplicateNumber();
         int[] nums = {1, 3, 4, 2, 2};
