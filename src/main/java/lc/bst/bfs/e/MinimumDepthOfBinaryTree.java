36e5a07d91674cc81f6156ea01fd717332fe1b9d
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-02 22:42:27 -0700

    lc. bst.bfs. MinimumDepthOfBinaryTree . O(n) O(n)

--- src/main/java/lc/bst/bfs/e/MinimumDepthOfBinaryTree.java
+++ src/main/java/lc/bst/bfs/e/MinimumDepthOfBinaryTree.java
@@ -1,26 +1,27 @@
 package lc.bst.bfs.e;
 
 import lc.bst.TreeNode;
+
 import java.util.LinkedList;
 import java.util.Queue;
+
 public class MinimumDepthOfBinaryTree {
     // O(n) O(n)
     public int minDepth(TreeNode root) {
-        if (root==null)
+        if (root == null)
             return 0;
         Queue<TreeNode> q = new LinkedList();
         q.add(root);
-        int depth=1;
-        while (!q.isEmpty())
-        {
+        int depth = 1;
+        while (!q.isEmpty()) {
             int size = q.size();
-            while (size-- >0) {
+            while (size-- > 0) {
                 root = q.poll();
                 if (root.left == null && root.right == null)
                     return depth;
-                if(root.left!=null)
+                if (root.left != null)
                     q.add(root.left);
-                if(root.right != null)
+                if (root.right != null)
                     q.add(root.right);
             }
             depth++;
