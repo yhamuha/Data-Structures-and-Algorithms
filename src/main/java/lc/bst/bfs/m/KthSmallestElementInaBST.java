1c1ac1b5a7df33162a612b77798543f8cee2b552
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-07 15:50:48 -0700

    lc. bst.bfs. KthSmallestElementInaBST . O(n log n) O(n)

--- src/main/java/lc/bst/bfs/m/KthSmallestElementInaBST.java
+++ src/main/java/lc/bst/bfs/m/KthSmallestElementInaBST.java
@@ -1,6 +1,7 @@
 package lc.bst.bfs.m;
 
 import lc.bst.TreeNode;
+
 import java.util.*;
 
 public class KthSmallestElementInaBST {
@@ -9,9 +10,9 @@ public class KthSmallestElementInaBST {
         Queue<TreeNode> queue = new LinkedList<>();
         List<Integer> list = new ArrayList<>();
         queue.offer(root);
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int levelSize = queue.size();
-            for (int i=0; i<levelSize; i++) {
+            for (int i = 0; i < levelSize; i++) {
                 TreeNode temp = queue.poll();
                 list.add(temp.val);
                 if (temp.left != null) queue.add(temp.left);
@@ -19,13 +20,13 @@ public class KthSmallestElementInaBST {
             }
         }
         Collections.sort(list);
-        return list.get(k-1);
+        return list.get(k - 1);
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3, new TreeNode(1), new TreeNode(4));
         root.left = new TreeNode(1, null, new TreeNode(2));
         root.right = new TreeNode(4, null, null);
-        System.out.println(kthSmallest(root,1));
+        System.out.println(kthSmallest(root, 1));
     }
 }
