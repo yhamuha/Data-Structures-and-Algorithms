78bf02700e21d360ea1cd35c2e5cae8887c91c4d
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-15 21:07:30 -0700

    lc. bst.dfs. BalancedBinaryTree . O(n) O(n)

--- src/main/java/lc/bst/dfs/e/BalancedBinaryTree.java
+++ src/main/java/lc/bst/dfs/e/BalancedBinaryTree.java
@@ -3,18 +3,19 @@ package lc.bst.dfs.e;
 import lc.bst.TreeNode;
 
 public class BalancedBinaryTree {
-    // O(n), O(n)
+    // O(n) O(n)
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
         if (height(root) == -1) return false;
         return true;
     }
+
     private int height(TreeNode root) {
         if (root == null) return 0;
         int lh = height(root.left);
         int rh = height(root.right);
         if (lh == -1 || rh == -1) return -1;
-        if (Math.abs (lh - rh) > 1) return -1;
+        if (Math.abs(lh - rh) > 1) return -1;
         return 1 + Math.max(lh, rh);
 
     }
