c36edc32c32815d87af2e423cef2ca505c751723
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-16 20:55:41 -0700

    lc. bst.dfs. SameTree . O(n) O(n)

--- src/main/java/lc/bst/dfs/e/SameTree.java
+++ src/main/java/lc/bst/dfs/e/SameTree.java
@@ -5,10 +5,10 @@ import lc.bst.TreeNode;
 public class SameTree {
     // O(n) O(n)
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null || q == null) {
+        if (p == null || q == null) {
             return (p == q);
         }
-        return  p.val == q.val
+        return p.val == q.val
                 && isSameTree(p.left, q.left)
                 && isSameTree(p.right, q.right);
     }
