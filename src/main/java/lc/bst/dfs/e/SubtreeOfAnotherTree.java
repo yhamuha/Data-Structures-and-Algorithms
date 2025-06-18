d091e42a7b029ecf1ec7a574541eb20b9d445532
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-17 21:12:33 -0700

    lc. bst.dfs. SubtreeOfAnotherTree. O(n^2) O(n)

--- src/main/java/lc/bst/dfs/e/SubtreeOfAnotherTree.java
+++ src/main/java/lc/bst/dfs/e/SubtreeOfAnotherTree.java
@@ -3,22 +3,23 @@ package lc.bst.dfs.e;
 import lc.bst.TreeNode;
 
 public class SubtreeOfAnotherTree {
-    // O(n*m), O(n)
+    // O(n^2) O(n)
     static boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (root == null) return false;
         if (subRoot == null) return true;
-        if (helper(root, subRoot)) return true;
-        return isSubtree(root.left, subRoot) || isSubtree(root.right,subRoot);
+        if (dfs(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-    private static boolean helper(TreeNode root1, TreeNode root2) {
+
+    private static boolean dfs(TreeNode root1, TreeNode root2) {
         if (root1 == null && root2 == null) return true;
-        return (root1 !=null && root2 != null && root1.val == root2.val) &&
-                helper(root1.left, root2.left) && helper (root1.right, root2.right);
+        return (root1 != null && root2 != null && root1.val == root2.val) &&
+                dfs(root1.left, root2.left) && dfs(root1.right, root2.right);
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(2)),
-                                        new TreeNode(5));
+                new TreeNode(5));
         TreeNode subRoot = new TreeNode(4, new TreeNode(1), new TreeNode(2));
         System.out.println(isSubtree(root, subRoot));
     }
