8dd31fccc432b18de268d75cd70926afe838da90
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-19 15:47:58 -0700

    lc. bst.dfs. MergeTwoBinaryTrees. O(n) O(n)|(log n) if balanced

--- src/main/java/lc/bst/dfs/e/MergeTwoBinaryTrees.java
+++ src/main/java/lc/bst/dfs/e/MergeTwoBinaryTrees.java
@@ -3,17 +3,18 @@ package lc.bst.dfs.e;
 import lc.bst.TreeNode;
 
 public class MergeTwoBinaryTrees {
-    // O(n); O(n) unbal, O(log n) bal
+    // O(n) O(n)|(log n) if balanced
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if(root1 == null)
+        if (root1 == null)
             return root2;
-        else if(root2 == null)
+        else if (root2 == null)
             return root1;
         root1.val += root2.val;
         root1.left = mergeTrees(root1.left, root2.left);
         root1.right = mergeTrees(root1.right, root2.right);
         return root1;
     }
+
     private static void preOrderTraversal(TreeNode node) {
         if (node == null)
             return;
@@ -25,7 +26,7 @@ public class MergeTwoBinaryTrees {
     public static void main(String[] args) {
         MergeTwoBinaryTrees merger = new MergeTwoBinaryTrees();
         TreeNode root1 = new TreeNode(1, new TreeNode(3, new TreeNode(5), null), new TreeNode(2));
-        TreeNode root2 = new TreeNode(2, new TreeNode(1,null, new TreeNode(4)),new TreeNode(3, null, new TreeNode(7)));
+        TreeNode root2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
         TreeNode mergedRoot = merger.mergeTrees(root1, root2);
         preOrderTraversal(mergedRoot);
     }
