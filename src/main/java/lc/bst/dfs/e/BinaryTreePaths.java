af6aae536f78184a44014fe12f67388c994412ff
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-20 23:48:16 -0700

    lc. bst.dfs. BinaryTreePaths. O(n*h) O(n*h)

--- src/main/java/lc/bst/dfs/e/BinaryTreePaths.java
+++ src/main/java/lc/bst/dfs/e/BinaryTreePaths.java
@@ -7,26 +7,28 @@ import java.util.List;
 
 public class BinaryTreePaths {
     // O(n*h) O(n*h)
-    ArrayList<String> list=new ArrayList<>();
+    ArrayList<String> list = new ArrayList<>();
+
     public List<String> binaryTreePaths(TreeNode root) {
-        String ans="";
-        path(root,ans);
+        String ans = "";
+        path(root, ans);
         return list;
     }
+
     void path(TreeNode root, String ans) {
-        if(root==null) return;
-        ans=ans+root.val;
-        if(root.left==null && root.right==null){
+        if (root == null) return;
+        ans = ans + root.val;
+        if (root.left == null && root.right == null) {
             list.add(ans);
             return;
         }
-        ans=ans+"->";
-        path(root.left,ans);
-        path(root.right,ans);
+        ans = ans + "->";
+        path(root.left, ans);
+        path(root.right, ans);
     }
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1,new TreeNode(2,null,new TreeNode(5)), new TreeNode(3));
+        TreeNode root = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3));
         BinaryTreePaths binaryTreePaths = new BinaryTreePaths();
         List<String> paths = binaryTreePaths.binaryTreePaths(root);
         for (String path : paths) {
