b113e7b230b57c492a8cfe0b0a23f7ba4e446ca9
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-28 12:19:26 -0700

    lc.backtracking. CombinationSum. O(2^n) O(n)

--- src/main/java/lc/backtracking/m/CombinationSum.java
+++ src/main/java/lc/backtracking/m/CombinationSum.java
@@ -5,29 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSum {
-// O(2^n) O(n)
+    // O(2^n) O(n)
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> resultList = new ArrayList<>();
         backtracking(resultList, new ArrayList<>(), candidates, target, 0);
         return resultList;
     }
+
     private void backtracking(List<List<Integer>> resultList, ArrayList<Integer> tempList,
-                                int[] candidates, int remains, int start) {
+                              int[] candidates, int remains, int start) {
         if (remains < 0) return;
         else if (remains == 0) resultList.add(new ArrayList<>(tempList));
         else {
-            for(int i=start; i<candidates.length; i++){
+            for (int i = start; i < candidates.length; i++) {
                 tempList.add(candidates[i]);
-                backtracking(resultList,tempList,candidates,remains-candidates[i],i);
-                tempList.remove(tempList.size()-1);
+                backtracking(resultList, tempList, candidates, remains - candidates[i], i);
+                tempList.remove(tempList.size() - 1);
             }
         }
     }
 
     public static void main(String[] args) {
         var cs = new CombinationSum();
-        int[] candidates = {2,3,6,7};
+        int[] candidates = {2, 3, 6, 7};
         int target = 7;
-        System.out.println(cs.combinationSum(candidates,target));
+        System.out.println(cs.combinationSum(candidates, target));
     }
 }
