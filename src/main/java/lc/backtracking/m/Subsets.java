b1a966764da039d70e151586a1dbfd7730023299
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-27 19:03:32 -0700

    lc.backtracking. Subsets. O(2^n) O(n)

--- src/main/java/lc/backtracking/m/Subsets.java
+++ src/main/java/lc/backtracking/m/Subsets.java
@@ -5,23 +5,24 @@ import java.util.List;
 
 public class Subsets {
     // O(2^n) O(n)
-    public List<List<Integer>> subsets (int[] nums){
+    public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> resultList = new ArrayList<>();
         backtrack(resultList, new ArrayList<>(), nums, 0);
         return resultList;
     }
+
     private void backtrack(List<List<Integer>> resultList, List<Integer> tempList, int[] nums, int start) {
         resultList.add(new ArrayList<>(tempList));
-        for(int i=start; i<nums.length; i++) {
+        for (int i = start; i < nums.length; i++) {
             tempList.add(nums[i]);
-            backtrack(resultList, tempList, nums, i+1);
-            tempList.remove(tempList.size()-1);
+            backtrack(resultList, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
         }
     }
 
     public static void main(String[] args) {
         var s = new Subsets();
-        int[] nums = {1,2,3};
+        int[] nums = {1, 2, 3};
         System.out.println(s.subsets(nums));
     }
 }
