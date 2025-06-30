6dc647def777f38b10b72d051ea8f138b92303de
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-30 08:54:16 -0700

    lc.backtracking. Permutations. O(n×n!) O(n×n!)

--- src/main/java/lc/backtracking/m/Permutations.java
+++ src/main/java/lc/backtracking/m/Permutations.java
@@ -4,28 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-    // O(n!) O(n)
+    // O(n×n!) O(n×n!)
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> resultList = new ArrayList<>();
         backtracking(resultList, new ArrayList<>(), nums);
         return resultList;
     }
-    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] nums){
-        if(tempList.size() == nums.length){
+
+    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {
             resultList.add(new ArrayList<>(tempList));
             return;
         }
-        for(int num : nums){
+        for (int num : nums) {
             if (tempList.contains(num)) continue;
             tempList.add(num);
             backtracking(resultList, tempList, nums);
-            tempList.remove(tempList.size()-1);
+            tempList.remove(tempList.size() - 1);
         }
     }
 
     public static void main(String[] args) {
         var p = new Permutations();
-        int[] nums= {1,2,3};
+        int[] nums = {1, 2, 3};
         System.out.println(p.permute(nums));
     }
 }
