050a6234e22c4e0c2684fc06c1e15c260f83a87c
Yaroslav Hamuha <yhamuha@gmail.com>
2025-06-30 08:39:50 -0700

    lc.backtracking. Combinations. O(C(n,k)×k) O(C(n,k)×k)

--- src/main/java/lc/backtracking/m/Combinations.java
+++ src/main/java/lc/backtracking/m/Combinations.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combinations {
-    // O(C(n, k)) O(k)
+    // O(C(n,k)×k) O(C(n,k)×k)
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         backtrack(1, n, k, new ArrayList<>(), result);
         return result;
     }
+
     private void backtrack(int start, int n, int k, List<Integer> combination, List<List<Integer>> result) {
         if (combination.size() == k) {
             result.add(new ArrayList<>(combination));
             return;
         }
-        for (int i = start; i <= n; ++i) {
+        for (int i = start; i <= n; i++) {
             combination.add(i);
             backtrack(i + 1, n, k, combination, result);
             combination.remove(combination.size() - 1);
