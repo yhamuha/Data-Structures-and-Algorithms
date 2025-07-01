64de526d0526c6aaf8fe2444cd3ef646e17f38d8
Yaroslav Hamuha <yhamuha@gmail.com>
2025-07-01 12:59:14 -0700

    lc.backtracking. SubsetsII. O(n * 2^n) O(n * 2^n)

--- src/main/java/lc/backtracking/m/SubsetsII.java
+++ src/main/java/lc/backtracking/m/SubsetsII.java
@@ -5,30 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SubsetsII {
-    // TC: O(2^n) because of line 19
+    // O(n * 2^n) O(n * 2^n)
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        // SC: O(n)
         List<List<Integer>> resultList = new ArrayList<>();
         Arrays.sort(nums);
         backtracking(resultList, new ArrayList<>(), nums, 0);
         return resultList;
     }
 
-    private void backtracking (List<List<Integer>> resultList, List<Integer> tempList, int[] nums, int start){
+    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] nums, int start) {
         if (resultList.contains(tempList)) return;
-
         resultList.add(new ArrayList<>(tempList));
-
-        for(int i=start; i<nums.length; i++) {
+        for (int i = start; i < nums.length; i++) {
             tempList.add(nums[i]);
-            backtracking(resultList, tempList, nums, i+1);
-            tempList.remove(tempList.size()-1);
+            backtracking(resultList, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
         }
     }
 
     public static void main(String[] args) {
         var sII = new SubsetsII();
-        int[] nums = {1,2,2};
+        int[] nums = {1, 2, 2};
         System.out.println(sII.subsetsWithDup(nums));
     }
 }
