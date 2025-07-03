a3c3eac7c50178a9f77e73455daa8bd1490f295b
Yaroslav Hamuha <yhamuha@gmail.com>
2025-07-03 15:52:47 -0700

    lc.backtracking. PalindromePartitioning. O(2^n * n) O(2^n * n)

--- src/main/java/lc/backtracking/m/PalindromePartitioning.java
+++ src/main/java/lc/backtracking/m/PalindromePartitioning.java
@@ -4,26 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PalindromePartitioning {
-    // O(n * 2^n) O(n)
+    // O(2^n * n) O(2^n * n)
     public List<List<String>> partition(String s) {
-        List<List<String>> list = new ArrayList<>();
-        backtrack(list, new ArrayList<>(), s, 0);
-        return list;
+        List<List<String>> resultList = new ArrayList<>();
+        backtrack(resultList, new ArrayList<>(), s, 0);
+        return resultList;
     }
-    private void backtrack (List<List<String>> list, List<String> tempList, String s, int start){
-        if(start == s.length())
+
+    private void backtrack(List<List<String>> list, List<String> tempList, String s, int start) {
+        if (start == s.length())
             list.add(new ArrayList<>(tempList));
-        else{
-            for(int i=start; i<s.length(); i++) {
-                if(isPalindrome(s,start,i)){
-                    tempList.add(s.substring(start,i+1));
-                    backtrack(list,tempList,s,i+1);
-                    tempList.remove(tempList.size()-1);
+        else {
+            for (int i = start; i < s.length(); i++) {
+                if (isPalindrome(s, start, i)) {
+                    tempList.add(s.substring(start, i + 1));
+                    backtrack(list, tempList, s, i + 1);
+                    tempList.remove(tempList.size() - 1);
                 }
             }
         }
     }
-    private boolean isPalindrome(String s, int low, int high){
+
+    private boolean isPalindrome(String s, int low, int high) {
         while (low < high)
             if (s.charAt(low++) != s.charAt(high--))
                 return false;
