2706239723b504c3f35ee19ef9c446ba17edde94
Yaroslav Hamuha <yhamuha@gmail.com>
2025-07-02 21:23:41 -0700

    lc.backtracking. CombinationSumII. O(2^n * n) O(2^n * n)

--- src/main/java/lc/backtracking/m/CombinationSumII.java
+++ src/main/java/lc/backtracking/m/CombinationSumII.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CombinationSumII {
-    // O(2^n) O(n)
+    // O(2^n * n) O(2^n * n)
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> resultList = new ArrayList<>();
-        Arrays.sort(candidates); // O(n log n)
+        Arrays.sort(candidates);
         backtracking(resultList, new ArrayList<>(), candidates, target, 0);
         return resultList;
     }
-    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] candidates, int remain, int start){
+
+    private void backtracking(List<List<Integer>> resultList, List<Integer> tempList, int[] candidates, int remain, int start) {
         if (remain < 0) return;
         else if (remain == 0)
             resultList.add(new ArrayList<>(tempList));
@@ -20,7 +21,7 @@ public class CombinationSumII {
             for (int i = start; i < candidates.length; i++) {
                 if (i > start && candidates[i] == candidates[i - 1]) continue;
                 tempList.add(candidates[i]);
-                backtracking(resultList, tempList, candidates, remain - candidates[i], i+1);
+                backtracking(resultList, tempList, candidates, remain - candidates[i], i + 1);
                 tempList.remove(tempList.size() - 1);
             }
         }
@@ -28,8 +29,8 @@ public class CombinationSumII {
 
     public static void main(String[] args) {
         var csII = new CombinationSumII();
-        int[] candidates = {10,1,2,7,6,1,5};
+        int[] candidates = {10, 1, 2, 7, 6, 1, 5};
         int target = 8;
-        System.out.println(csII.combinationSum2(candidates,target));
+        System.out.println(csII.combinationSum2(candidates, target));
     }
 }
