91d0bc441a54dbe99f7f71bd3d9183983537d539
Yaroslav Hamuha <yhamuha@gmail.com>
2025-07-07 15:32:32 -0700

    lc.backtracking. WordSearch. O(m * n * 4^l) O(l)

--- src/main/java/lc/backtracking/m/WordSearch.java
+++ src/main/java/lc/backtracking/m/WordSearch.java
@@ -1,34 +1,35 @@
 package lc.backtracking.m;
 
 public class WordSearch {
-    // O(m*n*4^l) O(l)
+    // O(m * n * 4^l) O(l)
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if(backtrack(board,word,i,j,0))
+                if (backtrack(board, word, i, j, 0))
                     return true;
             }
         }
         return false;
     }
+
     private boolean backtrack(char[][] board, String word, int i, int j, int index) {
-        if(index==word.length())
+        if (index == word.length())
             return true;
-        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]!=word.charAt(index))
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index))
             return false;
-        char temp=board[i][j];
-        board[i][j]=' ';
-        boolean found = backtrack(board,word,i+1,j,index+1) ||
-                backtrack(board,word,i,j+1,index+1) ||
-                backtrack(board,word,i-1,j,index+1) ||
-                backtrack(board,word,i,j-1,index+1);
-        board[i][j]=temp;
+        char temp = board[i][j];
+        board[i][j] = ' ';
+        boolean found = backtrack(board, word, i + 1, j, index + 1) ||
+                backtrack(board, word, i, j + 1, index + 1) ||
+                backtrack(board, word, i - 1, j, index + 1) ||
+                backtrack(board, word, i, j - 1, index + 1);
+        board[i][j] = temp;
         return found;
     }
 
     public static void main(String[] args) {
         var ws = new WordSearch();
-        char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
         String word = "ABCCED";
         System.out.println(ws.exist(board, word));
     }
