a27ead20e56f9e99bea3f1297a5488719ae2c22b
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-12 15:16:01 -0700

    lc. twopointer. ValidWordAbbreviation. O(m+n) O(1)

---
+++ src/main/java/lc/twopointer/e/ValidWordAbbreviation.java
@@ -0,0 +1,32 @@
+package lc.twopointer.e;
+
+public class ValidWordAbbreviation {
+    // O(m+n) O(1)
+    public static boolean validWordAbbreviation(String word, String abbr) {
+        int m = word.length(), n = abbr.length();
+        int i = 0, j = 0, x = 0;
+        for (; i < m && j < n; ++j) {
+            char c = abbr.charAt(j);
+            if (Character.isDigit(c)) {
+                if (c == '0' && x == 0) {
+                    return false;
+                }
+                x = x * 10 + (c - '0');
+            } else {
+                i += x;
+                x = 0;
+                if (i >= m || word.charAt(i) != c) {
+                    return false;
+                }
+                ++i;
+            }
+        }
+        return i + x == m && j == n;
+    }
+
+    public static void main(String[] args) {
+        String word = "internationalization";
+        String abbr = "i12iz4n";
+        System.out.println(validWordAbbreviation(word,abbr));
+    }
+}
