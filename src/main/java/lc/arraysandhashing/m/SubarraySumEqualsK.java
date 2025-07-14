f0068e598709c6c41048a826faf622ef0f568366
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-14 14:20:44 -0700

    lc. arraysandhashing. SubarraySumEqualsK. O(n) O(n)

--- src/main/java/lc/arraysandhashing/m/SubarraySumEqualsK.java
+++ src/main/java/lc/arraysandhashing/m/SubarraySumEqualsK.java
@@ -7,7 +7,7 @@ public class SubarraySumEqualsK {
     // O(n) O(n)
     public int subarraySum(int[] nums, int k) {
         int count = 0, sum = 0;
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
@@ -20,8 +20,8 @@ public class SubarraySumEqualsK {
 
     public static void main(String[] args) {
         SubarraySumEqualsK obj = new SubarraySumEqualsK();
-        int[] nums = {1,2,3,1,2,3,3};
-        int k =3;
+        int[] nums = {1, 2, 3, 1, 2, 3, 3};
+        int k = 3;
         System.out.println(obj.subarraySum(nums, k));
     }
 }
