f904829618f80d1a94912f02bdf28ddb204bfec4
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-15 16:48:38 -0700

    lc. stack. BasicCalculatorII. O(n) O(n)

--- src/main/java/lc/stack/m/BasicCalculatorII.java
+++ src/main/java/lc/stack/m/BasicCalculatorII.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class BasicCalculatorII {
     // O(n) O(n)
     public int calculate(String s) {
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         int num = 0;
         char operator = '+';
         for (int i = 0; i < s.length(); i++) {
@@ -13,19 +13,20 @@ public class BasicCalculatorII {
             if (Character.isDigit(c))
                 num = num * 10 + (c - '0');
             if (isOperator(c) || i == s.length() - 1) {
-                if (operator == '+') st.push(num);
-                else if (operator == '-') st.push(-num);
-                else if (operator == '*') st.push(st.pop() * num);
-                else if (operator == '/') st.push(st.pop() / num);
+                if (operator == '+') stack.push(num);
+                else if (operator == '-') stack.push(-num);
+                else if (operator == '*') stack.push(stack.pop() * num);
+                else if (operator == '/') stack.push(stack.pop() / num);
                 num = 0;
                 operator = c;
             }
         }
         int ans = 0;
-        while (!st.isEmpty())
-            ans += st.pop();
+        while (!stack.isEmpty())
+            ans += stack.pop();
         return ans;
     }
+
     private boolean isOperator(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/';
     }
