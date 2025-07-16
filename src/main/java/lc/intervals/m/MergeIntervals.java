d24ba1e32ed32ecac953a031d8e4d5f05e90fb9e
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-15 17:40:52 -0700

    lc. intervals. MergeIntervals. O(n log n) O(n)

--- src/main/java/lc/intervals/m/MergeIntervals.java
+++ src/main/java/lc/intervals/m/MergeIntervals.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 public class MergeIntervals {
-        // O(n log n) O(n)
-        static int[][] merge(int[][] intervals) {
-            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-            LinkedList<int[]> merged = new LinkedList<>();
-            for (int[] interval : intervals) {
-                if (merged.isEmpty() || merged.getLast()[1] < interval[0])
-                    merged.add(interval);
-                else
-                    merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
-            }
-            return merged.toArray(new int[merged.size()][]);
+    // O(n log n) O(n)
+    static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0])
+                merged.add(interval);
+            else
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
         }
+        return merged.toArray(new int[merged.size()][]);
+    }
 
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
         int[][] result = merge(intervals);
         for (int i = 0; i < result.length; i++) {
             System.out.print("[");
