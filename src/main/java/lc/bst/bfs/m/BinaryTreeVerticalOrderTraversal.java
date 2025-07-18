131ed279f54eaa40a363efbb67e13eba8b3dfd78
Yaroslav Hamuha <yhamuha@gmail.com>
2025-07-18 11:59:17 -0700

    lc. bst.bfs. BinaryTreeVerticalOrderTraversal. O(n log n) O(n)

--- src/main/java/lc/bst/bfs/m/BinaryTreeVerticalOrderTraversal.java
+++ src/main/java/lc/bst/bfs/m/BinaryTreeVerticalOrderTraversal.java
@@ -9,27 +9,24 @@ public class BinaryTreeVerticalOrderTraversal {
     // O(n log n) O(n)
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
+        if (root == null)
             return ans;
-        }
-        Deque<Pair<TreeNode, Integer>> q = new ArrayDeque<>();
-        q.offer(new Pair<>(root, 0));
-        TreeMap<Integer, List<Integer>> d = new TreeMap<>();
-        while (!q.isEmpty()) {
-            for (int n = q.size(); n > 0; --n) {
-                var p = q.pollFirst();
+        Deque<Pair<TreeNode, Integer>> queue = new ArrayDeque<>();
+        queue.offer(new Pair<>(root, 0));
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        while (!queue.isEmpty()) {
+            for (int n = queue.size(); n > 0; --n) {
+                var p = queue.pollFirst();
                 root = p.getKey();
                 int offset = p.getValue();
-                d.computeIfAbsent(offset, k -> new ArrayList()).add(root.val);
-                if (root.left != null) {
-                    q.offer(new Pair<>(root.left, offset - 1));
-                }
-                if (root.right != null) {
-                    q.offer(new Pair<>(root.right, offset + 1));
-                }
+                map.computeIfAbsent(offset, k -> new ArrayList()).add(root.val);
+                if (root.left != null)
+                    queue.offer(new Pair<>(root.left, offset - 1));
+                if (root.right != null)
+                    queue.offer(new Pair<>(root.right, offset + 1));
             }
         }
-        return new ArrayList<>(d.values());
+        return new ArrayList<>(map.values());
     }
 
     public static void main(String[] args) {
