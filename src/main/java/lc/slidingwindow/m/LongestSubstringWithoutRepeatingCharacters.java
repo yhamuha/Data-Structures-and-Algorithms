2f1dd97aa5aac9dc41354f552364f47ff9673751
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-20 16:23:31 -0700

    lc. slidingwindow. LongestSubstringWithoutRepeatingCharacters. O(n) O(n)

--- src/main/java/lc/slidingwindow/m/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/lc/slidingwindow/m/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,20 +5,21 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     // O(n) O(n)
-     static int lengthOfLongestSubstring(String s) {
+    static int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
         int left = 0;
         int maxLength = 0;
-        for (int right=0; right < s.length(); right++) {
+        for (int right = 0; right < s.length(); right++) {
             if (!set.contains(s.charAt(right))) {
                 set.add(s.charAt(right));
-                maxLength = Math.max(maxLength, right-left+1);
+                maxLength = Math.max(maxLength, right - left + 1);
             } else {
                 while (s.charAt(left) != s.charAt(right)) {
                     set.remove(s.charAt(left));
-                    left+=1;
+                    left += 1;
                 }
-                set.remove(s.charAt(left)); left+=1;
+                set.remove(s.charAt(left));
+                left += 1;
                 set.add(s.charAt(right));
             }
         }
@@ -26,6 +27,6 @@ public class LongestSubstringWithoutRepeatingCharacters {
     }
 
     public static void main(String[] args) {
-         System.out.println(lengthOfLongestSubstring(new String("abcabcbb")));
+        System.out.println(lengthOfLongestSubstring(new String("abcabcbb")));
     }
 }
