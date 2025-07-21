58b6a96852b8833ea08d8178d71678da6146407b
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-20 21:54:06 -0700

    lc. arraysandhashing. LongestSubstringWithoutRepeatingCharacters. O(n) O(n)

--- src/main/java/lc/arraysandhashing/m/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/lc/arraysandhashing/m/LongestSubstringWithoutRepeatingCharacters.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
-    // O(n) O(1)
+    // O(n) O(n)
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int maxLength = 0;
