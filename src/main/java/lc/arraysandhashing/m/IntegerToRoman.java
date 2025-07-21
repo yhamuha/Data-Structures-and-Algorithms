11d4c0276dc9a67b26a2b79e233e867925b48743
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-21 14:13:06 -0700

    lc. arraysandhashing. IntegerToRoman. O(n) O(1)

--- src/main/java/lc/arraysandhashing/m/IntegerToRoman.java
+++ src/main/java/lc/arraysandhashing/m/IntegerToRoman.java
@@ -4,16 +4,16 @@ public class IntegerToRoman {
     // O(n) O(1)
     static String intToRoman(int num) {
         final String[] romans = new String[]
-            {       "M",    "CM",   "D",    "CD",   "C",
-                    "XC",   "L",    "XL",   "X",
-                    "IX",   "V",    "IV",   "I"};
+                {"M", "CM", "D", "CD", "C",
+                        "XC", "L", "XL", "X",
+                        "IX", "V", "IV", "I"};
         final int[] decimals = new int[]
-                {   1000, 900,  500,  400,  100,
-                        90,   50,    40,   10,
-                        9,    5,    4,   1};
+                {1000, 900, 500, 400, 100,
+                        90, 50, 40, 10,
+                        9, 5, 4, 1};
         if (num > 3999 || num < 1) throw new IllegalArgumentException();
         StringBuilder roman = new StringBuilder();
-        int i =0;
+        int i = 0;
         while (num > 0) {
             if (num >= decimals[i]) {
                 roman.append(romans[i]);
