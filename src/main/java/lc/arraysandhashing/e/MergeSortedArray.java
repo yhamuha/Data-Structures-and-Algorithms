8909533f3e6656e3ade1f117806267581c658b1f
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-24 15:04:19 -0700

    lc. arraysandhashing. MergeSortedArray. O(m+n) O(1)

--- src/main/java/lc/arraysandhashing/e/MergeSortedArray.java
+++ src/main/java/lc/arraysandhashing/e/MergeSortedArray.java
@@ -3,15 +3,15 @@ package lc.arraysandhashing.e;
 public class MergeSortedArray {
     // O(m+n) O(1)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=nums1.length-1;
-        while(j>=0){
-            if(i>=0 && nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
+        int i = m - 1;
+        int j = n - 1;
+        int k = nums1.length - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
                 k--;
                 i--;
-            }else{
+            } else {
                 nums1[k] = nums2[j];
                 k--;
                 j--;
@@ -21,13 +21,13 @@ public class MergeSortedArray {
 
     public static void main(String[] args) {
         MergeSortedArray merger = new MergeSortedArray();
-        int[] nums1 = new int[6];
+        int m = 3;
+        int n = 3;
+        int[] nums1 = new int[(m + n)];
         nums1[0] = 1;
         nums1[1] = 2;
         nums1[2] = 3;
-        int m = 3;
         int[] nums2 = {2, 5, 6};
-        int n = 3;
         merger.merge(nums1, m, nums2, n);
         System.out.println("Merged array: ");
         for (int num : nums1)
