f6f7e473fc6690f9ed9051e613cc29023497b8a8
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-23 20:23:45 -0700

    lc. arraysandhashing. ProductOfArrayExceptSelf. O(n) O(1)

--- src/main/java/lc/arraysandhashing/m/ProductOfArrayExceptSelf.java
+++ src/main/java/lc/arraysandhashing/m/ProductOfArrayExceptSelf.java
@@ -20,7 +20,7 @@ public class ProductOfArrayExceptSelf {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,1,3,4};
+        int[] nums = {2, 1, 3, 4};
         ProductOfArrayExceptSelf poaes = new ProductOfArrayExceptSelf();
         System.out.println(Arrays.toString(poaes.productExceptSelf(nums)));
     }
