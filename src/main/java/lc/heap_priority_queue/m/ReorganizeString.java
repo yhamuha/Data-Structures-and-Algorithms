a585bc25247954f2ae26220fcf3df6cc2c65af2e
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-23 20:12:36 -0700

    lc. heap_priority_queue. ReorganizeString. O(n log n) O(1)

--- src/main/java/lc/heap_priority_queue/m/ReorganizeString.java
+++ src/main/java/lc/heap_priority_queue/m/ReorganizeString.java
@@ -8,25 +8,25 @@ public class ReorganizeString {
     // O(n log n) O(n)
     public String reorganizeString(String s) {
         Map<Character, Integer> freqMap = new HashMap<>();
-        for(char c : s.toCharArray())
-            freqMap.put(c,freqMap.getOrDefault(c,0)+1);
-        PriorityQueue<Character> maxHeap = new PriorityQueue((a,b)->(freqMap.get(b)-freqMap.get(a)));
+        for (char c : s.toCharArray())
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        PriorityQueue<Character> maxHeap = new PriorityQueue((a, b) -> (freqMap.get(b) - freqMap.get(a)));
         StringBuilder sb = new StringBuilder();
         maxHeap.addAll(freqMap.keySet());
-        while(maxHeap.size()>1){
+        while (maxHeap.size() > 1) {
             char first = maxHeap.poll();
             char second = maxHeap.poll();
             sb.append(first);
             sb.append(second);
-            freqMap.put(first, freqMap.get(first)-1);
-            freqMap.put(second, freqMap.get(second)-1);
+            freqMap.put(first, freqMap.get(first) - 1);
+            freqMap.put(second, freqMap.get(second) - 1);
             if (freqMap.get(first) > 0)
                 maxHeap.offer(first);
             if (freqMap.get(second) > 0)
                 maxHeap.offer(second);
         }
-        if(!maxHeap.isEmpty()){
-            if(freqMap.get(maxHeap.peek())>1)
+        if (!maxHeap.isEmpty()) {
+            if (freqMap.get(maxHeap.peek()) > 1)
                 return "";
             else
                 sb.append(maxHeap.poll());
