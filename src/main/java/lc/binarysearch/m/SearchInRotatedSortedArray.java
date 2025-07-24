f055a158e9f4817922fd0ea62dd5ecc1d2b4048c
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-23 20:02:41 -0700

    lc. binarysearch. SearchInRotatedSortedArray. O(log n) O(1)

--- src/main/java/lc/binarysearch/m/SearchInRotatedSortedArray.java
+++ src/main/java/lc/binarysearch/m/SearchInRotatedSortedArray.java
@@ -3,10 +3,10 @@ package lc.binarysearch.m;
 public class SearchInRotatedSortedArray {
     // O(log n) O(1)
     static int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        while(low <= high){
-            int mid=low + (high-low)/2;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             if (nums[mid] == target)
                 return mid;
             if (nums[low] <= nums[mid]) {
@@ -25,6 +25,6 @@ public class SearchInRotatedSortedArray {
     }
 
     public static void main(String[] args) {
-        System.out.println(search(new int[]{4,5,6,7,0,1,2},0));
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
     }
 }
