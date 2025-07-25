f65745613f3be939eb051c6274fd5b3f7b0ad355
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-24 22:51:26 -0700

    lc. twopointer. Find3Sum. O(n^2) O(n)

--- src/main/java/lc/twopointer/m/Find3Sum.java
+++ src/main/java/lc/twopointer/m/Find3Sum.java
@@ -7,37 +7,35 @@ import java.util.List;
 public class Find3Sum {
     // O(n^2) O(n)
     static List<List<Integer>> findThreeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> resultList = new ArrayList<>();
         int n = nums.length;
+        int k = n - 1;
         Arrays.sort(nums);
-        for(int  i = 0; i<n ; i++ ){
-            if(i != 0 && nums[i] == nums[i-1])
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
                 continue;
-            int j =i+1;
-            int k = n-1;
-            while(j<k){
+            int j = i + 1;
+            while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum<0){
+                if (sum < 0) {
                     j++;
-                }
-                else if(sum>0){
+                } else if (sum > 0) {
                     k--;
-                }
-                else {
-                    List<Integer> temp = Arrays.asList(nums[i] , nums[j],nums[k]);
-                    ans.add(temp);
+                } else {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    resultList.add(temp);
                     j++;
                     k--;
-                    while(k>j &&nums[k]== nums[k+1]) k--;
-                    while(k>j && nums[j]== nums[j-1]) j++;
+                    while (k > j && nums[k] == nums[k + 1]) k--;
+                    while (k > j && nums[j] == nums[j - 1]) j++;
                 }
             }
         }
-        return ans;
+        return resultList;
     }
 
     public static void main(String[] args) {
-        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = {-1, 0, 1, 2, -1, -4};
         System.out.println(findThreeSum(nums));
     }
 }
