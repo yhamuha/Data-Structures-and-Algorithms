c69eecda96b166654310f59d74fc472058171cd0
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-26 15:58:30 -0700

    lc. graphs. RottingOranges. O(n^2) O(n)

--- src/main/java/lc/graphs/m/RottingOranges.java
+++ src/main/java/lc/graphs/m/RottingOranges.java
@@ -4,33 +4,34 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class RottingOranges {
-    int[][] directions = {{-1,0}, {0,-1}, {1,0}, {0,1}};
+    int[][] directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
     // O(n^2) O(n)
     public int orangesRotting(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int freshOranges = 0;
-        for(int i=0; i< m; i++){
-            for(int j=0; j<n; j++) {
-                if(grid[i][j] == 1)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
                     ++freshOranges;
             }
         }
         Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2)
-                    queue.add(i * n +j);
+                    queue.add(i * n + j);
             }
         }
         int minutes = 0;
-        while(!queue.isEmpty() && freshOranges > 0) {
+        while (!queue.isEmpty() && freshOranges > 0) {
             int size = queue.size();
-            for(int i=0; i< size; i++){
+            for (int i = 0; i < size; i++) {
                 int num = queue.poll(), row = num / n, col = num % n;
-                for(int[] direction : directions) {
+                for (int[] direction : directions) {
                     int x = direction[0] + row, y = direction[1] + col;
-                    if(x>-1 && y>-1 && x<m && y< n && grid[x][y] == 1) {
-                        queue.add(x*n + y);
+                    if (x > -1 && y > -1 && x < m && y < n && grid[x][y] == 1) {
+                        queue.add(x * n + y);
                         --freshOranges;
                         grid[x][y] = 2;
                     }
@@ -38,14 +39,14 @@ public class RottingOranges {
             }
             ++minutes;
         }
-        return freshOranges == 0 ? minutes: -1;
+        return freshOranges == 0 ? minutes : -1;
     }
 
     public static void main(String[] args) {
         var rottingOranges = new RottingOranges();
-        int[][] grid = {{2,1,1},
-                        {1,1,0},
-                        {0,1,1}};
+        int[][] grid = {{2, 1, 1},
+                        {1, 1, 0},
+                        {0, 1, 1}};
         System.out.println(rottingOranges.orangesRotting(grid));
     }
 }
