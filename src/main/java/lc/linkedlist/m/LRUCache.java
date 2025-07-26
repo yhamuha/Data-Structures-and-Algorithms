5efcb63ffdefebfc2180673aed06638224935640
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-26 14:55:02 -0700

    lc. linkedlist. LRUCache. get/put: O(1) O(n)

--- src/main/java/lc/linkedlist/m/LRUCache.java
+++ src/main/java/lc/linkedlist/m/LRUCache.java
@@ -8,6 +8,7 @@ class Node2 {
     public int val;
     public Node2 next;
     public Node2 prev;
+
     public Node2(int key, int val) {
         this.key = key;
         this.val = val;
@@ -15,11 +16,13 @@ class Node2 {
         prev = null;
     }
 }
+
 public class LRUCache {
     private Map<Integer, Node2> m;
     private Node2 head;
     private Node2 tail;
     private int size;
+
     public LRUCache(int capacity) {
         size = capacity;
         m = new HashMap<>();
@@ -28,12 +31,14 @@ public class LRUCache {
         head.next = tail;
         tail.prev = head;
     }
+
     private void deleteNode(Node2 p) {
         Node2 pre = p.prev;
         Node2 nex = p.next;
         pre.next = nex;
         nex.prev = pre;
     }
+
     private void addNode(Node2 newnode) {
         Node2 temp = head.next;
         head.next = newnode;
@@ -41,6 +46,7 @@ public class LRUCache {
         newnode.next = temp;
         temp.prev = newnode;
     }
+
     // O(1) O(n)
     public int get(int key) {
         if (!m.containsKey(key))
@@ -51,6 +57,7 @@ public class LRUCache {
         m.put(key, head.next);
         return head.next.val;
     }
+
     // O(1) O(n)
     public void put(int key, int value) {
         if (m.containsKey(key)) {
