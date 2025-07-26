33d0d08700f3dfffd2133a42cf3cbb13115fc95b
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-07-25 18:07:42 -0700

    lc. slidingwindow. MinimumWindowSubstring. O(n) O(n)

--- src/main/java/lc/slidingwindow/h/MinimumWindowSubstring.java
+++ src/main/java/lc/slidingwindow/h/MinimumWindowSubstring.java
@@ -8,28 +8,27 @@ public class MinimumWindowSubstring {
     public String minWindow(String s, String t) {
         if (s.length() < t.length())
             return "";
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            map.merge(c, 1, Integer::sum);
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         }
         int count = 0, start = 0, min_length = Integer.MAX_VALUE, min_start = 0;
         for (int end = 0; end < s.length(); end++) {
-            if (map.containsKey(s.charAt(end))) {
-                if (map.get(s.charAt(end)) > 0)
+            if (freqMap.containsKey(s.charAt(end))) {
+                if (freqMap.get(s.charAt(end)) > 0)
                     count += 1;
-                map.put(s.charAt(end), map.get(s.charAt(end)) - 1);
+                freqMap.put(s.charAt(end), freqMap.get(s.charAt(end)) - 1);
             }
             if (count == t.length()) {
-                while (start < end && (!map.containsKey(s.charAt(start)) || map.get(s.charAt(start)) < 0)) {
-                    if (map.containsKey(s.charAt(start)))
-                        map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
+                while (start < end && (!freqMap.containsKey(s.charAt(start)) || freqMap.get(s.charAt(start)) < 0)) {
+                    if (freqMap.containsKey(s.charAt(start)))
+                        freqMap.put(s.charAt(start), freqMap.get(s.charAt(start)) + 1);
                     start += 1;
                 }
                 if (min_length > end - start + 1)
                     min_length = end - (min_start = start) + 1;
-                if (map.containsKey(s.charAt(start)))
-                    map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
+                if (freqMap.containsKey(s.charAt(start)))
+                    freqMap.put(s.charAt(start), freqMap.get(s.charAt(start)) + 1);
                 count -= 1;
                 start += 1;
             }
