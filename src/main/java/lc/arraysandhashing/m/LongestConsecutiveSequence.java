69cb8ffa081534cb15ade5455bea1c46d0c339cb
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-08-02 10:41:59 -0700

    lc. arraysandhashing. LongestConsecutiveSequence. O(n) O(n)

--- src/main/java/lc/arraysandhashing/m/LongestConsecutiveSequence.java
+++ src/main/java/lc/arraysandhashing/m/LongestConsecutiveSequence.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestConsecutiveSequence {
-        // O(n) O(n)
+    // O(n) O(n)
     public int longestConsecutive(int[] nums) {
         int longestLength = 0;
         Map<Integer, Boolean> map = new HashMap<>();
@@ -15,13 +15,13 @@ public class LongestConsecutiveSequence {
             int nextNum = num + 1;
             while (map.containsKey(nextNum) && !map.get(nextNum)) {
                 currentLength++;
-                map.put(nextNum,true);
+                map.put(nextNum, true);
                 nextNum++;
             }
             int prevNum = num - 1;
             while (map.containsKey(prevNum) && !map.get(prevNum)) {
                 currentLength++;
-                map.put(prevNum,true);
+                map.put(prevNum, true);
                 prevNum--;
             }
             longestLength = Math.max(longestLength, currentLength);
@@ -30,7 +30,7 @@ public class LongestConsecutiveSequence {
     }
 
     public static void main(String[] args) {
-        int[] nums = {100,4,200,1,3,2,5,6};
+        int[] nums = {100, 4, 200, 1, 3, 2, 5, 6};
         LongestConsecutiveSequence lcs = new LongestConsecutiveSequence();
         System.out.println(lcs.longestConsecutive(nums));
     }
