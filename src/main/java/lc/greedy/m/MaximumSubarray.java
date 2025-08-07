bb61cc620b8f7e413987cb0bd85109fafc4ef637
Yaroslav Hamuha <yhamuha@gmail.com>
2025-08-06 21:12:53 -0700

    lc. greedy. MaximumSubarray. O(n) O(1)

--- src/main/java/lc/greedy/m/MaximumSubarray.java
+++ src/main/java/lc/greedy/m/MaximumSubarray.java
@@ -6,7 +6,7 @@ public class MaximumSubarray {
         if (nums.length == 0) return 0;
         int currSum = nums[0];
         int max = nums[0];
-        for(int i=1; i<nums.length; i++){
+        for (int i = 1; i < nums.length; i++) {
             currSum = Math.max(nums[i], currSum + nums[i]);
             max = Math.max(max, currSum);
         }
@@ -14,7 +14,7 @@ public class MaximumSubarray {
     }
 
     public static void main(String[] args) {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(maxSubArray(nums));
     }
 }
