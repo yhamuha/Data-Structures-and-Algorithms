294b87df76d6ce381be7e0663a67193fd124842f
Yaroslav Hamuha <yhamuha@gmail.com>
2025-08-08 18:08:51 -0700

    lc. slidingwindow. BestTimeToBuyAndSellStock. O(n) O(1)

--- src/main/java/lc/slidingwindow/e/BestTimeToBuyAndSellStock.java
+++ src/main/java/lc/slidingwindow/e/BestTimeToBuyAndSellStock.java
@@ -5,10 +5,10 @@ public class BestTimeToBuyAndSellStock {
     static int maxProfit(int[] prices) {
         int maxProfit = 0;
         int prev = 0;
-        for(int next=0; next<prices.length; next++){
-            if(prices[prev] < prices[next]) {
+        for (int next = 0; next < prices.length; next++) {
+            if (prices[prev] < prices[next]) {
                 int currentProfit = prices[next] - prices[prev];
-                maxProfit = Math.max(currentProfit,maxProfit);
+                maxProfit = Math.max(currentProfit, maxProfit);
             } else
                 prev = next;
         }
@@ -16,6 +16,6 @@ public class BestTimeToBuyAndSellStock {
     }
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 }
