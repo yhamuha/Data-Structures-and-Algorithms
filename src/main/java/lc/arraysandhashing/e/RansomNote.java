202e7cf2d141658899850181c428f15279f65eec
Yaroslav Hamuha <yhamuha@gmail.com>
2025-08-12 12:27:39 -0700

    lc. arraysandhashing. RansomNote. O(m+n) O(1)

--- src/main/java/lc/arraysandhashing/e/RansomNote.java
+++ src/main/java/lc/arraysandhashing/e/RansomNote.java
@@ -7,8 +7,8 @@ public class RansomNote {
     // O(m+n) O(1)
     public boolean canConstruct(String ransomNote, String magazine) {
         Map<Character, Integer> freqMap = new HashMap<>();
-        for(char c : magazine.toCharArray())
-            freqMap.put(c,freqMap.getOrDefault(c,0)+1);
+        for (char c : magazine.toCharArray())
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         for (int i = 0; i < ransomNote.length(); i++) {
             char c = ransomNote.charAt(i);
             if (freqMap.containsKey(c) && freqMap.get(c) > 0)
