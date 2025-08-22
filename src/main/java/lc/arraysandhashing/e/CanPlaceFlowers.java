4cdf9f4ef2481fea12ccc36bbf78630802f20f52
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-08-21 22:44:09 -0700

    lc. arraysandhashing. CanPlaceFlowers. O(n) O(1)

--- src/main/java/lc/arraysandhashing/e/CanPlaceFlowers.java
+++ src/main/java/lc/arraysandhashing/e/CanPlaceFlowers.java
@@ -3,16 +3,20 @@ package lc.arraysandhashing.e;
 public class CanPlaceFlowers {
     // O(n) O(1)
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int len = flowerbed.length;
-        int i = 0;
-        while (i < len && n > 0) {
-            if (flowerbed[i] == 1) i += 2;
-            else if (i == len - 1 || flowerbed[i + 1] == 0) {
-                n--;
-                i += 2;
-            } else i += 3;
+        if (n <= 0) return true;
+        int m = flowerbed.length;
+        for (int i = 0; i < m; i++) {
+            if (flowerbed[i] == 0) {
+                boolean leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean rightEmpty = (i == m - 1) || (flowerbed[i + 1] == 0);
+                if (leftEmpty && rightEmpty) {
+                    flowerbed[i] = 1;
+                    n--;
+                    if (n == 0) return true;
+                }
+            }
         }
-        return n <= 0;
+        return false;
     }
 
     public static void main(String[] args) {
