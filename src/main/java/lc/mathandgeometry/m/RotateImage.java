ac562e5367956cea3b73e4fc808e9a950ca9e15c
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-08-25 13:26:01 -0700

    lc. mathandgeometry. RotateImage. O(n^2) O(1)

--- src/main/java/lc/mathandgeometry/m/RotateImage.java
+++ src/main/java/lc/mathandgeometry/m/RotateImage.java
@@ -1,12 +1,12 @@
 package lc.mathandgeometry.m;
 
 public class RotateImage {
-    // O(n^2) O(1)
+    // O(n^2) O(1) #ibm
     public void rotate(int[][] matrix) {
-        int m = matrix.length / 2, n = matrix.length-1;
+        int m = matrix.length / 2, n = matrix.length - 1;
         for (int i = 0; i < m; i++, n--)
             for (int j = i; j < n; j++) {
-                int xJ = matrix.length - j - 1;
+                int xJ = matrix.length - j - 1; // coordinate shift
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[xJ][i];
                 matrix[xJ][i] = matrix[n][xJ];
