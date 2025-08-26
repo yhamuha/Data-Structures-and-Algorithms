759d6e067b8e580678a8dc0efdc48392a4df2909
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-08-26 13:31:01 -0700

    lc. heap_priority_queue. TopKFrequentWords. O(n log k) O(n)

--- src/main/java/lc/heap_priority_queue/m/TopKFrequentWords.java
+++ src/main/java/lc/heap_priority_queue/m/TopKFrequentWords.java
@@ -3,15 +3,15 @@ package lc.heap_priority_queue.m;
 import java.util.*;
 
 public class TopKFrequentWords {
-    // O(n log k) O(n)
+    // O(n log k) O(n) #ibm
     static List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> freqMap = new HashMap<>();
         for (String word : words)
             freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
         PriorityQueue<String> minHeap = new PriorityQueue<>(
                 (s1, s2) -> freqMap.get(s1).equals(freqMap.get(s2))
-                            ? s2.compareTo(s1)
-                            : freqMap.get(s1) - freqMap.get(s2));
+                        ? s2.compareTo(s1)
+                        : freqMap.get(s1) - freqMap.get(s2));
         for (String word : freqMap.keySet()) {
             minHeap.offer(word);
             if (minHeap.size() > k)
@@ -25,7 +25,7 @@ public class TopKFrequentWords {
     }
 
     public static void main(String[] args) {
-        String[] words = {"i","love","leetcode","i","love","coding"};
+        String[] words = {"i", "love", "leetcode", "i", "love", "coding"};
         int k = 2;
         System.out.println(topKFrequent(words, k));
     }
