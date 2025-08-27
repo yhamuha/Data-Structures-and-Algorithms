6d2af2e4a9d51a1bf8067c96ce17fe70ee09f3ce
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-08-27 11:42:32 -0700

    lc. bitmanipulation. ReverseInteger. O(log n) O(1)

--- src/main/java/lc/bitmanipulation/m/ReverseInteger.java
+++ src/main/java/lc/bitmanipulation/m/ReverseInteger.java
@@ -1,26 +1,24 @@
 package lc.bitmanipulation.m;
 
 public class ReverseInteger {
-    // O(log n) O(1)
+    // O(log n) O(1) #ibm
     public int reverse(int x) {
-       long result = 0;
-       int lastDigit;
-       while(x!=0){
-           lastDigit=x%10; // get last digit
-           result= result * 10 + lastDigit; // form result
-           x=x/10; // form new number
+        long result = 0;
+        int lastDigit;
+        while (x != 0) {
+            lastDigit = x % 10;
+            result = result * 10 + lastDigit;
+            x = x / 10;
 
-           // check the constraints
-           if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
+            if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
                 return 0;
-       }
-       return (int) result;
+        }
+        return (int) result;
     }
+
     public static void main(String[] args) {
         ReverseInteger reverseInteger = new ReverseInteger();
         int num1 = 123;
-        int num2 = -456;
-        System.out.println("Reverse of " + num1 + " is: " + reverseInteger.reverse(num1));
-        System.out.println("Reverse of " + num2 + " is: " + reverseInteger.reverse(num2));
+        System.out.println(reverseInteger.reverse(num1));
     }
 }
