2c254788a471ea7814af36c4de1785776b2bb001
Yaroslav Hamuha <yhamuha@gmail.com>
2025-10-02 21:49:12 -0700

    lc. bst.bfs. DiagonalTraverse. O(m * n) O(m * n)

--- src/main/java/lc/bst/bfs/m/DiagonalTraverse.java
+++ src/main/java/lc/bst/bfs/m/DiagonalTraverse.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Queue;
 
 public class DiagonalTraverse {
-    // O(m * n) O(m * n)
+    // O(m * n) O(m * n) #walmart
     public int[] findDiagonalOrder(int[][] matrix) {
         if (matrix == null || matrix.length == 0) return new int[0];
         int m = matrix.length;
@@ -30,10 +30,10 @@ public class DiagonalTraverse {
                     queue.offer(new int[]{first, second + 1});
             }
             if (odd) {
-                for (int i = 0; i < layer.size(); i++)  // retrieve elements in natural order
+                for (int i = 0; i < layer.size(); i++)  // retrieve elements in natural order from the layer
                     answer[index++] = layer.get(i);
             } else {
-                for (int i = layer.size() - 1; i >= 0; i--) // retrieve elements in reverse order
+                for (int i = layer.size() - 1; i >= 0; i--) // retrieve elements in reverse order from the layer
                     answer[index++] = layer.get(i);
             }
             odd = !odd;
