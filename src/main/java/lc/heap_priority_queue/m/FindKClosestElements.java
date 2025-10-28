7af3389b94629fe379e1ff752d50bbfafc13acfe
Yaroslav Hamuha <yhamuha@gmail.com>
2025-10-27 19:47:49 -0700

    lc. heap_priority_queue. FindKClosestElements. O(n log k) O(k)

--- src/main/java/lc/heap_priority_queue/m/FindKClosestElements.java
+++ src/main/java/lc/heap_priority_queue/m/FindKClosestElements.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class FindKClosestElements {
-    // O(n log k) O(k)
+    // O(n log k) O(k) #1
     static List<Integer> findClosestElements(int[] arr, int k, int x) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         for (int integer : arr) {
@@ -24,8 +24,8 @@ public class FindKClosestElements {
     }
 
     public static void main(String[] args) {
-        int[] arr = {-1,1,3,4};
-        int k = 2;
+        int[] arr = {1,2,3,4,5};
+        int k = 4;
         int x = 3;
         System.out.println(findClosestElements(arr, k, x));
     }
