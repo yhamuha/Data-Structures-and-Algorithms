591203307af14516323ee2f592307a9164bd079c
Yaroslav Hamuha <yhamuha@gmail.com>
2025-11-19 21:06:38 -0800

    lc.binarysearch. Sqrt_x. O(log n) O(1)

--- src/main/java/lc/binarysearch/e/Sqrt_x.java
+++ src/main/java/lc/binarysearch/e/Sqrt_x.java
@@ -1,18 +1,15 @@
 package lc.binarysearch.e;
 
 public class Sqrt_x {
-        // O(log n) O(1)
+        // O(log n) O(1) #klarna
         public int mySqrt(int x) {
             if (x == 0 || x == 1)
                 return x;
-
             int start = 1;
             int end = x;
             int mid = -1;
-
             while (start <= end) {
                 mid = start + (end - start) / 2;
-
                 if ((long) mid * mid > (long) x)
                     end = mid - 1;
                 else if (mid * mid == x)
@@ -20,7 +17,6 @@ public class Sqrt_x {
                 else
                     start = mid + 1;
             }
-
             return Math.round(end);
         }
 
