cacb20d8ed1ed3961f59578151154716240b208b
Yaroslav Hamuha <yhamuha@gmail.com>
2026-02-26 21:36:58 -0800

    lc.hi.stack

--- src/main/java/lc/stack/e/ValidParentheses.java
+++ src/main/java/lc/stack/e/ValidParentheses.java
@@ -1,10 +1,12 @@
 package lc.stack.e;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class ValidParentheses {
     // O(n) O(n) #amazon #ibm #walmart
-    static boolean isValid(String s) {
+    /*static boolean isValid(String s) {
         if (s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack();
         for (char c : s.toCharArray()) {
@@ -19,10 +21,28 @@ public class ValidParentheses {
             }
         }
         return stack.isEmpty();
+    }*/
+
+    // O(1) time | O(n) space
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '('); mapping.put('}', '{'); mapping.put(']', '[');
+        for (char c : s.toCharArray()) {
+            if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || stack.peek() != mapping.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
-        String s = "{()}";
+        String s = "(){()}";
         System.out.println(isValid(s));
     }
 }
