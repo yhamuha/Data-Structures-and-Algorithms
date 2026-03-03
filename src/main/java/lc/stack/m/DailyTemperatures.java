7aac6a3ad16ac4e46a37b5ba7ecb258ba0f75c05
Yaroslav G Hamuha <yhamuha@gmail.com>
2026-03-02 21:44:10 -0800

    lc.hi.stack

--- src/main/java/lc/stack/m/DailyTemperatures.java
+++ src/main/java/lc/stack/m/DailyTemperatures.java
@@ -5,18 +5,34 @@ import java.util.Stack;
 
 public class DailyTemperatures {
     // O(n) O(n)
-    static int[] dailyTemperatures(int[] temperatures) {
+    /*static int[] dailyTemperatures(int[] temperatures) {
         Stack<Integer> stackOfIndexes = new Stack<>();
         int n = temperatures.length;
         int[] answer = new int[n];
         for (int i = 0; i < n; i++) {
-            while (!stackOfIndexes.isEmpty() && temperatures[i] > temperatures[stackOfIndexes.peek()]) {
+            while (!stackOfIndexes.isEmpty() &&
+                    temperatures[i] > temperatures[stackOfIndexes.peek()]) {
                 int index = stackOfIndexes.pop();
                 answer[index] = i - index;
             }
             stackOfIndexes.push(i);
         }
         return answer;
+    }*/
+
+    // O(n) O(n)
+    public static int[] dailyTemperatures(int[] temps) {
+        int n = temps.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temps[i] > temps[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
