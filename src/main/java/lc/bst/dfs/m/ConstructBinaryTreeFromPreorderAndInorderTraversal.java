27eca0c2aab1709a40a3102371f7dfe5e6c0df46
Yaroslav Hamuha <yhamuha@gmail.com>
2026-03-10 23:15:49 -0700

    lc.hi.bst.dfs

--- src/main/java/lc/bst/dfs/m/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/lc/bst/dfs/m/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -3,25 +3,27 @@ package lc.bst.dfs.m;
 import lc.bst.TreeNode;
 
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
-    // O(n^2); O(n) unbalanced O(log n) balanced
+    // O(n) unbalanced; O(log n) balanced
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTreeDfs(0,0,inorder.length-1,preorder, inorder);
+        return buildTreeDfs(0, 0, inorder.length - 1, preorder, inorder);
     }
-    private TreeNode buildTreeDfs(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder){
-        if(preStart>=preorder.length || inStart>inEnd)
+
+    private TreeNode buildTreeDfs(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart >= preorder.length || inStart > inEnd)
             return null;
         TreeNode node = new TreeNode(preorder[preStart]);
         int mid = 0;
-        for(int i=inStart;i<inorder.length;i++){
-            if(inorder[i]==node.val){
-                mid=i;
+        for (int i = inStart; i < inorder.length; i++) {
+            if (inorder[i] == node.val) {
+                mid = i;
                 break;
             }
         }
-        node.left = buildTreeDfs(preStart+1, inStart,mid-1,preorder,inorder);
-        node.right = buildTreeDfs(preStart+mid-inStart+1, mid+1,inEnd,preorder,inorder);
+        node.left = buildTreeDfs(preStart + 1, inStart, mid - 1, preorder, inorder);
+        node.right = buildTreeDfs(preStart + mid - inStart + 1, mid + 1, inEnd, preorder, inorder);
         return node;
     }
+
     public void printPreOrder(TreeNode node) {
         if (node == null) {
             System.out.print("null ");
