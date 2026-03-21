96f0ccaf7b332996f00b879d858e34e2cdd2b9cc
Yaroslav Hamuha <yhamuha@gmail.com>
2026-03-20 23:14:35 -0700

    lc.hi.dp

--- src/main/java/lc/dp/m/LongestPalindromicSubstring.java
+++ src/main/java/lc/dp/m/LongestPalindromicSubstring.java
@@ -1,29 +1,28 @@
 package lc.dp.m;
 
 public class LongestPalindromicSubstring {
-    // SC: O(1)
-    int resultStart =0;
-    int resultLength=0;
+    // O(n) O(1)
+    int resultStart = 0;
+    int resultLength = 0;
+
     public String longestPalindrome(String s) {
         int strLength = s.length();
-        if(s.length()<2) return s;
-        // TC: O(n)
-        for(int start =0; start<strLength; start++){
+        if (s.length() < 2) return s;
+        for (int start = 0; start < strLength; start++) {
             expandRange(s, start, start);
-            expandRange(s, start, start+1);
+            expandRange(s, start, start + 1);
         }
-        return s.substring(resultStart, resultStart+resultLength);
+        return s.substring(resultStart, resultStart + resultLength);
     }
 
-    private void expandRange(String s, int begin, int end){
-        // TC: O(n)
-        while(begin>=0 && end<s.length() && s.charAt(begin) == s.charAt(end)){
+    private void expandRange(String s, int begin, int end) {
+        while (begin >= 0 && end < s.length() && s.charAt(begin) == s.charAt(end)) {
             begin--;
             end++;
         }
         if (resultStart < end - begin - 1) {
-            resultStart = begin+1;
-            resultLength= end - begin -1;
+            resultStart = begin + 1;
+            resultLength = end - begin - 1;
         }
     }
 
