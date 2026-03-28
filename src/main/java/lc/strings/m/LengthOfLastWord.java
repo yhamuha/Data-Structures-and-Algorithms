41164893bb00a7db66fe370e75b3f1583d6ff54a
Yaroslav G Hamuha <yhamuha@gmail.com>
2026-03-28 14:05:21 -0700

    lc.strings.LengthOfLastWord

---
+++ src/main/java/lc/strings/m/LengthOfLastWord.java
@@ -0,0 +1,20 @@
+package lc.strings.m;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        int length = 0;
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello Word";
+        System.out.println(lengthOfLastWord(str));
+    }
+}
