dc1e2ab83eff3250a12caefdf83c588cdaaa05a2
Yaroslav G Hamuha <yhamuha@gmail.com>
2026-03-31 07:08:54 -0700

    lc.arraysandhashing.TwoSum

--- src/main/java/lc/arraysandhashing/e/TwoSum.java
+++ src/main/java/lc/arraysandhashing/e/TwoSum.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-
     // O(n) O(n) #meta #amazon #ibm #walmart
-    static public int[] findTwoSumImproved(int[] nums, int target) {
+    static public int[] findTwoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i< nums.length; i++){
             int difference = target - nums[i];
@@ -20,9 +19,6 @@ public class TwoSum {
 
     public static void main(String[] args) {
         int[] arr = new int[]{3,2,4};
-        /*
         System.out.println(Arrays.toString(findTwoSum(arr, 6)));
-        */
-        System.out.println(Arrays.toString(findTwoSumImproved(arr, 6)));
     }
 }
