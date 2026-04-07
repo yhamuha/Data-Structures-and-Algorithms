9c41772fd50f83de62a1cd024c2ea79ef0325f77
Yaroslav G Hamuha <yhamuha@gmail.com>
2026-04-06 20:35:29 -0700

    lc.greedy.JumpGameII

--- src/main/java/lc/greedy/m/JumpGameII.java
+++ src/main/java/lc/greedy/m/JumpGameII.java
@@ -3,16 +3,16 @@ package lc.greedy.m;
 public class JumpGameII {
     // O(n) O(1)
     int jump(int[] nums) {
-        int destIndex = nums.length -1;
+        int destIndex = nums.length - 1;
         int coverageIndex = 0, lastJumpIndex = 0;
         int totalJumps = 0;
-        for(int i=0; i<=nums.length; i++){
-            coverageIndex = Math.max(coverageIndex, i+nums[i]);
-            if(lastJumpIndex==i){
+        for (int i = 0; i <= nums.length; i++) {
+            coverageIndex = Math.max(coverageIndex, i + nums[i]);
+            if (lastJumpIndex == i) {
                 lastJumpIndex = coverageIndex;
                 totalJumps++;
-            if(coverageIndex >= destIndex)
-                return totalJumps;
+                if (coverageIndex >= destIndex)
+                    return totalJumps;
             }
         }
         return totalJumps;
@@ -20,7 +20,7 @@ public class JumpGameII {
 
     public static void main(String[] args) {
         var jg = new JumpGameII();
-        int[] nums = {2,3,1,1,4};
+        int[] nums = {2, 3, 1, 1, 4};
         System.out.println(jg.jump(nums));
     }
 }
