cafd09f8832cf043c91086d3b018fbd6843ad051
Yaroslav Hamuha <yhamuha@gmail.com>
2026-05-17 21:07:27 -0700

    lc.dp

--- src/main/java/lc/dp/ClimbingStairs.java
+++ src/main/java/lc/dp/ClimbingStairs.java
@@ -1,18 +1,14 @@
 package lc.dp;
 
 public class ClimbingStairs {
-    // O(n)
+    // O(n) O(1)
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 0 || n == 1)
             return 1;
-        }
-
         int[] dp = new int[n+1];
         dp[0] = dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++)
             dp[i] = dp[i-1] + dp[i-2];
-        }
         return dp[n];
     }
 
