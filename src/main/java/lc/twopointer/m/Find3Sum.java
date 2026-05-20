b0d828e92a941dbac382c2f98221f7ae235428d0
Yaroslav G Hamuha <yhamuha@gmail.com>
2026-05-20 07:16:17 -0700

    lc.twopointer

--- src/main/java/lc/twopointer/m/Find3Sum.java
+++ src/main/java/lc/twopointer/m/Find3Sum.java
@@ -5,34 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Find3Sum {
-    // O(n^2) O(aux: log n; worst n^2) #ibm #walmart
-    /*static List<List<Integer>> findThreeSum(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        int n = nums.length;
-        int k = n - 1;
-        Arrays.sort(nums);
-        for (int i = 0; i < n; i++) {
-            if (i != 0 && nums[i] == nums[i - 1])
-                continue;
-            int j = i + 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-                    resultList.add(temp);
-                    j++;
-                    k--;
-                    while (k > j && nums[k] == nums[k + 1]) k--;
-                    while (k > j && nums[j] == nums[j - 1]) j++;
-                }
-            }
-        }
-        return resultList;
-    }*/
+    // O(n log n) O(n)
     public static List<List<Integer>> findThreeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
