865a1b0f63da59f3e335a307cf31c4d0383f8bc2
Yaroslav Hamuha <yhamuha@gmail.com>
2026-06-22 15:17:42 -0700

    lc.linkedlist.PartitionList

---
+++ src/main/java/lc/linkedlist/m/PartitionList.java
@@ -0,0 +1,40 @@
+package lc.linkedlist.m;
+
+public class PartitionList {
+    public static ListNode partition(ListNode head, int x) {
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
+        }
+        greater.next = null;
+        less.next = greaterDummy.next;
+        return lessDummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(2);
+        int x = 3;
+        ListNode result = partition(head, x);
+        while (result != null) {
+            System.out.print(result.val);
+            if (result.next != null)
+                System.out.print(" -> ");
+            result = result.next;
+        }
+    }
+}
