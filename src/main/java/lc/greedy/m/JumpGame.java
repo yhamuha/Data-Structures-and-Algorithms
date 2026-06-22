6d4d3c63d75620286d29ab60e666521c73e0a0a7
Yaroslav Hamuha <yhamuha@gmail.com>
2026-06-22 14:45:27 -0700

    lc.greedy

--- src/main/java/lc/greedy/m/JumpGame.java
+++ src/main/java/lc/greedy/m/JumpGame.java
@@ -3,15 +3,13 @@ package lc.greedy.m;
 public class JumpGame {
     // O(n) O(1) #bloomberg
     public boolean canJump(int[] nums) {
-        int lastIndex = 0;
-        int n = nums.length - 1;
-        for (int i = n; i >= 0; i--) {
+        int lastIndex = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
             if (i + nums[i] >= lastIndex)
                 lastIndex = i;
         }
         return lastIndex == 0;
     }
-
     public static void main(String[] args) {
         var jgame = new JumpGame();
         int[] nums = {3,2,1,0,4};
