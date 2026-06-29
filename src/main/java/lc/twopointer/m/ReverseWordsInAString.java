558e826338e5bc0e9b36c15d6a3eb0ecc3674aa1
Yaroslav G Hamuha <yhamuha@gmail.com>
2026-06-29 14:36:29 -0700

    lc.twopointer.ReverseWordsInAString

--- src/main/java/lc/twopointer/m/ReverseWordsInAString.java
+++ src/main/java/lc/twopointer/m/ReverseWordsInAString.java
@@ -4,18 +4,16 @@ public class ReverseWordsInAString {
     // O(n) O(n)
     static String reverseWords(String s) {
         StringBuilder ans = new StringBuilder();
-        if (s.length() == 1 && s.charAt(0) == ' ') {
+        if (s.length() == 1 && s.charAt(0) == ' ')
             return ans.toString();
-        }
         int i = s.length() - 1;
         while (i >= 0) {
             if (s.charAt(i) == ' ')
                 --i;
              else {
                 int j = i;
-                if (ans.length() > 0) {
+                if (ans.length() > 0)
                     ans.append(" ");
-                }
                 while (j >= 0 && s.charAt(j) != ' ')
                     --j;
                 ans.append(s.substring(j + 1, i + 1));
